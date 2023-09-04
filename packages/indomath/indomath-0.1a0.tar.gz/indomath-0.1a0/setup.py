0147c9128c49f9b21cc2f468178978a92d3e7816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:08:17 +0000

    Add package indomath/0.1a0/indomath-0.1a0.tar.gz

---
+++ packages/indomath/indomath-0.1a0.tar.gz/indomath-0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from indomath import __version__
+
+setup(
+    name='indomath',
+    version=__version__,
+
+    url='https://github.com/sayuz394/matematika',
+    author='Sayuz',
+    author_email='sayuzvernando1@gmail.com',
+
+    py_modules=['indomath'],
+)

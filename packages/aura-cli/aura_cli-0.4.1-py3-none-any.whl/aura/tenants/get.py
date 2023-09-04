765d085e8d8b768b4428a9f8bc7a8267a3ccd2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 04:10:33 +0000

    Add package aura-cli/0.4.1/aura_cli-0.4.1-py3-none-any.whl

---
+++ packages/aura-cli/aura_cli-0.4.1-py3-none-any.whl/aura/tenants/get.py
@@ -0,0 +1,28 @@
+import click
+from aura.api_command import api_command
+from aura.api_repository import make_api_call
+from aura.config_repository import CLIConfig
+from aura.decorators import pass_config
+
+
+@api_command(name="get", help_text="Get details for a tenant", fixed_cmd_output="json")
+@click.option("--tenant-id", "-id", help="The ID of the tenant")
+@pass_config
+def get_tenant(config: CLIConfig, tenant_id: str):
+    """
+    Get details of a tenant.
+
+    Makes "GET /tenants/:tenantId" API request.
+
+    fixed_cmd_output is set to json because its a nested object that can not be
+    displayed in table or text.
+    """
+
+    if tenant_id is None:
+        tenant_id = config.get_option("default-tenant")
+    if tenant_id is None:
+        tenant_id = click.prompt("Tenant ID")
+
+    path = f"/tenants/{tenant_id}"
+
+    return make_api_call("GET", path)

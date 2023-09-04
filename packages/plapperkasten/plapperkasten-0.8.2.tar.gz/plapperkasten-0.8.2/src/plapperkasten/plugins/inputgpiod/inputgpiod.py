c5424769e7f639250e7d0470ecbf179a0b36f7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:07:11 +0000

    Add package plapperkasten/0.8.2/plapperkasten-0.8.2.tar.gz

---
+++ packages/plapperkasten/plapperkasten-0.8.2.tar.gz/plapperkasten-0.8.2/src/plapperkasten/plugins/inputgpiod/inputgpiod.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+"""Use `gpiodmonitor` to monitor pins (buttons) for input (presses).
+"""
+import queue
+import signal
+
+from gpiodmonitor import gpiodmonitor
+
+from plapperkasten import config as plkconfig
+from plapperkasten import event as plkevent
+from plapperkasten import plugin
+from plapperkasten.plklogging import plklogging
+
+logger: plklogging.PlkLogger = plklogging.get_logger(__name__)
+
+
+class Inputgpiod(plugin.Plugin):
+    """Monitor buttons for presses / releases.
+
+    Attributes:
+        _monitor: Instance of `gpiodmonitor.GPIODMonitor`
+    """
+
+    def on_init(self, config: plkconfig.Config) -> None:
+        """Setup pins.
+
+        Args:
+            config: The configuration.
+        """
+
+        self._monitor: gpiodmonitor.GPIODMonitor = gpiodmonitor.GPIODMonitor(
+            chip_number=config.get_int('plugins', 'inputgpiod', 'chip',
+                default=0))
+
+        long_press_duration: float = config.get_float('plugins',
+                                                  'inputgpiod',
+                                                  'long_press_duration',
+                                                  default=1.5)
+
+        press_pulse_interval: float = config.get_float('plugins',
+                                                  'inputgpiod',
+                                                  'press_pulse_interval',
+                                                  default=0.3)
+
+        for pin in config.get_list_int('plugins',
+                                       'inputgpiod',
+                                       'press_short',
+                                       default=[]):
+            self._monitor.register(pin, on_active=self.send_short_press_signal)
+
+        for pin in config.get_list_int('plugins',
+                                       'inputgpiod',
+                                       'press_long',
+                                       default=[]):
+            self._monitor.register_long_active(
+                pin,
+                callback=self.send_long_press_signal,
+                seconds=long_press_duration)
+
+        for pin in config.get_list_int('plugins',
+                                       'inputgpiod',
+                                       'press_pulse',
+                                       default=[]):
+            self._monitor.register_pulsed_active(
+                    pin,
+                    callback=self.send_pulsed_press_signal,
+                    seconds=press_pulse_interval)
+
+    def send_pulsed_press_signal(self, pin: int) -> None:
+        """Send a raw event to signal a short press.
+
+        Args:
+            pin: The number of the pin.
+        """
+
+        self.send_to_main('raw', f'{pin}_pulsed')
+
+    def send_short_press_signal(self, pin: int) -> None:
+        """Send a raw event to signal a short press.
+
+        Args:
+            pin: The number of the pin.
+        """
+
+        self.send_to_main('raw', f'{pin}_short')
+
+    def send_long_press_signal(self, pin: int) -> None:
+        """Send a raw event to signal a long press.
+
+        Args:
+            pin: The number of the pin.
+        """
+
+        self.send_to_main('raw', f'{pin}_long')
+
+    def run(self) -> None:
+        """Check status of the gpio pins at regular intervals.
+
+        Needs to reimplement `plugin.Plugin.run()` to use the
+        contextmanager implemented by `gpiodmonitor`.
+        """
+
+        logger.debug('%s running', self.get_name())
+
+        signal.signal(signal.SIGINT, self.on_interrupt)
+        signal.signal(signal.SIGTERM, self.on_interrupt)
+
+        self.on_before_run()
+
+        with self._monitor.open_chip():
+            while not self._terminate_signal:
+                self._monitor.tick()
+                try:
+                    event: plkevent.Event = self._to_plugin.get(
+                        True, self._monitor.check_interval / 1000)
+                    if hasattr(self, 'on_' + event.name):
+                        getattr(self, 'on_' + event.name)(*event.values,
+                                                          **event.params)
+                    else:
+                        logger.error('no method for event %s defined by %s',
+                                     event.name, self.get_name())
+                except queue.Empty:
+                    pass
+                except ValueError:
+                    logger.error('%s holds a closed queue', self.get_name())
+        self.on_after_run()
+        logger.debug('%s exited main loop', self.get_name())
---
+++ packages/plapperkasten/plapperkasten-0.8.2.tar.gz/plapperkasten-0.8.2/src/plapperkasten/plugins/soundeffects/soundeffects.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+"""Provide auditory feedback.
+"""
+
+import pathlib
+import pkg_resources
+import subprocess
+
+from plapperkasten import config as plkconfig
+from plapperkasten import plugin
+from plapperkasten.plklogging import plklogging
+
+logger: plklogging.PlkLogger = plklogging.get_logger(__name__)
+
+
+class Soundeffects(plugin.Plugin):
+    """Provide auditory feedback on certain events.
+
+    Attributes:
+        _path_sounds: Path to the directory containing the sound files.
+        _sounds: Dicitionary mapping events ('ready', 'shutdown', ...)
+            to names of sound files.
+    """
+
+    def on_init(self, config: plkconfig.Config) -> None:
+        """Register for events.
+
+        Args:
+            config: The configuration.
+        """
+
+        self.register_for('finished_loading')
+        self.register_for('shutdown')
+        self.register_for('error')
+        self.register_for('feedback')
+        self.register_for('beep')
+        self._path_sounds = pathlib.Path(
+            config.get_str('plugins',
+                           'soundeffects',
+                           'path',
+                           default=pkg_resources.resource_filename(
+                               __name__, 'sounds')))
+        self._sounds: dict[str, str] = config.get_dict_str_str('plugins',
+                                                               'soundeffects',
+                                                               'sounds',
+                                                               default={})
+
+    def on_finished_loading(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('ready')
+
+    def on_shutdown(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('shutdown')
+
+    def on_error(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('error')
+
+    def on_feedback(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('feedback')
+
+    def on_beep(self, *values: str, **params: str) -> None:
+        # pylint: disable=unused-argument
+        """Play sound to notify the user.
+
+        Args:
+            *values: Values attached to the event (ignored).
+            **params: Parameters attached to the event (ignored).
+        """
+        self.play_sound('beep')
+
+    def play_sound(self, sound: str):
+        """Play sound to notify the user.
+
+        Args:
+            sound: The name of the sound to play (must be a valid key
+            in plugins.soundeffects.sounds in config.yaml).
+        """
+        if not sound in self._sounds:
+            logger.error('no such sound configured: "%s"', sound)
+            return
+
+        call: list[str] = [
+            '/usr/bin/aplay', '-N',
+            str(self._path_sounds / self._sounds[sound])
+        ]
+        # capture output is new and in this case required with python >= 3.7
+        try:
+            subprocess.run(call,
+                           capture_output=True,
+                           encoding='utf-8',
+                           check=True)
+        except subprocess.CalledProcessError:
+            logger.error('could not play sound "%s"', sound)

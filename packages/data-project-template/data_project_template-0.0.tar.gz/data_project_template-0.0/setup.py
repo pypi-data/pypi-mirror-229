bcd4a417b6be60b2f2f5e4bf8d55f09c8f06b523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-04 03:07:16 +0000

    Add package data-project-template/0.0/data_project_template-0.0.tar.gz

---
+++ packages/data-project-template/data_project_template-0.0.tar.gz/data_project_template-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_project_template',
+    version='0.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+)

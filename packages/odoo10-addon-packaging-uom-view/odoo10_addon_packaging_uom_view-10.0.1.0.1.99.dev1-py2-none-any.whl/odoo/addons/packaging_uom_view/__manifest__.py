64b647632d4b275a51190fe30a86d5b7103a594e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:09:48 +0000

    Add package odoo10-addon-packaging-uom-view/10.0.1.0.1.99.dev1/odoo10_addon_packaging_uom_view-10.0.1.0.1.99.dev1-py2-none-any.whl

---
+++ packages/odoo10-addon-packaging-uom-view/odoo10_addon_packaging_uom_view-10.0.1.0.1.99.dev1-py2-none-any.whl/odoo/addons/packaging_uom_view/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Packaging Uom View',
+    'summary': """
+        If purchase is installed along with packaging_uom,
+        there is a duplicate view""",
+    'version': '10.0.1.0.1',
+    'development_status': 'Alpha',
+    'maintainers': ['rousseldenis'],
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/stock-logistics-warehouse',
+    'depends': [
+        'purchase',
+        'packaging_uom',
+    ],
+    'data': [
+        'views/product_template.xml',
+    ],
+    'auto_install': True,
+}

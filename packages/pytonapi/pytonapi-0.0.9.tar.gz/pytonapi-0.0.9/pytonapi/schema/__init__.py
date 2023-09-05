bc7f66f619d2bd9f417944c97da4d21c26498ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:07:04 +0000

    Add package pytonapi/0.0.9/pytonapi-0.0.9.tar.gz

---
+++ packages/pytonapi/pytonapi-0.0.9.tar.gz/pytonapi-0.0.9/pytonapi/schema/__init__.py
@@ -0,0 +1,19 @@
+from pytonapi.schema import accounts
+from pytonapi.schema import blockchain
+from pytonapi.schema import dns
+from pytonapi.schema import domains
+from pytonapi.schema import jettons
+from pytonapi.schema import nft
+from pytonapi.schema import rates
+from pytonapi.schema import traces
+
+__all__ = [
+    "accounts",
+    "blockchain",
+    "dns",
+    "domains",
+    "jettons",
+    "nft",
+    "rates",
+    "traces",
+]

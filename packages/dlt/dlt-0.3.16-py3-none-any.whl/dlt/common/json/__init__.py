3c72cf45670bbf2abe5134e92be983a5f01e1121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:14:23 +0000

    Add package dlt/0.3.16/dlt-0.3.16-py3-none-any.whl

---
+++ packages/dlt/dlt-0.3.16-py3-none-any.whl/dlt/common/json/__init__.py
@@ -0,0 +1,192 @@
+import os
+import base64
+import dataclasses
+from datetime import date, datetime, time  # noqa: I251
+from typing import Any, Callable, List, Protocol, IO, Union
+from uuid import UUID
+from hexbytes import HexBytes
+
+try:
+    from pydantic import BaseModel as PydanticBaseModel
+except ImportError:
+    PydanticBaseModel = None  # type: ignore[misc]
+
+from dlt.common.arithmetics import Decimal
+from dlt.common.wei import Wei
+from dlt.common.utils import map_nested_in_place
+from dlt.common.time import parse_iso_like_datetime
+
+
+class SupportsJson(Protocol):
+    """Minimum adapter for different json parser implementations"""
+
+    _impl_name: str
+    """Implementation name"""
+
+    def dump(self, obj: Any, fp: IO[bytes], sort_keys: bool = False, pretty:bool = False) -> None:
+        ...
+
+    def typed_dump(self, obj: Any, fp: IO[bytes], pretty:bool = False) -> None:
+        ...
+
+    def typed_dumps(self, obj: Any, sort_keys: bool = False, pretty: bool = False) -> str:
+        ...
+
+    def typed_loads(self, s: str) -> Any:
+        ...
+
+    def typed_dumpb(self, obj: Any, sort_keys: bool = False, pretty: bool = False) -> bytes:
+        ...
+
+    def typed_loadb(self, s: Union[bytes, bytearray, memoryview]) -> Any:
+        ...
+
+    def dumps(self, obj: Any, sort_keys: bool = False, pretty:bool = False) -> str:
+        ...
+
+    def dumpb(self, obj: Any, sort_keys: bool = False, pretty:bool = False) -> bytes:
+        ...
+
+    def load(self, fp: IO[bytes]) -> Any:
+        ...
+
+    def loads(self, s: str) -> Any:
+        ...
+
+    def loadb(self, s: Union[bytes, bytearray, memoryview]) -> Any:
+        ...
+
+
+def custom_encode(obj: Any) -> str:
+    if isinstance(obj, Decimal):
+        # always return decimals as string so they are not deserialized back to float
+        return str(obj)
+    # this works both for standard datetime and pendulum
+    elif isinstance(obj, datetime):
+        # See "Date Time String Format" in the ECMA-262 specification.
+        r = obj.isoformat()
+        # leave microseconds alone
+        # if obj.microsecond:
+        #     r = r[:23] + r[26:]
+        if r.endswith('+00:00'):
+            r = r[:-6] + 'Z'
+        return r
+    elif isinstance(obj, date):
+        return obj.isoformat()
+    elif isinstance(obj, time):
+        return obj.isoformat()
+    elif isinstance(obj, UUID):
+        return str(obj)
+    elif isinstance(obj, HexBytes):
+        return obj.hex()
+    elif isinstance(obj, bytes):
+        return base64.b64encode(obj).decode('ascii')
+    elif hasattr(obj, 'asdict'):
+        return obj.asdict()  # type: ignore
+    elif hasattr(obj, '_asdict'):
+        return obj._asdict()  # type: ignore
+    elif PydanticBaseModel and isinstance(obj, PydanticBaseModel):
+        return obj.dict()  # type: ignore[return-value]
+    elif dataclasses.is_dataclass(obj):
+        return dataclasses.asdict(obj)  # type: ignore
+    raise TypeError(repr(obj) + " is not JSON serializable")
+
+
+# use PUA range to encode additional types
+_DECIMAL = '\uF026'
+_DATETIME = '\uF027'
+_DATE = '\uF028'
+_UUIDT = '\uF029'
+_HEXBYTES = '\uF02A'
+_B64BYTES = '\uF02B'
+_WEI = '\uF02C'
+_TIME = '\uF02D'
+
+
+# define decoder for each prefix
+DECODERS: List[Callable[[Any], Any]] = [
+    Decimal,
+    parse_iso_like_datetime,
+    lambda s: parse_iso_like_datetime(s),
+    UUID,
+    HexBytes,
+    base64.b64decode,
+    Wei,
+    lambda s: s  # NOTE: we keep iso implementation until we have TIME, time.fromisoformat,
+]
+# how many decoders?
+PUA_CHARACTER_MAX = len(DECODERS)
+
+
+def custom_pua_encode(obj: Any) -> str:
+    # wei is subclass of decimal and must be checked first
+    if isinstance(obj, Wei):
+        return _WEI + str(obj)
+    elif isinstance(obj, Decimal):
+        return _DECIMAL + str(obj)
+    # this works both for standard datetime and pendulum
+    elif isinstance(obj, datetime):
+        r = obj.isoformat()
+        if r.endswith('+00:00'):
+            r = r[:-6] + 'Z'
+        return _DATETIME + r
+    elif isinstance(obj, date):
+        return _DATE + obj.isoformat()
+    elif isinstance(obj, time):
+        return _TIME + obj.isoformat()
+    elif isinstance(obj, UUID):
+        return _UUIDT + str(obj)
+    elif isinstance(obj, HexBytes):
+        return _HEXBYTES + obj.hex()
+    elif isinstance(obj, bytes):
+        return _B64BYTES + base64.b64encode(obj).decode('ascii')
+    elif hasattr(obj, 'asdict'):
+        return obj.asdict()  # type: ignore
+    elif hasattr(obj, '_asdict'):
+        return obj._asdict()  # type: ignore
+    elif dataclasses.is_dataclass(obj):
+        return dataclasses.asdict(obj)  # type: ignore
+    elif PydanticBaseModel and isinstance(obj, PydanticBaseModel):
+        return obj.dict()  # type: ignore[return-value]
+    raise TypeError(repr(obj) + " is not JSON serializable")
+
+
+def custom_pua_decode(obj: Any) -> Any:
+    if isinstance(obj, str) and len(obj) > 1:
+        c = ord(obj[0]) - 0xF026
+        # decode only the PUA space defined in DECODERS
+        if c >=0 and c <= PUA_CHARACTER_MAX:
+            return DECODERS[c](obj[1:])
+    return obj
+
+
+def custom_pua_decode_nested(obj: Any) -> Any:
+    if isinstance(obj, str):
+        return custom_pua_decode(obj)
+    elif isinstance(obj, (list, dict)):
+        return map_nested_in_place(custom_pua_decode, obj)
+    return obj
+
+
+def custom_pua_remove(obj: Any) -> Any:
+    """Removes the PUA data type marker and leaves the correctly serialized type representation. Unmarked values are returned as-is."""
+    if isinstance(obj, str) and len(obj) > 1:
+        c = ord(obj[0]) - 0xF026
+        # decode only the PUA space defined in DECODERS
+        if c >=0 and c <= PUA_CHARACTER_MAX:
+            return obj[1:]
+    return obj
+
+
+# pick the right impl
+json: SupportsJson = None
+if os.environ.get("DLT_USE_JSON") == "simplejson":
+    from dlt.common.json import _simplejson as _json_d
+    json = _json_d
+else:
+    try:
+        from dlt.common.json import _orjson as _json_or
+        json = _json_or
+    except ImportError:
+        from dlt.common.json import _simplejson as _json_simple
+        json = _json_simple
---
+++ packages/dlt/dlt-0.3.16-py3-none-any.whl/dlt/extract/schema.py
@@ -0,0 +1,206 @@
+from copy import copy, deepcopy
+from collections.abc import Mapping as C_Mapping
+from typing import List, TypedDict, cast, Any
+
+from dlt.common.schema.utils import DEFAULT_WRITE_DISPOSITION, merge_columns, new_column, new_table
+from dlt.common.schema.typing import TColumnNames, TColumnProp, TColumnSchema, TPartialTableSchema, TTableSchemaColumns, TWriteDisposition, TAnySchemaColumns
+from dlt.common.typing import TDataItem
+from dlt.common.validation import validate_dict_ignoring_xkeys
+
+from dlt.extract.incremental import Incremental
+from dlt.extract.typing import TFunHintTemplate, TTableHintTemplate
+from dlt.extract.exceptions import DataItemRequiredForDynamicTableHints, InconsistentTableTemplate, TableNameMissing
+from dlt.extract.utils import ensure_table_schema_columns_hint
+
+
+class TTableSchemaTemplate(TypedDict, total=False):
+    name: TTableHintTemplate[str]
+    # description: TTableHintTemplate[str]
+    write_disposition: TTableHintTemplate[TWriteDisposition]
+    # table_sealed: Optional[bool]
+    parent: TTableHintTemplate[str]
+    columns: TTableHintTemplate[TTableSchemaColumns]
+    primary_key: TTableHintTemplate[TColumnNames]
+    merge_key: TTableHintTemplate[TColumnNames]
+    incremental: Incremental[Any]
+
+
+class DltResourceSchema:
+    def __init__(self, name: str, table_schema_template: TTableSchemaTemplate = None):
+        self.__qualname__ = self.__name__ = self._name = name
+        self._table_name_hint_fun: TFunHintTemplate[str] = None
+        self._table_has_other_dynamic_hints: bool = False
+        self._table_schema_template: TTableSchemaTemplate = None
+        if table_schema_template:
+            self.set_template(table_schema_template)
+
+    @property
+    def table_name(self) -> TTableHintTemplate[str]:
+        """Get table name to which resource loads data. May return a callable."""
+        if self._table_name_hint_fun:
+            return self._table_name_hint_fun
+        return self._table_schema_template["name"] if self._table_schema_template else self._name
+
+    @table_name.setter
+    def table_name(self, value: TTableHintTemplate[str]) -> None:
+        self.apply_hints(table_name=value)
+
+    @property
+    def write_disposition(self) -> TWriteDisposition:
+        if self._table_schema_template is None or self._table_schema_template.get("write_disposition") is None:
+            return DEFAULT_WRITE_DISPOSITION
+        w_d = self._table_schema_template.get("write_disposition")
+        if callable(w_d):
+            raise DataItemRequiredForDynamicTableHints(self._name)
+        else:
+            return w_d
+
+    def table_schema(self, item: TDataItem =  None) -> TPartialTableSchema:
+        """Computes the table schema based on hints and column definitions passed during resource creation. `item` parameter is used to resolve table hints based on data"""
+        if not self._table_schema_template:
+            return new_table(self._name, resource=self._name)
+
+        # resolve a copy of a held template
+        table_template = copy(self._table_schema_template)
+        table_template["columns"] = copy(self._table_schema_template["columns"])
+
+        # if table template present and has dynamic hints, the data item must be provided
+        if self._table_name_hint_fun and item is None:
+            raise DataItemRequiredForDynamicTableHints(self._name)
+        # resolve
+        resolved_template: TTableSchemaTemplate = {k: self._resolve_hint(item, v) for k, v in table_template.items()}  # type: ignore
+        resolved_template.pop("incremental", None)
+        table_schema = self._merge_keys(resolved_template)
+        table_schema["resource"] = self._name
+        validate_dict_ignoring_xkeys(
+            spec=TPartialTableSchema,
+            doc=table_schema,
+            path=f"new_table/{self._name}",
+        )
+        return table_schema
+
+    def apply_hints(
+        self,
+        table_name: TTableHintTemplate[str] = None,
+        parent_table_name: TTableHintTemplate[str] = None,
+        write_disposition: TTableHintTemplate[TWriteDisposition] = None,
+        columns: TTableHintTemplate[TAnySchemaColumns] = None,
+        primary_key: TTableHintTemplate[TColumnNames] = None,
+        merge_key: TTableHintTemplate[TColumnNames] = None,
+        incremental: Incremental[Any] = None
+    ) -> None:
+        """Creates or modifies existing table schema by setting provided hints. Accepts both static and dynamic hints based on data.
+
+           This method accepts the same table hints arguments as `dlt.resource` decorator with the following additions.
+           Skip the argument or pass None to leave the existing hint.
+           Pass empty value (for particular type ie "" for a string) to remove hint
+
+           parent_table_name (str, optional): A name of parent table if foreign relation is defined. Please note that if you use merge you must define `root_key` columns explicitly
+           incremental (Incremental, optional): Enables the incremental loading for a resource.
+
+           Please note that for efficient incremental loading, the resource must be aware of the Incremental by accepting it as one if its arguments and then using is to skip already loaded data.
+           In non-aware resources, `dlt` will filter out the loaded values, however the resource will yield all the values again.
+        """
+        t = None
+        if columns is not None:
+            columns = ensure_table_schema_columns_hint(columns)
+        if not self._table_schema_template:
+            # if there's no template yet, create and set new one
+            t = self.new_table_template(table_name, parent_table_name, write_disposition, columns, primary_key, merge_key)
+        else:
+            # set single hints
+            t = deepcopy(self._table_schema_template)
+            if table_name is not None:
+                if table_name:
+                    t["name"] = table_name
+                else:
+                    t.pop("name", None)
+            if parent_table_name is not None:
+                if parent_table_name:
+                    t["parent"] = parent_table_name
+                else:
+                    t.pop("parent", None)
+            if write_disposition:
+                t["write_disposition"] = write_disposition
+            if columns is not None:
+                t["columns"] = columns
+            if primary_key is not None:
+                t["primary_key"] = primary_key
+            if merge_key is not None:
+                t["merge_key"] = merge_key
+            t["incremental"] = incremental
+        self.set_template(t)
+
+    def set_template(self, table_schema_template: TTableSchemaTemplate) -> None:
+        # if "name" is callable in the template then the table schema requires actual data item to be inferred
+        name_hint = table_schema_template["name"]
+        if callable(name_hint):
+            self._table_name_hint_fun = name_hint
+        else:
+            self._table_name_hint_fun = None
+        # check if any other hints in the table template should be inferred from data
+        self._table_has_other_dynamic_hints = any(callable(v) for k, v in table_schema_template.items() if k != "name")
+        self._table_schema_template = table_schema_template
+
+    @staticmethod
+    def _resolve_hint(item: TDataItem, hint: TTableHintTemplate[Any]) -> Any:
+            """Calls each dynamic hint passing a data item"""
+            if callable(hint):
+                return hint(item)
+            else:
+                return hint
+
+    @staticmethod
+    def _merge_key(hint: TColumnProp, keys: TColumnNames, partial: TPartialTableSchema) -> None:
+        if isinstance(keys, str):
+            keys = [keys]
+        for key in keys:
+            if key in partial["columns"]:
+                merge_columns(partial["columns"][key], {hint: True})  # type: ignore
+            else:
+                partial["columns"][key] = new_column(key, nullable=False)
+                partial["columns"][key][hint] = True
+
+    @staticmethod
+    def _merge_keys(t_: TTableSchemaTemplate) -> TPartialTableSchema:
+        """Merges resolved keys into columns"""
+        partial = cast(TPartialTableSchema, t_)
+        # assert not callable(t_["merge_key"])
+        # assert not callable(t_["primary_key"])
+        if "primary_key" in t_:
+            DltResourceSchema._merge_key("primary_key", t_.pop("primary_key"), partial)  # type: ignore
+        if "merge_key" in t_:
+            DltResourceSchema._merge_key("merge_key", t_.pop("merge_key"), partial)  # type: ignore
+
+        return partial
+
+    @staticmethod
+    def new_table_template(
+        table_name: TTableHintTemplate[str],
+        parent_table_name: TTableHintTemplate[str] = None,
+        write_disposition: TTableHintTemplate[TWriteDisposition] = None,
+        columns: TTableHintTemplate[TTableSchemaColumns] = None,
+        primary_key: TTableHintTemplate[TColumnNames] = None,
+        merge_key: TTableHintTemplate[TColumnNames] = None
+        ) -> TTableSchemaTemplate:
+        if not table_name:
+            raise TableNameMissing()
+
+        # create a table schema template where hints can be functions taking TDataItem
+        if isinstance(columns, C_Mapping):
+            # new_table accepts a sequence
+            column_list: List[TColumnSchema] = []
+            for name, column in columns.items():
+                column["name"] = name
+                column_list.append(column)
+            columns = column_list  # type: ignore
+
+        new_template: TTableSchemaTemplate = new_table(table_name, parent_table_name, write_disposition=write_disposition, columns=columns)  # type: ignore
+        if primary_key:
+            new_template["primary_key"] = primary_key
+        if merge_key:
+            new_template["merge_key"] = merge_key
+        # if any of the hints is a function then name must be as well
+        if any(callable(v) for k, v in new_template.items() if k != "name") and not callable(table_name):
+            raise InconsistentTableTemplate(f"Table name {table_name} must be a function if any other table hint is a function")
+        return new_template

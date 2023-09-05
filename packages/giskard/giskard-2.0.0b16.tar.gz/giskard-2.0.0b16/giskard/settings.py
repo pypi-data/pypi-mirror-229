4dcfb4e09b311d50436f2869ea0a36669a1741c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:10:02 +0000

    Add package giskard/2.0.0b16/giskard-2.0.0b16.tar.gz

---
+++ packages/giskard/giskard-2.0.0b16.tar.gz/giskard-2.0.0b16/giskard/settings.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+from pydantic.env_settings import BaseSettings
+
+
+def expand_env_var(env_var: Optional[str]) -> Optional[str]:
+    if not env_var:
+        return env_var
+    while True:
+        interpolated = os.path.expanduser(os.path.expandvars(str(env_var)))
+        if interpolated == env_var:
+            return interpolated
+        else:
+            env_var = interpolated
+
+
+class Settings(BaseSettings):
+    home: str = "~/giskard-home"
+    port: int = 50051
+    ws_port: int = 9000
+    ws_path: str = "/ml-worker"
+    host: str = "localhost"
+    max_workers: int = 10
+    max_send_message_length_mb: int = 1024
+    max_receive_message_length_mb: int = 1024
+    loglevel = "INFO"
+    cache_dir: str = "cache"
+    disable_analytics = False
+
+    class Config:
+        env_prefix = "GSK_"
+
+    @property
+    def home_dir(self) -> Path:
+        return Path(expand_env_var(self.home))
+
+
+settings = Settings()

b6df63bda05ce07a897c6b7c7fcf2fb73063d76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:09:34 +0000

    Add package QuantEdgeMarketData/0.1.1/QuantEdgeMarketData-0.1.1.tar.gz

---
+++ packages/QuantEdgeMarketData/QuantEdgeMarketData-0.1.1.tar.gz/QuantEdgeMarketData-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuantEdgeMarketData',
+    version='0.1.1',
+    description='A Python package for fetching and managing historical market data using the Alpha Vantage API.',
+    author='Your Name',
+    author_email='your@email.com',
+    url='https://github.com/yourusername/QuantEdgeMarketData',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+)

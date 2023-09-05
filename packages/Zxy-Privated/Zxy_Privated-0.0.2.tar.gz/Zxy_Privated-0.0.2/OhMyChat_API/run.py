9cd2cc2f9f8d99ca70c0a48849679b7325d9f6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:07:13 +0000

    Add package Zxy-Privated/0.0.2/Zxy_Privated-0.0.2.tar.gz

---
+++ packages/Zxy-Privated/Zxy_Privated-0.0.2.tar.gz/Zxy_Privated-0.0.2/OhMyChat_API/run.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+'''
+@Project_name    :  run
+@ProjectDescription: ..........
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/9/4 14:12     zxy       1.0         None
+'''
+from OhMyChat_API import GetAPI
+
+
+class Test_Class():
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    X = GetAPI(is_need_GUI=True)
+    X.run()
---
+++ packages/Zxy-Privated/Zxy_Privated-0.0.2.tar.gz/Zxy_Privated-0.0.2/setup.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="Zxy_Privated",
+    version="0.0.2",
+    author="AnQing",
+    description="需要到 https://aigptx.top/ 注册并获取api秘钥",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)

f62e82ec8e66e1dfcb499ffecf59c98dc572dc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:14:20 +0000

    Add package steveb890-distributions/0.1/steveb890_distributions-0.1.tar.gz

---
+++ packages/steveb890-distributions/steveb890_distributions-0.1.tar.gz/steveb890_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='steveb890_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)

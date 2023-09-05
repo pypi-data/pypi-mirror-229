245bb52dea4474c78ac5246013948cc760b493d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:09:07 +0000

    Add package aiofauna/0.1.94/aiofauna-0.1.94.tar.gz

---
+++ packages/aiofauna/aiofauna-0.1.94.tar.gz/aiofauna-0.1.94/aiofauna/cli.py
@@ -0,0 +1,149 @@
+import os
+import subprocess
+
+import click
+
+from .utils import setup_logging
+
+logger = setup_logging(__name__)
+
+prettyprint = logger.info
+
+
+@click.group()
+def main():
+    pass
+
+
+def print_tree(dirname, pref=""):
+    for item in os.listdir(dirname):
+        if item.startswith("."):
+            continue
+        prettyprint(pref + item)
+        if os.path.isdir(os.path.join(dirname, item)):
+            logger.info(
+                os.path.join(dirname, item), " " * len(pref) + "|" + ("_" * 5) + " "
+            )
+
+@main.command()
+def tree():
+    """Prints the directory tree of the current working directory."""
+    print_tree(os.getcwd())
+
+@main.command()
+@click.option(
+    "--port",
+    default=8080,
+    type=int,
+    help="Port in which the application will listen for requests.",
+)
+@click.option(
+    "--host",
+    default="0.0.0.0",
+    type=str,
+    help="Host that will run the application process.",
+)
+@click.option(
+    "--reload",
+    default=True,
+    flag_value=True,
+    help="When enabled, the server will reload on change.",
+)
+@click.option(
+    "--worker",
+    default=1,
+    type=int,
+    help="Number of workers to spawn. This is the number of processes that will be spawned.",
+)
+@click.option(
+    "--threads",
+    default=1,
+    type=int,
+    help="Number of threads to spawn. This is the number of threads that will be spawned per worker.",
+)
+@click.argument("name", default="main:app", type=str, nargs=1, required=False)
+def run(name, port, host, reload, worker, threads):
+    """Spins up a Gunicorn server which serves the app.\n
+    It comes with the following options:\n
+     --port: Port to run the server on\n
+     --host: Host to run the server on\n
+     --reload: Reload the server on change\n
+     --worker: Number of workers to spawn\n
+     --threads: Number of threads to utilize\n\n
+
+     name: Name of the app to run it comes in the format of <module>:<app>, where module is the name of your entry point file without the extension and app is the name of the global variable which contains the `APIServer` instance.\n\n
+
+     Example: aiofauna run main:app --port 5000 --host 0.0.0.0 --reload --worker 1 --threads 1 (This is the default configuration BTW, when you just execute `aiofauna run`)
+    """
+    subprocess.run(
+        [
+            "gunicorn",
+            "-b",
+            f"{host}:{port}",
+            "-k",
+            "aiohttp.worker.GunicornWebWorker",
+            "--reload" if reload else "",
+            "--workers",
+            str(worker),
+            "--threads",
+            str(threads),
+            name,
+        ],
+        check=True,
+    )
+
+@main.command()
+@click.argument("name", type=str, nargs=1, required=True)
+def module(name: str):
+    """Creates a new module in the current working directory.\n
+    This command creates a new directory with the given name and scaffolds a submodule of the app. Following files are created:\n
+    - __init__.py\n
+    - router.py\n
+    - model.py\n
+    - service.py\n
+    
+    Example: aiofauna module users
+    """
+    Name = name.capitalize()
+    os.mkdir(name)
+    with open(os.path.join(name, "__init__.py"), "w") as f:
+        f.write(
+            f"""
+from .router import app as {name}_app
+from .model import {Name}Model
+from .service import {Name}Service
+            """
+            )
+    with open(os.path.join(name, "router.py"), "w") as f:
+        f.write(
+            f"""
+from aiofauna import APIRouter
+
+app = APIRouter()
+
+@app.get("/")
+async def {name}_home():
+    return {{ "message": "Hello from {name}!" }}
+            """
+        )
+    with open(os.path.join(name, "model.py"), "w") as f:
+        f.write(
+            f"""
+from aiofauna import FaunaModel, Field
+
+class {Name}Model(FaunaModel):
+    pass
+            """
+        )
+    with open(os.path.join(name, "service.py"), "w") as f:
+        f.write(
+            f"""
+from dataclasses import field
+from aiofauna import APIClient
+
+class {Name}Service(APIClient):
+    base_url:str = field(default="http://jsonplaceholder.typicode.com")
+    headers:dict = field(default_factory=lambda: {{"Content-Type": "application/json"}})
+
+"""
+        )
---
+++ packages/aiofauna/aiofauna-0.1.94.tar.gz/aiofauna-0.1.94/aiofauna/typedefs.py
@@ -0,0 +1,219 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from datetime import date, datetime, time, timedelta
+from decimal import Decimal
+from enum import Enum
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Iterable,
+    List,
+    Type,
+    TypeVar,
+    Union,
+    cast,
+)
+from uuid import UUID
+
+from pydantic import BaseConfig  # pylint: disable=no-name-in-module
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from pydantic import Extra  # pylint: disable=no-name-in-module
+from pydantic import Field as PydanticField
+from typing_extensions import ParamSpec
+
+from .faunadb.objects import FaunaTime, Ref
+
+Vector = List[float]
+
+MetaData = Dict[str, str]
+
+Undefined = Union[None, type(...), type(Ellipsis)]
+NoArgAnyCallable = Callable[[], Any]
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+class Document(BaseModel):
+    """Document
+    Base document for all FaunaDB documents
+    """
+
+    class Config(BaseConfig):
+        """Configuration defaults for all documents"""
+
+        extra = Extra.allow
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            datetime: lambda v: v.astimezone().isoformat(),
+            date: lambda v: v.isoformat(),
+            time: lambda v: v.isoformat(),
+            timedelta: lambda v: v.total_seconds(),
+            UUID: lambda v: str(v),
+            Decimal: lambda v: float(v),
+            Ref: lambda v: v.id(),
+            FaunaTime: lambda v: v.value,
+            Enum: lambda v: v.value,
+        }
+
+    class Metadata:
+        subclasses: List[Type[Document]] = []
+
+    @classmethod
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if cls.__doc__ is None:
+            cls.__doc__ = f"```json\n{cls.schema_json(indent=2)}\n```"
+        cls.Metadata.subclasses.append(cls)
+
+
+D = TypeVar("D", bound=Document)
+
+
+class LazyProxy(Generic[T], ABC):
+    def __init__(self) -> None:
+        self.__proxied: T | None = None
+
+    def __getattr__(self, attr: str) -> object:
+        return getattr(self.__get_proxied__(), attr)
+
+    def __repr__(self) -> str:
+        return repr(self.__get_proxied__())
+
+    def __str__(self) -> str:
+        return str(self.__get_proxied__())
+
+    def __dir__(self) -> Iterable[str]:
+        return self.__get_proxied__().__dir__()
+
+    def __get_proxied__(self) -> T:
+        proxied = self.__proxied
+        if proxied is not None:
+            return proxied
+
+        self.__proxied = proxied = self.__load__()
+        return proxied
+
+    def __set_proxied__(self, value: T) -> None:
+        self.__proxied = value
+
+    def __as_proxied__(self) -> T:
+        """Helper method that returns the current proxy, typed as the loaded object"""
+        return cast(T, self)
+
+    @abstractmethod
+    def __load__(self) -> T:
+        ...
+
+
+def Field(
+    default: Any = Undefined,
+    *,
+    default_factory: NoArgAnyCallable | None = None,
+    alias: str | None = None,
+    title: str | None = None,
+    description: str | None = None,
+    const: bool | None = None,
+    gt: float | None = None,
+    ge: float | None = None,
+    lt: float | None = None,
+    le: float | None = None,
+    multiple_of: float | None = None,
+    allow_inf_nan: bool | None = None,
+    max_digits: int | None = None,
+    decimal_places: int | None = None,
+    min_items: int | None = None,
+    max_items: int | None = None,
+    unique_items: bool | None = None,
+    min_length: int | None = None,
+    max_length: int | None = None,
+    allow_mutation: bool = True,
+    regex: str | None = None,
+    discriminator: str | None = None,
+    repr: bool = True,
+    index: bool | None = None,
+    unique: bool | None = None,
+    **extra: Any,
+) -> Any:
+    """
+        Used to provide extra information about a field, either for the model schema or complex validation. Some arguments apply only to number fields (int, float, Decimal) and some apply only to str.
+
+    :param default: since this is replacing the field’s default, its first argument is used
+      to set the default, use ellipsis (...) to indicate the field is required
+    :param default_factory: callable that will be called when a default value is needed for this field
+      If both default and default_factory are set, an error is raised.
+    :param alias: the public name of the field
+    :param title: can be any string, used in the schema
+    :param description: can be any string, used in the schema
+    :param exclude: exclude this field while dumping.
+      Takes same values as the include and exclude arguments on the .dict method.
+    :param include: include this field while dumping.
+      Takes same values as the include and exclude arguments on the .dict method.
+    :param const: this field is required and *must* take it's default value
+    :param gt: only applies to numbers, requires the field to be "greater than". The schema
+      will have an exclusiveMinimum validation keyword
+    :param ge: only applies to numbers, requires the field to be "greater than or equal to". The
+      schema will have a minimum validation keyword
+    :param lt: only applies to numbers, requires the field to be "less than". The schema
+      will have an exclusiveMaximum validation keyword
+    :param le: only applies to numbers, requires the field to be "less than or equal to". The
+      schema will have a maximum validation keyword
+    :param multiple_of: only applies to numbers, requires the field to be "a multiple of". The
+      schema will have a multipleOf validation keyword
+    :param allow_inf_nan: only applies to numbers, allows the field to be NaN or infinity (+inf or -inf),
+        which is a valid Python float. Default True, set to False for compatibility with JSON.
+    :param max_digits: only applies to Decimals, requires the field to have a maximum number
+      of digits within the decimal. It does not include a zero before the decimal point or trailing decimal zeroes.
+    :param decimal_places: only applies to Decimals, requires the field to have at most a number of decimal places
+      allowed. It does not include trailing decimal zeroes.
+    :param min_items: only applies to lists, requires the field to have a minimum number of
+      elements. The schema will have a minItems validation keyword
+    :param max_items: only applies to lists, requires the field to have a maximum number of
+      elements. The schema will have a maxItems validation keyword
+    :param unique_items: only applies to lists, requires the field not to have duplicated
+      elements. The schema will have a uniqueItems validation keyword
+    :param min_length: only applies to strings, requires the field to have a minimum length. The
+      schema will have a minLength validation keyword
+    :param max_length: only applies to strings, requires the field to have a maximum length. The
+      schema will have a maxLength validation keyword
+    :param allow_mutation: a boolean which defaults to True. When False, the field raises a TypeError if the field is
+      assigned on an instance. The BaseModel Config must set validate_assignment to True
+    :param regex: only applies to strings, requires the field match against a regular expression
+      pattern string. The schema will have a pattern validation keyword
+    :param discriminator: only useful with a (discriminated a.k.a. tagged) Union of sub models with a common field.
+      The discriminator is the name of this common field to shorten validation and improve generated schema
+    :param repr: show this field in the representation
+    :param **extra: any additional keyword arguments will be added as is to the schema
+    """
+    return PydanticField(
+        default=default,
+        default_factory=default_factory,
+        alias=alias,
+        title=title,
+        description=description,
+        const=const,
+        gt=gt,
+        ge=ge,
+        lt=lt,
+        le=le,
+        multiple_of=multiple_of,
+        allow_inf_nan=allow_inf_nan,
+        max_digits=max_digits,
+        decimal_places=decimal_places,
+        min_items=min_items,
+        max_items=max_items,
+        unique_items=unique_items,
+        min_length=min_length,
+        max_length=max_length,
+        allow_mutation=allow_mutation,
+        regex=regex,
+        discriminator=discriminator,
+        repr=repr,
+        index=index,
+        unique=unique,
+        **extra,
+    )

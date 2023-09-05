6f0b357ee3e5a3841834c1290f53a5933b6db347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:09:48 +0000

    Add package risu/1.14.3.20230904132727/risu-1.14.3.20230904132727-py3-none-any.whl

---
+++ packages/risu/risu-1.14.3.20230904132727-py3-none-any.whl/risu-1.14.3.20230904132727.data/scripts/risu.py
@@ -0,0 +1,10 @@
+#!python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2021 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+import sys
+
+from risuclient.shell import main
+
+if __name__ == "__main__":
+    sys.exit(main())

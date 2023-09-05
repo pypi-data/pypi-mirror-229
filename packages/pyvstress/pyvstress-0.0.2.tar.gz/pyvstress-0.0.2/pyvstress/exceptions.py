ccc26eb6c26e02817288cd7e2ca289141de09d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:08:57 +0000

    Add package pyvstress/0.0.2/pyvstress-0.0.2.tar.gz

---
+++ packages/pyvstress/pyvstress-0.0.2.tar.gz/pyvstress-0.0.2/pyvstress/exceptions.py
@@ -0,0 +1,8 @@
+# I have lifted some code from other github repositories and those have been referenced
+# bascially because I either did not have enough knowledge or foresight to implement my own.
+# I would like to take this opportunity to thank them for their contribution to open source code.
+# ref: https://github.com/eng-tools/sfsimodels/blob/master/sfsimodels/exceptions.py
+
+
+class ParamError(Exception):
+    pass

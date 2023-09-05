a54d27e85608285ca5821b9ec5140ce031222ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:10:38 +0000

    Add package gembox/0.1.11.9/gembox-0.1.11.9.tar.gz

---
+++ packages/gembox/gembox-0.1.11.9.tar.gz/gembox-0.1.11.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='gembox',
+    version='0.1.11.9',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    url='https://github.com/stevieflyer/gembox',
+    author='Steve Flyer',
+    author_email='steveflyer7@gmail.com',
+    description='This is the gembox toolbox for steve flyer. You can also find your hammers here.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)

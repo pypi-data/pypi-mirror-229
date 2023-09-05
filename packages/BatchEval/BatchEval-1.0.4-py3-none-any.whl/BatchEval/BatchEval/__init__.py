94206b5236c5589401afae37d552193042c5088d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:08:29 +0000

    Add package BatchEval/1.0.4/BatchEval-1.0.4-py3-none-any.whl

---
+++ packages/BatchEval/BatchEval-1.0.4-py3-none-any.whl/BatchEval/BatchEval/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 8/10/23 5:38 PM
+# @Author  : zhangchao
+# @File    : __init__.py.py
+# @Email   : zhangchao5@genomics.cn
+from .BatchEval import check_raw
+from .BatchEval import batch_eval

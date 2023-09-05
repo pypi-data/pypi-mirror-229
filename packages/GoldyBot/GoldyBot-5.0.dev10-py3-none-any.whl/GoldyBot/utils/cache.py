522e12d88784634c01935acb04fb75b2cc241adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:15:24 +0000

    Add package GoldyBot/5.0.dev10/GoldyBot-5.0.dev10-py3-none-any.whl

---
+++ packages/GoldyBot/GoldyBot-5.0.dev10-py3-none-any.whl/GoldyBot/utils/cache.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+from typing import Any, Tuple, overload, List
+
+__all__ = ("cache_lookup",)
+
+@overload
+def cache_lookup(key: str|int, cache: dict) -> Any | None:
+    ...
+
+@overload
+def cache_lookup(key: str, cache: dict, cap_sensitive = True) -> Tuple[str, ...] | None:
+    ...
+
+
+@overload
+def cache_lookup(key: str|int, cache: List[tuple]) -> Tuple[str, ...] | None:
+    ...
+
+@overload
+def cache_lookup(key: str, cache: list, cap_sensitive = True) -> Tuple[str, ...] | None:
+    ...
+
+
+def cache_lookup(key: str|int, cache: dict|list|set, cap_sensitive = True) -> Tuple[str, ...] | Any | None:
+    """Finds and returns object using key from any goldy bot cache object."""
+    if cap_sensitive is False:
+        key = key.lower()
+
+    if isinstance(cache, (list, set)):
+        for obj in cache:
+            
+            if isinstance(obj, tuple):
+                if key == (lambda x: x.lower() if cap_sensitive is False else x)(obj[0]):
+                    return obj
+
+
+    if isinstance(cache, dict):
+
+        for obj in cache:
+            if key == (lambda x: x.lower() if cap_sensitive is False else x)(obj):
+                return cache[obj]
+
+    return None
+
+    # TODO: Add support for more different cache types.

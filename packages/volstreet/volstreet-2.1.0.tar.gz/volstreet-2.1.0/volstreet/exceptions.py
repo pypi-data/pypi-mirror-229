727b77f46611d7deb86cb207cd5576ce5bfc33b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:10:02 +0000

    Add package volstreet/2.1.0/volstreet-2.1.0.tar.gz

---
+++ packages/volstreet/volstreet-2.1.0.tar.gz/volstreet-2.1.0/volstreet/exceptions.py
@@ -0,0 +1,36 @@
+class VolStreetException(Exception):
+    """Base class for other exceptions."""
+
+    def __init__(self, message="VolStreet Exception", code=500):
+        self.message = message
+        self.code = code
+        super().__init__(self.message)
+
+
+class ApiKeyNotFound(VolStreetException):
+    """Exception raised for missing API Key in the environment variables."""
+
+    def __init__(self, message="API Key not found", code=404):
+        super().__init__(message, code)
+
+
+class IntrinsicValueError(VolStreetException):
+    """Exception raised when unable to calculate the greeks because of mismatch of intrinsic value and market price."""
+
+    def __init__(
+        self, message="Mismatch of intrinsic value and market price", code=3501
+    ):
+        super().__init__(message, code)
+
+
+class ScripsLocationError(VolStreetException):
+    """Exception raised when unable to locate something in the scrips file."""
+
+    def __init__(
+        self, message="Could not index scrips file", code=501, additional_info=""
+    ):
+        additional_info = (
+            f"\nAdditional info: {additional_info}" if additional_info else ""
+        )
+        message = message + additional_info
+        super().__init__(message, code)

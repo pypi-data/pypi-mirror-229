a5d7a8824e11a667c46c1361d99a120bddbfb857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:17:05 +0000

    Add package kemux/1.0.1b1/kemux-1.0.1b1-py3-none-any.whl

---
+++ packages/kemux/kemux-1.0.1b1-py3-none-any.whl/kemux/data/schema/input.py
@@ -0,0 +1,73 @@
+# pylint: disable=abstract-method
+import dataclasses
+import types
+
+import dateutil.parser
+import faust
+import faust.models.fields
+
+import kemux.data.schema.base
+
+
+class InputRecordT(kemux.data.schema.base.StreamRecordT):
+    def _validate(self) -> None:
+        ...
+
+
+# pylint: disable=protected-access
+@dataclasses.dataclass
+class InputSchema(kemux.data.schema.base.SchemaBase):
+    @classmethod
+    def construct_input_record_class(cls) -> None:
+        class InputRecord(
+            faust.Record,
+            serializer='json',
+            date_parser=dateutil.parser.parse
+        ):
+            def validate_message(self) -> None:
+                for field in cls.fields:
+                    validator_name = f'_{field}_validator'
+                    validator = getattr(
+                        self.__class__,
+                        validator_name
+                    )
+                    if not isinstance(validator, types.FunctionType):
+                        raise ValueError(f'Validator: {validator_name} is not callable')
+                    cls.logger.info(f'Validating field: {field}')
+                    actual_field_value = getattr(self, field)
+                    validator(actual_field_value)
+
+            def to_dict(self) -> dict:
+                return {
+                    field: self.__dict__.get(field)
+                    for field in cls.fields
+                }
+
+        for field_name, field_type in cls.fields.items():
+            InputRecord.__annotations__[field_name] = field_type
+            setattr(
+                InputRecord,
+                field_name,
+                faust.models.fields.FieldDescriptor(
+                    required=True,
+                    exclude=False,
+                    default=None,
+                    type=field_type
+                )
+            )
+
+        implemented_validators = [
+            getattr(cls, field)
+            for field in cls.__dict__
+            if field.endswith('_validator')
+        ]
+        for validator in implemented_validators:
+            if not isinstance(validator, types.FunctionType):
+                raise ValueError(f'Validator: {validator} is not callable')
+            setattr(InputRecord, validator.__name__, validator)
+
+        cls.record_class = InputRecord  # type: ignore
+
+    @classmethod
+    def asdict(cls) -> dict[str, type]:
+        return cls.record_class.asdict()
---
+++ packages/kemux/kemux-1.0.1b1-py3-none-any.whl/kemux/data/schema/output.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import dataclasses
+
+import faust
+
+import kemux.data.schema.base
+import kemux.data.schema.input
+
+
+class OutputRecordT(kemux.data.schema.base.SchemaBase):
+    pass
+
+
+@dataclasses.dataclass
+class OutputSchema(kemux.data.schema.base.SchemaBase):
+    @staticmethod
+    def transform(message: dict) -> dict:
+        raise NotImplementedError(f'{__name__}.transform() must be implemented!')
+
+    @classmethod
+    def construct_output_record_class(cls) -> None:
+        cls.record_class = type(
+            cls.__name__,
+            (faust.Record, ),
+            {
+                '__annotations__': {
+                    **faust.Record.__annotations__,
+                    **cls.fields
+                }
+            },
+        )  # type: ignore
+
+    @classmethod
+    def validate(cls, message: dict) -> bool:
+        field_descriptions = cls.record_class.__annotations__.items()
+        for field_name, field_type in field_descriptions:
+            if field_name not in message:
+                cls.logger.warning(f'Missing field: {field_name}')
+                return False
+            if not isinstance(
+                message[field_name],
+                field_type
+            ):
+                cls.logger.warning(f'Invalid field type: {field_name} ({field_type})')
+                return False
+        return True

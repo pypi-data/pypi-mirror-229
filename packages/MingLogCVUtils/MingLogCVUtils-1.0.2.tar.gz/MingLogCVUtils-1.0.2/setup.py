522d7144cd53865c2947a6fb61e7675e318b87b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:09:23 +0000

    Add package MingLogCVUtils/1.0.2/MingLogCVUtils-1.0.2.tar.gz

---
+++ packages/MingLogCVUtils/MingLogCVUtils-1.0.2.tar.gz/MingLogCVUtils-1.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MingLogCVUtils",
+    version="1.0.2",
+    packages=find_packages(),
+    py_modules=["CVUtils"],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pillow',
+        'opencv-python',
+        'aiofiles',
+        'scipy'
+    ],
+    include_package_data=True,
+    author="MingLog",
+    author_email="736704198@qq.com",
+    description="计算机识别工具库",
+    long_description=open("README.md", encoding='UTF-8').read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="http://minglog.hzbmmc.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)

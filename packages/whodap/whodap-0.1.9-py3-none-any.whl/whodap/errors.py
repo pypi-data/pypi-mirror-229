ca509ff509109f83792f4ee0bcb2f97ec657bdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:09:28 +0000

    Add package whodap/0.1.9/whodap-0.1.9-py3-none-any.whl

---
+++ packages/whodap/whodap-0.1.9-py3-none-any.whl/whodap/errors.py
@@ -0,0 +1,18 @@
+class WhodapError(Exception):
+    ...
+
+
+class NotFoundError(WhodapError):
+    ...
+
+
+class RateLimitError(WhodapError):
+    ...
+
+
+class MalformedQueryError(WhodapError):
+    ...
+
+
+class BadStatusCode(WhodapError):
+    ...

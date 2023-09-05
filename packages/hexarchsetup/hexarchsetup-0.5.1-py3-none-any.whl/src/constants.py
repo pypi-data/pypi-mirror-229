0a6dc40914adf3748a01b3d451434d06d6ead67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:08:01 +0000

    Add package hexarchsetup/0.5.1/hexarchsetup-0.5.1-py3-none-any.whl

---
+++ packages/hexarchsetup/hexarchsetup-0.5.1-py3-none-any.whl/src/constants.py
@@ -0,0 +1,26 @@
+"""
+Constants specific to that module, such as error codes
+"""
+
+# Error codes
+INVALID_NAME_PROJECT = "Invalid name project"
+PROJECT_ALREADY_EXISTS = "A directory with this project name already exists."
+INVALID_NAME_MODULE = "Invalid module name: {module}"
+
+# File paths
+SOURCE_PATH = "src"
+TESTS_PATH = "tests"
+TEMPLATES_MODULE = "src.templates"
+ROOT_STRUCTURE_JSON = "root_structure.json"
+BASE_STRUCTURE_JSON = "base_structure.json"
+MODULE_STRUCTURE_JSON = "module_structure.json"
+
+# Replacements
+MODULE_NAME_REPLACEMENT = "{module_name}"
+
+# Formats
+DESCRIPTION_FORMAT = '"""\n{value}\n"""'
+PREFIX_TEST_MODULE = "test_"
+
+# Files
+INIT_FILE = "__init__.py"

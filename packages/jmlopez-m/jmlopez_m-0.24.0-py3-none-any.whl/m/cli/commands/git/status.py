19f0c545c50236a1b1168ae31d4b8b48603916b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:16:14 +0000

    Add package jmlopez-m/0.24.0/jmlopez_m-0.24.0-py3-none-any.whl

---
+++ packages/jmlopez-m/jmlopez_m-0.24.0-py3-none-any.whl/m/cli/commands/git/status.py
@@ -0,0 +1,40 @@
+from m.cli import BaseModel, command, run_main
+
+
+class Arguments(BaseModel):
+    """Display a single word representing the current git status.
+
+    example::
+
+        $ m git status
+        clean
+
+    statuses::
+
+        unknown
+        untracked
+        stash
+        clean
+        ahead
+        behind
+        staged
+        dirty
+        diverged
+        ?
+    """
+
+
+def _get_status(status_desc: tuple[str, str]) -> str:
+    return status_desc[0]
+
+
+@command(
+    help='display the current git status',
+    model=Arguments,
+)
+def run():
+    from m import git
+    return run_main(
+        lambda: git.get_status().map(_get_status),
+        print,
+    )

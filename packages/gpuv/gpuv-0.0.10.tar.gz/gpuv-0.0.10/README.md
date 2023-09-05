93ece2b7000da54ff541dc0f3e793898bfcafe59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:07:49 +0000

    Add package gpuv/0.0.10/gpuv-0.0.10.tar.gz

---
+++ packages/gpuv/gpuv-0.0.10.tar.gz/gpuv-0.0.10/README.md
@@ -0,0 +1,55 @@
+# gpuv
+
+gpuv (gpu _visual_) is a python library that provides an interactive dashboard to monitor your gpu usage:
+
+![GPU Demo GIF](https://github.com/CambioML/gpuv/blob/main/gpudemo.gif)
+
+
+## Installation
+
+Install with `pip`:
+
+```
+pip3 install gpuv
+```
+
+## Usage
+
+To use `gpuv`, simply run the start command from the cli:
+
+```
+gpuv start
+```
+
+This will launch the interface on another port. Navigate there to monitor your gpu usage live.
+
+## EC2 Usage
+
+On EC2, you will have to tunnel to the port `5000`. This can be done by spinning up th ec2 with port forwarding enabled, e.g.
+
+```
+ssh -i <pemkey> -L 5000:127.0.0.1:5000 ubuntu@ec2<...>.com
+```
+
+Then, just run `gpuv start` in the cli of your ec2, navigate to that port, and you're good to go!
+
+## Demo
+
+Here's a quick demo video (for those who prefer watching vs reading):
+
+video
+
+## Dev Setup
+
+To set up gpuv for your own development/extension, use `poetry`:
+
+```
+pip3 install poetry
+poetry install --no-root
+python -m gpuv -s
+```
+
+## Coming Soon
+
+- [ ] Support multiple gpu's in one view.
+- [ ] Local data collection (that you can save to csv).
---
+++ packages/gpuv/gpuv-0.0.10.tar.gz/gpuv-0.0.10/gpuv/frontend/src/lib/GPU/GpuDashboard.svelte
@@ -0,0 +1,98 @@
+<script>
+  import { writable } from "svelte/store";
+  import { onMount } from "svelte";
+  import SharedLine from "./SharedLine.svelte";
+
+  export const gpuSvelteStore = writable([]);
+  let title = "";
+  let serial_number = "";
+  let vbios_version = "";
+
+  async function fetchGpuData() {
+    try {
+      const response = await fetch("/nvml");
+      const data = await response.json();
+      title = data.device_status[0].identification.name;
+      serial_number = data.device_status[0].identification.serial_number;
+      vbios_version = data.device_status[0].identification.vbios_version;
+      return data.device_status.map((device) => ({
+        timestamp: device.timestamp,
+        gpu: device.utilization.gpu,
+        memory: device.utilization.memory,
+        temperature: device.temp_fan.temperature,
+        fan_speed: device.temp_fan.fan_speed,
+        memory_usage: device.memory.memory_usage.toFixed(3),
+        power_usage: device.power.power_usage,
+      }));
+    } catch (error) {
+      console.error("Failed to fetch GPU data", error);
+      return [];
+    }
+  }
+
+  onMount(() => {
+    // Fetch data every 1000ms
+    const interval = setInterval(async () => {
+      const newData = await fetchGpuData();
+      gpuSvelteStore.update((data) => [...data, ...newData]);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  let vals = [
+    { y: "gpu", title: "GPU", yAxis: "%" },
+    { y: "memory", title: "Memory", yAxis: "%" },
+    { y: "temperature", title: "Temperature (C°)", yAxis: "C°" },
+    { y: "fan_speed", title: "Fan Speed (rpm)", yAxis: "rpm" },
+    {
+      y: "memory_usage",
+      title: "Memory Usage",
+      yAxis: "%",
+    },
+    {
+      y: "power_usage",
+      title: "Power Usage",
+      yAxis: "%",
+    },
+  ];
+</script>
+
+<h3>{title}</h3>
+<h4>Serial Number: {serial_number}</h4>
+<h4>Vbios Version: {vbios_version}</h4>
+<div id="chart">
+  {#each vals as d}
+    <SharedLine
+      data={$gpuSvelteStore}
+      x="timestamp"
+      y={d.y}
+      yAxisLine="false"
+      xAxisText="Timestamp"
+      yAxisText={""}
+      title={d.title}
+      tooltipColor="black"
+      tooltipFontSize="15"
+      subtitle=""
+      points="false"
+      tick_opacity=".1"
+      yTicks="true"
+    />
+  {/each}
+</div>
+
+<style>
+  #chart {
+    display: grid;
+    grid-template-columns: repeat(3, 33%);
+    grid-template-rows: repeat(4, 40%);
+    height: 100vh;
+    width: 90vw;
+    margin: 3rem auto;
+    justify-content: center;
+  }
+  h4 {
+    text-align: center;
+    margin: 4px 0;
+  }
+</style>

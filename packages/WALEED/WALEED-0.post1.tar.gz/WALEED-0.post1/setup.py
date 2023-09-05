0e47643b807b137d575a29e06cf211ab1aab6e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:07:22 +0000

    Add package WALEED/0.post1/WALEED-0.post1.tar.gz

---
+++ packages/WALEED/WALEED-0.post1.tar.gz/WALEED-0.post1/setup.py
@@ -0,0 +1,11 @@
+from ensurepip import version
+from unicodedata import name
+import setuptools
+setuptools.setup(
+    name='WALEED',
+    version='0,1',
+    author='WALEED',
+    description='For a test',
+    packages=setuptools.find_packages(),
+   
+)

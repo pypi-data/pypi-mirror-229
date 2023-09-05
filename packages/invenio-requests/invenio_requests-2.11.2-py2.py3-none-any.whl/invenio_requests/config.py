66970d40b3b98b800dd1482e8d6a5fd2b73cf9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:08:56 +0000

    Add package invenio-requests/2.11.2/invenio_requests-2.11.2-py2.py3-none-any.whl

---
+++ packages/invenio-requests/invenio_requests-2.11.2-py2.py3-none-any.whl/invenio_requests/config.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 - 2022 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module for generic and customizable requests."""
+
+from invenio_users_resources.entity_resolvers import GroupResolver, UserResolver
+
+from invenio_requests.services.requests import facets
+
+from .customizations import CommentEventType, LogEventType
+from .services.permissions import PermissionPolicy
+
+REQUESTS_PERMISSION_POLICY = PermissionPolicy
+"""Override the default requests/comments permission policy."""
+
+REQUESTS_REGISTERED_TYPES = []
+"""Configuration for registered Request Types."""
+
+REQUESTS_REGISTERED_EVENT_TYPES = [
+    LogEventType(),
+    CommentEventType(),
+]
+"""Configuration for registered Request Event Types."""
+
+REQUESTS_ENTITY_RESOLVERS = [UserResolver(), GroupResolver()]
+"""Registered resolvers for resolving/creating references in request metadata."""
+
+REQUESTS_ROUTES = {
+    "details": "/requests/<pid_value>",
+}
+"""Invenio requests ui endpoints."""
+
+REQUESTS_FACETS = {
+    "type": {
+        "facet": facets.type,
+        "ui": {
+            "field": "type",
+        },
+    },
+    "status": {
+        "facet": facets.status,
+        "ui": {
+            "field": "status",
+        },
+    },
+}
+"""Invenio requests facets."""
+
+REQUESTS_TIMELINE_PAGE_SIZE = 15
+"""Amount of items per page on the request details timeline"""
+
+
+REQUESTS_MODERATION_ROLE = "administration-moderation"
+"""ID of the Role used for moderation."""

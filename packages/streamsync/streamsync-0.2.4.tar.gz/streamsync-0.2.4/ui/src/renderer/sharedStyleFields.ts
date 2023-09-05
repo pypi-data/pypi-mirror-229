ca62738346c8e25f89b820c47fbcb7884c72c9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:13:51 +0000

    Add package streamsync/0.2.4/streamsync-0.2.4.tar.gz

---
+++ packages/streamsync/streamsync-0.2.4.tar.gz/streamsync-0.2.4/ui/src/renderer/sharedStyleFields.ts
@@ -0,0 +1,79 @@
+import { FieldCategory, FieldType } from "../streamsyncTypes";
+
+export const accentColor = {
+	name: "Accent",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const primaryTextColor = {
+	name: "Primary text",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const secondaryTextColor = {
+	name: "Secondary text",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const emptinessColor = {
+	name: "Emptiness",
+	desc: "Page background color",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const containerBackgroundColor = {
+	name: "Container background",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const containerShadow = {
+	name: "Container shadow",
+	type: FieldType.Shadow,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const separatorColor = {
+	name: "Separator",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const buttonColor = {
+	name: "Button",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const buttonTextColor = {
+	name: "Button text",
+	type: FieldType.Color,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const buttonShadow = {
+	name: "Button shadow",
+	type: FieldType.Shadow,
+	category: FieldCategory.Style,
+	applyStyleVariable: true,
+};
+
+export const cssClasses = {
+	name: "Custom CSS classes",
+	type: FieldType.Text,
+	category: FieldCategory.Style,
+	desc: "CSS classes, separated by spaces. You can define classes in custom stylesheets."
+};

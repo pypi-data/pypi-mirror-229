bd7f566007b54058d8621f8a6ae3e45804ff093b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:08:38 +0000

    Add package skynet-ml/0.0.8.4/skynet_ml-0.0.8.4-py3-none-any.whl

---
+++ packages/skynet-ml/skynet_ml-0.0.8.4-py3-none-any.whl/skynet_ml/deep/layers/activations.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+def linear(x: float) -> float:
+    """
+    Linear function.
+    """
+    return x
+
+
+def d_linear(x: float) -> int:
+    """
+    Derivative of the linear function.
+    """
+    return 1
+
+
+def sigmoid(x: float) -> float:
+    """
+    Sigmoid function.
+    """
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def d_sigmoid(x: float) -> float:
+    """
+    Derivative of the sigmoid function.
+    """
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def relu(x: float) -> float:
+    """
+    Relu activation function.
+    """
+    return np.maximum(0, x)
+
+
+def d_relu(x: float) -> int:
+    """
+    Derivative of the relu function.
+    """
+    return np.where(x < 0, 0, 1)
+
+
+def tanh(x: float) -> float:
+    """
+    Hyperbolic tangent activaiton function.
+    """
+    return np.tanh(x)
+
+
+def d_tanh(x: float) -> float:
+    """
+    Derivative of the tanh function.
+    """
+    return 1 - np.tanh(x)**2
+
+
+def softmax(s: np.array) -> np.array:
+    """
+    Softmax activation function.
+    """
+    exp_s = np.exp(s - np.max(s))  
+    return exp_s / np.sum(exp_s)
+
+
+def d_softmax(x: np.ndarray) -> np.ndarray:
+    """
+    Derivative of the softmax function.
+    """
+    p = softmax(x)
+    return np.diag(p) - np.outer(p, p)
+
+
+activations_map = {
+    "linear": linear,
+    "sigmoid": sigmoid,
+    "relu": relu,
+    "tanh": tanh,
+    "softmax": softmax
+}
+
+
+d_activations_map = {
+    "linear": d_linear,
+    "sigmoid": d_sigmoid,
+    "relu": d_relu,
+    "tanh": d_tanh,
+    "softmax": d_softmax
+}

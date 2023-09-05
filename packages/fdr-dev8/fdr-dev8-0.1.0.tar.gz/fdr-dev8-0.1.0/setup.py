6078230a9e698483b713877076902efc6a917f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:07:21 +0000

    Add package fdr-dev8/0.1.0/fdr-dev8-0.1.0.tar.gz

---
+++ packages/fdr-dev8/fdr-dev8-0.1.0.tar.gz/fdr-dev8-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fdr-dev8',
+    version='0.1.0',
+    description='Sample fdr lib',
+    author='Konstantinos Gyftodimos',
+    author_email='konstgyftodimos@gmail.com',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fdr = client.main:fdr_client',
+        ],
+    },
+)

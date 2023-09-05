40294df603e927a1c62294f80324848ab860ae0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:09:46 +0000

    Add package ocrpackage/0.0.34/ocrpackage-0.0.34.tar.gz

---
+++ packages/ocrpackage/ocrpackage-0.0.34.tar.gz/ocrpackage-0.0.34/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("/Users/nabilalasri/Downloads/OCRProofID copy/requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ocrpackage",
+    version="0.0.34",
+    author="Nabila Lasri",
+    author_email="lasri.nabilait@gmail.com",
+    description=("This repository contains a Python program designed to execute Optical Character Recognition (OCR)"
+                 " and Facial Recognition on images."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires="==3.6",
+    install_requires=required,
+    include_package_data=True,
+)

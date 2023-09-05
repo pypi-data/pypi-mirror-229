4ee93e5d126cfaef177b96a7408adb170c1ab2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:08:16 +0000

    Add package playai/0.0.1/playai-0.0.1.tar.gz

---
+++ packages/playai/playai-0.0.1.tar.gz/playai-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = (Path(__file__).parent / "README.md").read_text()
+
+# Setting up
+setup(
+        name="playai", 
+        version=VERSION,
+        author="Valentin Lopez",
+        author_email="<valentinlopezandres@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)

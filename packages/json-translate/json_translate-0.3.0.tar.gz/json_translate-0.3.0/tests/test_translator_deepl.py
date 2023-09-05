e22aac8a686f06c8640338604b2448ba18338e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:06:00 +0000

    Add package json-translate/0.3.0/json_translate-0.3.0.tar.gz

---
+++ packages/json-translate/json_translate-0.3.0.tar.gz/json_translate-0.3.0/tests/test_translator_deepl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import unittest
+from json_translate.translators.deepl import DeepLTranslator
+
+
+class TranslatorTest(unittest.TestCase):
+    """Test DeepL translator."""
+
+    def test_string_decode(self):
+        """Test encoded string."""
+        translator = DeepLTranslator("en")
+        self.assertEqual(translator.decode("m\u00b2"), "m²")

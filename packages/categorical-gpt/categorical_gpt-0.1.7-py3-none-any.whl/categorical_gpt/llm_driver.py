7f937256aaf0b1258eee26f9ffafdaac0ece8489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:08:25 +0000

    Add package categorical-gpt/0.1.7/categorical_gpt-0.1.7-py3-none-any.whl

---
+++ packages/categorical-gpt/categorical_gpt-0.1.7-py3-none-any.whl/categorical_gpt/llm_driver.py
@@ -0,0 +1,8 @@
+class LLMDriver:
+    def ask(self, prompt, is_json=True, variation=0, **kwargs):
+        print('Implement the ask function for your custom driver.')
+        pass
+
+    def get_answer(self, prompt, is_json=True, **kwargs):
+        print('Implement the get_answer function for your custom driver.')
+        pass

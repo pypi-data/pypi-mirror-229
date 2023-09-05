230bd3d442c20042343ca5e0b44b1f5df8120cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:09:20 +0000

    Add package kot-web/0.37.0/kot_web-0.37.0.tar.gz

---
+++ packages/kot-web/kot_web-0.37.0.tar.gz/kot_web-0.37.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setup(
+    name="kot_web",
+    version="0.37.0",
+    description="""This is WEB package for KOT""",
+    url="https://github.com/onuratakan/KOT",
+    author='Onur Atakan ULUSOY',
+    author_email='atadogan06@gmail.com',
+    license='MIT',
+    install_requires=install_requires,
+    python_requires=">= 3",
+    zip_safe=False,
+)
+

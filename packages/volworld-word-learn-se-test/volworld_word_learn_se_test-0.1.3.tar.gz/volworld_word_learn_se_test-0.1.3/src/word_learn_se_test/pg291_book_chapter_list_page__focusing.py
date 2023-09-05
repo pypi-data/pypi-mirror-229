20ce279f55e9cdf543846c8f4b12416ff2ccc2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:07:21 +0000

    Add package volworld-word-learn-se-test/0.1.3/volworld_word_learn_se_test-0.1.3.tar.gz

---
+++ packages/volworld-word-learn-se-test/volworld_word_learn_se_test-0.1.3.tar.gz/volworld_word_learn_se_test-0.1.3/src/word_learn_se_test/pg291_book_chapter_list_page__focusing.py
@@ -0,0 +1,8 @@
+from behave import *
+
+from focusing_app_bar import check_focusing_circles_and_slots_on_focusing_bar
+
+@then('{focusing_ch} focusing Chapters and {empty_slots} empty slots shown on focusing bar')
+def check_focusing_chapters_and_slot_on_focusing_bar(c, focusing_ch: str, empty_slots: str):
+    check_focusing_circles_and_slots_on_focusing_bar(c, focusing_ch, empty_slots)
+

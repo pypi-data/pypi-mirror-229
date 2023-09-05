099ceab5c2e1a030bfa3367c361c9904abdd14da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:07:01 +0000

    Add package tangelokit/0.0.4/tangelokit-0.0.4.tar.gz

---
+++ packages/tangelokit/tangelokit-0.0.4.tar.gz/tangelokit-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Library created to generate structured data from tangelo'
+
+# Setting up
+setup(
+    name="tangelokit",
+    version=VERSION,
+    author="Arnold Blandon",
+    author_email="arnold.blandon1@gmail.com",
+    description=DESCRIPTION,
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "jsonschema==4.0.0",
+    ],
+    keywords=['python', 'tangelo', 'kit', 'data'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

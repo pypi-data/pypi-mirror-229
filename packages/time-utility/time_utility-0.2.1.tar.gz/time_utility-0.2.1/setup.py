dc4961da7fee2c2d08562271483f4e156acd2a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:08:03 +0000

    Add package time-utility/0.2.1/time_utility-0.2.1.tar.gz

---
+++ packages/time-utility/time_utility-0.2.1.tar.gz/time_utility-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="time_utility",  # Replace with your own username
+    version="0.2.1",
+    author="Vieolo OÜ",
+    author_email="info@vieolo.com",
+    description="Time utility functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vieolo/python-time-utility.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)

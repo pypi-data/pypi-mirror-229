aedabae716b2ff0fbf9e17366fea3f69796fe34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:08:17 +0000

    Add package msstitch/3.14/msstitch-3.14-py3-none-any.whl

---
+++ packages/msstitch/msstitch-3.14-py3-none-any.whl/app/actions/lookups/sequence.py
@@ -0,0 +1,54 @@
+from Bio import SeqIO
+
+from app.actions.sequence import trypsinize
+
+PROTEIN_STORE_CHUNK_SIZE = 100000
+
+
+def create_searchspace_wholeproteins(lookup, fastafn, minpeplen):
+    fasta = SeqIO.parse(fastafn, 'fasta')
+    storeseqs = {}
+    peptotal, pepsubtotal = 0, 0
+    for record in fasta:
+        prot_id, protseq = record.id, str(record.seq)
+        storeseqs[prot_id] = {'seq': protseq, 'peps': []}
+        isoseq = protseq.replace('L', 'I') 
+        for pos in range(0, len(protseq) - minpeplen + 1):
+            possible_pep = isoseq[pos:pos + minpeplen]
+            pepsubtotal += 1
+            storeseqs[prot_id]['peps'].append((possible_pep, pos))
+        if pepsubtotal > PROTEIN_STORE_CHUNK_SIZE:
+            lookup.store_pep_proteins(storeseqs)
+            peptotal += pepsubtotal
+            pepsubtotal = 0
+            storeseqs = {}
+    peptotal += pepsubtotal
+    lookup.store_pep_proteins(storeseqs)
+    lookup.index_proteins()
+    print(f'Stored {peptotal} peptides')
+
+
+def create_searchspace(lookup, infile, minlen, proline_cut=False, reverse_seqs=True,
+        do_trypsinize=True, miss_cleavage=False):
+    """Given a FASTA database, proteins are trypsinized and resulting peptides
+    stored in a database or dict for lookups"""
+    allpeps = []
+    def treat_pep(seq, do_trypsinize, proline_cut, miss_cleavage, minlen): 
+        if do_trypsinize:
+            pepseqs = trypsinize(seq, proline_cut, miss_cleavage=miss_cleavage)
+        else:
+            pepseqs = [seq]
+        # Exchange all leucines to isoleucines because MS can't differ
+        pepseqs = [(pep.replace('L', 'I'),) for pep in pepseqs]
+        if minlen:
+            pepseqs = [pep for pep in pepseqs if len(pep[0]) >= minlen]
+        return pepseqs
+    with open(infile) as fp:
+        ftype = 'fasta' if fp.read(1) == '>' else 'txt'
+        fp.seek(0)
+        if ftype == 'fasta':
+            records = (str(x.seq) for x in SeqIO.parse(fp, 'fasta'))
+        elif ftype == 'txt':
+            records = (x.strip('\n') for x in fp)
+        lookup.write_peps((pep for x in records for pep in treat_pep(x, do_trypsinize, proline_cut, miss_cleavage, minlen)), reverse_seqs)
+    lookup.index_peps(reverse_seqs)
---
+++ packages/msstitch/msstitch-3.14-py3-none-any.whl/app/actions/psmtable/percolator.py
@@ -0,0 +1,87 @@
+import os
+from app.readers import mzidplus as readers
+from app.readers import tsv as tsvreader
+from app.readers import xml
+from app.readers import percolator as percoreader 
+from app.dataformats import mzidtsv as psmheaders
+
+
+def calculate_target_decoy_competition(percofn):
+    """
+    From a percolator XML output file, take the FDR (q-value) and PEP and apply
+    to the PSM table
+    """
+    ns = xml.get_namespace_from_top(percofn, None)
+    # Get PSM FDRs
+    psms = {
+            p.attrib['{%s}psm_id' % ns['xmlns']]: {
+                'svm': float(p.find('{%s}svm_score' % ns['xmlns']).text),
+                'qval': float(p.find('{%s}q_value' % ns['xmlns']).text),
+                'pep': float(p.find('{%s}pep' % ns['xmlns']).text),
+                'td': 'decoy' if p.attrib['{%s}decoy' % ns['xmlns']] == 'true' else 'target',
+                }
+            for p in percoreader.generate_psms(percofn, ns)
+            }
+    # Then get peptide FDRs and add them to PSMs
+    for peptide in percoreader.generate_peptides(percofn, ns):
+        psm_ids = peptide.find('{%s}psm_ids' % ns['xmlns'])
+        for psm_id in psm_ids:
+            try:
+                psms[psm_id.text].update({
+                    'pepqval': float(peptide.find('{%s}q_value' % ns['xmlns']).text),
+                    'peppep': float(peptide.find('{%s}pep' % ns['xmlns']).text),
+                    })
+            except KeyError:
+                # Edgecase, sometimes filtering percolator data causes this:
+                # when there is a peptide with a PSM ID not in percolator PSMs 
+                # set all its PSMs pep-q-values to 1
+                [psms[x].update({'pepqval': 1, 'peppep': 1}) for x in psm_ids if x in psms]
+                break
+    return psms
+
+
+def add_fdr_to_mzidtsv(psms, mzid_specidr, mzns, percodata):
+    """Takes PSMs from an mzIdentML and its MSGF+ TSV and a corresponding 
+    percolator XML. 
+    """
+    # mzId results and PSM lines can be zipped
+    scan = 0
+    for specidr in mzid_specidr:
+        try:
+            scan = int({x.split('=')[0]: x.split('=')[1] for x in specidr.attrib['spectrumID'].split(' ')}['scan'])
+        except KeyError:
+            # in e.g. timstof data there are no true scan numbers, percolator sets it by increment
+            scan += 1
+        for specidi in specidr.findall('{%s}SpectrumIdentificationItem' % mzns['xmlns']):
+            psm = next(psms)
+            # percolator psm ID is: samplename_SII_scanindex_sii.ix_scannr_charge_rank
+            # SII_scanindex_sii.ix is in SII.attrib['id']
+            spfile = os.path.splitext(psm[psmheaders.HEADER_SPECFILE])[0]
+            try:
+                percopsm = percodata['{fn}_{sii_id}_{sc}_{ch}_{rk}'.format(fn=spfile,
+                    sii_id=specidi.attrib['id'], sc=scan, rk=specidi.attrib['rank'], ch=psm['Charge'])]
+            except KeyError:
+                continue
+            outpsm = {k: v for k,v in psm.items()}
+            outpsm.update({
+                psmheaders.HEADER_SVMSCORE: percopsm['svm'], 
+                psmheaders.HEADER_PSMQ: percopsm['qval'], 
+                psmheaders.HEADER_PSM_PEP: percopsm['pep'], 
+                psmheaders.HEADER_PEPTIDE_Q: percopsm['pepqval'], 
+                psmheaders.HEADER_PEPTIDE_PEP: percopsm['peppep'], 
+                psmheaders.HEADER_TARGETDECOY: percopsm['td'],
+                })
+            # Remove all decoy protein matches from target proteins, to ensure downstream
+            # processing does not trip up on them, e.g. having a decoy master protein.
+            if percopsm['td'] != 'decoy':
+                outprots = []
+                for prot in outpsm[psmheaders.HEADER_PROTEIN].split(';'):
+                    if not prot.startswith(psmheaders.DECOY_PREFIX):
+                        outprots.append(prot)
+                outpsm[psmheaders.HEADER_PROTEIN] = ';'.join(outprots)
+            yield outpsm
+
+
+def get_header_with_percolator(oldheader):
+    ix = oldheader.index(psmheaders.HEADER_EVALUE) + 1
+    return oldheader[:ix] + psmheaders.PERCO_HEADER + oldheader[ix:]

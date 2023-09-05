5b6743a373b94c6edc95b128c6ef5e65bcb30787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:10:19 +0000

    Add package logger-local/0.0.53/logger-local-0.0.53.tar.gz

---
+++ packages/logger-local/logger-local-0.0.53.tar.gz/logger-local-0.0.53/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='logger-local',
+    version='0.0.53',  # https://pypi.org/project/logger-local/
+    author="Circles",
+    author_email="info@circles.life",
+    description="PyPI Package for Circles Logger Python Local",
+    long_description="This is a package for sharing common Logger function used in different repositories",
+    long_description_content_type="text/markdown",
+    url="https://github.com/circles-zone/logger-local-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+)

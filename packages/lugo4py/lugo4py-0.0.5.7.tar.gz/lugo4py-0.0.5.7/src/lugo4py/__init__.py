7c7c97568334e99037187fa520ef61318ee98d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:08:17 +0000

    Add package lugo4py/0.0.5.7/lugo4py-0.0.5.7.tar.gz

---
+++ packages/lugo4py/lugo4py-0.0.5.7.tar.gz/lugo4py-0.0.5.7/src/lugo4py/__init__.py
@@ -0,0 +1,23 @@
+from .src.client import PROTOCOL_VERSION
+from .src.client import LugoClient
+from .src.client import NewClientFromConfig
+
+from .src.goal import Goal
+
+from .src.interface import Bot, PLAYER_STATE, PlayerState
+
+from .src.loader import EnvVarLoader
+
+from .src.lugo import Order, OrderSet, Point, Vector, new_vector, new_velocity, Velocity, Team, TeamSide, \
+    OrderResponse, \
+    CommandResponse, State, Player, PlayerProperties, BallProperties, GameProperties, GameSnapshot, Ball, \
+    ResumeListeningResponse, \
+    ResumeListeningRequest, PauseResumeRequest, JoinRequest, NextOrderRequest, NextTurnRequest, StatusCode, Jump, Kick, \
+    Move, Catch, \
+    ShotClock, RemoteServicer
+
+
+
+from .src.snapshot import GameSnapshotReader
+
+from .src.specs import *

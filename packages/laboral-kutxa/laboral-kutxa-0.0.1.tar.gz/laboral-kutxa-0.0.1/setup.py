75375fda96d2f7c7fe586190466e89b81554e181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:05:46 +0000

    Add package laboral-kutxa/0.0.1/laboral-kutxa-0.0.1.tar.gz

---
+++ packages/laboral-kutxa/laboral-kutxa-0.0.1.tar.gz/laboral-kutxa-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="laboral-kutxa",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Unofficial Laboral Kutxa Python library",
+)

9d6e624e86d76e34966b17683df2db99832d3cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:09:37 +0000

    Add package comun-va/1.1/comun_va-1.1.tar.gz

---
+++ packages/comun-va/comun_va-1.1.tar.gz/comun_va-1.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='comun_va',
+      version='1.1',
+      py_modules=['comun_va', 'comun_mail'],
+      install_requires=['comun_sqlsrv', 'opencv-python', 'tensorflow', 'numpy'])

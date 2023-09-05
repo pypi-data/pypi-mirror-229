70f4e62788300c9ca5b0d0e24a6e4a83e484f3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:15:32 +0000

    Add package haplot/0.0.2/haplot-0.0.2-py3-none-any.whl

---
+++ packages/haplot/haplot-0.0.2-py3-none-any.whl/haplot/theme.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/6/28 10:56
+# @Author  : LXD
+# @Email   : lxd1997xy@163.com
+# @File    : theme.py
+
+
+from matplotlib import figure, axes
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+
+class Theme(object):
+    def __str__(self):
+        return "Theme"
+
+    __repr__ = __str__
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
+
+    @staticmethod
+    def apply(ax: axes.Axes = None):
+        if ax is None:
+            ax = plt.gca()
+
+        # Set edge color for top and right spines
+        ax.spines['top'].set_edgecolor('lightgrey')
+        ax.spines['right'].set_edgecolor('lightgrey')
+
+        # ax.spines[['top', 'right']].set_visible(False)
+
+        ax.yaxis.grid(
+            True,
+            linestyle='--',
+            which='major',
+            color='lightgrey',
+            alpha=.5
+        )
+
+        # Hide these grid behind plot objects
+        ax.set_axisbelow(True)
+
+        return ax
+
+
+class BoxTheme(object):
+    """
+    Boxplot theme
+    """
+    def __str__(self):
+        return "BoxTheme"
+
+    __repr__ = __str__
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
+
+    @staticmethod
+    def apply(ax: axes.Axes):
+        if ax is None:
+            ax = plt.gca()
+
+        # color palette:
+        if len(ax.patches) == 0:
+            print("No patches in boxplot.")
+            return ax
+
+        box_face_colors = cycle(['skyblue', 'steelblue'])
+        box_edge_colors = cycle(['black'])
+
+        for p in ax.patches:
+            p.set_facecolor(next(box_face_colors))
+            p.set_edgecolor(next(box_edge_colors))
+
+        # Add a horizontal grid to the boxplot, but make it very light in color
+        ax.yaxis.grid(
+            True,
+            linestyle='--',
+            which='major',
+            color='lightgrey',
+            alpha=.5
+        )
+
+        # Hide these grid behind plot objects
+        ax.set_axisbelow(True)
+
+        # Set edge color for top and right spines
+        ax.spines['top'].set_edgecolor('lightgrey')
+        ax.spines['right'].set_edgecolor('lightgrey')
+        ax.spines['bottom'].set_position(('outward', 10))
+
+        ax.spines['left'].set_bounds(ax.get_ybound())
+        ax.spines['right'].set_bounds(ax.get_ybound())
+
+        # Rotate the tick labels and set their alignment.
+        # ax.tick_params(axis='x', colors='green', size=10, width=5, rotation=45)
+
+        # Add custom rectangle annotation under axes
+        for label in ax.xaxis.get_ticklabels():
+            # label is a Text instance
+            label.set_fontsize(16)
+
+        tick_face_colors = cycle(['skyblue', 'steelblue'])
+        tick_edge_colors = cycle(['skyblue', 'steelblue'])
+
+        for tick in ax.xaxis.get_major_ticks():
+            # line is a Line2D instance
+            tick.tick1line.set_markerfacecolor(next(tick_face_colors))
+            tick.tick1line.set_markeredgecolor(next(tick_edge_colors))
+
+        ax.tick_params(axis='x',
+                       pad=5,
+                       size=5,  # tick height
+                       width=ax.get_window_extent().width / len(ax.xaxis.get_major_ticks()) * .72,  # tick width
+                       rotation=0)
+        return ax
+
+
+class ManhattanTheme(object):
+    def __str__(self):
+        return "ManhattanTheme"
+
+    __repr__ = __str__
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
+
+    @staticmethod
+    def apply(fig: figure.Figure = None):
+        if fig is None:
+            fig = plt.gcf()
+
+        if len(fig.axes) == 0:
+            print("No axes in figure.")
+            return fig
+        else:
+            for ax in fig.axes:
+                Theme.apply(ax)
+                ax.set_ylabel(r"$\mathrm{-log}_{10}(\mathit{p})$")
+                ax.set_xmargin(0.01)
+                ax.set_ylim(bottom=0, top=None)
+            fig.axes[-1].set_xlabel("Chromosome")
+            fig.axes[-1].tick_params(axis='x', rotation=0)
+        return fig
+
+
+class QQTheme(object):
+    def __str__(self):
+        return "QQTheme"
+
+    __repr__ = __str__
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
+
+    @staticmethod
+    def apply(ax: axes.Axes = None):
+        if ax is None:
+            ax = plt.gca()
+        Theme.apply(ax)
+        ax.set_xlabel("Expected -log10(p-value)")
+        ax.set_ylabel("Observed -log10(p-value)")
+        return ax
+
---
+++ packages/haplot/haplot-0.0.2-py3-none-any.whl/haplot/utils.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/1 11:54
+# @Author  : LXD
+# @Email   : lxd1997xy@163.com
+# @File    : utils.py
+from abc import ABC
+
+import numpy as np
+from matplotlib.offsetbox import AnchoredOffsetbox, DrawingArea
+from matplotlib.lines import Line2D
+from matplotlib.text import Text
+
+
+def adjust_text(texts, ax):
+    """
+    Automatically adjust text position to avoid overlap.
+
+    Parameters
+    ----------
+    :param texts: a list of Text object
+    :param ax: Axes object
+    :return: None
+    """
+    return
+
+
+def text_fit_patch(text, patch, ax):
+    """
+    Automatically adjust the font size of text to fit the patch.
+
+    Parameters
+    ----------
+    :param text: Text object
+    :param patch: Patch object
+    :param ax: Axes object
+    :return: None
+    """
+    font_size = text.get_fontsize()
+    patch_width = patch.get_window_extent(ax.figure.canvas.get_renderer()).width
+    text_width = text.get_window_extent(ax.figure.canvas.get_renderer()).width
+    while text_width > patch_width and font_size > 1:
+        font_size -= 1
+        text.set_fontsize(font_size)
+
+
+class AnchoredSizeLegend(AnchoredOffsetbox, ABC):
+    def __init__(self,
+                 size,
+                 label,
+                 label_size=10,
+                 *args,
+                 **kwargs):
+        """
+        Draw a size legend with given size in points.
+
+        Parameters
+        ----------
+        :param size: a list of size in points
+        :param label: a list of label
+        :param label_size: font size of label
+        :param args: other args of AnchoredOffsetbox
+        :param kwargs: other kwargs of AnchoredOffsetbox
+        """
+        if isinstance(size, int):
+            size = np.sqrt([size])
+        else:
+            size = np.sqrt(size)
+
+        if isinstance(label, str):
+            label = [label]
+
+        # self.box = AuxTransformBox(transform)
+        self.box = DrawingArea(np.max(size) * 2.2, np.max(size) * 1.2, 0, 0)
+
+        for i in range(len(size)):
+            x, y = np.max(size) / 2, size[i] / 2
+            self.box.add_artist(
+                Line2D([x], [y], marker='o', color='w',
+                       markerfacecolor='none',
+                       markeredgecolor='k',
+                       markersize=size[i])
+            )
+            self.box.add_artist(
+                Line2D([x, x + size[i]], [size[i], size[i]], color="k")
+            )
+            self.box.add_artist(
+                Text(x + size[i], size[i],
+                     label[i], size=label_size,
+                     color='k', ha="left", va="center")
+            )
+        super().__init__(*args, child=self.box, **kwargs)

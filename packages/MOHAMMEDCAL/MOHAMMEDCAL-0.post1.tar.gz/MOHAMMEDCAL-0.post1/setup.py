0c81e81b21d756a60f5793b7c85bf8d8010824b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:06:47 +0000

    Add package MOHAMMEDCAL/0.post1/MOHAMMEDCAL-0.post1.tar.gz

---
+++ packages/MOHAMMEDCAL/MOHAMMEDCAL-0.post1.tar.gz/MOHAMMEDCAL-0.post1/setup.py
@@ -0,0 +1,11 @@
+from ensurepip import version
+from unicodedata import name
+import setuptools
+setuptools.setup(
+    name='MOHAMMEDCAL',
+    version='0,1',
+    author='MO-SALAH',
+    description='For a test',
+    packages=setuptools.find_packages(),
+   
+)

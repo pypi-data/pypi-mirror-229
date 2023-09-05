171ec449a89b157ac32479302fcd90b31081e26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:08:06 +0000

    Add package pronotepy/2.12.0/pronotepy-2.12.0-py3-none-any.whl

---
+++ packages/pronotepy/pronotepy-2.12.0-py3-none-any.whl/pronotepy/__init__.py
@@ -0,0 +1,13 @@
+"""
+An API wrapper for pronote.
+"""
+
+__title__ = "pronotepy"
+__author__ = "bain, Xiloe"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020-2022 bain, Xiloe"
+__version__ = "2.12.0"
+
+from .dataClasses import *
+from .clients import *
+from .exceptions import *

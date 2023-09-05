6e963b10b21f38e61b5eb774ff89a8bbdca9857a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:16:40 +0000

    Add package arianeDev/0.1.5/arianeDev-0.1.5.tar.gz

---
+++ packages/arianeDev/arianeDev-0.1.5.tar.gz/arianeDev-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arianeDev',
+    version='0.1.5',
+    description='arianeDev',
+    author='alandevcol',
+    author_email='alandevcol@gmail.com',
+    url='https://github.com/alandevcol/arianeDevs',
+    packages=find_packages(),
+    install_requires=[
+        "pydantic>=2.0.0",
+    ],
+    include_package_data=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)

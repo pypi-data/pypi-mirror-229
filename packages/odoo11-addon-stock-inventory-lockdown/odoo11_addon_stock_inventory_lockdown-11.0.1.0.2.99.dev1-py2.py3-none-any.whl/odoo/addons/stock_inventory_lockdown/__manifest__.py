4504f06ef0750f464127f7158efaaab63330a5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:09:48 +0000

    Add package odoo11-addon-stock-inventory-lockdown/11.0.1.0.2.99.dev1/odoo11_addon_stock_inventory_lockdown-11.0.1.0.2.99.dev1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-stock-inventory-lockdown/odoo11_addon_stock_inventory_lockdown-11.0.1.0.2.99.dev1-py2.py3-none-any.whl/odoo/addons/stock_inventory_lockdown/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2013-2016 Numérigraphe SARL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Inventory Lock Down",
+    "summary": "Lock down stock locations during inventories.",
+    "version": "11.0.1.0.2",
+    "development_status": "Mature",
+    "depends": ["stock"],
+    "author": "Numérigraphe, Eficent, Odoo Community Association (OCA)",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "images": [
+        "images/move_error.png",
+        "images/location_locked.png",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}

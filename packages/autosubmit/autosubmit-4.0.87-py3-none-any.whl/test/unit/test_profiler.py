56d8f47753a704662fb97cd4c2dbcb58e7db39d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:07:07 +0000

    Add package autosubmit/4.0.87/autosubmit-4.0.87-py3-none-any.whl

---
+++ packages/autosubmit/autosubmit-4.0.87-py3-none-any.whl/test/unit/test_profiler.py
@@ -0,0 +1,50 @@
+from unittest import TestCase, mock
+from autosubmit.profiler.profiler import Profiler
+from log.log import AutosubmitCritical
+from pathlib import Path
+
+
+class TestProfiler(TestCase):
+    def setUp(self):
+        self.profiler = Profiler("a000")
+
+    # Black box techniques for status machine based software
+    #
+    #   O---->__init__------> start
+    #                           |
+    #                           |
+    #                         stop ----> report --->0
+
+    # Status coverage
+    def test_status_machine(self):
+        self.profiler.start()
+        self.profiler.stop()
+
+    # Transition coverage
+    def test_transitions(self):
+        # __init__ -> start
+        self.profiler.start()
+
+        # start -> stop
+        self.profiler.stop()
+
+    def test_transitions_fail_cases(self):
+        # __init__ -> stop
+        self.assertRaises(AutosubmitCritical, self.profiler.stop)
+
+        # start -> start
+        self.profiler.start()
+        self.assertRaises(AutosubmitCritical, self.profiler.start)
+
+    # White box tests
+    @mock.patch("os.access")
+    def test_writing_permission_check_fails(self, mock_response):
+        mock_response.return_value = False
+
+        self.profiler.start()
+        self.assertRaises(AutosubmitCritical, self.profiler.stop)
+
+    def test_memory_profiling_loop(self):
+        self.profiler.start()
+        bytearray(1024*1024)
+        self.profiler.stop()

579e4a7e3dd76f881cc6cc97b74e8385afcec4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:06:59 +0000

    Add package paleopoint/0.0.2/paleopoint-0.0.2.tar.gz

---
+++ packages/paleopoint/paleopoint-0.0.2.tar.gz/paleopoint-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+'''with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()'''
+
+setup(
+    name="paleopoint",
+    version="0.0.2",
+    author="Bailong Zhao",
+    author_email="bailongzhao@163.com",
+    description="A package calculates location to paleopoint",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

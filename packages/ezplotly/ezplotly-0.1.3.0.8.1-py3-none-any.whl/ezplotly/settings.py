098f653c4dfcb3472c38924c04dde5be5a89cedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:14:26 +0000

    Add package ezplotly/0.1.3.0.8.1/ezplotly-0.1.3.0.8.1-py3-none-any.whl

---
+++ packages/ezplotly/ezplotly-0.1.3.0.8.1-py3-none-any.whl/ezplotly/settings.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+import plotly
+
+SUPPRESS_PLOTS: bool = False
+
+
+class EnvironmentSettings(Enum):
+    """
+    Enum for environment settings.
+    """
+
+    JUPYTER = "Jupyter"
+    TERMINAL = "Terminal"
+    UNKNOWN = "Unknown"
+
+
+def get_environment() -> EnvironmentSettings:
+    """
+    Gets the environment settings of plotly.
+    :return: the environment that the code is running in.
+    """
+    try:
+        from IPython import get_ipython
+
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return EnvironmentSettings.JUPYTER
+        elif shell == "TerminalInteractiveShell":
+            return EnvironmentSettings.TERMINAL
+        else:
+            return EnvironmentSettings.UNKNOWN
+    except:
+        return EnvironmentSettings.UNKNOWN
+
+
+# initialize plotly for jupyter notebook environment
+if get_environment() == EnvironmentSettings.JUPYTER:
+    plotly.offline.init_notebook_mode(connected=True)

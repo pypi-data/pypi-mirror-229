efaa0a054c7239fa18d1c218a90fd4034031ecf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:10:56 +0000

    Add package deepfos/1.1.3/deepfos-1.1.3.tar.gz

---
+++ packages/deepfos/deepfos-1.1.3.tar.gz/deepfos-1.1.3/deepfos/core/__init__.py
@@ -0,0 +1,27 @@
+from deepfos.lazy_import import lazy_callable
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:  # pragma: no cover
+    from .cube import SysCube, Cube, as_function_node
+    from .dimension import DimMember, SysDimension, read_expr, Dimension, DimExprAnalysor, ElementDimension
+    from .logictable import SQLCondition, BaseTable, MetaTable, TreeRenderer
+
+SysCube, Cube, as_function_node = lazy_callable('deepfos.core.cube',  # noqa
+                                                'SysCube',
+                                                'Cube',
+                                                'as_function_node')
+
+(DimMember, SysDimension, read_expr,  # noqa
+ Dimension, DimExprAnalysor, ElementDimension) = lazy_callable('deepfos.core.dimension',  # noqa
+                                                               'DimMember',
+                                                               'SysDimension',
+                                                               'read_expr',
+                                                               'Dimension',
+                                                               'DimExprAnalysor',
+                                                               'ElementDimension')
+
+SQLCondition, BaseTable, MetaTable, TreeRenderer = lazy_callable('deepfos.core.logictable',  # noqa
+                                                                 'SQLCondition',
+                                                                 'BaseTable',
+                                                                 'MetaTable',
+                                                                 'TreeRenderer')

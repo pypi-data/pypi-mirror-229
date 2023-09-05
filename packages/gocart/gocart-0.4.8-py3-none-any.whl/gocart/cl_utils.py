78671279a400cdd89426703e664d3aa84751dd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:07:19 +0000

    Add package gocart/0.4.8/gocart-0.4.8-py3-none-any.whl

---
+++ packages/gocart/gocart-0.4.8-py3-none-any.whl/gocart/cl_utils.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Documentation for gocart can be found here: http://gocart.readthedocs.org
+
+Usage:
+    gocart init
+    gocart [-p] echo <daysAgo> [-s <pathToSettingsFile>]
+    gocart [-p] (listen|quit|restart|status) [-s <pathToSettingsFile>]
+
+Options:
+    init                                   setup the gocart settings file for the first time
+    echo <daysAgo>                         relisten to alerts from N <daysAgo> until now and then exit
+    listen                                 reconnect to kafka stream and listen from where you left off (or from now on if connectiong for the first time).
+
+    -h, --help                             show this help message
+    -v, --version                          show version
+    -p, --plugins                          execute plugins everytime an alert is read
+    -s, --settings <pathToSettingsFile>    the settings file
+    -t, --test                             test, only collect 1 map
+"""
+from subprocess import Popen, PIPE, STDOUT
+from fundamentals import tools, times
+from docopt import docopt
+import pickle
+import glob
+import readline
+import sys
+import os
+import shutil
+os.environ['TERM'] = 'vt100'
+
+
+def tab_complete(text, state):
+    return (glob.glob(text + '*') + [None])[state]
+
+
+def main(arguments=None):
+    """
+    *The main function used when `cl_utils.py` is run as a single script from the cl, or when installed as a cl command*
+    """
+    # setup the command-line util settings
+    su = tools(
+        arguments=arguments,
+        docString=__doc__,
+        logLevel="WARNING",
+        options_first=False,
+        projectName="gocart",
+        defaultSettingsFile=True
+    )
+    arguments, settings, log, dbConn = su.setup()
+
+    # tab completion for raw_input
+    readline.set_completer_delims(' \t\n;')
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(tab_complete)
+
+    # UNPACK REMAINING CL ARGUMENTS USING `EXEC` TO SETUP THE VARIABLE NAMES
+    # AUTOMATICALLY
+    a = {}
+    for arg, val in list(arguments.items()):
+        if arg[0] == "-":
+            varname = arg.replace("-", "") + "Flag"
+        else:
+            varname = arg.replace("<", "").replace(">", "")
+        a[varname] = val
+        log.debug('%s = %s' % (varname, val,))
+
+    firstConnect = False
+    if "gcn-kafka" in settings and (not settings["gcn-kafka"]["group_id"] or len(str(settings["gcn-kafka"]["group_id"])) < 7):
+
+        from os.path import expanduser
+        import uuid as pyuuid
+        import re
+        group_id = pyuuid.uuid1().int
+        settings["gcn-kafka"]["group_id"] = group_id
+
+        home = expanduser("~")
+        filepath = home + "/.config/gocart/gocart.yaml"
+        import codecs
+        with codecs.open(filepath, encoding='utf-8', mode='r') as readFile:
+            content = readFile.read()
+            regex = re.compile(r'group_id\:.*')
+            content = regex.sub(f"group_id: {group_id}", content, count=1)
+        with codecs.open(filepath, encoding='utf-8', mode='w') as writeFile:
+            writeFile.write(content)
+        firstConnect = True
+    elif "gcn-kafka" not in settings:
+        return
+
+    ## START LOGGING ##
+    startTime = times.get_now_sql_datetime()
+    log.info(
+        '--- STARTING TO RUN THE cl_utils.py AT %s' %
+        (startTime,))
+
+    # TEST IF PLUG FOLDER EXISTS
+    from os.path import expanduser
+    home = expanduser("~")
+    pluginPath = home + "/.config/gocart/plugins"
+    exists = os.path.exists(pluginPath)
+    if not exists:
+        shutil.copytree(os.path.dirname(__file__) + "/resources/plugins", pluginPath)
+
+    if a["init"]:
+        from os.path import expanduser
+        home = expanduser("~")
+        filepath = home + "/.config/gocart/gocart.yaml"
+
+    topic = 'igwn.gwalert'
+
+    if len(settings['gcn-kafka']['client_id']) < 6 or len(settings['gcn-kafka']['client_secret']) < 6:
+        print("Please add your gcn-kafka client ID and secret to the gocart.yaml settings file.")
+        return
+
+    if a['listen'] or a['echo']:
+
+        if a['listen']:
+            verb = "Listening for"
+        else:
+            verb = "Echoing"
+
+        parse_mock_events = settings["lvk"]["parse_mock_events"]
+        parse_real_events = settings["lvk"]["parse_real_events"]
+        # WHERE TO DOWNLOAD MAPS TO
+        if "download_dir" in settings["lvk"] and settings["lvk"]["download_dir"]:
+            download_dir = settings["lvk"]["download_dir"]
+            # MAKE RELATIVE HOME PATH ABSOLUTE
+            from os.path import expanduser
+            home = expanduser("~")
+            if download_dir == "~":
+                download_dir = download_dir.replace("~", home)
+        else:
+            download_dir = "."
+
+        mockDir = download_dir + "/mockevents/"
+        evertDir = download_dir + "/superevents/"
+        if parse_mock_events:
+            print(f"{verb} Mockevents")
+            if not os.path.exists(mockDir):
+                os.makedirs(mockDir)
+        if parse_real_events:
+            print(f"{verb} Superevents")
+            if not os.path.exists(evertDir):
+                os.makedirs(evertDir)
+
+    if a['listen'] or a["quit"] or a["status"] or a["restart"]:
+
+        # ADD SOMETHING LIKE THE FOLLOWING TO THE CL USAGE:
+        """
+        Usage:
+            myCommand (start|stop|restart|status)
+
+            Options:
+                start                 start the myCommand daemon
+                stop                  stop the myCommand daemon
+                restart               restart the myCommand daemon
+                status                print the staus of the myCommand daemon
+        """
+
+        from fundamentals import daemonise
+
+        class myDaemon(daemonise):
+            def action(
+                    self,
+                    **kwargs):
+                self.log.info('starting the ``action`` method')
+
+                from gcn_kafka import Consumer
+                from confluent_kafka import TopicPartition
+                from gocart.parsers import lvk
+
+                firstConnect = kwargs["firstConnect"]
+                pluginsFlag = kwargs["pluginsFlag"]
+
+                from datetime import datetime, date, time
+                now = datetime.now()
+                now = now.strftime("%Y%m%dt%H%M%S")
+
+                print(f"gocart listen started at {now}")
+
+                config = {
+                    'group.id': settings["gcn-kafka"]["group_id"],
+                    'enable.auto.commit': False,
+                    'auto.offset.reset': 'earliest'
+                }
+
+                consumer = Consumer(config=config, client_id=settings['gcn-kafka']['client_id'],
+                                    client_secret=settings['gcn-kafka']['client_secret'], domain='gcn.nasa.gov')
+                consumer.subscribe([topic])
+
+                stop = False
+                test = 0
+                more = True
+                while not stop:
+                    # IF FISRT TIME CONNECTING THEN SKIP MESSAGES
+                    if firstConnect:
+                        count = 0
+                        index = 1
+                        print("Marking previous messages as read, this can take a few minutes")
+                        while more:
+                            messages = consumer.consume(num_messages=300, timeout=5)
+                            for message in messages:
+                                count += 1
+                                consumer.commit(message)
+                            if index > 1:
+                                # Cursor up one line and clear line
+                                sys.stdout.write("\x1b[1A\x1b[2K")
+                            index += 1
+                            print(f"{count} messages read ...")
+                            if not len(messages):
+                                more = False
+
+                        firstConnect = False
+                        print(f"This is your first time using the listen command. gocart will now listen for all new incoming alerts (skipping the {count} previous alerts currently in this topic). If you stop listening and restart sometime later, gocart will immediately collect all alerts missed while off-line.")
+                    for message in consumer.consume(timeout=5):
+                        try:
+                            parser = lvk(
+                                log=log,
+                                record=message.value(),
+                                settings=settings,
+                                plugins=pluginsFlag
+                            ).parse()
+                            consumer.commit(message)
+                        except Exception as e:
+                            consumer.commit(message)
+                            log.error(f'could not parse alert! Failed with error: {e}')
+
+                self.log.info('completed the ``action`` method')
+                return None
+
+        d = myDaemon(log=log, name="gocart", firstConnect=firstConnect, pluginsFlag=a["pluginsFlag"])
+
+        if a['listen']:
+            d.start()
+        elif a['quit']:
+            d.stop()
+        elif a['restart']:
+            d.restart()
+        elif a['status']:
+            d.status()
+
+    if a['echo'] and a['daysAgo']:
+        # GET MESSAGES OCCURRING IN LAST N DAYS
+        from gcn_kafka import Consumer
+        from confluent_kafka import TopicPartition
+        from gocart.parsers import lvk
+        import datetime
+
+        consumer = Consumer(client_id=settings['gcn-kafka']['client_id'],
+                            client_secret=settings['gcn-kafka']['client_secret'], domain='gcn.nasa.gov')
+
+        since_utc = datetime.datetime.now() - datetime.timedelta(days=float(a['daysAgo']))
+        timestamp1 = int((since_utc).timestamp() * 1000)
+        since_utc = since_utc.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"Echoing alerts since {since_utc} UTC")
+
+        start = consumer.offsets_for_times(
+            [TopicPartition(topic, 0, timestamp1)])
+
+        consumer.assign(start)
+
+        more = True
+        while more:
+            messages = consumer.consume(num_messages=1, timeout=10)
+            for message in messages:
+                parser = lvk(
+                    log=log,
+                    record=message.value(),
+                    settings=settings,
+                    plugins=a["pluginsFlag"]
+                ).parse()
+            if not len(messages):
+                more = False
+
+    ## FINISH LOGGING ##
+    endTime = times.get_now_sql_datetime()
+    runningTime = times.calculate_time_difference(startTime, endTime)
+    log.info('-- FINISHED ATTEMPT TO RUN THE cl_utils.py AT %s (RUNTIME: %s) --' %
+             (endTime, runningTime, ))
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/gocart/gocart-0.4.8-py3-none-any.whl/gocart/commonutils/flatten_healpix_map.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+*Flatten a Healpix Multi-Order Map*
+
+:Author:
+    David Young
+
+:Date Created:
+    March 24, 2023
+"""
+from fundamentals import tools
+from builtins import object
+import sys
+import os
+os.environ['TERM'] = 'vt100'
+
+
+def flatten_healpix_map(
+        log,
+        mapPath,
+        nside=64):
+    """flatten a multiorder healpix map to a specific nside*
+
+    **Key Arguments:**
+        - ``log`` -- logger
+        - ``mapPath`` -- path to the multiorder map
+        - ``nside`` -- the nside index to flatten the map to. Default *64* (~0.9 deg2 pixels)
+
+    **Return:**
+        - ``skymap`` -- the astropy table of the map
+
+    ```eval_rst
+    .. todo::
+
+        - create a sublime snippet for usage
+        - add a tutorial about ``subtract_calibrations`` to documentation
+    ```
+
+    ```python
+    from gocart.commonutils import flatten_healpix_map
+    skymap = flatten_healpix_map(
+        log=log,
+        mapPath=pathToOutputDir + "/bayestar.multiorder.fits",
+        nside=64
+    )
+    ```
+    """
+    log.debug('starting the ``flatten_healpix_map`` function')
+
+    from mhealpy.pixelfunc.moc import uniq2range
+    import tempfile
+    from astropy.io import fits
+    from astropy.table import Table
+    import astropy_healpix as ah
+    import astropy.units as u
+    import numpy as np
+    import pandas as pd
+    from tabulate import tabulate
+
+    # OPEN MAP TO DATAFRAME
+    skymap = Table.read(mapPath)
+    tableData = skymap.to_pandas()
+    # FIND LEVEL AND NSIDE PIXEL INDEX FOR EACH MULTI-RES PIXEL
+    tableData['LEVEL'], tableData['IPIX'] = ah.uniq_to_level_ipix(tableData['UNIQ'])
+    tableData['NSIDE'] = ah.level_to_nside(tableData['LEVEL'])
+    # DETERMINE THE PIXEL AREA AND PROB OF EACH PIXEL
+    tableData['AREA'] = ah.nside_to_pixel_area(tableData['NSIDE']).to_value(u.steradian)
+    tableData['PROB'] = tableData['AREA'] * tableData["PROBDENSITY"]
+
+    level = ah.nside_to_level(nside)
+
+    # UPSAMPLE TABLE
+    # FIND THE HIGH-LEVEL PIXEL INDEXES FOR EACH UNIQ PIXEL
+    # MAKE A NEW DATAFRAME WITH IPIX64, UNIQ
+    # MATCH EACH NEW UNIQ AGAINST ORIGINAL FRAME UNIQ TO GENERATE DATA FOR IPIX^$ FRAME
+    mask = (tableData['NSIDE'] <= nside)
+    upTable = tableData.loc[mask].copy()
+    upTable.reset_index(inplace=True)
+    this = uniq2range(nside, upTable['UNIQ'])
+    upTable[f'IPIX{nside}'] = [list(range(i, j)) for i, j in zip(this[0], this[1])]
+    upTable[f'UNIQLIST'] = upTable.apply(lambda x: [x['UNIQ']] * len(x[f'IPIX{nside}']), axis=1)
+
+    ipixNside = np.concatenate(upTable[f'IPIX{nside}'])
+    uniqList = np.concatenate(upTable[f'UNIQLIST'])
+    # CREATE DATA FRAME FROM A DICTIONARY OF LISTS
+    myDict = {
+        f'IPIX{nside}': ipixNside,
+        f'UNIQ': uniqList
+    }
+    upTable = pd.DataFrame(myDict)
+    # MERGE DATAFRAMES
+    if "DISTMU" in upTable.columns:
+        upTable = upTable.merge(tableData, on=['UNIQ'], how='inner')[[f'IPIX{nside}', 'PROBDENSITY', 'DISTMU', 'DISTSIGMA', 'DISTNORM']]
+    else:
+        upTable = upTable.merge(tableData, on=['UNIQ'], how='inner')[[f'IPIX{nside}', 'PROBDENSITY']]
+    pixArea = ah.nside_to_pixel_area(nside).to_value(u.steradian)
+    upTable["PROB"] = upTable["PROBDENSITY"] * pixArea
+    # REMOVE COLUMN FROM DATA FRAME
+    upTable.drop(columns=['PROBDENSITY'], inplace=True)
+
+    # DOWNSAMPLE TABLE
+    mask = tableData['NSIDE'] > nside
+    downTable = tableData.loc[mask].copy()
+    downTable.reset_index(inplace=True)
+
+    # FIND THE PIXEL INDEX AT ORDER NSIDE
+    downTable[f'IPIX{nside}'] = np.floor_divide(downTable['IPIX'], np.power(4, (downTable['LEVEL'].values - level)))
+
+    # GROUP RESULTS
+    if "DISTMU" in downTable.columns:
+        downTable = downTable.groupby([f'IPIX{nside}']).agg({'PROB': 'sum', 'DISTMU': 'mean', 'DISTSIGMA': 'mean', 'DISTNORM': 'mean'})
+    else:
+        downTable = downTable.groupby([f'IPIX{nside}']).agg({'PROB': 'sum'})
+    downTable.reset_index(inplace=True)
+
+    skymap = pd.concat([downTable, upTable], ignore_index=True)
+    # SORT BY COLUMN NAME
+    skymap.sort_values([f'IPIX{nside}'],
+                       ascending=[True], inplace=True)
+
+    # SET INDEX AND SORT DATA FRAME
+    skymap.reset_index(inplace=True)
+    skymap.drop(columns=[f'IPIX{nside}', 'index'], inplace=True)
+
+    # REMOVE FILTERED ROWS FROM DATA FRAME
+    if "DISTMU" in downTable.columns:
+        mask = (skymap['DISTMU'].isnull())
+        skymap.loc[mask, 'DISTMU'] = np.inf
+
+    log.debug('completed the ``flatten_healpix_map`` function')
+    return skymap

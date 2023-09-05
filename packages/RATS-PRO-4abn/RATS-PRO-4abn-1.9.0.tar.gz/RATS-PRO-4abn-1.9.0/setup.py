870081a61b0ed0c55ea93f08db318c9c78b800c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:07:47 +0000

    Add package RATS-PRO-4abn/1.9.0/RATS-PRO-4abn-1.9.0.tar.gz

---
+++ packages/RATS-PRO-4abn/RATS-PRO-4abn-1.9.0.tar.gz/RATS-PRO-4abn-1.9.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RATS-PRO-4abn',
+    version='1.9.0',
+    description='Data collation Flask app for RATS PRO',
+    author='Christoforos Lapathiotis',
+    author_email='clapathiotis@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'pandas',
+        'requests',
+        'requests-oauthlib'
+    ],
+    package_data={
+        'src': ['templates/*', 'static/*'],
+        '': ['client_data/*']
+    }
+)

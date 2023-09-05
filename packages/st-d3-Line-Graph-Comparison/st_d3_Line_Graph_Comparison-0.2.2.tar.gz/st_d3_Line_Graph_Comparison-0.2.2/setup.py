6a155e00e38c30b61ec5b7566926471eadb44a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:07:13 +0000

    Add package st-d3-Line-Graph-Comparison/0.2.2/st_d3_Line_Graph_Comparison-0.2.2.tar.gz

---
+++ packages/st-d3-Line-Graph-Comparison/st_d3_Line_Graph_Comparison-0.2.2.tar.gz/st_d3_Line_Graph_Comparison-0.2.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="st_d3_Line_Graph_Comparison",
+    version="0.2.2",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)

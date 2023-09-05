e9f7894607230b042a6a3fb1eb9d9d5b03903449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:09:48 +0000

    Add package risu/1.14.3.20230904132727/risu-1.14.3.20230904132727.tar.gz

---
+++ packages/risu/risu-1.14.3.20230904132727.tar.gz/risu-1.14.3.20230904132727/tests/test_dmidecode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2021, 2023 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+import os
+import sys
+from unittest import TestCase
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/" + "../"))
+
+from risuclient.tools.dmidecode import (
+    _get_output,
+    _parse_handle_section,
+    _show,
+    parse_dmi,
+    profile,
+)
+
+
+class CitellusTest(TestCase):
+    def test_dmidecode(self):
+        with open("tests/other/dmidecode", "r") as f:
+            content = f.read()
+            output = parse_dmi(content)
+            assert output != "1"

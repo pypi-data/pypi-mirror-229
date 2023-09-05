ddfca0b09f6e3966f7c5d922152c2d191bf54827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:08:41 +0000

    Add package bilibili-api-dev/16.0.1b2/bilibili-api-dev-16.0.1b2.tar.gz

---
+++ packages/bilibili-api-dev/bilibili-api-dev-16.0.1b2.tar.gz/bilibili-api-dev-16.0.1b2/bilibili_api/exceptions/__init__.py
@@ -0,0 +1,20 @@
+"""
+bilibili_api.exceptions
+
+错误
+"""
+
+from .LoginError import *
+from .ApiException import *
+from .ArgsException import *
+from .NetworkException import *
+from .ResponseException import *
+from .VideoUploadException import *
+from .ResponseCodeException import *
+from .DanmakuClosedException import *
+from .LiveException import LiveException
+from .CredentialNoBuvid3Exception import *
+from .CredentialNoBiliJctException import *
+from .CredentialNoSessdataException import *
+from .CredentialNoDedeUserIDException import *
+from .DynamicExceedImagesException import DynamicExceedImagesException

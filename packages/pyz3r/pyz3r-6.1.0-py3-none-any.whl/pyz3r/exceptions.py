20af7d3666a6c75d2f1590311ca8b2e67db01d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:07:21 +0000

    Add package pyz3r/6.1.0/pyz3r-6.1.0-py3-none-any.whl

---
+++ packages/pyz3r/pyz3r-6.1.0-py3-none-any.whl/pyz3r/exceptions.py
@@ -0,0 +1,17 @@
+class Pyz3rException(Exception):
+    pass
+
+
+class AlttprFailedToRetrieve(Pyz3rException):
+    pass
+
+
+class AlttprFailedToGenerate(Pyz3rException):
+    pass
+
+
+class UnableToRetrieve(Pyz3rException):
+    pass
+
+class UnableToGenerate(Pyz3rException):
+    pass

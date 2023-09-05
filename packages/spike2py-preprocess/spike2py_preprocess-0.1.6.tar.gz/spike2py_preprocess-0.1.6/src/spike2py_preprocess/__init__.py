b350eacff4d0c82a40f4161e74b57161d5150478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:08:36 +0000

    Add package spike2py-preprocess/0.1.6/spike2py_preprocess-0.1.6.tar.gz

---
+++ packages/spike2py-preprocess/spike2py_preprocess-0.1.6.tar.gz/spike2py_preprocess-0.1.6/src/spike2py_preprocess/__init__.py
@@ -0,0 +1,4 @@
+import spike2py_preprocess.subject as subject
+import spike2py_preprocess.trial as trial
+import spike2py_preprocess.utils as utils
+import spike2py_preprocess.trial_sections as trial_sections

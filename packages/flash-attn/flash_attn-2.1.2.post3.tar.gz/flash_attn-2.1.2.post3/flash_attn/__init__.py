7cc29dca84fea6e69eccb04999fcb53b8748f009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:07:49 +0000

    Add package flash-attn/2.1.2.post3/flash_attn-2.1.2.post3.tar.gz

---
+++ packages/flash-attn/flash_attn-2.1.2.post3.tar.gz/flash_attn-2.1.2.post3/flash_attn/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "2.1.2.post3"
+
+from flash_attn.flash_attn_interface import (
+    flash_attn_func,
+    flash_attn_kvpacked_func,
+    flash_attn_qkvpacked_func,
+    flash_attn_varlen_func,
+    flash_attn_varlen_kvpacked_func,
+    flash_attn_varlen_qkvpacked_func,
+)

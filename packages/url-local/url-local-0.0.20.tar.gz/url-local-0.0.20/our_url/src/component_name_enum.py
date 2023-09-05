d40c07e2b8d6cdda3b7445d51547ab92a2abe9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:05:44 +0000

    Add package url-local/0.0.20/url-local-0.0.20.tar.gz

---
+++ packages/url-local/url-local-0.0.20.tar.gz/url-local-0.0.20/our_url/src/component_name_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class ComponentName(Enum):
+    MARKETPLACE_GOODS = "marketplace-goods"
+    EVENT = "event"
+    GENDER_DETECTION = "gender-detection"
+    LOGGER = "logger"
+    GROUP = "group"
+    USER_REGISTRATION = "user-registration"
+    AUTHENTICATION = "authentication" # login
+    TIMELINE = "timeline"
+    GROUP_PROFILE="group-profile"

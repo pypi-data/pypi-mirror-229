d6f8908f6e780ffaa559ea5418ff1649b6dbeb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:09:39 +0000

    Add package pythermalcomfort/2.8.2/pythermalcomfort-2.8.2.tar.gz

---
+++ packages/pythermalcomfort/pythermalcomfort-2.8.2.tar.gz/pythermalcomfort-2.8.2/tests/test_jos3.py
@@ -0,0 +1,15 @@
+import pytest
+from pythermalcomfort.jos3_functions.construction import bsa_rate
+
+
+def test_bsa_rate_with_defaults():
+    assert bsa_rate() == pytest.approx(1.00051943)
+
+
+def test_bsa_rate_with_custom_height_weight():
+    assert bsa_rate(height=1.80, weight=80.0) == pytest.approx(1.0662, rel=1e-4)
+
+
+def test_bsa_rate_with_invalid_formula():
+    with pytest.raises(ValueError):
+        bsa_rate(height=1.72, weight=74.43, bsa_equation="ciao")

98ab6c3120f4dc09a20aed1536a73dc574f4bec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:12:19 +0000

    Add package structlog-gcp/0.2.3/structlog_gcp-0.2.3-py3-none-any.whl

---
+++ packages/structlog-gcp/structlog_gcp-0.2.3-py3-none-any.whl/structlog_gcp/base.py
@@ -0,0 +1,20 @@
+from structlog.typing import Processor
+
+from . import errors, processors
+
+
+def build_processors(
+    service: str | None = None,
+    version: str | None = None,
+) -> list[Processor]:
+    procs = []
+
+    procs.extend(processors.CoreCloudLogging().setup())
+    procs.extend(processors.LogSeverity().setup())
+    procs.extend(processors.CodeLocation().setup())
+    procs.extend(errors.ReportException().setup())
+    procs.extend(errors.ReportError(["CRITICAL"]).setup())
+    procs.extend(errors.ServiceContext(service, version).setup())
+    procs.extend(processors.FormatAsCloudLogging().setup())
+
+    return procs

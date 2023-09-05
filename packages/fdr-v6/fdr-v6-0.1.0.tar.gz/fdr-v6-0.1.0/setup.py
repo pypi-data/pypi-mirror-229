77f342d33171624a9a73f44c663dbc1b2bfcb4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:08:01 +0000

    Add package fdr-v6/0.1.0/fdr-v6-0.1.0.tar.gz

---
+++ packages/fdr-v6/fdr-v6-0.1.0.tar.gz/fdr-v6-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Read dependencies from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Setup
+setup(
+    name='fdr-v6',
+    version='0.1.0',
+    description='Sample fdr lib',
+    author='Konstantinos Gyftodimos',
+    author_email='konstgyftodimos@gmail.com',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fdr = client.main:fdr_client',
+        ],
+    },
+    install_requires=requirements
+)

9589848f3c761c639254bdc6fa8989c357facd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:05:57 +0000

    Add package PieBedrock/1.0.0/PieBedrock-1.0.0-py3-none-any.whl

---
+++ packages/PieBedrock/PieBedrock-1.0.0-py3-none-any.whl/piebedrock/server.py
@@ -0,0 +1,10 @@
+from pieraknet import Server
+from pieraknet.packets.game_packet import GamePacket
+from pieraknet.connection import Connection
+import logging
+import os
+import time
+import random
+
+class BedrockServer:
+    pass

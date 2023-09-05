1d81e55dac8848e562ddee0a46832f1e72be82da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:09:23 +0000

    Add package caixa-de-ferramentas/0.1.2/caixa_de_ferramentas-0.1.2.tar.gz

---
+++ packages/caixa-de-ferramentas/caixa_de_ferramentas-0.1.2.tar.gz/caixa_de_ferramentas-0.1.2/caixa_de_ferramentas/testing_package.py
@@ -0,0 +1,12 @@
+
+
+class PackageTester:
+
+    def __init__(self):
+        pass
+
+    def test_soma(self, num_1, num_2):
+        return num_1 + num_2
+    
+    def test_produto(self, num_1, num_2):
+        return num_1 * num_2

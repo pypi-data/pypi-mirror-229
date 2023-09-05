c362b20e94c4998c33df1b2d235f7d3a8003d4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:07:28 +0000

    Add package lambda-agent-utils/1.0.2/lambda_agent_utils-1.0.2.tar.gz

---
+++ packages/lambda-agent-utils/lambda_agent_utils-1.0.2.tar.gz/lambda_agent_utils-1.0.2/lambda_agent_utils/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+# Define package-level variables
+PACKAGE_VERSION = "1.0.2"
+
+# Import modules to make them accessible when the package is imported
+from .input_handler import input_handler
+from .response_handler import response_handler
+
+

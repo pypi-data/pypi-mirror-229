05f1915db00cfe493cc04ab38dc7c88f525ed1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:09:16 +0000

    Add package slack-machine/0.35.0/slack_machine-0.35.0.tar.gz

---
+++ packages/slack-machine/slack_machine-0.35.0.tar.gz/slack_machine-0.35.0/machine/plugins/builtin/debug.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+from structlog.stdlib import get_logger
+
+from machine.plugins.base import MachineBasePlugin
+from machine.plugins.decorators import process
+
+logger = get_logger(__name__)
+
+
+class EchoPlugin(MachineBasePlugin):
+    @process(slack_event_type="message")
+    async def echo_message(self, event: dict[str, Any]) -> None:
+        logger.debug("Message received: %s", event)
+        if (
+            "subtype" in event and (event["subtype"] == "bot_message" or event["subtype"] == "message_replied")
+        ) or event["user"] == self.bot_info["user_id"]:
+            return
+        if "thread_ts" in event:
+            thread_ts = event["thread_ts"]
+        else:
+            thread_ts = None
+        await self.say(event["channel"], event["text"], thread_ts=thread_ts)

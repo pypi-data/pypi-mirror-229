877c1721a371249fcb5d457ac5b3c9d79d4c6b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 22:06:32 +0000

    Add package hexarchsetup/0.4/hexarchsetup-0.4.tar.gz

---
+++ packages/hexarchsetup/hexarchsetup-0.4.tar.gz/hexarchsetup-0.4/setup.py
@@ -0,0 +1,27 @@
+"""
+This file is used to build your package. It is used by the command
+`python setup.py install` or `python setup.py develop`.
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hexarchsetup',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[
+        # Your dependencies here
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            # This allows you to run your main function from the command line.
+            'hexsetup=src.main:main',
+        ],
+    },
+    author='Éric Dominguez Morales',
+    author_email='ericdominguezm@gmail.com',
+    description='Automatically generate a hexagonal architecture project structure in Python. \
+        Easily customizable to include user-defined modules.',
+)

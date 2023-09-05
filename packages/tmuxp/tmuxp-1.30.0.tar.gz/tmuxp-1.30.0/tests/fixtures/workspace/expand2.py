1fb2f17fe22a720d23458fb823193f929b86bd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 21:08:57 +0000

    Add package tmuxp/1.30.0/tmuxp-1.30.0.tar.gz

---
+++ packages/tmuxp/tmuxp-1.30.0.tar.gz/tmuxp-1.30.0/tests/fixtures/workspace/expand2.py
@@ -0,0 +1,13 @@
+import pathlib
+
+from .. import utils as test_utils
+
+
+def unexpanded_yaml():
+    return test_utils.read_workspace_file("workspace/expand2-unexpanded.yaml")
+
+
+def expanded_yaml():
+    return test_utils.read_workspace_file("workspace/expand2-expanded.yaml").format(
+        HOME=str(pathlib.Path().home())
+    )

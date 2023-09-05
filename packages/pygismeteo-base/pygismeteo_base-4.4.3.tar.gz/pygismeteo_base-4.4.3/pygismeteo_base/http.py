8d9905b55aef3f401af7ab97a371755eb766d24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:10:45 +0000

    Add package pygismeteo-base/4.4.3/pygismeteo_base-4.4.3.tar.gz

---
+++ packages/pygismeteo-base/pygismeteo_base-4.4.3.tar.gz/pygismeteo_base-4.4.3/pygismeteo_base/http.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any, Generic, Optional, Tuple, TypeVar
+
+from . import constants, types, validators
+
+T = TypeVar("T")
+
+
+class BaseHttpClient(Generic[T]):
+    __slots__ = ("session", "settings")
+
+    def __init__(
+        self, session: Optional[T], settings: validators.Settings
+    ) -> None:
+        self.session = session
+        self.settings = settings
+
+    def _get_params_and_headers(
+        self, params: types.Params
+    ) -> Tuple[types.Params, types.Headers]:
+        if self.settings.lang:
+            params = (
+                {"lang": self.settings.lang, **params}
+                if params
+                else {"lang": self.settings.lang}
+            )
+        token = self.settings.token or constants.DEFAULT_TOKEN
+        headers = {"X-Gismeteo-Token": token}
+        return params, headers
+
+
+THttpClient = TypeVar("THttpClient", bound=BaseHttpClient[Any])
---
+++ packages/pygismeteo-base/pygismeteo_base-4.4.3.tar.gz/pygismeteo_base-4.4.3/pygismeteo_base/step_n/mixins.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
+from typing import Type, Union
+
+from .. import models, types, validators
+
+
+class StepNMixin(metaclass=ABCMeta):
+    __slots__ = ()
+
+    @property
+    @abstractmethod
+    def _endpoint(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def _model(self) -> types.StepNModel:
+        pass
+
+    @property
+    @abstractmethod
+    def _days_validator(
+        self,
+    ) -> Type[
+        Union[validators.Step3Days, validators.Step6Days, validators.Step24Days]
+    ]:
+        pass
+
+
+class Step3Mixin(StepNMixin):
+    __slots__ = ()
+
+    @property
+    def _endpoint(self) -> str:
+        return "weather/forecast"
+
+    @property
+    def _model(self) -> Type[models.step3.Model]:
+        return models.step3.Model
+
+    @property
+    def _days_validator(self) -> Type[validators.Step3Days]:
+        return validators.Step3Days
+
+
+class Step6Mixin(StepNMixin):
+    __slots__ = ()
+
+    @property
+    def _endpoint(self) -> str:
+        return "weather/forecast/by_day_part"
+
+    @property
+    def _model(self) -> Type[models.step6.Model]:
+        return models.step6.Model
+
+    @property
+    def _days_validator(self) -> Type[validators.Step6Days]:
+        return validators.Step6Days
+
+
+class Step24Mixin(StepNMixin):
+    __slots__ = ()
+
+    @property
+    def _endpoint(self) -> str:
+        return "weather/forecast/aggregate"
+
+    @property
+    def _model(self) -> Type[models.step24.Model]:
+        return models.step24.Model
+
+    @property
+    def _days_validator(self) -> Type[validators.Step24Days]:
+        return validators.Step24Days

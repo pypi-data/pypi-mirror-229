cc7bc31af08b4dfebbf10f64ed410bddba734a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:06:48 +0000

    Add package minidevice/2.2.3/minidevice-2.2.3-py3-none-any.whl

---
+++ packages/minidevice/minidevice-2.2.3-py3-none-any.whl/minidevice/__init__.py
@@ -0,0 +1,5 @@
+from minidevice.DroidCast import DroidCast
+from minidevice.adbcap import ADBcap
+from minidevice.adbtouch import ADBtouch
+from minidevice.minicap import Minicap
+from minidevice.minitouch import Minitouch

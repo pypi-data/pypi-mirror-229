9182f0e6f8459666fa56141f8dc4e8ddfe0006f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 20:10:46 +0000

    Add package VUDA/0.0.10/VUDA-0.0.10-py3-none-any.whl

---
+++ packages/VUDA/VUDA-0.0.10-py3-none-any.whl/VUDA/io/errors.py
@@ -0,0 +1,8 @@
+error_msg = {}
+
+error_msg[
+    "TimeSamplesException"
+] = "Data subset can be specified either in time or in samples, but not both"
+error_msg[
+    "ChannelIDException"
+] = "Listed channel IDs inconsistent with total number of channels)."

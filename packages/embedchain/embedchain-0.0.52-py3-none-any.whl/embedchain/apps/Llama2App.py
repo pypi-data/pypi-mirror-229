d429d436f242cfd8e80a8d864a19d3bca82a5e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 19:17:48 +0000

    Add package embedchain/0.0.52/embedchain-0.0.52-py3-none-any.whl

---
+++ packages/embedchain/embedchain-0.0.52-py3-none-any.whl/embedchain/apps/Llama2App.py
@@ -0,0 +1,40 @@
+import os
+from typing import Optional
+
+from langchain.llms import Replicate
+
+from embedchain.config import AppConfig, ChatConfig
+from embedchain.embedchain import EmbedChain
+
+
+class Llama2App(EmbedChain):
+    """
+    The EmbedChain Llama2App class.
+    Has two functions: add and query.
+
+    adds(data_type, url): adds the data from the given URL to the vector db.
+    query(query): finds answer to the given query using vector database and LLM.
+    """
+
+    def __init__(self, config: AppConfig = None, system_prompt: Optional[str] = None):
+        """
+        :param config: AppConfig instance to load as configuration. Optional.
+        :param system_prompt: System prompt string. Optional.
+        """
+        if "REPLICATE_API_TOKEN" not in os.environ:
+            raise ValueError("Please set the REPLICATE_API_TOKEN environment variable.")
+
+        if config is None:
+            config = AppConfig()
+
+        super().__init__(config, system_prompt)
+
+    def get_llm_model_answer(self, prompt, config: ChatConfig = None):
+        # TODO: Move the model and other inputs into config
+        if self.system_prompt or config.system_prompt:
+            raise ValueError("Llama2App does not support `system_prompt`")
+        llm = Replicate(
+            model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+            input={"temperature": 0.75, "max_length": 500, "top_p": 1},
+        )
+        return llm(prompt)
---
+++ packages/embedchain/embedchain-0.0.52-py3-none-any.whl/embedchain/vectordb/base_vector_db.py
@@ -0,0 +1,30 @@
+from embedchain.helper_classes.json_serializable import JSONSerializable
+
+
+class BaseVectorDB(JSONSerializable):
+    """Base class for vector database."""
+
+    def __init__(self):
+        self.client = self._get_or_create_db()
+
+    def _get_or_create_db(self):
+        """Get or create the database."""
+        raise NotImplementedError
+
+    def _get_or_create_collection(self):
+        raise NotImplementedError
+
+    def get(self):
+        raise NotImplementedError
+
+    def add(self):
+        raise NotImplementedError
+
+    def query(self):
+        raise NotImplementedError
+
+    def count(self):
+        raise NotImplementedError
+
+    def reset(self):
+        raise NotImplementedError

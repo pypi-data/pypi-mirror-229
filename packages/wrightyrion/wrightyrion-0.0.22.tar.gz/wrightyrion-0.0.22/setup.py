e1f2c9e671b71a0237d3cab14021784cc7534cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-05 23:07:20 +0000

    Add package wrightyrion/0.0.22/wrightyrion-0.0.22.tar.gz

---
+++ packages/wrightyrion/wrightyrion-0.0.22.tar.gz/wrightyrion-0.0.22/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open('requirements.txt', 'r') as f:
+        requirements = f.readlines()
+    return [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
+
+setup(
+    name='wrightyrion',
+    version='0.0.22',
+    packages=find_packages(exclude=['test', 'test.*']),
+    install_requires=read_requirements(),
+    author='steveflyer',
+    author_email='steveflyer7@gmail.com',
+    description='',
+    # long_description=open('./README.md').read(),
+    long_description_content_type='text/markdown',
+)

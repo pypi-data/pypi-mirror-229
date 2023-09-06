4d5437417cff0e74a394d3f7ba385a1d1faf2e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:47 +0000

    Add package teachpyx/0.3.1/teachpyx-0.3.1.tar.gz

---
+++ packages/teachpyx/teachpyx-0.3.1.tar.gz/teachpyx-0.3.1/teachpyx/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.3.1"
+__author__ = "Xavier Dupré"
+__github__ = "https://github.com/sdpython/teachpyx"
+__url__ = "https://sdpython.github.io/doc/teachpyx/dev/"
+__license__ = "MIT License"

d29cfe77856c4eeb6111207e4d2a122cf265629a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:20 +0000

    Add package UEVaultManager/1.9.2/UEVaultManager-1.9.2.tar.gz

---
+++ packages/UEVaultManager/UEVaultManager-1.9.2.tar.gz/UEVaultManager-1.9.2/UEVaultManager/lfs/utils.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import logging
+from pathlib import Path
+
+logger = logging.getLogger('LFS Utils')
+
+
+def clean_filename(filename: str) -> str:
+    """
+    Clean a filename from invalid characters.
+    :param filename: The filename to clean.
+    :return:  The cleaned filename.
+    """
+    return ''.join(i for i in filename if i not in '<>:"/\\|?*')
+
+
+def get_dir_size(path: str) -> int:
+    """
+    Get the size of a directory.
+    :param path: The path to the directory.
+    :return:
+    """
+    return sum(f.stat().st_size for f in Path(path).glob('**/*') if f.is_file())
+
+
+def path_join(*paths):
+    """
+    Join multiple paths together. Make the return value unified
+    :param paths: The paths to join.
+    :return: The joined paths.
+    """
+    return Path(*paths).resolve().as_posix()

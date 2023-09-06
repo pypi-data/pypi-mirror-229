6c0dcddcb7413dd1ab541ce62c93d18faa989793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:54 +0000

    Add package atoti-directquery-synapse/0.8.4/atoti_directquery_synapse-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-synapse/atoti_directquery_synapse-0.8.4-py3-none-any.whl/atoti_directquery_synapse/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery on `Azure Synapse Analytics <https://azure.microsoft.com/en-us/services/synapse-analytics>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

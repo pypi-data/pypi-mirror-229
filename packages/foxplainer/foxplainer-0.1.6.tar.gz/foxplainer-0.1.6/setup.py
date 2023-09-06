e1ae949650ae4a4856da466710eff8f2c1afcf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 14:09:07 +0000

    Add package foxplainer/0.1.6/foxplainer-0.1.6.tar.gz

---
+++ packages/foxplainer/foxplainer-0.1.6.tar.gz/foxplainer-0.1.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['foxplainer', 'foxplainer.pysat', 'foxplainer.pysat.examples']
+
+package_data = \
+{'': ['*'], 'foxplainer': ['dataset/*', 'global_model/*']}
+
+install_requires = \
+['anytree>=2.8.0,<3.0.0',
+ 'ipywidgets>=7.7.1,<8.0.0',
+ 'numpy>=1.23.2,<2.0.0',
+ 'pandas>=1.4.3,<2.0.0',
+ 'scikit-learn==1.1.2',
+ 'six>=1.16.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'foxplainer',
+    'version': '0.1.6',
+    'description': 'FoX: a Fo rmal eX plainer for JIT Defect Prediction',
+    'long_description': None,
+    'author': 'Jinqiang Yu',
+    'author_email': 'trustablefox@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

71c71dff605ebe8880d5aa6bdd0c5435d17a7cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:00 +0000

    Add package atoti-directquery-bigquery/0.8.4/atoti_directquery_bigquery-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-bigquery/atoti_directquery_bigquery-0.8.4-py3-none-any.whl/atoti_directquery_bigquery/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery on `Google BigQuery <https://cloud.google.com/bigquery>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

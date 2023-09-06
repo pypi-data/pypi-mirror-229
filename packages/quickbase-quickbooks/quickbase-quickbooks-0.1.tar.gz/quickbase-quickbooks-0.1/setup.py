37fd6279534c8620da323cd5edad04cb7b61825e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:09:02 +0000

    Add package quickbase-quickbooks/0.1/quickbase-quickbooks-0.1.tar.gz

---
+++ packages/quickbase-quickbooks/quickbase-quickbooks-0.1.tar.gz/quickbase-quickbooks-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='quickbase-quickbooks',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list packages your project depends on
+    ],
+)

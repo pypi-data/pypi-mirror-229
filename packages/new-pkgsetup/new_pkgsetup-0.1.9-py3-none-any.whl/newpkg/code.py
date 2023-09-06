4a273a4a3408fa95548d6e9319265d16ec6d8117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:13 +0000

    Add package new-pkgsetup/0.1.9/new_pkgsetup-0.1.9-py3-none-any.whl

---
+++ packages/new-pkgsetup/new_pkgsetup-0.1.9-py3-none-any.whl/newpkg/code.py
@@ -0,0 +1,22 @@
+class Package(object):
+    
+     def __init__(self): #,instance="Instance Name"):
+         self._id = id(self)
+         self._name="Package"
+         self._desc="Package for test builds"
+         self._vers="v0.01.09" 
+         self._model = "" 
+         self._about="About package class"
+         self._instance_name = ""
+         self._debug_flag = False
+       
+     def whoami(self):
+          print(self._name,self._vers,self._model)
+         
+     def name(self):
+          print(self._name)
+         
+     def vers(self):
+          return self._vers
+         
+       

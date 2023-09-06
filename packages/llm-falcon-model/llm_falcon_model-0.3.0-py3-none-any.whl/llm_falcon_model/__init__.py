867a2f3e60836b015078f54e9ae68f25a4b1fe4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 00:32:16 +0000

    Add package llm-falcon-model/0.3.0/llm_falcon_model-0.3.0-py3-none-any.whl

---
+++ packages/llm-falcon-model/llm_falcon_model-0.3.0-py3-none-any.whl/llm_falcon_model/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.3.0"
+
+from .modelling_RW import RWForCausalLM
+from .configuration_RW import read_config_from_json
+from .tokenization import load_tokenizer
---
+++ packages/llm-falcon-model/llm_falcon_model-0.3.0-py3-none-any.whl/llm_falcon_model/tokenization.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from tokenizers import Tokenizer
+
+
+def load_tokenizer() -> Tokenizer:
+    json_file = Path(__file__).parent / f'falcon7b_tokenizer.json'
+    return Tokenizer.from_file(str(json_file))

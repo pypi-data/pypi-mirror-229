1b862351490cb7c8c7271bfa755178710ceec37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:13 +0000

    Add package OCR-file-name-changer/1.0.0/OCR_file_name_changer-1.0.0-py3-none-any.whl

---
+++ packages/OCR-file-name-changer/OCR_file_name_changer-1.0.0-py3-none-any.whl/ocr_file_name_changer/exceptions.py
@@ -0,0 +1,16 @@
+class IncorrectImageException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DuplicateSNException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class FileExistsException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)

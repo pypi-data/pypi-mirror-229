cff8b9e1ee9c610f12302820e1a328e3e1a84cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:08 +0000

    Add package chinese-whispers/0.8.2.post1/chinese_whispers-0.8.2.post1.tar.gz

---
+++ packages/chinese-whispers/chinese_whispers-0.8.2.post1.tar.gz/chinese_whispers-0.8.2.post1/chinese_whispers/__init__.py
@@ -0,0 +1,31 @@
+"""
+Chinese Whispers
+
+An implementation of the Chinese Whispers clustering algorithm.
+"""
+
+__version__ = '0.8.2.post1'
+__author__ = 'Dmitry Ustalov'
+__credits__ = ['Alexander Panchenko', 'Alexander Chambers', 'Frederik Wille']
+__license__ = 'MIT'
+
+from .chinese_whispers import (
+    WEIGHTING,
+    top_weighting,
+    linear_weighting,
+    log_weighting,
+    chinese_whispers,
+    random_argmax,
+    aggregate_clusters
+)
+
+__all__ = [
+    '__version__',
+    'WEIGHTING',
+    'top_weighting',
+    'linear_weighting',
+    'log_weighting',
+    'chinese_whispers',
+    'random_argmax',
+    'aggregate_clusters'
+]

e2c1b234326a15468d57fc15112fd930488a1580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:49 +0000

    Add package atoti-directquery-mssql/0.8.4/atoti_directquery_mssql-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-mssql/atoti_directquery_mssql-0.8.4-py3-none-any.whl/atoti_directquery_mssql/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery with `Microsoft SQL Server <https://www.microsoft.com/en-us/sql-server>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

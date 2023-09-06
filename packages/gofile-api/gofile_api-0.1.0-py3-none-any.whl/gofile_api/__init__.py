ff4473030d23f9333eae146c4e80d2d8704f9f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:16 +0000

    Add package gofile-api/0.1.0/gofile_api-0.1.0-py3-none-any.whl

---
+++ packages/gofile-api/gofile_api-0.1.0-py3-none-any.whl/gofile_api/__init__.py
@@ -0,0 +1,16 @@
+"""# GoFile API
+### Description
+A python library for communicating with the Gofile API.
+
+### Features
+Get an available server. <br>
+Upload a file, to a directory, or to the root folder. <br>
+Create a guest account. <br>
+Get contents of a folder. <br>
+Create a folder. <br>
+Get the account information. <br>
+Set option for a content id.
+"""
+from .api import GoFileSession as GoFileSession
+
+# TODO: Import exceptions

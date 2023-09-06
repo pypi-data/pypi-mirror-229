9f9492b54f1b72a98f58baa708ea9208fc7a1862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 04:11:54 +0000

    Add package wireup/0.1.0/wireup-0.1.0.tar.gz

---
+++ packages/wireup/wireup-0.1.0.tar.gz/wireup-0.1.0/readme.md
@@ -0,0 +1,55 @@
+# WireUp
+
+Effortless dependency injection in Python.
+
+
+**1. Set application parameters** 
+```python
+container.params.update({
+    "db.connection_str": "sqlite://memory",
+    "auth.user": os.environ.get("USER"),
+    "cache_dir": "/var/cache/",
+    "env": os.environ.get("ENV", "dev")
+})
+```
+
+**2. Register dependencies**
+
+```python
+@container.register
+class DbService:
+    def __init__(
+        self,
+        # Inject a parameter by name
+        connection_str: str = wire(param="db.connection_str"),
+        # Or by interpolating multiple parameters into a string
+        cache_dir: str = wire(expr="${cache_dir}/${auth.user}/db"),
+    ):
+        self.connection_str = connection_str
+        self.cache_dir = cache_dir
+        
+# Constructor injection is also supported for dataclasses
+# resulting in a more compact syntax.
+@container.register
+@dataclass  
+class UserRepository:
+    db: DbService # Dependencies may also depend on other dependencies.
+    user: str = container.wire(param="auth.user") 
+```
+
+**3. Inject**
+
+```python
+# Decorate all methods where the library must perform injection. 
+@app.route("/greet/<str:name>")
+@container.autowire
+# Classes are automatically injected based on annotated type. 
+# Parameters will be located based on the hint given in their default value.
+# Unknown arguments will not be processed.
+def greet(name: str, user_repository: UserRepository, env: str = wire(param="env")):
+  ...
+```
+
+## Documentation
+
+For more information [read our documentation](#)
---
+++ packages/wireup/wireup-0.1.0.tar.gz/wireup-0.1.0/wireup/ioc/parameter.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+import re
+from typing import Any
+
+from .container_util import ParameterReference, TemplatedString
+
+
+class ParameterBag:
+    """Parameter flat key-value store for use with a container.
+
+    Allows storing and retrieving of parameters in the bag. Templated strings can be used to interpolate a string
+    referencing multiple parameters.
+
+    """
+
+    def __init__(self) -> None:
+        """Initialize an empty ParameterBag.
+
+        The ParameterBag holds a flat key-value store of parameter values.
+        """
+        self.__bag: dict[str, Any] = {}
+        self.__cache: dict[str, str] = {}
+
+    def put(self, name: str, val: Any) -> None:
+        """Put a parameter value into the bag. This overwrites any previous values.
+
+        :param name: The name of the parameter.
+        :param val: The value of the parameter.
+        """
+        self.__bag[name] = val
+
+    def get_all(self) -> dict[str, Any]:
+        """Get all parameters stored in the bag.
+
+        :return: A dictionary containing all parameter names and their values.
+        """
+        return self.__bag
+
+    def get(self, param: ParameterReference) -> Any:
+        """Get the value of a parameter.
+
+        If the parameter is templated, interpolate it first by replacing placeholders with parameter values.
+
+        :param param: The parameter to retrieve.
+        :return: The parameter's value.
+        """
+        return (
+            self.__interpolate(param.value) if isinstance(param, TemplatedString) else self.__get_value_from_name(param)
+        )
+
+    def update(self, new_params: dict[str, Any]) -> None:
+        """Update the bag with new set of parameters.
+
+        Parameters from new_params will overwrite any existing parameters set with the same name.
+
+        :param new_params: A dictionary of parameter names and their updated values.
+        """
+        self.__bag.update(new_params)
+
+    def __get_value_from_name(self, name: str) -> Any:
+        if name not in self.__bag:
+            msg = f"Unknown parameter {name} requested"
+            raise ValueError(msg)
+
+        return self.__bag[name]
+
+    def __interpolate(self, val: str) -> str:
+        if val in self.__cache:
+            return self.__cache[val]
+
+        res = re.sub(
+            r"\${(.*?)}",  # Let's accept anything here as we don't impose any rules when adding params
+            # Since we're concatenating strings we need to convert any parameters we get to str
+            lambda match: str(self.__get_value_from_name(match.group(1))),
+            val,
+            flags=re.DOTALL,
+        )
+        self.__cache[val] = res
+
+        return res

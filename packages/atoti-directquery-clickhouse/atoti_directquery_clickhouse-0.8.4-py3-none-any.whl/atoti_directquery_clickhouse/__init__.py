e53a20acc4ab0a26e489424b2591165bcf417700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:57 +0000

    Add package atoti-directquery-clickhouse/0.8.4/atoti_directquery_clickhouse-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-clickhouse/atoti_directquery_clickhouse-0.8.4-py3-none-any.whl/atoti_directquery_clickhouse/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery on `ClickHouse <https://clickhouse.com>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

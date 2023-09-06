ef8c393db6fad17c1c844b91bdf565f77fee9b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:49 +0000

    Add package kinguru-database/0.1.0/kinguru_database-0.1.0.tar.gz

---
+++ packages/kinguru-database/kinguru_database-0.1.0.tar.gz/kinguru_database-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='kinguru_database',
+    version='0.1.0',
+    description='Crud  simplificado',
+    author='Gleison de souza luiz',
+    author_email='gleisonnanet@gmail.com',
+    packages=['kinguru_database'],
+)

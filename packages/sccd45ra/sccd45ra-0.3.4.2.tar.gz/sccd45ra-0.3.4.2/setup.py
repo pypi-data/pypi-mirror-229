377621e6e2170d2523dcf4ce84de283933aec2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:54 +0000

    Add package sccd45ra/0.3.4.2/sccd45ra-0.3.4.2.tar.gz

---
+++ packages/sccd45ra/sccd45ra-0.3.4.2.tar.gz/sccd45ra-0.3.4.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sccd45ra",
+    version="0.3.4.2",
+    packages=find_packages(),
+    package_data={
+        'sccd45ra': ['model/*.h5ad', 'model/*.joblib','feature/*.xlsx'],
+    },
+    install_requires=[
+        "scanpy",
+        "numpy",
+        "pandas",
+        "anndata>=0.8.0",
+        "joblib",
+        "scikit-learn",
+        "setuptools"
+    ]
+)

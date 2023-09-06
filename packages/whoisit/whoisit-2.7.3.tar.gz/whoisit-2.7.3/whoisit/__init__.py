3337f42f867b6a5eec68996b382fae52bcfe8bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 14:10:13 +0000

    Add package whoisit/2.7.3/whoisit-2.7.3.tar.gz

---
+++ packages/whoisit/whoisit-2.7.3.tar.gz/whoisit-2.7.3/whoisit/__init__.py
@@ -0,0 +1,65 @@
+import requests
+from .bootstrap import Bootstrap
+from .query import QueryBuilder, Query
+from .parser import parse
+from .logger import get_logger
+from .utils import create_session, get_session
+from .version import version
+
+
+# Private methods
+
+
+_bootstrap = Bootstrap()
+_query_builder = QueryBuilder(_bootstrap)
+
+
+# Expose class methods as the public API
+
+
+is_bootstrapped = _bootstrap.is_bootstrapped
+clear_bootstrapping = _bootstrap.clear_bootstrapping
+bootstrap = _bootstrap.bootstrap
+save_bootstrap_data = _bootstrap.save_bootstrap_data
+load_bootstrap_data = _bootstrap.load_bootstrap_data
+bootstrap_is_older_than = _bootstrap.bootstrap_is_older_than
+build_query = _query_builder.build
+
+
+# Query helpers
+
+
+def asn(as_number, rir=None, raw=False, allow_insecure_ssl=False, session=None):
+    session = get_session(session, allow_insecure_ssl=allow_insecure_ssl)
+    method, url, exact_match = build_query(
+        query_type='asn', query_value=as_number, rir=rir)
+    q = Query(session, method, url)
+    response = q.request()
+    return response if raw else parse(_bootstrap, 'autnum', as_number, response)
+
+
+def domain(domain_name, raw=False, allow_insecure_ssl=False, session=None):
+    session = get_session(session, allow_insecure_ssl=allow_insecure_ssl)
+    method, url, exact_match = build_query(
+        query_type='domain', query_value=domain_name)
+    q = Query(session, method, url)
+    response = q.request()
+    return response if raw else parse(_bootstrap, 'domain', domain_name, response)
+
+
+def ip(ip_address_or_network, rir=None, raw=False, allow_insecure_ssl=False, session=None):
+    session = get_session(session, allow_insecure_ssl=allow_insecure_ssl)
+    method, url, exact_match = build_query(
+        query_type='ip', query_value=ip_address_or_network, rir=rir)
+    q = Query(session, method, url)
+    response = q.request()
+    return response if raw else parse(_bootstrap, 'ip', ip_address_or_network, response)
+
+
+def entity(entity_handle, rir=None, raw=False, allow_insecure_ssl=False, session=None):
+    session = get_session(session, allow_insecure_ssl=allow_insecure_ssl)
+    method, url, exact_match = build_query(
+        query_type='entity', query_value=entity_handle, rir=rir)
+    q = Query(session, method, url)
+    response = q.request()
+    return response if raw else parse(_bootstrap, 'entity', entity_handle, response)
---
+++ packages/whoisit/whoisit-2.7.3.tar.gz/whoisit-2.7.3/whoisit/overrides.py
@@ -0,0 +1,34 @@
+"""
+    Various RDAP servers are either incorrect or not listed in the IANA
+    bootstrap data. This file contains a list of overrides that are overlayed
+    onto the IANA data by default.
+"""
+
+iana_overrides = {
+
+    'domain': {
+
+        # 2021-11-30 - .de has an RDAP endpoint it's just not listed
+        'de': ['https://rdap.denic.de/'],
+
+        # 2022-01-15 - .ch has an RDAP endpoint it's just not listed
+        'ch': ['https://rdap.nic.ch/'],
+
+        # 2023-08-21 - .li has an RDAP endpoint it's just not listed
+        'li': ['https://rdap.nic.li/'],
+
+        # 2022-10-17 - .gl and .gl SLDs have RDAP endpoints they're just not listed
+        'gl': ['https://rdap.centralnic.com/gl/'],
+        'co.gl': ['https://rdap.centralnic.com/co.gl/'],
+        'com.gl': ['https://rdap.centralnic.com/com.gl/'],
+        'edu.gl': ['https://rdap.centralnic.com/edu.gl/'],
+        'gov.gl': ['https://rdap.centralnic.com/gov.gl/'],
+        'net.gl': ['https://rdap.centralnic.com/net.gl/'],
+        'org.gl': ['https://rdap.centralnic.com/org.gl/'],
+
+        # 2023-09-05 - .nl has an RDAP endpoint it's just not listed
+        'nl': ['https://rdap.sidn.nl/'],
+
+    }
+
+}

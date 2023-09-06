a6f5529b902cb8c0b610b57e9e97977d35d96c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 15:12:55 +0000

    Add package flm-core/0.3.0a13/flm_core-0.3.0a13-py3-none-any.whl

---
+++ packages/flm-core/flm_core-0.3.0a13-py3-none-any.whl/flm/__init__.py
@@ -0,0 +1,8 @@
+
+
+# Version is hard-coded here to avoid any other dependencies (no importlib
+# magic, no pkg_resources, no other stuff like that), especially for possible
+# future use with transcrypt.
+__version__ = "0.3.0alpha000013"
+
+# ALSO BUMP VERSION NUMBER IN pyproject.toml !!

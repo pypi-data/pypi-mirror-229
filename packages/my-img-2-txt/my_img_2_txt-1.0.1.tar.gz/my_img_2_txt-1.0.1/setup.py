84ddd9bc81155eca3c7f76b0bfbbe3bdaaf772d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 04:12:28 +0000

    Add package my-img-2-txt/1.0.1/my_img_2_txt-1.0.1.tar.gz

---
+++ packages/my-img-2-txt/my_img_2_txt-1.0.1.tar.gz/my_img_2_txt-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="my_img_2_txt",
+    version="1.0.1",
+    author="Michelle Santos",
+    author_email="michellesantosvet@gmail.com",
+    description="Transform images with texts on it into a single text",
+    long_description="Transforms images into text using cv2 and pytesseract libraries",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)

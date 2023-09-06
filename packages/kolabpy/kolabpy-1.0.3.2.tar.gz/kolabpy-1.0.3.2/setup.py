999633c2f29cd5e3e58c74f44807d0f0ebf34540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 15:13:46 +0000

    Add package kolabpy/1.0.3.2/kolabpy-1.0.3.2.tar.gz

---
+++ packages/kolabpy/kolabpy-1.0.3.2.tar.gz/kolabpy-1.0.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+	  name='kolabpy', #module 이름
+	  version='1.0.3.2',  
+	  description='',
+	  long_description= 'Contributor:apjh2529@naver.com,kkd8326@snu.ac.kr', 
+	  author='Kolab',
+	  author_email='kkd8326@snu.ac.kr',
+	  url='https://aric.snu.ac.kr/',
+	  license='MIT',
+	  py_modules=['pymodule'], #업로드할 module
+	  python_requires='>=3', #파이썬 버전 
+	  install_requires=['saspy==5.3.0', 'geopandas>=0.10', 'folium>=0.8'], #module 필요한 다른 module
+	  packages=['kolabpy'] #업로드할 module이 있는 폴더
+)

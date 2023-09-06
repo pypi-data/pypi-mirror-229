4c99365f955676d714ed4f9679599f61e603bcbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 15:12:26 +0000

    Add package qalita/1.1.6/qalita-1.1.6.tar.gz

---
+++ packages/qalita/qalita-1.1.6.tar.gz/qalita-1.1.6/qalita/internal/utils.py
@@ -0,0 +1,10 @@
+"""
+# QALITA (c) COPYRIGHT 2023 - ALL RIGHTS RESERVED -
+"""
+from qalita.internal.logger import init_logging
+
+logger = init_logging()
+
+
+def get_version():
+    return "0.0.1-dev"

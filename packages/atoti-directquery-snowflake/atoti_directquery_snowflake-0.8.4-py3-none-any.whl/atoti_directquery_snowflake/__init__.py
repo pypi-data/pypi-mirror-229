13bcad72c7c97c726dac697d2353a1c81a540823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:59 +0000

    Add package atoti-directquery-snowflake/0.8.4/atoti_directquery_snowflake-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-snowflake/atoti_directquery_snowflake-0.8.4-py3-none-any.whl/atoti_directquery_snowflake/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery on `Snowflake <https://www.snowflake.com>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

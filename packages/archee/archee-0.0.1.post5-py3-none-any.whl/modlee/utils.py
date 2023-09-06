d915fba6c032744964d40351f11f17e4f80a0a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 14:09:12 +0000

    Add package archee/0.0.1.post5/archee-0.0.1.post5-py3-none-any.whl

---
+++ packages/archee/archee-0.0.1.post5-py3-none-any.whl/modlee/utils.py
@@ -0,0 +1,54 @@
+import json
+from urllib.parse import urlparse, unquote
+import os
+import pathlib
+import pickle
+import requests
+
+
+def safe_mkdir(target_dir):
+    root, ext = os.path.splitext(target_dir)
+    # is a file
+    if len(ext) > 0:
+        target_dir = os.path.split(root)
+    else:
+        target_dir = f"{target_dir}/"
+    # if os.path.isfile(target_dir):
+    #     target_dir,_ = os.path.split(target_dir.split('.')[0])
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+
+
+
+def get_fashion_mnist(batch_size=64):
+    training_loader = DataLoader(
+        tv_datasets.FashionMNIST(
+            root='data',
+            train=True,
+            download=True,
+            transform=ToTensor(),
+        ), batch_size=batch_size, shuffle=True,
+    )
+    test_loader = DataLoader(
+        tv_datasets.FashionMNIST(
+            root='data',
+            train=False,
+            download=True,
+            transform=ToTensor(),
+        ), batch_size=batch_size, shuffle=True,
+    )
+    return training_loader, test_loader
+
+
+def uri_to_path(uri):
+    parsed_uri = urlparse(uri)
+    path = unquote(parsed_uri.path)
+    return path
+
+
+def is_cacheable(x):
+    try:
+        json.dumps(x)
+        return True
+    except:
+        return False

6a2248b26b628a0ce2f52d66dbb2446a80ebda0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:35 +0000

    Add package sabrmetrics/0.6.0/sabrmetrics-0.6.0.tar.gz

---
+++ packages/sabrmetrics/sabrmetrics-0.6.0.tar.gz/sabrmetrics-0.6.0/sabrmetrics/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+import datetime
+import pkg_resources
+
+
+TODAY = datetime.datetime.today()
+
+
+__version__ = pkg_resources.get_distribution("sabrmetrics").version
---
+++ packages/sabrmetrics/sabrmetrics-0.6.0.tar.gz/sabrmetrics-0.6.0/sabrmetrics/sfbb/__init__.py
@@ -0,0 +1,7 @@
+"""
+Web scraper for the `Smart Fantasy Baseball`_ website.
+
+.. _Smart Fantasy Baseball: https://smartfantasybaseball.com/
+"""
+
+from ._tools import PlayerIDMap

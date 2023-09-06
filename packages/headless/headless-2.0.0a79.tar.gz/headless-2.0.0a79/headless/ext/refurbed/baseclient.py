a3cbe96f9aa7eff9509010104d2314611490254a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 15:13:31 +0000

    Add package headless/2.0.0a79/headless-2.0.0a79.tar.gz

---
+++ packages/headless/headless-2.0.0a79.tar.gz/headless-2.0.0a79/headless/ext/refurbed/baseclient.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import os
+from typing import Any
+
+from headless.core import httpx
+from .credential import RefurbedCredential
+
+
+class BaseClient(httpx.Client):
+    __module__: str = 'headless.ext.refurbed'
+
+    def __init__(self, access_token: str | None = None):
+        access_token = access_token or os.environ['REFURBED_API_KEY']
+        super().__init__(
+            base_url='',
+            credential=RefurbedCredential(access_token)
+        )
+
+    def get_base_url(self, base_url: str) -> str:
+        raise NotImplementedError
+    
+    async def update_offer(self, sku: str, **params: Any) -> None:
+        """Updates an Offer."""
+        response = await self.post(
+            url="/refb.merchant.v1.OfferService/UpdateOffer",
+            json={
+                **params,
+                'identifier': {
+                    'sku': sku
+                }
+            }
+        )
+        response.raise_for_status()

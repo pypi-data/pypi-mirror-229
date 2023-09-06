40de7ff622959bf5808447b8a1df035926005013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:29 +0000

    Add package dbgear/0.6.0/dbgear-0.6.0.tar.gz

---
+++ packages/dbgear/dbgear-0.6.0.tar.gz/dbgear-0.6.0/dbgear/models/datagrid/data.py
@@ -0,0 +1,35 @@
+from typing import Any
+from ..base import BaseSchema
+
+
+class DataModel(BaseSchema):
+    id: str
+    instance: str
+    table_name: str
+    description: str = ''
+    layout: str
+    settings: dict[str, object]
+    sync_mode: str
+    value: str | None = None
+    caption: str | None = None
+    x_axis: str | None = None
+    y_axis: str | None = None
+    cells: list[str] | None = None
+
+
+class GridColumn(BaseSchema):
+    field: str
+    type: str
+    header_name: str
+    width: int
+    editable: bool
+    hide: bool = False
+    items: list | None = None
+    fixed_value: str | None = None
+    call_value: str | None = None
+
+
+class DataInfo(BaseSchema):
+    grid_columns: list[GridColumn]
+    grid_rows: list[dict[str, Any]]
+    allow_line_addition_and_removal: bool
---
+++ packages/dbgear/dbgear-0.6.0.tar.gz/dbgear-0.6.0/dbgear/models/fileio.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import yaml
+from typing import Any
+
+from . import const
+
+
+def get_environ_name(folder: str, id: str):
+    return f'{folder}/{id}'
+
+
+def get_mapping_name(folder: str, id: str):
+    return f'{folder}/{id}/{const.FNAME_MAPPING}'
+
+
+def get_data_model_name(folder: str, id: str, instance: str, table: str) -> str:
+    return f'{folder}/{id}/{instance}@{table}.yaml'
+
+
+def get_data_dat_name(folder: str, id: str, instance: str, table: str) -> str:
+    return f'{folder}/{id}/{instance}@{table}.dat'
+
+
+def save_model(fname: str, model: Any, exclude: list[str] = None) -> None:
+    with open(fname, 'w', encoding='utf-8') as f:
+        yaml.dump(model.model_dump(exclude=exclude), f, indent=2, allow_unicode=True)
+
+
+def save_yaml(fname: str, data: object) -> None:
+    with open(fname, 'w', encoding='utf-8') as f:
+        yaml.dump(data, f, indent=2, allow_unicode=True)
+
+
+def load_model(fname: str, clazz: Any, **kwargs) -> Any:
+    return clazz(**load_yaml(fname), **kwargs)
+
+
+def load_yaml(fname: str) -> Any:
+    with open(fname, 'r', encoding='utf-8') as f:
+        data = yaml.safe_load(f)
+    return data
+
+
+def _is_exist_raw_data(folder: str, id: str, ins: str, tbl: str) -> bool:
+    return os.path.isfile(get_data_dat_name(folder, id, ins, tbl))
+
+
+def load_data(folder: str, id: str, ins: str, tbl: str, none: bool = False) -> Any:
+    data = None if none else []
+    if _is_exist_raw_data(folder, id, ins, tbl):
+        data = load_yaml(get_data_dat_name(folder, id, ins, tbl))
+    return data
+
+
+def make_folder(dirname: str) -> None:
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+
+
+def delete_folder(dirname: str) -> None:
+    shutil.rmtree(dirname)

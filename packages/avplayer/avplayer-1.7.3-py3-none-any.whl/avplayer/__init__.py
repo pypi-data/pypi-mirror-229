fd256ebe2be69dc55ad929ceb18a525857370c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 14:09:30 +0000

    Add package avplayer/1.7.3/avplayer-1.7.3-py3-none-any.whl

---
+++ packages/avplayer/avplayer-1.7.3-py3-none-any.whl/avplayer/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from avplayer.apps.default import AioApp, IoApp
+from avplayer.apps.default import default_main as av_main
+from avplayer.apps.interface.av_interface import AsyncAvInterface, AvInterface
+from avplayer.av.av_io import AvIo
+from avplayer.avconfig import AvConfig
+
+__version__ = "1.7.3"
+__all__ = [
+    "__version__",
+    "AioApp",
+    "AsyncAvInterface",
+    "AvConfig",
+    "AvInterface",
+    "AvIo",
+    "IoApp",
+    "av_main",
+]

ccc6067020e600b7b3b17aeb6546283de5fcdc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 04:12:03 +0000

    Add package jijzeptlab-client/0.5.0/jijzeptlab_client-0.5.0-cp39-cp39-win_amd64.whl

---
+++ packages/jijzeptlab-client/jijzeptlab_client-0.5.0-cp39-cp39-win_amd64.whl/jijzeptlab/exception.py
@@ -0,0 +1,10 @@
+class JijZeptLabError(Exception):
+    """Base class for exceptions in jijzeptlab."""
+
+    pass
+
+
+class JijZeptLabSolverError(JijZeptLabError):
+    """Exception raised for errors in the solver."""
+
+    pass

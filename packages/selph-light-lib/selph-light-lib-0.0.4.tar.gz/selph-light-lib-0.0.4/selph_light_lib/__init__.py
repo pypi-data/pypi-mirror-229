caafa581974c72ad7755de658ad20817a3dc61d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:07 +0000

    Add package selph-light-lib/0.0.4/selph-light-lib-0.0.4.tar.gz

---
+++ packages/selph-light-lib/selph-light-lib-0.0.4.tar.gz/selph-light-lib-0.0.4/selph_light_lib/__init__.py
@@ -0,0 +1,11 @@
+import _selph_light_lib
+
+from .pkginfo import __author__, __version__
+
+assert _sepl_light_lib.__version__ == __version__, \
+    ImportError(f"selph_light_lib {__version__} and _selph_light_lib {_sepl_light_lib.__version__} version mismatch!")
+
+import selph_light_lib.color as color
+import selph_light_lib.wave as wave
+
+from selph_light_lib.const import *
---
+++ packages/selph-light-lib/selph-light-lib-0.0.4.tar.gz/selph-light-lib-0.0.4/selph_light_lib/pkginfo.py
@@ -0,0 +1,2 @@
+__author__ = "Sersoid"
+__version__ = "0.0.4"

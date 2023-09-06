07023927182ac256e9b3049d8e8c7af0ca14fb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:09 +0000

    Add package EarthMC/3.4.1/EarthMC-3.4.1.tar.gz

---
+++ packages/EarthMC/EarthMC-3.4.1.tar.gz/EarthMC-3.4.1/EarthMC/Classes/OAPI/Nation.py
@@ -0,0 +1,28 @@
+from EarthMC.Utils import AutoRepr
+
+
+class OAPI_Nation(AutoRepr):
+    def __init__(self, data):
+        timestamps = data['timestamps']
+        stats = data['stats']
+
+        self.timestamps = {
+            'founded': timestamps['registered'],
+        }
+
+        strings = data['strings']
+
+        self.board = strings['board']
+        self.name = strings['nation']
+        self.capital = strings['capital']
+        self.king = strings['king']
+
+        self.spawn = data['spawn']
+        self.area = stats['numTownBlocks']
+        self.residents = data['residents']
+        self.towns = data['towns']
+
+        self.allies = data['allies']
+        self.enemies = data['enemies']
+        self.color = strings['mapColorHexCode']
+

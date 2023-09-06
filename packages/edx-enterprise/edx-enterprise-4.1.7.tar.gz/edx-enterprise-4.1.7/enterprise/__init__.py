9c5be3c99c5ae47dd86582d063d40864b3b7bfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:03 +0000

    Add package edx-enterprise/4.1.7/edx-enterprise-4.1.7.tar.gz

---
+++ packages/edx-enterprise/edx-enterprise-4.1.7.tar.gz/edx-enterprise-4.1.7/enterprise/__init__.py
@@ -0,0 +1,7 @@
+"""
+Your project description goes here.
+"""
+
+__version__ = "4.1.7"
+
+default_app_config = "enterprise.apps.EnterpriseConfig"
---
+++ packages/edx-enterprise/edx-enterprise-4.1.7.tar.gz/edx-enterprise-4.1.7/integrated_channels/xapi/__init__.py
@@ -0,0 +1,8 @@
+"""
+xAPI module.
+"""
+
+__version__ = "0.1.0"
+
+default_app_config = ("integrated_channels.xapi.apps."
+                      "XAPIConfig")

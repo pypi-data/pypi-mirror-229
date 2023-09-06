8f408484efb8ab37a45dd47a71ad0c3530e46a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:35 +0000

    Add package sabrmetrics/0.6.0/sabrmetrics-0.6.0-py3-none-any.whl

---
+++ packages/sabrmetrics/sabrmetrics-0.6.0-py3-none-any.whl/sabrmetrics/playerids.py
@@ -0,0 +1,11 @@
+"""
+Wrapper for various Player ID databases.
+
+Supported website databases:
+
+- `Smart Fantasy Baseball`_: :py:class:`playerids.SFBB`
+
+.. _Smart Fantasy Baseball: https://smartfantasybaseball.com/
+"""
+
+from sabrmetrics.sfbb import PlayerIDMap as SFBB

3f580872ed8fac2f1d3ccfcfd5aac27fa175a355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:51 +0000

    Add package atoti-directquery-redshift/0.8.4/atoti_directquery_redshift-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-redshift/atoti_directquery_redshift-0.8.4-py3-none-any.whl/atoti_directquery_redshift/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery on Amazon `Redshift <https://aws.amazon.com/redshift/>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

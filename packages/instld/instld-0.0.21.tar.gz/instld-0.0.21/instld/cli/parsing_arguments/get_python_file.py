03c6332aa806c771287fd5679556533b6f78811e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 04:11:59 +0000

    Add package instld/0.0.21/instld-0.0.21.tar.gz

---
+++ packages/instld/instld-0.0.21.tar.gz/instld-0.0.21/instld/cli/parsing_arguments/get_python_file.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def get_python_file():
+    if len(sys.argv) < 2:
+        print('usage: instld python_file.py [argv ...]', file=sys.stderr)
+        sys.exit(1)
+
+    return sys.argv[1]

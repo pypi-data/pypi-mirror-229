5a59766ca4cb03a67db8d973c489f00ebd02b518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:09:16 +0000

    Add package sync-composer/0.0.2/sync-composer-0.0.2.tar.gz

---
+++ packages/sync-composer/sync-composer-0.0.2.tar.gz/sync-composer-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='sync-composer',
+    version='0.0.2',    
+    description='sync composer and local',
+    url='https://github.com/paulhtremblay/composer_sync',
+    author='Henry Tremblay',
+    author_email='paulhtremblay@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=['sync_composer/'
+        ],
+     classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing :: Mocking'
+    ],
+)

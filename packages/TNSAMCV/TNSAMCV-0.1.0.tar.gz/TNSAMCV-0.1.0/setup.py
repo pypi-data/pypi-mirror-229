2f0ff25de679a427096858693401cd4730b36b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 14:09:52 +0000

    Add package TNSAMCV/0.1.0/TNSAMCV-0.1.0.tar.gz

---
+++ packages/TNSAMCV/TNSAMCV-0.1.0.tar.gz/TNSAMCV-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TNSAMCV',
+    version='0.1.0',
+    description='TNSAMCV is a module by TNSA which is used in computer vision',
+    author='TNSA',
+    author_email='thishyakethabimalla@gmail.com',
+    packages=find_packages(),
+)

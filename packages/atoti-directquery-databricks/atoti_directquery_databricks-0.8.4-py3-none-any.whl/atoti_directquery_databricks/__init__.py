68c3f477a74d89ada9942cc05b2537630a683026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:10:52 +0000

    Add package atoti-directquery-databricks/0.8.4/atoti_directquery_databricks-0.8.4-py3-none-any.whl

---
+++ packages/atoti-directquery-databricks/atoti_directquery_databricks-0.8.4-py3-none-any.whl/atoti_directquery_databricks/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin to use DirectQuery with `Databricks <https://www.databricks.com>`__."""
+
+from .connection import *
+from .connection_info import *
+from .table import *
+from .table_options import *

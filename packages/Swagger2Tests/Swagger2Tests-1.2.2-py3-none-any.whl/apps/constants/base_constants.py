13e8459e22290d871247d451d649d7262455e67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 00:31:59 +0000

    Add package Swagger2Tests/1.2.2/Swagger2Tests-1.2.2-py3-none-any.whl

---
+++ packages/Swagger2Tests/Swagger2Tests-1.2.2-py3-none-any.whl/apps/constants/base_constants.py
@@ -0,0 +1,8 @@
+from apps.helpers.helper import Helper
+from apps.helpers.helper import RequestInfo
+
+
+API_PREFIX = ""
+REQUEST_APPID = "f0c1500d81600732"
+REQUEST_SECRECT = "BF2168570D05DBE2EF987970263ABC91"
+BASE_URL = "https://aaa.xxx.cn"

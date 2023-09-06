eb812561f08388fc19c739cc80d41736695a5214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:09:53 +0000

    Add package screeve-embeddings/0.1.1/screeve-embeddings-0.1.1.tar.gz

---
+++ packages/screeve-embeddings/screeve-embeddings-0.1.1.tar.gz/screeve-embeddings-0.1.1/tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.embeddings.model import Word2VecModel
+from src.embeddings.tokenizer import Tokenizer, WhitespaceTokenizer, BasicTokenizer, NumSubTokenizer, Word2VecTokenizer
+
+
+@pytest.fixture
+def whitespace_tokenizer() -> Tokenizer:
+    return WhitespaceTokenizer()
+
+
+@pytest.fixture
+def basic_tokenizer() -> Tokenizer:
+    return BasicTokenizer()
+
+
+@pytest.fixture
+def basic_tokenizer_with_number_replacement() -> Tokenizer:
+    return NumSubTokenizer()
+
+
+@pytest.fixture
+def word2vec_tokenizer() -> Tokenizer:
+    return Word2VecTokenizer()
+
+@pytest.fixture
+def word2vec() -> Word2VecModel:
+    return Word2VecModel()

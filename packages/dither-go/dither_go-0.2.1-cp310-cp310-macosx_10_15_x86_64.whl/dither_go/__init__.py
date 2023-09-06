5e04d9ee4dab0269a2c65122d41662071f2fa4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 03:11:17 +0000

    Add package dither-go/0.2.1/dither_go-0.2.1-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/dither-go/dither_go-0.2.1-cp310-cp310-macosx_10_15_x86_64.whl/dither_go/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023, tfuxu <https://github.com/tfuxu>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+__version__ = "0.2.1"
+
+from .wrapper import *
+from .matrices import *
+from .exceptions import DitherGoError, InvalidColorError

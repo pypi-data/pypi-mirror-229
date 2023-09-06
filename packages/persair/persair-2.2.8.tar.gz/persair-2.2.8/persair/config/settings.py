2a58b71194ed4ebb3c117310f260150aa340f984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 04:12:05 +0000

    Add package persair/2.2.8/persair-2.2.8.tar.gz

---
+++ packages/persair/persair-2.2.8.tar.gz/persair-2.2.8/persair/config/settings.py
@@ -0,0 +1,15 @@
+import  os
+from    pydantic_settings  import BaseSettings
+
+class Keys(BaseSettings):
+    DBauthPath:str      = '/db/init.json'
+    ReadWriteKey:str    = "local"
+
+class Mongo(BaseSettings):
+    MD_URI:str          = "localhost:27017"
+    MD_DB:str           = "default"
+    MD_username:str     = "username"
+    MD_password:str     = "password"
+
+keys            = Keys()
+mongosettings   = Mongo()

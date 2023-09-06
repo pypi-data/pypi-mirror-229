47835a8c27040e361f0a32ecee4958f86d0322e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 14:08:40 +0000

    Add package Axha/1.8.2/Axha-1.8.2.tar.gz

---
+++ packages/Axha/Axha-1.8.2.tar.gz/Axha-1.8.2/README.md
@@ -0,0 +1,1 @@
+TNSA AXHA is a module and prouduct by TNSA which is used in the development of Voice Assistants 
---
+++ packages/Axha/Axha-1.8.2.tar.gz/Axha-1.8.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Axha',
+    version='1.8.2',
+    description='TNSA AXHA IS A VOICE ASSISTANT MODULE USED TO DEVELOP AI VOICE ASSISTANT EASILY.',
+    author='TNSA',
+    author_email='thishyakethabimalla@gmail.com',
+    packages=['Axha'],
+)

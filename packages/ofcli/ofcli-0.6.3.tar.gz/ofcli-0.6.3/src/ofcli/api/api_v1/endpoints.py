726ca4ece2d0c5e4d23403ee2d9d80aaa082c3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 04:12:19 +0000

    Add package ofcli/0.6.3/ofcli-0.6.3.tar.gz

---
+++ packages/ofcli/ofcli-0.6.3.tar.gz/ofcli-0.6.3/src/ofcli/api/api_v1/endpoints.py
@@ -0,0 +1,222 @@
+from fastapi import Request
+from fastapi.params import Query, Path
+from fastapi.responses import JSONResponse, PlainTextResponse, Response
+from typing import Annotated, Optional
+from pydantic import HttpUrl
+
+from ofcli import __version__
+from ofcli import core
+from ofcli.utils import (
+    APIRouter,
+    EntityType,
+    OutputType,
+    subtree_to_string,
+)
+from ofcli.exceptions import (
+    InternalException,
+)
+
+router = APIRouter()
+
+
+@router.get(path="/", name="index", description="Retrieve general API information.")
+async def index(request: Request) -> list[dict[str, str]]:
+    def get_route_info(route):
+        info = {
+            "path": route.path,
+            "name": route.name,
+        }
+        if hasattr(route, "summary") and route.summary:
+            info["summary"] = route.summary
+        if hasattr(route, "description") and route.description:
+            info["description"] = route.description
+        return info
+
+    url_list = [
+        get_route_info(route)
+        for route in request.app.routes
+        if route.include_in_schema and route.path.startswith(request.url.path)
+    ]
+    return url_list
+
+
+@router.get(
+    path="/entity/{entity_id:path}",
+    name="entity",
+    description="Returns the decoded self-signed entity configuration for given entity_id.",
+)
+async def entity(
+    entity_id: Annotated[
+        HttpUrl,
+        Path(
+            description="Entity ID",
+        ),
+    ],
+    verify: Annotated[
+        bool,
+        Query(description="Whether to verify the entity configuration's signature"),
+    ] = False,
+):
+    configuration = core.get_entity_configuration(
+        entity_id.unicode_string(), verify=verify
+    )
+    return configuration
+
+
+@router.get(path="/fetch", name="fetch", description="Fetches an entity statement.")
+async def fetch(
+    entity_id: Annotated[
+        HttpUrl,
+        Query(
+            description="Entity ID",
+        ),
+    ],
+    issuer: Annotated[
+        HttpUrl,
+        Query(
+            description="Issuer ID",
+        ),
+    ],
+):
+    statement = core.fetch_entity_statement(
+        entity_id.unicode_string(), issuer.unicode_string()
+    )
+    return statement
+
+
+@router.get(
+    path="/list/{entity_id:path}",
+    name="list",
+    description="Lists all subordinates of an entity.",
+)
+async def list_subordinates(
+    entity_id: Annotated[
+        HttpUrl,
+        Path(
+            description="Entity ID",
+        ),
+    ],
+    entity_type: Annotated[
+        Optional[EntityType],
+        Query(
+            description="Entity type to filter for.",
+        ),
+    ] = None,
+):
+    subordinates = core.list_subordinates(
+        entity_id.unicode_string(),
+        entity_type=entity_type.value if entity_type else None,
+    )
+    return subordinates
+
+
+@router.get(
+    path="/trustchains/{entity_id:path}",
+    name="trustchains",
+    description="Builds all trustchains for a given entity and prints them. If any trust anchor is specified, only trustchains ending in the trust anchor will be returned.",
+)
+async def trustchains(
+    entity_id: Annotated[
+        HttpUrl,
+        Path(
+            description="Entity ID to build trustchains for.",
+        ),
+    ],
+    ta: Annotated[
+        list[HttpUrl],
+        Query(
+            description="Trust anchor ID to use for building trustchains (multiple TAs possible)."
+        ),
+    ] = [],
+    format: OutputType = OutputType.json,
+):
+    chains, graph = core.get_trustchains(
+        entity_id.unicode_string(),
+        [ta_item.unicode_string() for ta_item in ta],
+        format == OutputType.dot,
+    )
+    if format == OutputType.dot:
+        if not graph:
+            raise InternalException("No graph to export.")
+        return Response(graph.to_string(), media_type="file/dot")
+    if format == OutputType.text:
+        response = ""
+        for chain in chains:
+            response += "* " + (str(chain)) + "\n"
+        return PlainTextResponse(response)
+    # if format == OutputType.json:
+    response = {}
+    for i, chain in enumerate(chains):
+        response[f"chain {i}"] = chain.to_json()
+    return JSONResponse(response)
+
+
+@router.get(
+    path="/subtree/{entity_id:path}",
+    name="subtree",
+    description="Discover federation subtree using given entity as root.",
+)
+async def subtree(
+    entity_id: Annotated[
+        HttpUrl,
+        Path(
+            description="Entity ID to use as root for federation subtree.",
+        ),
+    ],
+    format: OutputType = OutputType.json,
+):
+    tree, graph = core.subtree(entity_id.unicode_string(), format == OutputType.dot)
+    if format == OutputType.dot:
+        if not graph:
+            raise InternalException("No graph to export.")
+        return Response(graph.to_string(), media_type="file/dot")
+    if format == OutputType.text:
+        return PlainTextResponse(subtree_to_string(tree))
+    # if format == OutputType.json:
+    return JSONResponse(tree)
+
+
+@router.get(
+    path="/resolve/{entity_id:path}",
+    name="resolve",
+    description="Resolve metadata and Trust Marks for an entity, given a trust anchor and entity type.",
+)
+async def resolve(
+    entity_id: Annotated[
+        HttpUrl,
+        Path(
+            description="Entity ID to resolve.",
+        ),
+    ],
+    ta: Annotated[HttpUrl, Query(description="Trust anchor ID to use for resolving.")],
+    entity_type: Annotated[EntityType, Query()],
+):
+    metadata = core.resolve_entity(
+        entity_id.unicode_string(), ta.unicode_string(), entity_type.value
+    )
+    return metadata
+
+
+@router.get(
+    path="/discovery/{entity_id:path}",
+    name="discovery",
+    description="Discover all OPs in the federation available to a given RP. If no trust anchor is specified, all possible trust anchors will be used.",
+)
+async def discovery(
+    entity_id: Annotated[
+        HttpUrl,
+        Path(
+            description="Relying Party ID.",
+        ),
+    ],
+    ta: Annotated[
+        list[HttpUrl],
+        Query(
+            description="Trust anchor ID to use for OP discovery (multiple TAs possible)."
+        ),
+    ] = [],
+):
+    ops = core.discover(
+        entity_id.unicode_string(), [ta_item.unicode_string() for ta_item in ta]
+    )
+    return ops
---
+++ packages/ofcli/ofcli-0.6.3.tar.gz/ofcli-0.6.3/src/ofcli/core.py
@@ -0,0 +1,154 @@
+"""
+API for OIDC Federation exploration.
+"""
+import pygraphviz
+from ofcli import utils, trustchain, fedtree
+from ofcli.utils import URL
+from ofcli.message import EntityStatement, Metadata
+from ofcli.exceptions import InternalException
+
+
+def get_entity_configuration(entity_id: str, verify: bool = False) -> dict:
+    """Fetches an entity configuration from a given entity ID.
+
+    :param entity_id: The entity ID to fetch the entity configuration from (URL).
+    :param verify: Whether to verify the entity configuration. Defaults to False.
+    :return: The decoded entity configuration as a dictionary.
+    """
+    configuration = utils.get_payload(
+        utils.get_self_signed_entity_configuration(URL(entity_id))
+    )
+    if verify:
+        EntityStatement(**configuration).verify()
+    return configuration
+
+
+def get_entity_metadata(entity_id: str, verify: bool = False) -> dict:
+    """Fetches the OIDC metadata of a given entity ID.
+
+    :param entity_id: The entity ID to fetch the OIDC metadata from (URL).
+    :param verify: Whether to verify the metadata. Defaults to False.
+    :return: The decoded OIDC metadata as a dictionary.
+    """
+    metadata = utils.get_payload(
+        utils.get_self_signed_entity_configuration(URL(entity_id))
+    ).get("metadata", None)
+    if not metadata:
+        raise InternalException("No metadata found in entity configuration.")
+    if verify:
+        Metadata(**metadata).verify()
+    return metadata
+
+
+def get_entity_jwks(entity_id: str) -> dict:
+    """Fetches the federation JWKS of a given entity ID.
+
+    :param entity_id: The entity ID to fetch the JWKS from (URL).
+    :return: The decoded JWKS as a dictionary.
+    """
+    jwks = utils.get_payload(
+        utils.get_self_signed_entity_configuration(URL(entity_id))
+    ).get("jwks")
+    if not jwks:
+        raise InternalException("No jwks found in entity configuration.")
+    return jwks
+
+
+def get_trustchains(
+    entity_id: str, trust_anchors: list[str] = [], export_graph: bool = False
+) -> tuple[list[trustchain.TrustChain], pygraphviz.AGraph | None]:
+    """Builds all trustchains for a given entity ID.
+
+    :param entity_id: The entity ID to build the trustchains for (URL).
+    :param trust_anchors: The trust anchor to use for building the trustchains. If not set (empty list), all possible trust chains until the root are built.
+    :param export_graph: Whether to export the trustchains as a graph. Defaults to False.
+    :return: A tuple containing the trustchains as a list of lists of entity IDs, and the graph representation of the trustchains.
+    """
+    resolver = trustchain.TrustChainResolver(
+        URL(entity_id), [URL(ta) for ta in trust_anchors]
+    )
+    resolver.resolve()
+    graph = None
+    if export_graph:
+        graph = resolver.to_graph()
+    return resolver.chains(), graph
+
+
+def fetch_entity_statement(entity_id: str, issuer: str) -> dict:
+    return utils.get_payload(utils.fetch_entity_statement(URL(entity_id), URL(issuer)))
+
+
+def list_subordinates(
+    entity_id: str,
+    entity_type: str | None = None,
+    trust_marked: bool = False,
+    trust_mark_id: str | None = None,
+) -> list[str]:
+    entity = EntityStatement(
+        **utils.get_payload(utils.get_self_signed_entity_configuration(URL(entity_id)))
+    )
+    return utils.get_subordinates(entity, entity_type, trust_marked, trust_mark_id)
+
+
+def discover(entity_id: str, tas: list[str] = []) -> list[str]:
+    """Discovers all OPs available in the federation to a given RP.
+
+    :param entity_id: The entity ID to discover the OPs for (URL).
+    :param tas: The trust anchors to use for discovery. Defaults to [].
+    :return: The discovered OPs.
+    """
+    metadata = get_entity_metadata(entity_id=entity_id)
+    # check if it is an openid_relying_party
+    if not metadata.get("openid_relying_party"):
+        raise InternalException("Entity is not an OpenID Relying Party.")
+    # if no trust anchors are given, infer them from building the trustchains
+    trust_anchors = []
+    if len(tas) == 0:
+        resolver = trustchain.TrustChainResolver(URL(entity_id), [])
+        resolver.resolve()
+        chains = resolver.chains()
+        if len(chains) == 0:
+            raise InternalException("Could not find any trust anchors.")
+        for chain in chains:
+            trust_anchors.append(chain.get_trust_anchor())
+        # filter duplicates
+        trust_anchors = list(set(trust_anchors))
+    else:
+        trust_anchors = [URL(ta) for ta in tas]
+    return fedtree.discover_ops(trust_anchors)
+
+
+def subtree(
+    entity_id: str, export_graph: bool = False
+) -> tuple[dict, pygraphviz.AGraph | None]:
+    """Builds the entire federation subtree for given entity_id as root.
+
+    :param entity_id: The entity ID to use as root for the subtree (URL)
+    :param export_graph: Whether to export the subtree as a graph. Defaults to False.
+    :return: The subtree serialized as a dict.
+    """
+    subtree = fedtree.FedTree(
+        utils.get_self_signed_entity_configuration(URL(entity_id))
+    )
+    subtree.discover()
+    graph = None
+    if export_graph:
+        graph = subtree.to_graph()
+    return subtree.serialize(), graph
+
+
+def resolve_entity(entity_id: str, ta: str, entity_type: str) -> dict:
+    """Resolves an entity ID's metadata given a trust anchor.
+
+    :param entity_id: The entity ID to resolve (URL).
+    :param ta: The trust anchor to use for resolving the entity ID.
+    :param entity_type: The entity type to resolve.
+    :return: The resolved metadata.
+    """
+    resolver = trustchain.TrustChainResolver(URL(entity_id), [URL(ta)])
+    resolver.resolve()
+    chains = resolver.chains()
+    if len(chains) == 0:
+        raise InternalException("Could not build trustchain to trust anchor.")
+    # TODO: select the shortest chain if more than one
+    return chains[0].get_metadata(entity_type)

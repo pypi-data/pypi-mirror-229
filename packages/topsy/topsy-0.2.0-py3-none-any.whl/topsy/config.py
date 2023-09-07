840081cb660638ca31dcb0d78364b6bd89829ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:16:47 +0000

    Add package topsy/0.2.0/topsy-0.2.0-py3-none-any.whl

---
+++ packages/topsy/topsy-0.2.0-py3-none-any.whl/topsy/config.py
@@ -0,0 +1,11 @@
+DEFAULT_RESOLUTION = 500
+DEFAULT_COLORMAP = 'twilight_shifted'
+
+DEFAULT_SCALE = 200.0 # viewport width in kpc
+
+TARGET_FPS = 30 # will use downsampling to achieve this
+FULL_RESOLUTION_RENDER_AFTER = 0.3 # inactivity seconds to wait before rendering without downs
+STATUS_LINE_UPDATE_INTERVAL = 0.2 # seconds
+
+COLORBAR_ASPECT_RATIO = 0.15
+COLORMAP_NUM_SAMPLES = 1000

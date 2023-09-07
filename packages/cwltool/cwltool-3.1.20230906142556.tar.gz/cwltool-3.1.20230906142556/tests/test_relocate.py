aadeee70aa440dd23eee0bc6e7d1627c6df8b1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:15:41 +0000

    Add package cwltool/3.1.20230906142556/cwltool-3.1.20230906142556.tar.gz

---
+++ packages/cwltool/cwltool-3.1.20230906142556.tar.gz/cwltool-3.1.20230906142556/tests/test_relocate.py
@@ -0,0 +1,71 @@
+import json
+import sys
+from pathlib import Path
+
+from cwltool.main import main
+
+from .util import get_data, needs_docker
+
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+
+@needs_docker
+def test_for_910(tmp_path: Path) -> None:
+    assert main(["--outdir", str(tmp_path), get_data("tests/wf/910.cwl")]) == 0
+    assert main(["--outdir", str(tmp_path), get_data("tests/wf/910.cwl")]) == 0
+
+
+@needs_docker
+def test_for_conflict_file_names(tmp_path: Path) -> None:
+    stream = StringIO()
+
+    assert (
+        main(
+            ["--debug", "--outdir", str(tmp_path), get_data("tests/wf/conflict.cwl")],
+            stdout=stream,
+        )
+        == 0
+    )
+
+    out = json.loads(stream.getvalue())
+    assert out["b1"]["basename"] == out["b2"]["basename"]
+    assert out["b1"]["location"] != out["b2"]["location"]
+    assert Path(out["b1"]["path"]).exists()
+    assert Path(out["b2"]["path"]).exists()
+
+
+def test_for_conflict_file_names_nodocker(tmp_path: Path) -> None:
+    stream = StringIO()
+
+    assert (
+        main(
+            ["--debug", "--outdir", str(tmp_path), get_data("tests/wf/conflict.cwl")],
+            stdout=stream,
+        )
+        == 0
+    )
+
+    out = json.loads(stream.getvalue())
+    assert out["b1"]["basename"] == out["b2"]["basename"]
+    assert out["b1"]["location"] != out["b2"]["location"]
+    assert Path(out["b1"]["path"]).exists()
+    assert Path(out["b2"]["path"]).exists()
+
+
+def test_relocate_symlinks(tmp_path: Path) -> None:
+    assert (
+        main(
+            [
+                "--debug",
+                "--outdir",
+                get_data("tests/reloc") + "/dir2",
+                get_data("tests/reloc/test.cwl"),
+                "--inp",
+                get_data("tests/reloc") + "/dir2",
+            ]
+        )
+        == 0
+    )

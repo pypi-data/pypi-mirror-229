23c5fa9381a96abb2dfe178890c9787b98060936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:09:17 +0000

    Add package qBitrr2/3.7.3/qBitrr2-3.7.3-py3-none-any.whl

---
+++ packages/qBitrr2/qBitrr2-3.7.3-py3-none-any.whl/qBitrr/bundled_data.py
@@ -0,0 +1,6 @@
+version = "3.7.3"
+git_hash = "b05a96e"
+license_text = (
+    "Licence can be found on:\n\nhttps://github.com/Feramance/qBitrr/blob/master/LICENSE"
+)
+patched_version = f"{version}-{git_hash}"

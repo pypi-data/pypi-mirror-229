20b86c420e27151fd7bf9cdc979ff4023599e9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:29:47 +0000

    Add package fovehicle/0.2.0/fovehicle-0.2.0-py3-none-any.whl

---
+++ packages/fovehicle/fovehicle-0.2.0-py3-none-any.whl/fovehicle/engine.py
@@ -0,0 +1,30 @@
+import numpy
+from fofunction import Curve
+
+# TODO engine configuration from JSON file
+class Engine(object):
+    '''
+    Class engine offers support for engine calculation and estimation.
+    '''
+
+    def __init__(self,n=[600,1000,2000,4000,6000],t=[100,500,1000,800,100]):
+        super().__init__()
+        self._n = n
+        self._t = t
+        self.t = Curve(n, t)
+
+    def t_max(self):
+        return max(self._t)
+
+    def n_max(self):
+        return max(self._n)
+
+    def torque(self,speed,limit=100):
+        '''
+        Return the torque at an engine speed with a max percentage torque ( 100% is full torque)
+        :param speed: engine speed in rpm
+        :param limit: maximum percent torque from curve
+        :return: engine torque in Nm
+        '''
+        return self.t.y(speed)*limit/100
+

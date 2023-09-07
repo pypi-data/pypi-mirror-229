994f54a31061d61bcbbbee8be00436c953e0b1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:31:36 +0000

    Add package redisvl/0.0.4/redisvl-0.0.4.tar.gz

---
+++ packages/redisvl/redisvl-0.0.4.tar.gz/redisvl-0.0.4/redisvl/llmcache/semantic.py
@@ -0,0 +1,237 @@
+from typing import List, Optional, Union
+
+from redis.commands.search.field import Field, VectorField
+
+from redisvl.index import SearchIndex
+from redisvl.llmcache.base import BaseLLMCache
+from redisvl.query import VectorQuery
+from redisvl.utils.utils import array_to_buffer
+from redisvl.vectorize.base import BaseVectorizer
+from redisvl.vectorize.text import HFTextVectorizer
+
+
+class SemanticCache(BaseLLMCache):
+    """Cache for Large Language Models."""
+
+    # TODO allow for user to change default fields
+    _vector_field_name: str = "prompt_vector"
+    _default_fields: List[Field] = [
+        VectorField(
+            _vector_field_name,
+            "FLAT",
+            {"DIM": 768, "TYPE": "FLOAT32", "DISTANCE_METRIC": "COSINE"},
+        ),
+    ]
+
+    def __init__(
+        self,
+        index_name: str = "cache",
+        prefix: str = "llmcache",
+        threshold: float = 0.9,
+        ttl: Optional[int] = None,
+        vectorizer: BaseVectorizer = HFTextVectorizer(
+            "sentence-transformers/all-mpnet-base-v2"
+        ),
+        redis_url: str = "redis://localhost:6379",
+        connection_args: Optional[dict] = None,
+        index: Optional[SearchIndex] = None,
+    ):
+        """Semantic Cache for Large Language Models.
+
+        Args:
+            index_name (str, optional): The name of the index. Defaults to "cache".
+            prefix (str, optional): The prefix for the index. Defaults to "llmcache".
+            threshold (float, optional): Semantic threshold for the cache. Defaults to 0.9.
+            ttl (Optional[int], optional): The TTL for the cache. Defaults to None.
+            vectorizer (BaseVectorizer, optional): The vectorizer for the cache.
+                Defaults to HFTextVectorizer("sentence-transformers/all-mpnet-base-v2").
+            redis_url (str, optional): The redis url. Defaults to "redis://localhost:6379".
+            connection_args (Optional[dict], optional): The connection arguments for the redis client. Defaults to None.
+            index (Optional[SearchIndex], optional): The underlying search index to use for the semantic cache. Defaults to None.
+
+        Raises:
+            ValueError: If the threshold is not between 0 and 1.
+            ValueError: If the index name or prefix is not supplied when constructing index manually.
+        """
+        self._ttl = ttl
+        self._vectorizer = vectorizer
+        self.set_threshold(threshold)
+        connection_args = connection_args or {}
+
+        if not index:
+            if index_name and prefix:
+                index = SearchIndex(
+                    name=index_name, prefix=prefix, fields=self._default_fields
+                )
+                index.connect(url=redis_url, **connection_args)
+            else:
+                raise ValueError(
+                    "Index name and prefix must be provided if not constructing from an existing index."
+                )
+
+        # create index if non-existent
+        if not index.exists():
+            index.create()
+
+        self._index = index
+
+    @classmethod
+    def from_index(cls, index: SearchIndex, **kwargs):
+        """Create a SemanticCache from a pre-existing SearchIndex.
+
+        Args:
+            index (SearchIndex): The SearchIndex object to use as the backbone of the cache.
+
+        Returns:
+            SemanticCache: A SemanticCache object.
+        """
+        return cls(index=index, **kwargs)
+
+    @property
+    def ttl(self) -> Optional[int]:
+        """Returns the TTL for the cache.
+
+        Returns:
+            Optional[int]: The TTL for the cache.
+        """
+        return self._ttl
+
+    def set_ttl(self, ttl: int):
+        """Sets the TTL for the cache.
+
+        Args:
+            ttl (int): The TTL for the cache.
+
+        Raises:
+            ValueError: If the TTL is not an integer.
+        """
+        self._ttl = int(ttl)
+
+    @property
+    def index(self) -> SearchIndex:
+        """Returns the index for the cache.
+
+        Returns:
+            SearchIndex: The index for the cache.
+        """
+        return self._index
+
+    @property
+    def threshold(self) -> float:
+        """Returns the threshold for the cache."""
+        return self._threshold
+
+    def set_threshold(self, threshold: float):
+        """Sets the threshold for the cache.
+
+        Args:
+            threshold (float): The threshold for the cache.
+
+        Raises:
+            ValueError: If the threshold is not between 0 and 1.
+        """
+        if not 0 <= float(threshold) <= 1:
+            raise ValueError("Threshold must be between 0 and 1.")
+        self._threshold = float(threshold)
+
+    def clear(self):
+        """Clear the LLMCache of all keys in the index"""
+        client = self._index.client
+        if client:
+            with client.pipeline(transaction=False) as pipe:
+                for key in client.scan_iter(match=f"{self._index._prefix}:*"):
+                    pipe.delete(key)
+                pipe.execute()
+        else:
+            raise RuntimeError("LLMCache is not connected to a Redis instance.")
+
+    def check(
+        self,
+        prompt: Optional[str] = None,
+        vector: Optional[List[float]] = None,
+        num_results: int = 1,
+        fields: List[str] = ["response"],
+    ) -> List[str]:
+        """Checks whether the cache contains the specified prompt or vector.
+
+        Args:
+            prompt (Optional[str], optional): The prompt to check. Defaults to None.
+            vector (Optional[List[float]], optional): The vector to check. Defaults to None.
+            num_results (int, optional): The number of results to return. Defaults to 1.
+            fields (List[str], optional): The fields to return. Defaults to ["response"].
+
+        Raises:
+            ValueError: If neither prompt nor vector is specified.
+
+        Returns:
+            List[str]: The response(s) if the cache contains the prompt or vector.
+        """
+        if not prompt and not vector:
+            raise ValueError("Either prompt or vector must be specified.")
+
+        if not vector:
+            vector = self._vectorizer.embed(prompt)  # type: ignore
+
+        v = VectorQuery(
+            vector=vector,
+            vector_field_name=self._vector_field_name,
+            return_fields=fields,
+            num_results=num_results,
+            return_score=True,
+        )
+
+        cache_hits: List[str] = []
+        results = self._index.search(v.query, query_params=v.params)
+        for result in results.docs:
+            sim = similarity(result["vector_distance"])
+            if sim > self.threshold:
+                self._refresh_ttl(result["id"])
+                cache_hits.append(
+                    result["response"]
+                )  # TODO - in the future what do we actually want to return here?
+        return cache_hits
+
+    def store(
+        self,
+        prompt: str,
+        response: str,
+        vector: Optional[List[float]] = None,
+        metadata: Optional[dict] = {},
+    ) -> None:
+        """Stores the specified key-value pair in the cache along with metadata.
+
+        Args:
+            prompt (str): The prompt to store.
+            response (str): The response to store.
+            vector (Optional[List[float]], optional): The vector to store. Defaults to None.
+            metadata (Optional[dict], optional): The metadata to store. Defaults to {}.
+
+        Raises:
+            ValueError: If neither prompt nor vector is specified.
+        """
+        # TODO - foot gun for schema mismatch if user has a different index
+        vector = vector or self._vectorizer.embed(prompt)
+        payload = {
+            "id": self.hash_input(prompt),
+            "prompt": prompt,
+            "response": response,
+            self._vector_field_name: array_to_buffer(vector),
+        }
+        if metadata:
+            payload.update(metadata)
+
+        # Load LLMCache entry with TTL
+        self._index.load([payload], ttl=self._ttl, key_field="id")
+
+    def _refresh_ttl(self, key: str):
+        """Refreshes the TTL for the specified key."""
+        client = self._index.client
+        if client:
+            if self.ttl:
+                client.expire(key, self.ttl)
+        else:
+            raise RuntimeError("LLMCache is not connected to a Redis instance.")
+
+
+def similarity(distance: Union[float, str]) -> float:
+    return 1 - float(distance)
---
+++ packages/redisvl/redisvl-0.0.4.tar.gz/redisvl-0.0.4/redisvl/query/filter.py
@@ -0,0 +1,488 @@
+from enum import Enum
+from functools import wraps
+from typing import Any, Callable, Dict, List, Optional, Union
+
+from redisvl.utils.utils import TokenEscaper
+
+# disable mypy error for dunder method overrides
+# mypy: disable-error-code="override"
+
+
+class FilterOperator(Enum):
+    EQ = 1
+    NE = 2
+    LT = 3
+    GT = 4
+    LE = 5
+    GE = 6
+    OR = 7
+    AND = 8
+    LIKE = 9
+    IN = 10
+
+
+class FilterField:
+    escaper: TokenEscaper = TokenEscaper()
+    OPERATORS: Dict[FilterOperator, str] = {}
+
+    def __init__(self, field: str):
+        self._field = field
+        self._value: Any = None
+        self._operator: FilterOperator = FilterOperator.EQ
+
+    def equals(self, other: "FilterField") -> bool:
+        if not isinstance(other, type(self)):
+            return False
+        return (self._field == other._field) and (self._value == other._value)
+
+    def _set_value(self, val: Any, val_type: type, operator: FilterOperator):
+        # check that the operator is supported by this class
+        if operator not in self.OPERATORS:
+            raise ValueError(
+                f"Operator {operator} not supported by {self.__class__.__name__}. "
+                + f"Supported operators are {self.OPERATORS.values()}"
+            )
+
+        if not isinstance(val, val_type):
+            raise TypeError(
+                f"Right side argument passed to operator {self.OPERATORS[operator]} "
+                f"with left side "
+                f"argument {self.__class__.__name__} must be of type {val_type}"
+            )
+        self._value = val
+        self._operator = operator
+
+
+def check_operator_misuse(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(instance: Any, *args: List[Any], **kwargs: Dict[str, Any]) -> Any:
+        # Extracting 'other' from positional arguments or keyword arguments
+        other = kwargs.get("other") if "other" in kwargs else None
+        if not other:
+            for arg in args:
+                if isinstance(arg, type(instance)):
+                    other = arg
+                    break
+
+        if isinstance(other, type(instance)):
+            raise ValueError(
+                "Equality operators are overridden for FilterExpression creation. Use "
+                ".equals() for equality checks"
+            )
+        return func(instance, *args, **kwargs)
+
+    return wrapper
+
+
+class Tag(FilterField):
+    """A Tag is a FilterField representing a tag in a Redis index."""
+
+    OPERATORS: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "==",
+        FilterOperator.NE: "!=",
+        FilterOperator.IN: "==",
+    }
+
+    OPERATOR_MAP: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "@%s:{%s}",
+        FilterOperator.NE: "(-@%s:{%s})",
+        FilterOperator.IN: "@%s:{%s}",
+    }
+
+    def __init__(self, field: str):
+        """Create a Tag FilterField
+
+        Args:
+            field (str): The name of the tag field in the index to be queried against
+        """
+        super().__init__(field)
+
+    def _set_tag_value(self, other: Union[List[str], str], operator: FilterOperator):
+        if isinstance(other, list):
+            if not all(isinstance(tag, str) for tag in other):
+                raise ValueError("All tags must be strings")
+        else:
+            other = [other]
+        self._set_value(other, list, operator)
+
+    @check_operator_misuse
+    def __eq__(self, other: Union[List[str], str]) -> "FilterExpression":
+        """Create a Tag equality filter expression
+
+        Args:
+            other (Union[List[str], str]): The tag(s) to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Tag
+            >>> filter = Tag("brand") == "nike"
+        """
+        self._set_tag_value(other, FilterOperator.EQ)
+        return FilterExpression(str(self))
+
+    @check_operator_misuse
+    def __ne__(self, other) -> "FilterExpression":
+        """Create a Tag inequality filter expression
+
+        Args:
+            other (Union[List[str], str]): The tag(s) to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Tag
+            >>> filter = Tag("brand") != "nike"
+        """
+        self._set_tag_value(other, FilterOperator.NE)
+        return FilterExpression(str(self))
+
+    @property
+    def _formatted_tag_value(self) -> str:
+        return "|".join([self.escaper.escape(tag) for tag in self._value])
+
+    def __str__(self) -> str:
+        """Return the Redis Query syntax for a Tag filter expression"""
+        return self.OPERATOR_MAP[self._operator] % (
+            self._field,
+            self._formatted_tag_value,
+        )
+
+
+class Geo(FilterField):
+    """A Geo is a FilterField representing a geographic (lat/lon)
+    field in a Redis index.
+
+    """
+
+    OPERATORS: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "==",
+        FilterOperator.NE: "!=",
+    }
+    OPERATOR_MAP: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "@%s:[%f %f %i %s]",
+        FilterOperator.NE: "(-@%s:[%f %f %i %s])",
+    }
+
+    @check_operator_misuse
+    def __eq__(self, other) -> "FilterExpression":
+        """Create a Geographic equality filter expression
+
+        Args:
+            other (GeoSpec): The geographic spec to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Geo, GeoRadius
+            >>> filter = Geo("location") == GeoRadius(-122.4194, 37.7749, 1, unit="m")
+        """
+        self._set_value(other, GeoSpec, FilterOperator.EQ)
+        return FilterExpression(str(self))
+
+    @check_operator_misuse
+    def __ne__(self, other) -> "FilterExpression":
+        """Create a Geographic inequality filter expression
+
+        Args:
+            other (GeoSpec): The geographic spec to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Geo, GeoRadius
+            >>> filter = Geo("location") != GeoRadius(-122.4194, 37.7749, 1, unit="m")
+        """
+        self._set_value(other, GeoSpec, FilterOperator.NE)
+        return FilterExpression(str(self))
+
+    def __str__(self) -> str:
+        """Return the Redis Query syntax for a Geographic filter expression"""
+        if not self._value:
+            raise ValueError(
+                f"Operator must be used before calling __str__. Operators are "
+                f"{self.OPERATORS.values()}"
+            )
+
+        return self.OPERATOR_MAP[self._operator] % (
+            self._field,
+            *self._value.get_args(),
+        )
+
+
+class GeoSpec:
+    GEO_UNITS = ["m", "km", "mi", "ft"]
+
+    # class for the operand for FilterExpressions with Geo
+    def __init__(self, longitude: float, latitude: float, unit: str = "km"):
+        if unit.lower() not in self.GEO_UNITS:
+            raise ValueError(f"Unit must be one of {self.GEO_UNITS}")
+        self._longitude = longitude
+        self._latitude = latitude
+        self._unit = unit.lower()
+
+
+class GeoRadius(GeoSpec):
+    """A GeoRadius is a GeoSpec representing a geographic radius"""
+
+    def __init__(
+        self,
+        longitude: float,
+        latitude: float,
+        radius: int = 1,
+        unit: str = "km",
+    ):
+        """Create a GeoRadius specification (GeoSpec)
+
+        Args:
+            longitude (float): The longitude of the center of the radius.
+            latitude (float): The latitude of the center of the radius.
+            radius (int, optional): The radius of the circle. Defaults to 1.
+            unit (str, optional): The unit of the radius. Defaults to "km".
+
+        Raises:
+            ValueError: If the unit is not one of "m", "km", "mi", or "ft".
+
+        """
+        super().__init__(longitude, latitude, unit)
+        self._radius = radius
+
+    def get_args(self) -> List[Union[float, int, str]]:
+        return [self._longitude, self._latitude, self._radius, self._unit]
+
+
+class Num(FilterField):
+    """A Num is a FilterField representing a numeric field in a Redis index."""
+
+    OPERATORS: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "==",
+        FilterOperator.NE: "!=",
+        FilterOperator.LT: "<",
+        FilterOperator.GT: ">",
+        FilterOperator.LE: "<=",
+        FilterOperator.GE: ">=",
+    }
+    OPERATOR_MAP: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "@%s:[%i %i]",
+        FilterOperator.NE: "(-@%s:[%i %i])",
+        FilterOperator.GT: "@%s:[(%i +inf]",
+        FilterOperator.LT: "@%s:[-inf (%i]",
+        FilterOperator.GE: "@%s:[%i +inf]",
+        FilterOperator.LE: "@%s:[-inf %i]",
+    }
+
+    def __str__(self) -> str:
+        """Return the Redis Query syntax for a Numeric filter expression"""
+        if not self._value:
+            raise ValueError(
+                f"Operator must be used before calling __str__. Operators are "
+                f"{self.OPERATORS.values()}"
+            )
+
+        if self._operator == FilterOperator.EQ or self._operator == FilterOperator.NE:
+            return self.OPERATOR_MAP[self._operator] % (
+                self._field,
+                self._value,
+                self._value,
+            )
+        else:
+            return self.OPERATOR_MAP[self._operator] % (self._field, self._value)
+
+    def __eq__(self, other: str) -> "FilterExpression":
+        """Create a Numeric equality filter expression
+
+        Args:
+            other (int): The value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Num
+            >>> filter = Num("zipcode") == 90210
+        """
+        self._set_value(other, int, FilterOperator.EQ)
+        return FilterExpression(str(self))
+
+    def __ne__(self, other: str) -> "FilterExpression":
+        """Create a Numeric inequality filter expression
+
+        Args:
+            other (int): The value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Num
+            >>> filter = Num("zipcode") != 90210
+        """
+        self._set_value(other, int, FilterOperator.NE)
+        return FilterExpression(str(self))
+
+    def __gt__(self, other: str) -> "FilterExpression":
+        """Create a Numeric greater than filter expression
+
+        Args:
+            other (int): The value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Num
+            >>> filter = Num("age") > 18
+        """
+        self._set_value(other, int, FilterOperator.GT)
+        return FilterExpression(str(self))
+
+    def __lt__(self, other: str) -> "FilterExpression":
+        """Create a Numeric less than filter expression
+
+        Args:
+            other (int): The value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Num
+            >>> filter = Num("age") < 18
+        """
+        self._set_value(other, int, FilterOperator.LT)
+        return FilterExpression(str(self))
+
+    def __ge__(self, other: str) -> "FilterExpression":
+        """Create a Numeric greater than or equal to filter expression
+
+        Args:
+            other (int): The value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Num
+            >>> filter = Num("age") >= 18
+        """
+        self._set_value(other, int, FilterOperator.GE)
+        return FilterExpression(str(self))
+
+    def __le__(self, other: str) -> "FilterExpression":
+        """Create a Numeric less than or equal to filter expression
+
+        Args:
+            other (int): The value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Num
+            >>> filter = Num("age") <= 18
+        """
+        self._set_value(other, int, FilterOperator.LE)
+        return FilterExpression(str(self))
+
+
+class Text(FilterField):
+    """A Text is a FilterField representing a text field in a Redis index."""
+
+    OPERATORS: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: "==",
+        FilterOperator.NE: "!=",
+        FilterOperator.LIKE: "%",
+    }
+    OPERATOR_MAP: Dict[FilterOperator, str] = {
+        FilterOperator.EQ: '@%s:"%s"',
+        FilterOperator.NE: '(-@%s:"%s")',
+        FilterOperator.LIKE: "@%s:%s",
+    }
+
+    @check_operator_misuse
+    def __eq__(self, other: str) -> "FilterExpression":
+        """Create a Text equality filter expression
+
+        Args:
+            other (str): The text value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Text
+            >>> filter = Text("job") == "engineer"
+        """
+        self._set_value(other, str, FilterOperator.EQ)
+        return FilterExpression(str(self))
+
+    @check_operator_misuse
+    def __ne__(self, other: str) -> "FilterExpression":
+        """Create a Text inequality filter expression
+
+        Args:
+            other (str): The text value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Text
+            >>> filter = Text("job") != "engineer"
+        """
+        self._set_value(other, str, FilterOperator.NE)
+        return FilterExpression(str(self))
+
+    def __mod__(self, other: str) -> "FilterExpression":
+        """Create a Text like filter expression
+
+        Args:
+            other (str): The text value to filter on.
+
+        Example:
+            >>> from redisvl.query.filter import Text
+            >>> filter = Text("job") % "engineer"
+        """
+        self._set_value(other, str, FilterOperator.LIKE)
+        return FilterExpression(str(self))
+
+    def __str__(self) -> str:
+        if not self._value:
+            raise ValueError(
+                f"Operator must be used before calling __str__. Operators are "
+                f"{self.OPERATORS.values()}"
+            )
+        return self.OPERATOR_MAP[self._operator] % (self._field, self._value)
+
+
+class FilterExpression:
+    """A FilterExpression is a logical expression of FilterFields.
+
+    FilterExpressions can be combined using the & and | operators to create
+    complex logical expressions that evaluate to the Redis Query language.
+
+    This presents an interface by which users can create complex queries
+    without having to know the Redis Query language.
+
+    Filter expressions are not created directly. Instead they are built
+    by combining FilterFields using the & and | operators.
+
+    Examples:
+
+        >>> from redisvl.query.filter import Tag, Num
+        >>> brand_is_nike = Tag("brand") == "nike"
+        >>> price_is_over_100 = Num("price") < 100
+        >>> filter = brand_is_nike & price_is_over_100
+        >>> print(str(filter))
+        (@brand:{nike} @price:[-inf (100)])
+
+    This can be combined with the VectorQuery class to create a query:
+
+        >>> from redisvl.query import VectorQuery
+        >>> v = VectorQuery(
+        ...     vector=[0.1, 0.1, 0.5, ...],
+        ...     vector_field_name="product_embedding",
+        ...     return_fields=["product_id", "brand", "price"],
+        ...     filter_expression=filter,
+        ... )
+    """
+
+    def __init__(
+        self,
+        _filter: Optional[str] = None,
+        operator: Optional[FilterOperator] = None,
+        left: Optional["FilterExpression"] = None,
+        right: Optional["FilterExpression"] = None,
+    ):
+        self._filter = _filter
+        self._operator = operator
+        self._left = left
+        self._right = right
+
+    def __and__(self, other) -> "FilterExpression":
+        return FilterExpression(operator=FilterOperator.AND, left=self, right=other)
+
+    def __or__(self, other) -> "FilterExpression":
+        return FilterExpression(operator=FilterOperator.OR, left=self, right=other)
+
+    def __str__(self) -> str:
+        # top level check that allows recursive calls to __str__
+        if not self._filter and not self._operator:
+            raise ValueError("Improperly initialized FilterExpression")
+
+        if self._operator:
+            operator_str = " | " if self._operator == FilterOperator.OR else " "
+            return f"({str(self._left)}{operator_str}{str(self._right)})"
+
+        # check that base case, the filter is set
+        if not self._filter:
+            raise ValueError("Improperly initialized FilterExpression")
+        return self._filter

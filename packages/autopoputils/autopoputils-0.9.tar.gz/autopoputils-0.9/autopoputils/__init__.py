1c0098df8b269f438ff67cfa00ab6470448c6c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:31:48 +0000

    Add package autopoputils/0.9/autopoputils-0.9.tar.gz

---
+++ packages/autopoputils/autopoputils-0.9.tar.gz/autopoputils-0.9/autopoputils/__init__.py
@@ -0,0 +1,16 @@
+"""autopop: Auto Populate utils."""
+
+__version__ = '0.9'
+
+from .aws_util import get_sm_client, get_s3_client,
+from .aws_util import get_creds, get_secret_key, persist_to_s3
+from .recording_util import get_recording_data
+
+__all__ = [
+    'get_s3_client',
+    'get_sm_client',
+    'get_creds',
+    'get_secret_key',
+    'persist_to_s3',
+    'get_recording_data'
+]

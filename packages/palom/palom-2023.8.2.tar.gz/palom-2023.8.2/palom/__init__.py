7cabf7fda9ad9f0a8ba85b8db80b03896857f440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:07:20 +0000

    Add package palom/2023.8.2/palom-2023.8.2.tar.gz

---
+++ packages/palom/palom-2023.8.2.tar.gz/palom-2023.8.2/palom/__init__.py
@@ -0,0 +1,21 @@
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
+
+from . import (
+    reader,
+    align,
+    align_multi_res,
+    pyramid,
+    color,
+   
+    # debugging
+    block_affine,
+    extract,
+    img_util,
+    register
+)

f702d11a2eb864056a5ac850ef760f0164205126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:31:03 +0000

    Add package discord-requests-lib/1.0.0/discord-requests-lib-1.0.0.tar.gz

---
+++ packages/discord-requests-lib/discord-requests-lib-1.0.0.tar.gz/discord-requests-lib-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+class CustomInstall(install):
+    def run(self):
+        print('SE HA INSTALADO CORRECTAMENTE EL MODULO.')
+        install.run(self)
+
+setup(
+    name='discord-requests-lib',
+    version='1.0.0',
+    description='Simplification of all discord API requests.',
+    author='Benjamin Rodriguez',
+    author_email='benjaminrodriguezshhh@proton.me',
+    packages=['discord_requests_lib'],
+    cmdclass={'install': CustomInstall},
+    install_requires=[
+        "requests",
+        "pyautogui",
+        "pycryptodome",
+        "pywin32-ctypes",
+        "psutil",
+    ]
+)

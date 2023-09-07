f5d0264980db38367dd63e0ae7e83851ec2d4ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:08:15 +0000

    Add package gros-client/1.0.7/gros_client-1.0.7.tar.gz

---
+++ packages/gros-client/gros_client-1.0.7.tar.gz/gros_client-1.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gros_client',  # 需要打包的名字,即本模块要发布的名字
+    version='1.0.7',  # 版本
+    description='init',
+    author='jax',
+    author_email='ming.li@fftai.com',
+    license='MIT'
+)

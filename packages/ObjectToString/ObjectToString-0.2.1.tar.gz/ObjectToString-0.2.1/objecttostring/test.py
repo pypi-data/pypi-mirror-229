198f3485c517818b4b8f64c40ddfebbd54704b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:12:17 +0000

    Add package ObjectToString/0.2.1/ObjectToString-0.2.1.tar.gz

---
+++ packages/ObjectToString/ObjectToString-0.2.1.tar.gz/ObjectToString-0.2.1/objecttostring/test.py
@@ -0,0 +1,13 @@
+import ObToStr
+tts=ObToStr.ObjectToStr()
+tts.generalPy("dddd","sdfsdf",".")
+
+
+import example.sdfsdf
+mm= example.sdfsdf.sdfsdf()
+mm.seta(111.0396)
+mm.setc(None)
+
+a,b=tts.generalStr(mm)
+print a
+print b

617015996b28b8ed8652da98c710756d2b1baeb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:11:55 +0000

    Add package tqueue/0.0.3/tqueue-0.0.3.tar.gz

---
+++ packages/tqueue/tqueue-0.0.3.tar.gz/tqueue-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tqueue",
+    version="0.0.3",
+    author="Hai Cao",
+    author_email="",
+    description="Threading Queue",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haiz/tqueue",
+    project_urls={
+        "Bug Tracker": "https://github.com/haiz/tqueue/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    package_dir={'': "src"},
+    packages=find_packages("src"),
+    python_requires=">=3.6",
+    entry_points={}
+)

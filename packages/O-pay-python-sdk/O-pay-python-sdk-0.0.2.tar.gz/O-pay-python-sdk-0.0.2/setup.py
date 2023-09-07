a3dfdcaca2432b66a98a2e8fbfcca2b804e776b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:12 +0000

    Add package O-pay-python-sdk/0.0.2/O-pay-python-sdk-0.0.2.tar.gz

---
+++ packages/O-pay-python-sdk/O-pay-python-sdk-0.0.2.tar.gz/O-pay-python-sdk-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import setuptools
+    
+setuptools.setup(
+    name = "O-pay-python-sdk",
+    version = "0.0.2",
+    author = "Tomycat",
+    author_email="tangminghong.tom@gmail.com",
+    description="O'pay SDK for python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/TommcyOWO/O-pay-python-sdk",                                         
+    packages=setuptools.find_packages(),     
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3'
+    )

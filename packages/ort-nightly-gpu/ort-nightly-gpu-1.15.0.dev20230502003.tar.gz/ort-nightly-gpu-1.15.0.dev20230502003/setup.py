9c4f2bc5cef8cac1280f75c4e9020338a66fc7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 05:10:07 +0000

    Add package ort-nightly-gpu/1.15.0.dev20230502003/ort-nightly-gpu-1.15.0.dev20230502003.tar.gz

---
+++ packages/ort-nightly-gpu/ort-nightly-gpu-1.15.0.dev20230502003.tar.gz/ort-nightly-gpu-1.15.0.dev20230502003/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name="ort-nightly-gpu",
+      version='1.15.0.dev20230502003',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+

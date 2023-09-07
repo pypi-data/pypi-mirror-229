aacdc3851b3bfb9809afed43b37315d812832f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:11:14 +0000

    Add package TNSALTK/0.1.3/TNSALTK-0.1.3.tar.gz

---
+++ packages/TNSALTK/TNSALTK-0.1.3.tar.gz/TNSALTK-0.1.3/README.md
@@ -0,0 +1,1 @@
+TNSA TLTK is a Tool kit for developing a LLM (Large Language)It is also used in TNSA NGpt 1.3(Expirement)
---
+++ packages/TNSALTK/TNSALTK-0.1.3.tar.gz/TNSALTK-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TNSALTK',
+    version='0.1.3',
+    description='TNSALTK is a module by TNSA which is use in development of LLM',
+    author='TNSA',
+    author_email='thishyakethabimalla@gmail.com',
+    packages=find_packages(),
+)
+
+
+

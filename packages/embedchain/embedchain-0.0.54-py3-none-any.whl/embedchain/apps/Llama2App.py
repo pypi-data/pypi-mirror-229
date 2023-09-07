814b2f8d71bd07c859708280f8cfbf1ff5451ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:08:50 +0000

    Add package embedchain/0.0.54/embedchain-0.0.54-py3-none-any.whl

---
+++ packages/embedchain/embedchain-0.0.54-py3-none-any.whl/embedchain/apps/Llama2App.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from embedchain.apps.CustomApp import CustomApp
+from embedchain.config import CustomAppConfig
+from embedchain.embedder.openai_embedder import OpenAiEmbedder
+from embedchain.helper_classes.json_serializable import register_deserializable
+from embedchain.llm.llama2_llm import Llama2Llm
+from embedchain.vectordb.chroma_db import ChromaDB
+
+
+@register_deserializable
+class Llama2App(CustomApp):
+    """
+    The EmbedChain Llama2App class.
+
+    Methods:
+    add(source, data_type): adds the data from the given URL to the vector db.
+    query(query): finds answer to the given query using vector database and LLM.
+    chat(query): finds answer to the given query using vector database and LLM, with conversation history.
+    """
+
+    def __init__(self, config: CustomAppConfig = None, system_prompt: Optional[str] = None):
+        """
+        :param config: CustomAppConfig instance to load as configuration. Optional.
+        :param system_prompt: System prompt string. Optional.
+        """
+
+        if config is None:
+            config = CustomAppConfig()
+
+        super().__init__(
+            config=config, llm=Llama2Llm(), db=ChromaDB(), embedder=OpenAiEmbedder(), system_prompt=system_prompt
+        )

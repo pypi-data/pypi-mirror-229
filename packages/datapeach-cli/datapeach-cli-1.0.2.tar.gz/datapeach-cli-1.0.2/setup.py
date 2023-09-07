2a70aec53e1134a5320907864791d9104ed99229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 16:21:57 +0000

    Add package datapeach-cli/1.0.2/datapeach-cli-1.0.2.tar.gz

---
+++ packages/datapeach-cli/datapeach-cli-1.0.2.tar.gz/datapeach-cli-1.0.2/setup.py
@@ -0,0 +1,27 @@
+# python setup.py install
+
+from setuptools import setup, find_packages
+
+setup(
+    name='datapeach-cli',
+    version='1.0.2',
+    py_modules=['datapeachcli', 'analysis_usedecorator'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'datapeach_cli': ['templates/*', 'config.json']},
+    install_requires=[
+        'click',
+        'jinja2',
+        'nbformat',
+        'nbconvert',
+        'pandas',
+        'pyyaml',
+        'ipython',
+        'requests',
+        'datapeach_wrapper',
+    ],
+    entry_points='''
+        [console_scripts]
+        datapeach=datapeachcli:cli
+    ''',
+)

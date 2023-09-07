bd8468a0ed74a1456d5a10d864588fe843be0c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:11:53 +0000

    Add package fastutils/0.43.0/fastutils-0.43.0-py3-none-any.whl

---
+++ packages/fastutils/fastutils-0.43.0-py3-none-any.whl/fastutils/jsonutils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+from zenutils.sixutils import *
+from zenutils.jsonutils import *
+
+import zenutils.jsonutils
+__all__ = [] + zenutils.jsonutils.__all__
---
+++ packages/fastutils/fastutils-0.43.0-py3-none-any.whl/fastutils/nameutils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+from zenutils.sixutils import *
+from zenutils.nameutils import *
+
+import zenutils.nameutils
+__all__ = [] + zenutils.nameutils.__all__

a139c18917c31897c0c60b985d6c82e08bd6b345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:04 +0000

    Add package glue-jupyter/0.18.0/glue_jupyter-0.18.0-py3-none-any.whl

---
+++ packages/glue-jupyter/glue_jupyter-0.18.0-py3-none-any.whl/glue_jupyter/common/state_widgets/layer_profile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>
+            <glue-float-field label="line width" suffix="px" :value.sync="glue_state.linewidth" />
+        </div>
+        <div>
+            <v-select label="attribute" :items="attribute_items" v-model="attribute_selected" hide-details />
+        </div>
+        <div>
+            <v-switch label="Plot as steps" v-model="as_steps" />
+        </div>
+    </div>
+</template>

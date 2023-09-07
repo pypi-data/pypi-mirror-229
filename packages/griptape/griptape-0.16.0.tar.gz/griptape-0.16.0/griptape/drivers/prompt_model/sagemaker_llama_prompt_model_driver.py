b24333d0508bf2ffd0d3940c3350d037b46d1185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:08:36 +0000

    Add package griptape/0.16.0/griptape-0.16.0.tar.gz

---
+++ packages/griptape/griptape-0.16.0.tar.gz/griptape-0.16.0/griptape/drivers/prompt_model/sagemaker_llama_prompt_model_driver.py
@@ -0,0 +1,44 @@
+from attr import define, field, Factory
+from transformers import LlamaTokenizerFast
+from griptape.artifacts import TextArtifact
+from griptape.utils import PromptStack
+from griptape.drivers import BasePromptModelDriver
+from griptape.tokenizers import BaseTokenizer, HuggingFaceTokenizer
+
+
+@define
+class SageMakerLlamaPromptModelDriver(BasePromptModelDriver):
+    DEFAULT_MAX_TOKENS = 4000
+
+    tokenizer: BaseTokenizer = field(
+        default=Factory(
+            lambda self: HuggingFaceTokenizer(
+                tokenizer=LlamaTokenizerFast.from_pretrained(
+                    "hf-internal-testing/llama-tokenizer",
+                    model_max_length=self.prompt_driver.max_tokens
+                    if self.prompt_driver.max_tokens else self.DEFAULT_MAX_TOKENS
+                )
+            ),
+            takes_self=True
+        ),
+        kw_only=True
+    )
+
+    def prompt_stack_to_model_input(self, prompt_stack: PromptStack) -> list:
+        return [[
+            {"role": i.role, "content": i.content}
+            for i in prompt_stack.inputs
+        ]]
+
+    def prompt_stack_to_model_params(self, prompt_stack: PromptStack) -> dict:
+        prompt = self.prompt_driver.prompt_stack_to_string(prompt_stack)
+
+        return {
+            "max_new_tokens": self.prompt_driver.max_output_tokens(prompt),
+            "temperature": self.prompt_driver.temperature
+        }
+
+    def process_output(self, output: list[dict]) -> TextArtifact:
+        return TextArtifact(
+            output[0]["generation"]["content"].strip()
+        )

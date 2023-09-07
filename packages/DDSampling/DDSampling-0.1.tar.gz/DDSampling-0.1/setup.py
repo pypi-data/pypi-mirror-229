31e74716611c2577c109ecbc56dc527fc81b4054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:15:54 +0000

    Add package DDSampling/0.1/DDSampling-0.1.tar.gz

---
+++ packages/DDSampling/DDSampling-0.1.tar.gz/DDSampling-0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#@author: DarkSourceOfCode
+from setuptools import setup, find_packages
+
+setup(
+    name='DDSampling',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your project dependencies
+        'numpy'
+    ],
+    author='DarkSourceOfCode',
+    author_email='jbani004@ucr.edu',
+    description='A short description',
+)

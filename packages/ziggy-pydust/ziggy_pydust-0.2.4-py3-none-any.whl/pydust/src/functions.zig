d6c36ca0f1e69e48561e59b73f81082cfc546849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:12:19 +0000

    Add package ziggy-pydust/0.2.4/ziggy_pydust-0.2.4-py3-none-any.whl

---
+++ packages/ziggy-pydust/ziggy_pydust-0.2.4-py3-none-any.whl/pydust/src/functions.zig
@@ -0,0 +1,331 @@
+const std = @import("std");
+const ffi = @import("ffi.zig");
+const tramp = @import("trampoline.zig");
+const py = @import("pydust.zig");
+const PyError = @import("errors.zig").PyError;
+const Type = std.builtin.Type;
+
+const MethodType = enum { STATIC, CLASS, INSTANCE };
+
+pub const Signature = struct {
+    name: []const u8,
+    selfParam: ?type = null,
+    argsParam: ?type = null,
+    returnType: type,
+    nargs: usize = 0,
+    nkwargs: usize = 0,
+};
+
+const reservedNames = .{
+    "__new__",
+    "__init__",
+    "__len__",
+    "__del__",
+    "__buffer__",
+    "__release_buffer__",
+};
+
+/// Parse the arguments of a Zig function into a Pydust function siganture.
+pub fn parseSignature(comptime name: []const u8, comptime func: Type.Fn, comptime SelfTypes: []const type) Signature {
+    var sig = Signature{
+        .returnType = func.return_type orelse @compileError("Pydust functions must always return or error."),
+        .name = name,
+    };
+
+    switch (func.params.len) {
+        2 => {
+            sig.selfParam = func.params[0].type.?;
+            sig.argsParam = func.params[1].type.?;
+        },
+        1 => {
+            const param = func.params[0];
+            if (isSelfArg(param, SelfTypes)) {
+                sig.selfParam = param.type.?;
+            } else {
+                sig.argsParam = param.type.?;
+                checkArgsParam(sig.argsParam.?);
+            }
+        },
+        0 => {},
+        else => @compileError("Pydust function can have at most 2 parameters. A self ptr and a parameters struct."),
+    }
+
+    // Count up the parameters
+    if (sig.argsParam) |p| {
+        sig.nargs = argCount(p);
+        sig.nkwargs = kwargCount(p);
+    }
+
+    return sig;
+}
+
+pub fn argCount(comptime argsParam: type) usize {
+    var n: usize = 0;
+    inline for (@typeInfo(argsParam).Struct.fields) |field| {
+        if (field.default_value == null) {
+            n += 1;
+        }
+    }
+    return n;
+}
+
+pub fn kwargCount(comptime argsParam: type) usize {
+    var n: usize = 0;
+    inline for (@typeInfo(argsParam).Struct.fields) |field| {
+        if (field.default_value != null) {
+            n += 1;
+        }
+    }
+    return n;
+}
+
+pub fn isReserved(comptime name: []const u8) bool {
+    for (reservedNames) |reserved| {
+        if (std.mem.eql(u8, name, reserved)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// Check whether the first parameter of the function is one of the valid "self" types.
+fn isSelfArg(comptime param: Type.Fn.Param, comptime SelfTypes: []const type) bool {
+    for (SelfTypes) |SelfType| {
+        if (param.type.? == SelfType) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn checkArgsParam(comptime Args: type) void {
+    const typeInfo = @typeInfo(Args);
+    if (typeInfo != .Struct) {
+        @compileError("Pydust args must be defined as a struct");
+    }
+
+    const fields = typeInfo.Struct.fields;
+    var kwargs = false;
+    for (fields) |field| {
+        if (field.default_value != null) {
+            kwargs = true;
+        } else {
+            if (kwargs) {
+                @compileError("Args struct cannot have positional fields after keyword (defaulted) fields");
+            }
+        }
+    }
+}
+
+pub fn wrap(comptime func: anytype, comptime sig: Signature, comptime flags: c_int) type {
+    return struct {
+        const doc = docTextSignature(sig);
+
+        /// Return a PyMethodDef for this wrapped function.
+        pub fn aspy() ffi.PyMethodDef {
+            return .{
+                .ml_name = sig.name.ptr ++ "",
+                .ml_meth = if (sig.nkwargs > 0) @ptrCast(&fastcallKwargs) else @ptrCast(&fastcall),
+                .ml_flags = ffi.METH_FASTCALL | flags | if (sig.nkwargs > 0) ffi.METH_KEYWORDS else 0,
+                .ml_doc = &doc,
+            };
+        }
+
+        fn fastcall(
+            pyself: *ffi.PyObject,
+            pyargs: [*]ffi.PyObject,
+            nargs: ffi.Py_ssize_t,
+        ) callconv(.C) ?*ffi.PyObject {
+            const resultObject = internal(
+                .{ .py = pyself },
+                @as([*]py.PyObject, @ptrCast(pyargs))[0..@intCast(nargs)],
+            ) catch return null;
+            return resultObject.py;
+        }
+
+        inline fn internal(pyself: py.PyObject, pyargs: []py.PyObject) PyError!py.PyObject {
+            const self = if (sig.selfParam) |Self| try tramp.Trampoline(Self).unwrap(pyself) else null;
+            const resultTrampoline = tramp.Trampoline(sig.returnType);
+
+            if (sig.argsParam) |Args| {
+                // Create an args struct and populate it with pyargs.
+                var args: Args = undefined;
+                if (argCount(Args) != pyargs.len) {
+                    return py.TypeError.raiseComptimeFmt("expected {d} args", .{argCount(Args)});
+                }
+                inline for (@typeInfo(Args).Struct.fields, 0..) |field, i| {
+                    @field(args, field.name) = try tramp.Trampoline(field.type).unwrap(pyargs[i]);
+                }
+
+                var callArgs = if (sig.selfParam) |_| .{ self, args } else .{args};
+                const result = @call(.always_inline, func, callArgs);
+                return resultTrampoline.wrap(result);
+            } else {
+                var callArgs = if (sig.selfParam) |_| .{self} else .{};
+                const result = @call(.always_inline, func, callArgs);
+                return resultTrampoline.wrap(result);
+            }
+        }
+
+        fn fastcallKwargs(
+            pyself: *ffi.PyObject,
+            pyargs: [*]ffi.PyObject,
+            nargs: ffi.Py_ssize_t,
+            kwnames: ?*ffi.PyObject,
+        ) callconv(.C) ?*ffi.PyObject {
+            const allArgs: [*]py.PyObject = @ptrCast(pyargs);
+            const args = allArgs[0..@intCast(nargs)];
+
+            const nkwargs = if (kwnames) |names| py.len(names) catch return null else 0;
+            const kwargs = allArgs[args.len .. args.len + nkwargs];
+
+            const resultObject = internalKwargs(
+                .{ .py = pyself },
+                args,
+                kwargs,
+                if (kwnames) |names| py.PyTuple.of(.{ .py = names }) catch return null else null,
+            ) catch return null;
+            return resultObject.py;
+        }
+
+        inline fn internalKwargs(pyself: py.PyObject, pyargs: []py.PyObject, pykwargs: []py.PyObject, kwnames: ?py.PyTuple) PyError!py.PyObject {
+            const Args = sig.argsParam.?; // We must have args if we know we have kwargs
+            var args: Args = undefined;
+
+            if (pyargs.len < argCount(Args)) {
+                return py.TypeError.raiseComptimeFmt("expected {d} args", .{argCount(Args)});
+            }
+
+            inline for (@typeInfo(Args).Struct.fields, 0..) |field, i| {
+                if (field.default_value) |def_value| {
+                    // We have a kwarg.
+                    const fieldName = try py.PyString.fromSlice(field.name);
+                    defer fieldName.decref();
+
+                    const defaultValue: *field.type = @alignCast(@ptrCast(@constCast(def_value)));
+
+                    if (kwnames) |names| {
+                        if (try names.contains(fieldName)) {
+                            const idx = try names.index(fieldName);
+                            const arg = try tramp.Trampoline(field.type).unwrap(pykwargs[idx]);
+                            @field(args, field.name) = arg;
+                        } else {
+                            @field(args, field.name) = defaultValue.*;
+                        }
+                    } else {
+                        @field(args, field.name) = defaultValue.*;
+                    }
+                } else {
+                    // We have an arg
+                    const arg = try tramp.Trampoline(field.type).unwrap(pyargs[i]);
+                    @field(args, field.name) = arg;
+                }
+            }
+
+            // Now we loop over the kwnames at runtime and check they all exist in the fieldNames
+            const fieldNames = std.meta.fieldNames(Args);
+            // TODO(ngates): use PySeq iterator when we support it
+            for (0..pykwargs.len) |i| {
+                const names = kwnames orelse @panic("Expected kwnames with non-empty kwargs slice");
+
+                const kwname = try (try py.PyString.of(try names.getItem(i))).asSlice();
+                var exists = false;
+                for (fieldNames) |name| {
+                    if (std.mem.eql(u8, name, kwname)) {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists) {
+                    return py.TypeError.raiseFmt("unexpected kwarg '{s}'", .{kwname});
+                }
+            }
+
+            const self = if (sig.selfParam) |Self| try tramp.Trampoline(Self).unwrap(pyself) else null;
+            var callArgs = if (sig.selfParam) |_| .{ self, args } else .{args};
+            const result = @call(.always_inline, func, callArgs);
+            return tramp.Trampoline(sig.returnType).wrap(result);
+        }
+    };
+}
+
+/// Generate minimal function docstring to populate __text_signature__ function field.
+/// Format is `funcName($self, arg0Name...)\n--\n\n`.
+/// Self arg can be named however but must start with `$`
+fn docTextSignature(comptime sig: Signature) [sigSize(sig):0]u8 {
+    const args = sigArgs(sig) catch @compileError("Too many arguments");
+    const argSize = sigSize(sig);
+
+    var buffer: [argSize:0]u8 = undefined;
+    writeTextSig(sig.name, args, &buffer) catch @compileError("Text signature buffer is too small");
+    return buffer;
+}
+
+fn writeTextSig(name: []const u8, args: []const []const u8, buffer: [:0]u8) !void {
+    var buf = std.io.fixedBufferStream(buffer);
+    const writer = buf.writer();
+    try writer.writeAll(name);
+    try writer.writeByte('(');
+    for (args, 0..) |arg, i| {
+        try writer.writeAll(arg);
+
+        if (i < args.len - 1) {
+            try writer.writeAll(", ");
+        }
+    }
+    try writer.writeAll(")\n--\n\n");
+    buffer[buffer.len] = 0;
+}
+
+fn sigSize(comptime sig: Signature) usize {
+    const args = sigArgs(sig) catch @compileError("Too many arguments");
+    var argSize: u64 = sig.name.len;
+    // Count the size of the output string
+    for (args) |arg| {
+        argSize += arg.len + 2;
+    }
+
+    if (args.len > 0) {
+        argSize -= 2;
+    }
+
+    // The size is the size of all arguments plus the padding after argument list
+    return argSize + 8;
+}
+
+fn sigArgs(comptime sig: Signature) ![]const []const u8 {
+    var sigargs: std.BoundedArray([]const u8, 100) = std.BoundedArray([]const u8, 100).init(0) catch @compileError("OOM");
+    if (sig.selfParam) |_| {
+        try sigargs.append("$self");
+    }
+
+    if (sig.argsParam) |Args| {
+        const fields = @typeInfo(Args).Struct.fields;
+
+        var inKwargs = false;
+        for (fields) |field| {
+            if (field.default_value == null) {
+                // We have an arg
+                try sigargs.append(field.name);
+            } else {
+                // We have a kwarg
+                if (!inKwargs) {
+                    inKwargs = true;
+                    // Marker for end of positional only args
+                    try sigargs.append("/");
+                    // Marker for start of keyword only args
+                    try sigargs.append("*");
+                }
+                try sigargs.append(field.name);
+            }
+        }
+
+        if (!inKwargs) {
+            // Always mark end of positional only args
+            try sigargs.append("/");
+        }
+    }
+
+    return sigargs.constSlice();
+}
---
+++ packages/ziggy-pydust/ziggy_pydust-0.2.4-py3-none-any.whl/pydust/src/pytypes.zig
@@ -0,0 +1,227 @@
+// https://docs.python.org/3/extending/newtypes_tutorial.html
+
+const std = @import("std");
+const ffi = @import("ffi.zig");
+const py = @import("pydust.zig");
+const funcs = @import("functions.zig");
+const PyError = @import("errors.zig").PyError;
+const PyMemAllocator = @import("mem.zig").PyMemAllocator;
+const tramp = @import("trampoline.zig");
+const Type = std.builtin.Type;
+
+pub fn State(comptime definition: type) type {
+    return struct {
+        obj: ffi.PyObject,
+        state: definition,
+    };
+}
+
+/// Wrap a user-defined class struct into a unique struct that itself wraps the trampolined functions.
+pub fn define(comptime class: py.ClassDef) type {
+    return struct {
+        const Self = @This();
+        pub const name: [:0]const u8 = class.name;
+
+        pub const pyName: [:0]const u8 = blk: {
+            const moduleName = py.findContainingModule(class.definition);
+            break :blk moduleName[0..moduleName.len] ++ "." ++ name;
+        };
+
+        // Declare a struct representing an instance of the object.
+        const Instance = State(class.definition);
+
+        const slots = Slots(class.definition, Instance);
+
+        const spec = ffi.PyType_Spec{
+            .name = pyName.ptr,
+            .basicsize = @sizeOf(Instance),
+            .itemsize = 0,
+            .flags = ffi.Py_TPFLAGS_DEFAULT | ffi.Py_TPFLAGS_BASETYPE,
+            .slots = @constCast(slots.slots.ptr),
+        };
+
+        pub fn init(module: py.PyModule) !py.PyType {
+            var basesPtr: ?*ffi.PyObject = null;
+            if (class.bases.len > 0) {
+                const basesTuple = try py.PyTuple.new(class.bases.len);
+                inline for (class.bases, 0..) |base, i| {
+                    const baseType = try module.obj.get(py.getClassName(base));
+                    try basesTuple.setItem(i, baseType);
+                }
+                basesPtr = basesTuple.obj.py;
+            }
+
+            var pytype = ffi.PyType_FromModuleAndSpec(module.obj.py, @constCast(&spec), basesPtr) orelse return PyError.Propagate;
+            return .{ .obj = .{ .py = pytype } };
+        }
+    };
+}
+
+fn Slots(comptime definition: type, comptime Instance: type) type {
+    const empty = ffi.PyType_Slot{ .slot = 0, .pfunc = null };
+
+    return struct {
+        const methods = Methods(definition, Instance);
+
+        /// Slots populated in the PyType
+        pub const slots: [:empty]const ffi.PyType_Slot = blk: {
+            var slots_: [:empty]const ffi.PyType_Slot = &.{};
+
+            if (@hasDecl(definition, "__doc__")) {
+                const doc: [:0]const u8 = @field(definition, "__doc__");
+                slots_ = slots_ ++ .{ffi.PyType_Slot{
+                    .slot = ffi.Py_tp_doc,
+                    .pfunc = @ptrCast(@constCast(doc.ptr)),
+                }};
+            }
+
+            if (@hasDecl(definition, "__new__")) {
+                slots_ = slots_ ++ .{ffi.PyType_Slot{
+                    .slot = ffi.Py_tp_new,
+                    .pfunc = @ptrCast(@constCast(&tp_new)),
+                }};
+            }
+
+            if (@hasDecl(definition, "__del__")) {
+                slots_ = slots_ ++ .{ffi.PyType_Slot{
+                    .slot = ffi.Py_tp_finalize,
+                    .pfunc = @ptrCast(@constCast(&tp_finalize)),
+                }};
+            }
+
+            if (@hasDecl(definition, "__buffer__")) {
+                slots_ = slots_ ++ .{ffi.PyType_Slot{
+                    .slot = ffi.Py_bf_getbuffer,
+                    .pfunc = @ptrCast(@constCast(&bf_getbuffer)),
+                }};
+            }
+
+            if (@hasDecl(definition, "__release_buffer__")) {
+                slots_ = slots_ ++ .{ffi.PyType_Slot{
+                    .slot = ffi.Py_bf_releasebuffer,
+                    .pfunc = @ptrCast(@constCast(&bf_releasebuffer)),
+                }};
+            }
+
+            if (@hasDecl(definition, "__len__")) {
+                slots_ = slots_ ++ .{ffi.PyType_Slot{
+                    .slot = ffi.Py_mp_length,
+                    .pfunc = @ptrCast(@constCast(&mp_length)),
+                }};
+            }
+
+            slots_ = slots_ ++ .{ffi.PyType_Slot{
+                .slot = ffi.Py_tp_methods,
+                .pfunc = @ptrCast(@constCast(&methods.pydefs)),
+            }};
+
+            slots_ = slots_ ++ .{empty};
+
+            break :blk slots_;
+        };
+
+        fn tp_new(subtype: *ffi.PyTypeObject, pyargs: [*c]ffi.PyObject, pykwargs: [*c]ffi.PyObject) callconv(.C) ?*ffi.PyObject {
+            const pyself: *ffi.PyObject = ffi.PyType_GenericAlloc(subtype, 0) orelse return null;
+            // Cast it into a supertype instance. Note: we check at comptime that subclasses of this class
+            // include our own state object as the first field in their struct.
+            const self: *Instance = @ptrCast(pyself);
+
+            // Allow the definition to initialize the state field.
+            self.state = tp_new_internal(
+                if (pyargs) |pa| py.PyTuple.of(.{ .py = pa }) catch return null else null,
+                if (pykwargs) |pk| py.PyDict.of(.{ .py = pk }) catch return null else null,
+            ) catch return null;
+
+            return pyself;
+        }
+
+        fn tp_new_internal(pyargs: ?py.PyTuple, pykwargs: ?py.PyDict) !definition {
+            const sig = funcs.parseSignature("__new__", @typeInfo(@TypeOf(definition.__new__)).Fn, &.{});
+            if (sig.selfParam) |_| @compileError("__new__ must not take a self parameter");
+
+            const Args = sig.argsParam orelse @compileError("__new__ must take an args struct");
+            const args = try tramp.Trampoline(Args).unwrapCallArgs(.{ .args = pyargs, .kwargs = pykwargs });
+
+            return try definition.__new__(args);
+        }
+
+        /// Wrapper for the user's __del__ function.
+        /// Note: tp_del is deprecated in favour of tp_finalize.
+        ///
+        /// See https://docs.python.org/3/c-api/typeobj.html#c.PyTypeObject.tp_finalize.
+        fn tp_finalize(pyself: *ffi.PyObject) void {
+            // The finalize slot shouldn't alter any exception that is currently set.
+            // So it's recommended we save the existing one (if any) and restore it afterwards.
+            // NOTE(ngates): we may want to move this logic to PyErr if it happens more?
+            var error_type: ?*ffi.PyObject = undefined;
+            var error_value: ?*ffi.PyObject = undefined;
+            var error_tb: ?*ffi.PyObject = undefined;
+            ffi.PyErr_Fetch(&error_type, &error_value, &error_tb);
+
+            const instance: *Instance = @ptrCast(pyself);
+            definition.__del__(&instance.state);
+
+            ffi.PyErr_Restore(error_type, error_value, error_tb);
+        }
+
+        fn bf_getbuffer(pyself: *ffi.PyObject, view: *ffi.Py_buffer, flags: c_int) callconv(.C) c_int {
+            // In case of any error, the view.obj field must be set to NULL.
+            view.obj = null;
+
+            const self: *Instance = @ptrCast(pyself);
+            definition.__buffer__(&self.state, @ptrCast(view), flags) catch return -1;
+            return 0;
+        }
+
+        fn bf_releasebuffer(pyself: *ffi.PyObject, view: *ffi.Py_buffer) callconv(.C) void {
+            const self: *Instance = @ptrCast(pyself);
+            return definition.__release_buffer__(&self.state, @ptrCast(view));
+        }
+
+        fn mp_length(pyself: *ffi.PyObject) callconv(.C) isize {
+            const lenFunc = @field(definition, "__len__");
+            const self: *const Instance = @ptrCast(pyself);
+            const result = @as(isize, @intCast(lenFunc(&self.state)));
+            if (@typeInfo(@typeInfo(@TypeOf(lenFunc)).Fn.return_type.?) == .ErrorUnion) {
+                return result catch return -1;
+            }
+            return result;
+        }
+    };
+}
+
+fn Methods(comptime definition: type, comptime Instance: type) type {
+    _ = Instance;
+    const empty = ffi.PyMethodDef{ .ml_name = null, .ml_meth = null, .ml_flags = 0, .ml_doc = null };
+
+    return struct {
+        const defs: []const type = blk: {
+            var defs_: []const type = &.{};
+
+            for (@typeInfo(definition).Struct.decls) |decl| {
+                const value = @field(definition, decl.name);
+                const typeInfo = @typeInfo(@TypeOf(value));
+
+                // For now, we skip non-function declarations.
+                if (typeInfo != .Fn or funcs.isReserved(decl.name)) {
+                    continue;
+                }
+
+                // The valid types for a "self" parameter are either the module state struct (definition), or a py.PyModule.
+                const sig = funcs.parseSignature(decl.name, typeInfo.Fn, &.{ py.PyObject, *definition, *const definition });
+                const def = funcs.wrap(value, sig, 0);
+                defs_ = defs_ ++ .{def};
+            }
+
+            break :blk defs_;
+        };
+
+        pub const pydefs: [defs.len:empty]ffi.PyMethodDef = blk: {
+            var pydefs_: [defs.len:empty]ffi.PyMethodDef = undefined;
+            for (0..defs.len) |i| {
+                pydefs_[i] = defs[i].aspy();
+            }
+            break :blk pydefs_;
+        };
+    };
+}

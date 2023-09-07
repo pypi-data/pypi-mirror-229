7fdf5ace56cd50de5d0fe9d539aac978922393d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 16:22:17 +0000

    Add package cekit/4.9.0/cekit-4.9.0.tar.gz

---
+++ packages/cekit/cekit-4.9.0.tar.gz/cekit-4.9.0/cekit/builders/buildah.py
@@ -0,0 +1,57 @@
+import logging
+import os
+
+from cekit.builder import Builder
+from cekit.cekit_types import DependencyDefinition
+from cekit.tools import run_wrapper
+
+LOGGER = logging.getLogger("cekit")
+
+
+class BuildahBuilder(Builder):
+    """This class represents buildah builder in build-using-dockerfile mode."""
+
+    def __init__(self, params):
+        super(BuildahBuilder, self).__init__("buildah", params)
+
+    @staticmethod
+    def dependencies(params=None) -> DependencyDefinition:
+        deps = {}
+
+        deps["buildah"] = {"package": "buildah", "executable": "buildah"}
+
+        return deps
+
+    def run(self) -> None:
+        """Build container image using buildah."""
+        tags = self.params.tags
+        cmd = ["buildah", "build-using-dockerfile"]
+
+        if not tags:
+            tags = self.generator.get_tags()
+
+        if not self.params.no_squash:
+            cmd.append("--squash")
+
+        if self.params.pull:
+            cmd.append("--pull-always")
+
+        if self.params.platform:
+            cmd.append("--platform")
+            cmd.append(self.params.platform)
+
+        # Custom tags for the container image
+        LOGGER.debug("Building image with tags: '{}'".format("', '".join(tags)))
+
+        for tag in tags:
+            cmd.extend(["-t", tag])
+
+        LOGGER.info("Building container image...")
+
+        cmd.append(os.path.join(self.target, "image"))
+
+        run_wrapper(cmd, False, f"Could not run buildah {cmd}")
+
+        LOGGER.info(
+            "Image built and available under following tags: {}".format(", ".join(tags))
+        )
---
+++ packages/cekit/cekit-4.9.0.tar.gz/cekit-4.9.0/cekit/builders/podman.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import shutil
+from typing import List
+
+from cekit.builder import Builder
+from cekit.cekit_types import DependencyDefinition
+from cekit.tools import run_wrapper
+
+LOGGER = logging.getLogger("cekit")
+
+
+class PodmanBuilder(Builder):
+    """This class represents podman builder in build mode."""
+
+    def __init__(self, params):
+        super(PodmanBuilder, self).__init__("podman", params)
+
+    @staticmethod
+    def dependencies(params=None) -> DependencyDefinition:
+        deps = {}
+
+        deps["podman"] = {"package": "podman", "executable": "podman"}
+
+        return deps
+
+    def run(self):
+        """Build container image using podman."""
+
+        podman = shutil.which("podman")
+        if podman is None:
+            raise RuntimeError("podman binary was not found in the system.")
+        tags: List[str] = self.params.tags
+        cmd: List[str] = [podman, "build"]
+
+        if not tags:
+            tags = self.generator.get_tags()
+
+        if self.params.pull:
+            cmd.append("--pull-always")
+
+        if not self.params.no_squash:
+            cmd.append("--squash")
+
+        if self.params.platform:
+            cmd.append("--platform")
+            cmd.append(self.params.platform)
+
+        # Custom tags for the container image
+        LOGGER.debug("Building image with tags: '{}'".format("', '".join(tags)))
+
+        for tag in tags:
+            cmd.extend(["-t", tag])
+
+        LOGGER.info("Building container image...")
+
+        cmd.append(os.path.join(self.target, "image"))
+
+        run_wrapper(cmd, False, f"Could not run podman {cmd}")
+
+        LOGGER.info(
+            "Image built and available under following tags: {}".format(", ".join(tags))
+        )

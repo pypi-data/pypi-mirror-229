4c85c65b1bea15110b1772807e6ca85cc6b3949e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:15:00 +0000

    Add package pyDolarVenezuela/1.1.9/pyDolarVenezuela-1.1.9-py3-none-any.whl

---
+++ packages/pyDolarVenezuela/pyDolarVenezuela-1.1.9-py3-none-any.whl/base/request.py
@@ -0,0 +1,15 @@
+from httpx import Response
+import httpx
+
+def _ensure_200_and_return_content(response: Response) -> bytes:
+    if response.status_code != httpx.codes.OK:
+        raise ValueError("")
+    return response.content
+
+def ask(url: str, params: dict):
+    response = httpx.get(url, params=params, timeout=10.0)
+    return _ensure_200_and_return_content(response)
+
+def content(url: str) -> bytes:
+    response = httpx.get(url, timeout=10.0)
+    return _ensure_200_and_return_content(response)

07ad88dbb2f34a29d492f349d963e15a64998943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:08:38 +0000

    Add package fileextensions/0.1.1/fileextensions-0.1.1.tar.gz

---
+++ packages/fileextensions/fileextensions-0.1.1.tar.gz/fileextensions-0.1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='fileextensions',
+    version='0.1.1',
+    description='Provides an organized collection of lists of file extensions, categorized by their usage or type, such as documents, videos, audios, and others',
+    license='MIT',
+    author='Marcos Lopez Castro',
+    author_email='marcoslopezcastroon@gmail.com',
+    keywords=['Extensions', 'FileExtensions'],
+    download_url='https://pypi.org/project/fileextensions/'
+)

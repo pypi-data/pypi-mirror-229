6d873ea8f63e0fb7caeeb42330ed0684617561e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 16:22:27 +0000

    Add package Finance-Jindowin/1.3.5/Finance-Jindowin-1.3.5.tar.gz

---
+++ packages/Finance-Jindowin/Finance-Jindowin-1.3.5.tar.gz/Finance-Jindowin-1.3.5/jdw/mfc/entropy/deformer/fusionx/stock.py
@@ -0,0 +1,18 @@
+from jdw.mfc.entropy.deformer.fusionx.base import Base
+
+
+class Stock(Base):
+
+    def __init__(self,
+                 batch,
+                 freq,
+                 horizon,
+                 id=None,
+                 directory=None,
+                 is_full=False):
+        super(Stock, self).__init__(batch=batch,
+                                    freq=freq,
+                                    horizon=horizon,
+                                    id=id,
+                                    directory=directory,
+                                    is_full=is_full)

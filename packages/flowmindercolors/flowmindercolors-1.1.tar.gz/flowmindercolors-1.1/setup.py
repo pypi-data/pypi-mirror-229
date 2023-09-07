42293f81f2a00cf59c60f8479ef239d71891afcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:08:16 +0000

    Add package flowmindercolors/1.1/flowmindercolors-1.1.tar.gz

---
+++ packages/flowmindercolors/flowmindercolors-1.1.tar.gz/flowmindercolors-1.1/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='flowmindercolors',
+      version='1.1',
+      py_modules=['flowmindercolors'],
+      install_requires=['matplotlib', 'cycler', 'numpy']
+      )

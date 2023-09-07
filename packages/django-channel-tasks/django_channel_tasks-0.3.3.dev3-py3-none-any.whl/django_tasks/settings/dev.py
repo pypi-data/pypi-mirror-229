26bfebd59adb81a850dae8bec098a253114a577c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:09:25 +0000

    Add package django-channel-tasks/0.3.3.dev3/django_channel_tasks-0.3.3.dev3-py3-none-any.whl

---
+++ packages/django-channel-tasks/django_channel_tasks-0.3.3.dev3-py3-none-any.whl/django_tasks/settings/dev.py
@@ -0,0 +1,15 @@
+from django_tasks.settings.base import *  # noqa
+
+
+INSTALLED_APPS.append('django.contrib.postgres')  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'HOST': 'db',
+        'PASSWORD': 'postgres',
+        'PORT': 5433,
+        'USER': 'postgres',
+    }
+}

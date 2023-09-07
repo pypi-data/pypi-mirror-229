2e089d59ed44c090a566c8c0760d3b69d450152e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:49 +0000

    Add package faasmctl/0.13.1/faasmctl-0.13.1-py3-none-any.whl

---
+++ packages/faasmctl/faasmctl-0.13.1-py3-none-any.whl/faasmctl/tasks/__init__.py
@@ -0,0 +1,23 @@
+from invoke import Collection
+
+from . import cli
+from . import delete
+from . import deploy
+from . import flush
+from . import invoke
+from . import logs
+from . import restart
+from . import status
+from . import upload
+
+task_ns = Collection(
+    cli,
+    delete,
+    deploy,
+    flush,
+    invoke,
+    logs,
+    restart,
+    status,
+    upload,
+)

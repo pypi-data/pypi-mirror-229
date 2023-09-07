09dd503b246f8ba68ab77d2579b3d26fbaaa7988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:29:50 +0000

    Add package up2b/0.7.1a7/up2b-0.7.1a7-py3-none-any.whl

---
+++ packages/up2b/up2b-0.7.1a7-py3-none-any.whl/up2b/up2b_lib/log.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author:      thepoy
+# @Email:       thepoy@163.com
+# @File Name:   log.py
+# @Created At:  2023-02-07 12:13:41
+# @Modified At: 2023-04-03 20:55:06
+# @Modified By: thepoy
+
+from colorful_logger import get_logger, child_logger as cl
+from colorful_logger.logger import is_debug, get_level_from_env
+from up2b.up2b_lib.constants import CONFIG_FOLDER_PATH
+
+log_file_path = None
+show = True
+
+if is_debug():
+    log_file_path = CONFIG_FOLDER_PATH / "up2b.log"
+    show = True
+
+
+def child_logger(name: str):
+    return cl(name, logger)
+
+
+logger = get_logger(
+    "up2b",
+    show=show,
+    level=get_level_from_env(),
+    file_path=log_file_path,
+    add_file_path=False,
+    disable_line_number_filter=True,
+    asynchronous=False,
+)

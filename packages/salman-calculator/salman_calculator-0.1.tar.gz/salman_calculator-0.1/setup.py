d902fc343ad4604de1cbc1570b66cedaebcb1ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:15:13 +0000

    Add package salman-calculator/0.1/salman_calculator-0.1.tar.gz

---
+++ packages/salman-calculator/salman_calculator-0.1.tar.gz/salman_calculator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='salman_calculator',
+    version='0.1',
+    description='A simple calculator package',
+    author='Salman Latif',
+    author_email='salu33956@gmail.com',
+    packages=[],
+    install_requires=[],
+)

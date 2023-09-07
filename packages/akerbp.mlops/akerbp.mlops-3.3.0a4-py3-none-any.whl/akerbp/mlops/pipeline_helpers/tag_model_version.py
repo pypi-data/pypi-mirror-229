6704cac48ba976816f510be4308d7d326099119d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:09:23 +0000

    Add package akerbp.mlops/3.3.0a4/akerbp.mlops-3.3.0a4-py3-none-any.whl

---
+++ packages/akerbp.mlops/akerbp.mlops-3.3.0a4-py3-none-any.whl/akerbp/mlops/pipeline_helpers/tag_model_version.py
@@ -0,0 +1,35 @@
+import os
+import logging
+import argparse
+import subprocess
+import akerbp.mlops.model_manager as mm
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_name", type=str, help="Name of model")
+    args = parser.parse_args()
+    model_name = args.model_name
+
+    logging.disable()
+    mm.setup()
+    env = os.environ.get("MODEL_ENV")
+    try:
+        latest_deployed_model = (
+            mm.get_model_version_overview(
+                model_name=model_name,
+                env=env,
+                output_logs=False,
+            )
+            .sort_values(by="uploaded_time", ascending=False)
+            .iloc[0]
+        )
+        version_number = latest_deployed_model.external_id.split("/")[-1]
+        try:
+            subprocess.run(["./tag_commit.sh", version_number])
+        except subprocess.CalledProcessError as e:
+            print(f"Tagging commit returned an error: {e}")
+    except IndexError as e:
+        raise Exception(
+            f"No version of model {model_name} found in the model registry"
+        ) from e

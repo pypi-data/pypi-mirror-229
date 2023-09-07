20b0dcf8a2c7c1a824129bac0461a3572b7dc2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 05:09:50 +0000

    Add package feudalAdapter/0.7.0/feudalAdapter-0.7.0.tar.gz

---
+++ packages/feudalAdapter/feudalAdapter-0.7.0.tar.gz/feudalAdapter-0.7.0/ldf_adapter/feudal_simulator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# vim: tw=100 foldmethod=expr
+"""Simulate how the user would be created. 
+"""
+import sys
+import json
+
+# Must be before the first ldf_adapter import
+from feudal_globalconfig import globalconfig
+
+from ldf_adapter import User
+from ldf_adapter.cmdline_params import args
+
+
+def main():
+    try:
+        data = json.load(sys.stdin)
+    except json.decoder.JSONDecodeError as e:
+        message = "Cannot decode the input json. Please verify the input!"
+        print(message)
+        sys.exit(2)
+
+    if data["user"]["userinfo"] is None:
+        message = "Cannot process null input"
+        print(message)
+
+    user = User(data)
+
+    print(f"User: {user.data.username}")
+    for grp in sorted(user.data.groups):
+        print(f"   {grp}")
+
+
+if __name__ == "__main__":
+    sys.exit(main())

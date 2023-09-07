5a36529b45389643af871860e17c2f0a11fd672b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:09:32 +0000

    Add package ds-format/4.0.0/ds-format-4.0.0.tar.gz

---
+++ packages/ds-format/ds-format-4.0.0.tar.gz/ds-format-4.0.0/ds_format/bin/ds.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+import warnings
+warnings.simplefilter('ignore')
+from ds_format.cmd.main import main
+if __name__ == '__main__':
+	main()

15ef8f773834a2cc610a91d584184e8568f90528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:08:08 +0000

    Add package yak-server/0.34.0/yak_server-0.34.0.tar.gz

---
+++ packages/yak-server/yak_server-0.34.0.tar.gz/yak_server-0.34.0/yak_server/cli/__init__.py
@@ -0,0 +1,96 @@
+import click
+
+from yak_server import create_app
+
+from .database import (
+    backup_database,
+    create_admin,
+    create_database,
+    delete_database,
+    drop_database,
+    initialize_database,
+    setup_migration,
+)
+from .env import init_env
+
+
+@click.command()
+def create() -> None:
+    """Create all database tables."""
+    create_database()
+
+
+@click.command()
+def init() -> None:
+    """Initialize database."""
+    app = create_app()
+    initialize_database(app)
+
+
+@click.command()
+def drop() -> None:
+    """Drop all tables."""
+    app = create_app()
+    drop_database(app)
+
+
+@click.command()
+def delete() -> None:
+    """Delete all records."""
+    app = create_app()
+    delete_database(app)
+
+
+@click.command()
+def admin() -> None:
+    """Create admin account in database."""
+    create_admin()
+
+
+@click.command()
+def backup() -> None:
+    """Backup database in a sql file."""
+    backup_database()
+
+
+@click.command()
+def migration() -> None:
+    """Help to run database migration scripts."""
+    setup_migration()
+
+
+@click.group()
+def db() -> None:
+    pass
+
+
+db.add_command(create)
+db.add_command(init)
+db.add_command(drop)
+db.add_command(delete)
+db.add_command(admin)
+db.add_command(backup)
+db.add_command(migration)
+
+
+@click.command()
+def init() -> None:
+    """Build the env files you need to start the server."""
+    init_env()
+
+
+@click.group()
+def env() -> None:
+    pass
+
+
+env.add_command(init)
+
+
+@click.group()
+def main() -> None:
+    pass
+
+
+main.add_command(db)
+main.add_command(env)

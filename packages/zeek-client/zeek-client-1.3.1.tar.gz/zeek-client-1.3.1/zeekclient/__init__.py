d7b106d3dec2616e95d3abc029d3364ef94913b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:32:00 +0000

    Add package zeek-client/1.3.1/zeek-client-1.3.1.tar.gz

---
+++ packages/zeek-client/zeek-client-1.3.1.tar.gz/zeek-client-1.3.1/zeekclient/__init__.py
@@ -0,0 +1,41 @@
+"""This package defines a Python interface for interacting with a Zeek cluster
+controller. It provides a Controller class for sending and retrieving events, a
+Zeek event abstraction on top of Broker, and various types that mirror those
+defined in Zeek's policy/management/frameworks/management/types.zeek, needed
+for request/response events.
+"""
+import sys
+
+from . import brokertypes
+from . import cli
+from . import config
+from . import consts
+from . import controller
+from . import events
+from . import logs
+from . import ssl
+from . import types
+from . import utils
+
+from .config import CONFIG
+
+from .consts import (
+    CONTROLLER_TOPIC,
+    CONFIG_FILE,
+)
+
+from .logs import LOG
+
+__version__ = "1.3.1"
+__all__ = [
+    "brokertypes",
+    "cli",
+    "config",
+    "consts",
+    "controller",
+    "events",
+    "logs",
+    "ssl",
+    "types",
+    "utils",
+]

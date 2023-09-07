9e5d2e87f67ca8bfedb9f3de68facda0e51df63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:09:33 +0000

    Add package splight-lib/3.3.5/splight-lib-3.3.5.tar.gz

---
+++ packages/splight-lib/splight-lib-3.3.5.tar.gz/splight-lib-3.3.5/splight_lib/models/attribute.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from splight_lib.models.base import SplightDatabaseBaseModel
+
+
+class Attribute(SplightDatabaseBaseModel):
+    id: Optional[str]
+    name: str
+    asset: str

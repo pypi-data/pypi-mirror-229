757bd88ff65733f32b972b58ad5178274ee3261f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:14:25 +0000

    Add package sink-pypi/0.2.8/sink_pypi-0.2.8.tar.gz

---
+++ packages/sink-pypi/sink_pypi-0.2.8.tar.gz/sink_pypi-0.2.8/src/sink/resources/names/__init__.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .names import Names, AsyncNames
+from .params import Params, AsyncParams
+from .documents import Documents, AsyncDocuments
+from .reserved_names import ReservedNames, AsyncReservedNames
+
+__all__ = [
+    "Documents",
+    "AsyncDocuments",
+    "ReservedNames",
+    "AsyncReservedNames",
+    "Params",
+    "AsyncParams",
+    "Names",
+    "AsyncNames",
+]

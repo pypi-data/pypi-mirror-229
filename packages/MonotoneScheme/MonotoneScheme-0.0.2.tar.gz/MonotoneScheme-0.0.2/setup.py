efb139bb658fdcdadba3778d56104697f94ab234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:08:07 +0000

    Add package MonotoneScheme/0.0.2/MonotoneScheme-0.0.2.tar.gz

---
+++ packages/MonotoneScheme/MonotoneScheme-0.0.2.tar.gz/MonotoneScheme-0.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11 import get_cmake_dir
+
+import sys
+
+__version__ = "0.0.2"
+
+
+ext_modules = [
+    Pybind11Extension("MonotoneScheme",
+        ["src/main.cpp"],
+        define_macros      = [('VERSION_INFO', __version__)],
+        extra_compile_args = ['-std=c++14'],
+        include_dirs=["src/"],  # -I
+        ),
+]
+
+setup(
+    name="MonotoneScheme",
+    version=__version__,
+    author="Wonjun Lee",
+    author_email="wlee@ucla.edu",
+    description="Python wrapper for the monotone discretization problem in 2D and 3D Cartesian grids (join work with Jeff Calder (UMN))",
+    long_description="""
+        (the details will be added)
+        """,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wonjunee/monotone-scheme",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent"],
+    install_requires=[  'numpy', 
+                        'scipy', 
+                        'scikit-learn', 
+                        'matplotlib'],
+    python_requires='>=3.6',
+    zip_safe=False,
+)

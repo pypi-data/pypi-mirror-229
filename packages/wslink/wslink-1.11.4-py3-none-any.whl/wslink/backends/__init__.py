4b851dbee9097339b37792c3ddac94c0eeb757c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:16:15 +0000

    Add package wslink/1.11.4/wslink-1.11.4-py3-none-any.whl

---
+++ packages/wslink/wslink-1.11.4-py3-none-any.whl/wslink/backends/__init__.py
@@ -0,0 +1,36 @@
+def create_webserver(server_config, backend="aiohttp"):
+    if backend == "aiohttp":
+        from .aiohttp import create_webserver
+
+        return create_webserver(server_config)
+
+    if backend == "generic":
+        from .generic import create_webserver
+
+        return create_webserver(server_config)
+
+    if backend == "tornado":
+        from .tornado import create_webserver
+
+        return create_webserver(server_config)
+
+    raise Exception(f"{backend} backend is not implemented")
+
+
+def launcher_start(args, config, backend="aiohttp"):
+    if backend == "aiohttp":
+        from .aiohttp.launcher import startWebServer
+
+        return startWebServer(args, config)
+
+    if backend == "generic":
+        from .generic import startWebServer
+
+        return startWebServer(args, config)
+
+    if backend == "tornado":
+        from .tornado import startWebServer
+
+        return startWebServer(args, config)
+
+    raise Exception(f"{backend} backend is not implemented")

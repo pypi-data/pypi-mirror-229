0891c6cc493534302b168a4fae3d89b86e1a0273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:07:31 +0000

    Add package seismic-rna/0.8.3/seismic_rna-0.8.3-py3-none-any.whl

---
+++ packages/seismic-rna/seismic_rna-0.8.3-py3-none-any.whl/seismicrna/core/sim.py
@@ -0,0 +1,12 @@
+"""
+Core -- Simulation Module
+========================================================================
+Auth: Matty
+Date: 2023-06-06
+"""
+
+
+import numpy as np
+
+# Initialize a pseudo-random number generator.
+rng = np.random.default_rng()

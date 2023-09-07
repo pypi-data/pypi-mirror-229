f286793e0c34aa62030a874c19d3bcddd1534b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 04:12:45 +0000

    Add package nonebot-plugin-mqtt/0.1.2/nonebot_plugin_mqtt-0.1.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-mqtt/nonebot_plugin_mqtt-0.1.2-py3-none-any.whl/nonebot_plugin_mqtt/__init__.py
@@ -0,0 +1,14 @@
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+from .mqtt import *
+
+__plugin_meta__ = PluginMetadata(
+    name="MQTT",
+    description="Nonebot2 MQTT 插件",
+    usage="",
+    type="library",
+    homepage="https://github.com/synodriver/nonebot_plugin_mqtt",
+    config=Config,
+    supported_adapters=None,
+)

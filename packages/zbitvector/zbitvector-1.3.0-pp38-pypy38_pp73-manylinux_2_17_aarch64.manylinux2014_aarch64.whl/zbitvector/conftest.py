3680f39a82f7e9991b59ec6e635475e043464cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:10:08 +0000

    Add package zbitvector/1.3.0/zbitvector-1.3.0-pp38-pypy38_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/zbitvector/zbitvector-1.3.0-pp38-pypy38_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/zbitvector/conftest.py
@@ -0,0 +1,37 @@
+"""Configuration for pytest. *Not* part of the public API."""
+
+from typing import Any, Dict, Literal
+
+import pytest
+from typing_extensions import TypeAlias
+
+from . import *
+
+Uint8: TypeAlias = Uint[Literal[8]]
+Uint64: TypeAlias = Uint[Literal[64]]
+
+Int8: TypeAlias = Int[Literal[8]]
+Int64: TypeAlias = Int[Literal[64]]
+
+
+def pytest_ignore_collect(path: Any) -> bool:
+    # Don't check  _z3.py for doctests: it doesn't have any, and importing the
+    # file will raise an error if Z3 isn't installed.
+    if str(path).endswith("/_z3.py"):
+        return True
+    return False
+
+
+@pytest.fixture(autouse=True)
+def setup_doctest(doctest_namespace: Dict[str, Any]) -> None:
+    doctest_namespace.update(
+        {
+            "Array": Array,
+            "Solver": Solver,
+            "Constraint": Constraint,
+            "Uint8": Uint8,
+            "Uint64": Uint64,
+            "Int8": Int8,
+            "Int64": Int64,
+        }
+    )

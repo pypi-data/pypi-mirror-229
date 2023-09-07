ef4889e5b3af3d080d07d88d901d15b0d52fec02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:09:57 +0000

    Add package sett-rs/0.5.0/sett_rs-0.5.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/sett-rs/sett_rs-0.5.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/sett_rs/gnupg.py
@@ -0,0 +1,9 @@
+from ._sett_rs import (
+    gnupg_extract_cert as gnupg_extract_cert,
+    gnupg_list_certs as gnupg_list_certs,
+)
+
+__all__ = [
+    "gnupg_extract_cert",
+    "gnupg_list_certs",
+]

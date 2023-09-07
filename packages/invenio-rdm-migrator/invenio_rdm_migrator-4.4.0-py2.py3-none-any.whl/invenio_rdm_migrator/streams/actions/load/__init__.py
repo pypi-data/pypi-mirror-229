1929c209d6b0080a8c571884d5187e0c432a1f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:09:59 +0000

    Add package invenio-rdm-migrator/4.4.0/invenio_rdm_migrator-4.4.0-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-migrator/invenio_rdm_migrator-4.4.0-py2.py3-none-any.whl/invenio_rdm_migrator/streams/actions/load/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio RDM migration load actions."""
+
+from .communities import (
+    CommunityCreateAction,
+    CommunityDeleteAction,
+    CommunityUpdateAction,
+)
+from .drafts import DraftCreateAction, DraftEditAction, DraftPublishAction
+from .files import DraftFileUploadAction
+from .users import UserDeactivationAction, UserEditAction, UserRegistrationAction
+
+__all__ = (
+    "CommunityCreateAction",
+    "CommunityDeleteAction",
+    "CommunityUpdateAction",
+    "DraftCreateAction",
+    "DraftEditAction",
+    "DraftFileUploadAction",
+    "DraftPublishAction",
+    "UserDeactivationAction",
+    "UserEditAction",
+    "UserRegistrationAction",
+)

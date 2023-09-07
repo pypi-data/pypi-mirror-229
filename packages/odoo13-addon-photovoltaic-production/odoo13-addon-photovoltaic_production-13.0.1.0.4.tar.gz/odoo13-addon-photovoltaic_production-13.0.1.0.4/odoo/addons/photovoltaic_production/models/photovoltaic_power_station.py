ce12fe8932e6ac884a574806e5014647847fd56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:35 +0000

    Add package odoo13-addon-photovoltaic-production/13.0.1.0.4/odoo13-addon-photovoltaic_production-13.0.1.0.4.tar.gz

---
+++ packages/odoo13-addon-photovoltaic-production/odoo13-addon-photovoltaic_production-13.0.1.0.4.tar.gz/odoo13-addon-photovoltaic_production-13.0.1.0.4/odoo/addons/photovoltaic_production/models/photovoltaic_power_station.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+
+class PhotovoltaicPowerStation(models.Model):
+    _inherit = "photovoltaic.power.station"
+
+    production_name = fields.Char()
+    billing_error_margin = fields.Float(default='5', string='Billing error margin [%]')
+
+    link_address = fields.Integer()
+    measure_address = fields.Integer()
+    measure_key = fields.Integer()
+    connection_type = fields.Selection([('gsm', 'GSM'), ('ip', 'IP')])
+
+    ip_address = fields.Char(string='IP address')
+    ip_port = fields.Integer(string='IP port')
+
+    phone = fields.Char()

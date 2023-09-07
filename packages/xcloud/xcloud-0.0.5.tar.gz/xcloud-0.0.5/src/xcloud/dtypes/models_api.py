9b0443b510f4dd85b9be6e5a6c9808c4c9ff1b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:34 +0000

    Add package xcloud/0.0.5/xcloud-0.0.5.tar.gz

---
+++ packages/xcloud/xcloud-0.0.5.tar.gz/xcloud-0.0.5/src/xcloud/dtypes/models_api.py
@@ -0,0 +1,75 @@
+from pydantic import BaseModel, Field, validator
+from enum import Enum
+from typing import Optional
+from pathlib import Path
+from xcloud.dtypes.shared import Metadata
+from xcloud.dtypes.deployments import Inference, DeploymentSpecs
+from xcloud.utils.validations import is_jsonl_file_valid
+
+
+
+class ModelsAPIModelFamily(str, Enum):
+    LLAMA_V2_7B = "llama_v2_7b"
+    LLAMA_V2_13B = "llama_v2_13b"
+    LLAMA_V2_7B_CHAT = "llama_v2_7b_chat"
+    LLAMA_V2_13B_CHAT = "llama_v2_13b_chat"
+
+
+class ModelsAPIFinetuningType(str, Enum):
+    UNSUPERVISED_FINETUNING = "unsupervised_finetuning"
+    INSTRUCTION_FINETUNING = "instruction_finetuning"
+
+
+class ModelsAPIFinetuning(BaseModel):
+    workspace_id: Optional[str]
+    finetuning_name: str
+    model_name: str
+    model_family: Optional[ModelsAPIModelFamily]
+    num_epochs: Optional[int] = 3
+    dataset_id: Optional[str]
+    finetuning_type: ModelsAPIFinetuningType
+    status: Optional[str]
+    metadata: Optional[Metadata] = Field(default_factory=Metadata)
+    
+    class Config:
+        validate_all = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        
+    # @validator("dataset_path")
+    # def validate_dataset_path(cls, v):
+    #     if v is None:
+    #         assert Path(v).is_file(), f"{v} is not a file. Please make sure it is a file"
+    #         assert is_jsonl_file_valid(v), f"This JSONL file {v} is not valid"
+        
+    #     return v
+    
+
+class ModelsAPIDeployment(BaseModel):
+    workspace_id: Optional[str]
+    deployment_name: str
+    model_name: str
+    deployment_specs: Optional[DeploymentSpecs] = Field(default_factory=DeploymentSpecs)
+    inference: Optional[Inference]
+    status: Optional[str]
+    metadata: Optional[Metadata] = Field(default_factory=Metadata)
+
+    
+class ModelsAPIModelType(str, Enum):
+    BASE_MODEL = "base_model"
+    FINETUNED_MODEL = "finetuned_model"
+    
+    
+class ModelsAPIModelStatus(str, Enum):
+    READY = "ready"
+    NOT_READY = "not_ready"
+    
+
+class ModelsAPIModel(BaseModel):
+    workspace_id: Optional[str]
+    model_name: str
+    model_type: ModelsAPIModelType
+    model_family: ModelsAPIModelFamily
+    status: ModelsAPIModelStatus
+    deleted: bool = False
+    metadata: Optional[Metadata] = Field(default_factory=Metadata)

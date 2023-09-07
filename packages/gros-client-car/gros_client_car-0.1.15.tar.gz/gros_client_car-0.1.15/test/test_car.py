f6347628f44b1dee955035aab7a6858d561e2dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:31:43 +0000

    Add package gros-client-car/0.1.15/gros_client_car-0.1.15.tar.gz

---
+++ packages/gros-client-car/gros_client_car-0.1.15.tar.gz/gros_client_car-0.1.15/test/test_car.py
@@ -0,0 +1,35 @@
+# 安装依赖
+# pip install gros_client
+
+# 引入依赖
+from gros_client_car import Car
+
+# 实例化human对象
+car = Car(host='192.168.9.17')
+# 调用启动方法
+car.start()
+
+
+
+
+import unittest
+
+from src.gros_client_car import Car
+
+car = Car()
+
+
+class TestCar(unittest.TestCase):
+
+    def test_start(self):
+        res = car.start()
+        print(f'car.test_start: {res}')
+        assert res.get('code') == 0
+
+    def test_stop(self):
+        res = car.stop()
+        print(f'cat.test_stop: {res}')
+        assert res.get('code') == 0
+
+    def test_move(self):
+        car.move(1, 0.8)

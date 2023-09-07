d2dbb00dd1646041747836df04560c3e56a35f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:33 +0000

    Add package seqsearch/2.2.1/seqsearch-2.2.1.tar.gz

---
+++ packages/seqsearch/seqsearch-2.2.1.tar.gz/seqsearch-2.2.1/seqsearch/databases/nr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Built-in modules #
+
+# Internal modules #
+
+# First party modules #
+
+###############################################################################
+class NonRedundant(object):
+    """
+    The NR database from NCBI.
+    NR contains non-redundant sequences from GenBank translations
+    (i.e. GenPept) together with sequences from other databanks
+    (Refseq, PDB, SwissProt, PIR and PRF).
+    """
+

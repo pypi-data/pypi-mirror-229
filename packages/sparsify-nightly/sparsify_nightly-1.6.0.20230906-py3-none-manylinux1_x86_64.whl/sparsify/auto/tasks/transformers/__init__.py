7b7d22acf2c813a9b1c1ffde29db6eac112b82a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:51 +0000

    Add package sparsify-nightly/1.6.0.20230906/sparsify_nightly-1.6.0.20230906-py3-none-manylinux1_x86_64.whl

---
+++ packages/sparsify-nightly/sparsify_nightly-1.6.0.20230906-py3-none-manylinux1_x86_64.whl/sparsify/auto/tasks/transformers/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# flake8: noqa
+# isort: skip_file
+
+
+def _check_nm_install():
+    try:
+        from .runner import *
+    except ImportError as exception:
+        raise ImportError(
+            "Please install sparsify[nm] to use this pathway."
+        ) from exception
+
+
+_check_nm_install()
+
+from .args import *
+from .runner import *

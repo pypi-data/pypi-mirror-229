9788391c53030a2257f1fcab3fe5d7dde7e956be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:10:10 +0000

    Add package MuLP/1.0.1/MuLP-1.0.1.tar.gz

---
+++ packages/MuLP/MuLP-1.0.1.tar.gz/MuLP-1.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_descripiton = fh.read()
+
+setuptools.setup(
+    name="MuLP",
+    version="1.0.1",
+    author="Sigurjon Thorsteinsson, Emiliano Penaloza, Cristian Bravo Roman, María Óskarsdóttir",
+    author_email="grjoni80@gmail.com, emilianopp550@gmail.com, cbravoro@uwo.ca, mariaoskars@ru.is",
+    description="Python implementation of the Multilayer Personalized Page Rank algorithm",
+    long_description=long_descripiton,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Banking-Analytics-Lab/MuLP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    setup_requires=["numpy","scikit-learn","igraph"], 
+    install_requires=[
+        "scikit-learn",
+        "numpy",
+        "scipy",
+        "igraph",
+        "pandas"
+        ]
+)
+
+

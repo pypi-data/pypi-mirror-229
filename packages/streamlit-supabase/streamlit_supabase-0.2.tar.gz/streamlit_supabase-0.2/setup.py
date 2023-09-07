71ab83b4bf2142893c80728e2637ae962bab086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 14:09:55 +0000

    Add package streamlit-supabase/0.2/streamlit_supabase-0.2.tar.gz

---
+++ packages/streamlit-supabase/streamlit_supabase-0.2.tar.gz/streamlit_supabase-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamlit_supabase",
+    version="0.2",
+    description="Supabase functions in the Streamlit framework",
+    author="Matt Majestic",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)

02d7e27fa5aff09f9a88bb8b3340f105103b2e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:10:42 +0000

    Add package wagtail-cjkcms/23.9.1/wagtail_cjkcms-23.9.1-py2.py3-none-any.whl

---
+++ packages/wagtail-cjkcms/wagtail_cjkcms-23.9.1-py2.py3-none-any.whl/cjkcms/models/admin_sidebar.py
@@ -0,0 +1,17 @@
+from .snippet_models import Navbar
+from wagtail.snippets.views.snippets import SnippetViewSet
+
+
+class NavbarSnippet(SnippetViewSet):
+    model = Navbar
+    menu_label = "Navigation"
+    menu_icon = "link"  # change as required
+    add_to_admin_menu = True
+    list_display = (
+        "name",
+        "custom_css_class",
+        "custom_id",
+    )
+    search_fields = [
+        "name",
+    ]

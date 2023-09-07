bf1ec91d5972f76afdceadc40d21ad1af44bb329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 03:08:16 +0000

    Add package pygameFPS/1.0.5/pygameFPS-1.0.5.tar.gz

---
+++ packages/pygameFPS/pygameFPS-1.0.5.tar.gz/pygameFPS-1.0.5/src/pygameFPS/__init__.py
@@ -0,0 +1,35 @@
+import time
+import pygame
+fps = []
+
+class Frame():
+    frameTime: int = 0
+    
+    def __init__(self, frameTime: int):
+        self.frameTime = frameTime
+        
+    def getFrameTime(self) -> float:
+        return self.frameTime
+    
+    def getDeltaFramteime(self):
+        return time.time() - self.getFrameTime()
+    
+    def canRemove(self) -> bool:
+        return self.getDeltaFramteime() >= 1
+    
+    def tick(self):
+        if self.canRemove():
+            fps.remove(self)
+                
+def getFPS() -> int:
+    return len(fps)
+                
+def tick():
+    for frame in fps:
+        frame.tick()
+    fps.append(Frame(time.time()))
+    
+def render(surface: pygame.surface, x = 20, y = 20, size = 24, colour = (255, 255, 255)):
+    font = pygame.font.SysFont("Play", size)
+    img = font.render(f'FPS: {getFPS()}', True, colour)
+    surface.blit(img, (x, y))

1d815b2685de5ff3aebcd8d50668ce82292ddb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 16:22:42 +0000

    Add package abaqus-mtx-parser/1.0.1/abaqus-mtx-parser-1.0.1.tar.gz

---
+++ packages/abaqus-mtx-parser/abaqus-mtx-parser-1.0.1.tar.gz/abaqus-mtx-parser-1.0.1/tests/test_abaqus_mtx_parser.py
@@ -0,0 +1,50 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+import numpy as np
+import unittest
+
+from importlib.resources import files
+from abaqus_mtx_parser import parse_mtx
+
+
+class TestAbaqusMtxParser(unittest.TestCase):
+
+    def test_unsymmetric_stiffness(self):
+        mtx = files("abaqus_mtx_parser.mtx").joinpath("unsym.mtx")
+        result = parse_mtx(mtx)
+
+        # Test nodes and dof
+        self.assertListEqual(result.nodes, [2, 3, 4, 5, 6, 7])
+        self.assertListEqual(result.dof, [1, 2, 3, 4, 5, 6])
+
+        # Test stiffness matrix
+        matrix = result.stiffness
+        for v_pair in [
+            (matrix[1, 3], -.17762711516914E-09),
+            (matrix[3, 1], -.17680651844574E-09),
+        ]:
+            self.assertAlmostEqual(
+                v_pair[0], v_pair[1], delta = v_pair[1] * 1e-5)
+
+    def test_symmetric_stiffness(self):
+        mtx = files("abaqus_mtx_parser.mtx").joinpath("sym.mtx")
+        result = parse_mtx(mtx)
+        print(result.data)
+        # Test nodes and dof
+        self.assertListEqual(result.nodes, [2, 3, 4, 5, 6, 7])
+        self.assertListEqual(result.dof, [1, 2, 3, 4, 5, 6])
+
+        # Test stiffness matrix
+        matrix = result.stiffness
+        for v_pair in [
+            (matrix[1, 3], -.17773628812504E-09),
+            (matrix[3, 1], -.17773628812504E-09),
+        ]:
+            self.assertAlmostEqual(
+                v_pair[0], v_pair[1], delta = v_pair[1] * 1e-5)
+
+
+if __name__ == "__main__":
+    unittest.main()

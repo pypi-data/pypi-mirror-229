58a80a6a3bfbb9eb0ac71356b391da2e8fe33521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 15:30:37 +0000

    Add package djapy/0.0.3/djapy-0.0.3.tar.gz

---
+++ packages/djapy/djapy-0.0.3.tar.gz/djapy-0.0.3/djapy/views/generics.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod, ABC
+
+from djapy.utils.mapper import DjapyJsonMapper
+
+
+class DjapyView(ABC):
+    model_fields = None
+    node_bounded_mode: str = "__strict__"
+
+    def __init__(self):
+        if not self.model_fields:
+            raise Exception('Please specify model fields')
+
+    def __render__(self, request):
+        queryset = self.get_queryset(request)
+        json_node = DjapyJsonMapper(queryset, self.model_fields, node_bounded_mode=self.node_bounded_mode)
+        return json_node.nodify()
+
+    @abstractmethod
+    def get_queryset(self, request):
+        pass
+
+    @classmethod
+    def as_view(cls):
+        view = cls()
+        return view.__render__

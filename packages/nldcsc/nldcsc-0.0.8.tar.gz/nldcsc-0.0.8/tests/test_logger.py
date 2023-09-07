4ee16cf52b52ae32778b07ab09ad1df6bdd1464a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-07 05:10:23 +0000

    Add package nldcsc/0.0.8/nldcsc-0.0.8.tar.gz

---
+++ packages/nldcsc/nldcsc-0.0.8.tar.gz/nldcsc-0.0.8/tests/test_logger.py
@@ -0,0 +1,46 @@
+from tests.helpers.capture_logging import catch_logs, records_to_tuples
+
+
+class TestLogger:
+    def test_logger_import(self):
+        try:
+            import logging
+            from nldcsc.loggers.app_logger import AppLogger
+
+            logging.setLoggerClass(AppLogger)
+
+            logger_name = "TEST"
+            logger = logging.getLogger(logger_name)
+            logger.propagate = True
+
+            with catch_logs(level=logging.DEBUG, logger=logger) as handler:
+                logger.debug("Debug message")
+                logger.info("Info message")
+                logger.warning("Warning message")
+                logger.error("Error message")
+                logger.critical("Critical message")
+                assert records_to_tuples(handler.records) == [
+                    (logger_name, logging.DEBUG, "Debug message"),
+                    (logger_name, logging.INFO, "Info message"),
+                    (logger_name, logging.WARNING, "Warning message"),
+                    (logger_name, logging.ERROR, "Error message"),
+                    (logger_name, logging.CRITICAL, "Critical message"),
+                ]
+
+        except Exception:
+            raise
+
+    def test_handler_types(self):
+        import logging
+        from nldcsc.loggers.app_logger import AppLogger
+        from logging.handlers import RotatingFileHandler
+        from nldcsc.loggers.handlers.gelf_handler import DCSCGelfUDPHandler
+
+        logging.setLoggerClass(AppLogger)
+
+        logger = logging.getLogger("TEST")
+
+        assert len(logger.handlers) == 3
+        assert isinstance(logger.handlers[0], logging.StreamHandler)
+        assert isinstance(logger.handlers[1], RotatingFileHandler)
+        assert isinstance(logger.handlers[2], DCSCGelfUDPHandler)

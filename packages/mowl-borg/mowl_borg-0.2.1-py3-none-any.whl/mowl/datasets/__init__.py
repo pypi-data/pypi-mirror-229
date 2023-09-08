1a3cd2682743926ea8c545677b7571d2f7d2ce96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:07:54 +0000

    Add package mowl-borg/0.2.1/mowl_borg-0.2.1-py3-none-any.whl

---
+++ packages/mowl-borg/mowl_borg-0.2.1-py3-none-any.whl/mowl/datasets/__init__.py
@@ -0,0 +1,6 @@
+import mowl
+import jpype
+
+from mowl.datasets.base import Dataset, PathDataset, RemoteDataset, TarFileDataset
+from mowl.datasets.el import ELDataset
+from mowl.datasets.alc import ALCDataset

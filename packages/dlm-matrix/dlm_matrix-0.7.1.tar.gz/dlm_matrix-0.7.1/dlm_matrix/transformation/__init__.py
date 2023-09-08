621370cc163b79a722ea4806e7d124f6fe50d664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 06:13:39 +0000

    Add package dlm-matrix/0.7.1/dlm_matrix-0.7.1.tar.gz

---
+++ packages/dlm-matrix/dlm_matrix-0.7.1.tar.gz/dlm_matrix-0.7.1/dlm_matrix/transformation/__init__.py
@@ -0,0 +1,5 @@
+from .base import *
+from .tree import *
+from .coordinate import *
+from .traverse import *
+from .utils import *
---
+++ packages/dlm-matrix/dlm_matrix-0.7.1.tar.gz/dlm_matrix-0.7.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="dlm_matrix",
+    version="0.7.1",
+    author="Mohamed Diomande",
+    author_email="gdiomande7907@gmail.com",
+    description="Divergent Language Matrix",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/diomandeee/dl_matrix",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
+

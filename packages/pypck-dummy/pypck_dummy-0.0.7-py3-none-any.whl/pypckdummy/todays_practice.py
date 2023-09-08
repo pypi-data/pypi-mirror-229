3960ad80317b162efbd8b70e510b70237a95cfb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:36:18 +0000

    Add package pypck-dummy/0.0.7/pypck_dummy-0.0.7-py3-none-any.whl

---
+++ packages/pypck-dummy/pypck_dummy-0.0.7-py3-none-any.whl/pypckdummy/todays_practice.py
@@ -0,0 +1,15 @@
+import random
+
+def main():
+	pracs = ['aerobic', 'sprints', 'technique', 'gym', 'mostly talk']
+	prac = random.choice(pracs)
+	print("Today's practice will be %s." % prac)
+	if prac in ['aerobic', 'mostly talk']:
+		print('Sorry...')
+	else:
+		print('Yes!')
+	
+	return(prac)
+
+if __name__ == '__main__':
+	main()

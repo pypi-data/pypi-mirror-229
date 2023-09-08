d3e4911ed8d5b6368f994735fc7015e5cad88f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:45 +0000

    Add package scanx1/0.1.1/scanx1-0.1.1.tar.gz

---
+++ packages/scanx1/scanx1-0.1.1.tar.gz/scanx1-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scanx1',
+    version='0.1.1',
+    author='scannerx',
+    author_email='dastscannerx@gmail.com',
+    description='Web Vulnerability Scanner X1',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

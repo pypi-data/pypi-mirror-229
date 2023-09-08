0518c5d5f97280958ce702e9c67a1c099d95f463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:50 +0000

    Add package oarepo-ui/5.0.54/oarepo-ui-5.0.54.tar.gz

---
+++ packages/oarepo-ui/oarepo-ui-5.0.54.tar.gz/oarepo-ui-5.0.54/oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/index.js
@@ -0,0 +1,9 @@
+export * from "./RelatedSelectField";
+export * from "./LanguageSelectField";
+
+export * from "./I18nString";
+export * from "./I18nTextInputField";
+export * from "./I18nRichInputField";
+export * from "./MultilingualTextInput";
+
+export * from "./BaseForm";

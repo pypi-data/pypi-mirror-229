d3f45b97f1518de29f230658e313502ec5026534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:22:35 +0000

    Add package yaytarch/1.1.0/yaytarch-1.1.0.tar.gz

---
+++ packages/yaytarch/yaytarch-1.1.0.tar.gz/yaytarch-1.1.0/src/yaytarch/cli.py
@@ -0,0 +1,42 @@
+import sys
+import threading
+import webbrowser
+import argparse
+from .__init__ import create_app
+from .tools import videodownload
+
+
+# Defines the behaviour of the user side CLI
+def cli():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-f", "--folder", help="Open viewer on folder.", default='', action='store_true')
+    argparser.add_argument("-d", "--download", help="Download video, playlist, or channel.", default='')
+    argparser.add_argument("-i", "--initdb", help="Initialise database. Specify the default download location.", default='')
+    args = argparser.parse_args()
+
+    #  -f argument
+    if args.folder:  # specifies folder for use in local mode
+        webbrowser.open("http://127.0.0.1:5000/local")
+        view()
+
+    #  -d argument
+    if args.download:
+        videodownload.dl(args.download, None, True)
+        sys.exit(0)
+
+    #  no argument
+    #  TODO: Fix app context
+    #if args.folder == '' and args.download == '':
+    #    webbrowser.open("http://127.0.0.1:5000")
+    #    view()
+
+
+
+
+def view():
+    app = create_app()
+    threading.Thread(target=lambda: app.run(port=5000)).run()
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/yaytarch/yaytarch-1.1.0.tar.gz/yaytarch-1.1.0/src/yaytarch/videos.py
@@ -0,0 +1,97 @@
+import click
+from flask import (
+    Blueprint, render_template, send_from_directory, request
+)
+
+from .tools import videodownload
+from .tools.config import *
+from .model.db import videocollectionrelmodel, videomodel
+from .model.file import jsonvideomodel
+
+# This blueprint takes care of the video view page and any future feature of it
+
+bp = Blueprint('videos', __name__)
+
+
+# TODO: Remove ability to remove videos from "All Collections"?
+@bp.route('/video/<int:videoid>', methods=['GET', 'POST'])
+def viewvideo(videoid):
+    if request.method == 'POST':
+        # Request type: Remove
+        if 'valueR' in request.form.keys():  # Remove from playlist buttom
+            message = videocollectionrelmodel.removevideocollectionmembershipentry(videoid, request.form['valueR'])
+
+        # Request type: Add
+        if 'value' in request.form.keys():  # Add to playlist button
+            if videomodel.addvideotocollection(videoid, request.form['value']) is not None:
+                message = "Video added to collection"
+            else:
+                message = "Video already part of collection."
+
+        # Request type: Refresh
+        if 'buttonrefresh' in request.form.keys():
+            videodownload.dlbyid(videoid)
+
+    # Fetch all collections the video is in to display values
+    incollections = videocollectionrelmodel.getvideocollectionmembershipbyid(videoid)
+    # Fetch all collections the video is NOT in to display values
+    notincollections = videocollectionrelmodel.getinversedvideocollectionmembershipbyid(videoid)
+    video = videomodel.getvideobyid(videoid)
+    return render_template('videoplay.html', video=video, incollections=incollections,
+                           notincollections=notincollections)
+
+
+@bp.route('/video/<string:videoshurl>')
+def viewlocalvideo(videoshurl):
+    discoverysingleton = jsonvideomodel.folderdiscoveryresult()
+    if hasattr(discoverysingleton, 'discovered'):
+        match = discoverysingleton.find_discovered_entry(videoshurl)
+        if match is not None and match.localvideoobj is not None:
+            return render_template('localvideoplay.html', video=match.localvideoobj)
+
+
+@bp.route("/video/source/<int:videoid>")
+def load_video(videoid):
+    video = videomodel.getvideobyid(videoid)
+
+    file_name = os.path.basename(video.loc)
+    dir_name = os.path.dirname(video.loc)
+
+    return send_from_directory(dir_name, file_name)
+
+
+@bp.route("/video/source/<string:videoshurl>")
+def load_localvideo(videoshurl):
+    discoverysingleton = jsonvideomodel.folderdiscoveryresult()
+    if hasattr(discoverysingleton, 'discovered'):
+        match = discoverysingleton.find_discovered_entry(videoshurl)
+        if match is not None:
+            file_name = os.path.basename(match.videofilename)
+            dir_name = os.path.dirname(match.videofilename)
+            return send_from_directory(dir_name, file_name)
+
+
+@bp.route("/video/source/thumb/<string:videoshurl>")
+def load_localpicture(videoshurl):
+    discoverysingleton = jsonvideomodel.folderdiscoveryresult()
+    if hasattr(discoverysingleton, 'discovered'):
+        match = discoverysingleton.find_discovered_entry(videoshurl)
+        if match is not None:
+            file_name = os.path.basename(match.thumbfilename)
+            dir_name = os.path.dirname(match.thumbfilename)
+            return send_from_directory(dir_name, file_name)
+
+
+@bp.route("/video/source/thumb/<int:videoid>")
+def load_picture(videoid):
+    video = videomodel.getvideobyid(videoid)
+    dirname = os.path.dirname(video.thumbnail)
+    filename = os.path.basename(video.thumbnail)
+
+    return send_from_directory(dirname, filename)
+
+
+@bp.cli.command("dl")
+@click.argument("link")
+def dl(link):
+    videodownload.dl(link)

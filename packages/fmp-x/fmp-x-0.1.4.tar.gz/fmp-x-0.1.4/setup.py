c9ac7a6587ca922c6c4052fa894998c906c57d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:43 +0000

    Add package fmp-x/0.1.4/fmp-x-0.1.4.tar.gz

---
+++ packages/fmp-x/fmp-x-0.1.4.tar.gz/fmp-x-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fmp-x",
+    version="0.1.4",
+    description="fmp quant client",
+    author="Ron Stark",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.25.2",
+        "pandas==2.1.0",
+        "python-dotenv==1.0.0",
+        "python_dateutil==2.8.2",
+        "Requests==2.31.0",
+        "retry==0.9.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            # 如果你的包包含可执行脚本，这里列出它们
+        ],
+    },
+)

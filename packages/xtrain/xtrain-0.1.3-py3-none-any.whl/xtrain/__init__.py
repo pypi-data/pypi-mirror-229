32e5fd49b89ca0f6199c5300dfbd408267338d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:22:15 +0000

    Add package xtrain/0.1.3/xtrain-0.1.3-py3-none-any.whl

---
+++ packages/xtrain/xtrain-0.1.3-py3-none-any.whl/xtrain/__init__.py
@@ -0,0 +1,5 @@
+from .data import *
+from .loss import LossLog, loss_func_on, partial_loss_func
+from .strategy import JIT, Core, Distributed, Eager, VMapped
+from .trainer import Trainer
+from .utils import Inputs

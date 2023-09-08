729f7a07fdd99939d77833fdd8c97221441db209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:22:53 +0000

    Add package sunpeek-exampledata/0.1.1/sunpeek_exampledata-0.1.1-py3-none-any.whl

---
+++ packages/sunpeek-exampledata/sunpeek_exampledata-0.1.1-py3-none-any.whl/sunpeek_exampledata/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+DEMO_CONFIG_PATH = Path(__file__).with_name('plant_config_FHW_Arcon_South.json')
+
+DEMO_DATA_PATH_2DAYS = Path(__file__).with_name('FHW__array_ArcS__2017-05-01__2017-05-02__1m__UTC.csv')
+DEMO_DATA_PATH_1MONTH = Path(__file__).with_name('FHW__array_ArcS__2017-05-01__2017-05-31__1m__UTC.csv')
+DEMO_DATA_PATH_1YEAR = Path(__file__).with_name('FHW__array_ArcS__2017-01-01__2017-12-31__1m__UTC.csv')
+
+DEMO_FLUID_RHO_PATH = Path(__file__).with_name('Pekasolar, pdf export, density.csv')
+DEMO_FLUID_CP_PATH = Path(__file__).with_name('Pekasolar, pdf export, heat capacity.csv')
+

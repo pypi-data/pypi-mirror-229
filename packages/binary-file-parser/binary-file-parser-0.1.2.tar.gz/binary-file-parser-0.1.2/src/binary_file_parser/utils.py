bb78739b4ca71a2c2ec8f1ca945cbe240e80b685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:10:10 +0000

    Add package binary-file-parser/0.1.2/binary-file-parser-0.1.2.tar.gz

---
+++ packages/binary-file-parser/binary-file-parser-0.1.2.tar.gz/binary-file-parser-0.1.2/src/binary_file_parser/utils.py
@@ -0,0 +1,16 @@
+class Version(tuple[int, ...]):
+    """
+    - Functions equivalently to ``tuple[int, ...]``, e.g.: ``Version((1, 2, 3))`` is exactly the same as ``(1, 2, 3)``
+    - Exists solely for custom ``str()`` and ``repr()`` so ``Version((1, 2, 3))`` is printed as ``v1.2.3``
+    """
+    def __repr__(self):
+        return f"Version({super().__repr__()})"
+
+    def __str__(self):
+        return "v"+'.'.join(map(str, self))
+
+    def __init__(self, tup: tuple[int, ...]):
+        ...
+
+def indentify(repr_str: str, indent = 4) -> str:
+    return f"\n{' '*indent}".join(repr_str.splitlines())

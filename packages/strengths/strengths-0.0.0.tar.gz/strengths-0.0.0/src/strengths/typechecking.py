afdedf33d7f9fe34fc865b557d4aa893f471a59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:08:34 +0000

    Add package strengths/0.0.0/strengths-0.0.0.tar.gz

---
+++ packages/strengths/strengths-0.0.0.tar.gz/strengths-0.0.0/src/strengths/typechecking.py
@@ -0,0 +1,41 @@
+import numbers
+import collections
+import numpy as np
+import types
+
+def isnone(x) :
+    """
+    Returns True if x is None, False otherwise.
+    """
+
+    return type(x) == types.NoneType
+
+def isnumber(x):
+    """
+    Returns True if x is a number, False otherwise.
+    """
+
+    return isinstance(x, numbers.Number)
+
+def isarray(x):
+    """
+    Returns True if x is an array (numpy.ndarray [#numpy_ndarray]_ or list), False otherwise.
+    """
+    # references :
+    # .. [#numpy_ndarray] Numpy Developers. numpy 1.25 API reference : numpy.ndarray. (consulted on september 04, 2023). https://numpy.org/doc/stable/reference/generated/numpy.ndarray.html
+    
+    return type(x) == np.ndarray or type(x) == list
+
+def isdict(x) :
+    """
+    Returns True if x is a dict, False otherwise.
+    """
+
+    return type(x) == dict
+
+def isstr(x) :
+    """
+    Returns True if x is a str, False otherwise.
+    """
+
+    return type(x) == str

6bf30a67528ca2b5d8277d1d7ed358194c667dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:36:22 +0000

    Add package numpy-neural-network/0.1.3/numpy_neural_network-0.1.3.tar.gz

---
+++ packages/numpy-neural-network/numpy_neural_network-0.1.3.tar.gz/numpy_neural_network-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='numpy_neural_network',
+    version='0.1.3',    
+    description='A custom neural network implementation in numpy.',
+    url='https://github.com/AsimWattoo/Numpy_NN_Package.git',
+    author='Muhammad Asim',
+    author_email='asimwattoo6@gmail.com',
+    license='BSD 2-clause',
+    packages=['numpy_nn'],
+    install_requires=['pandas',
+                      'numpy',
+                      'matplotlib'                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: Freeware', 
+        'Natural Language :: English',
+    ],
+)

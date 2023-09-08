1f6da677048120058cd788afb054863c7199f8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:50 +0000

    Add package so-ml-tools/0.1.13/so-ml-tools-0.1.13.tar.gz

---
+++ packages/so-ml-tools/so-ml-tools-0.1.13.tar.gz/so-ml-tools-0.1.13/so_ml_tools/__init__.py
@@ -0,0 +1,33 @@
+from . import (
+    data,
+    multiclass,
+    nlp,
+    pandas,
+    regression,
+    tf,
+    util
+)
+
+# We follow Semantic Versioning (https://semver.org/spec/v2.0.0.html)
+_MAJOR_VERSION = '0'
+_MINOR_VERSION = '1'
+_PATCH_VERSION = '13'
+
+__version__ = '.'.join([
+    _MAJOR_VERSION,
+    _MINOR_VERSION,
+    _PATCH_VERSION,
+])
+
+__all__ = [
+    "data",
+    "multiclass",
+    "nlp",
+    "pandas",
+    "regression",
+    "tf",
+    "util",
+    "__version__"
+]
+
+
---
+++ packages/so-ml-tools/so-ml-tools-0.1.13.tar.gz/so-ml-tools-0.1.13/so_ml_tools/util/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    io,
+    label,
+    mixed_precision,
+    tensorboard
+)
+
+__all__ = [
+    "io",
+    "label",
+    "mixed_precision",
+    "tensorboard"
+]

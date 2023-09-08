b3c26685992aa462ec2a9384e68a68a94675e258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:22:05 +0000

    Add package Number4/4.0.0b1/Number4-4.0.0b1.tar.gz

---
+++ packages/Number4/Number4-4.0.0b1.tar.gz/Number4-4.0.0b1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='Number4',
+    version='4.0.0b1',
+    py_modules=['Number4'],
+    license='MIT License',
+    keywords='number',
+    python_requires='>=3.10, <4',
+    )

c61ba2ffb29c7a282da1be727f8a0e98bdc576a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:35 +0000

    Add package pypck-dummy/0.0.5/pypck_dummy-0.0.5.tar.gz

---
+++ packages/pypck-dummy/pypck_dummy-0.0.5.tar.gz/pypck_dummy-0.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='pypck_dummy',
+	version='0.0.5',
+	description='This is a test.',
+	author='',
+	author_email='',
+	url='https://gitub.com/...',
+	packages=find_packages(),
+	install_requires=[]
+)

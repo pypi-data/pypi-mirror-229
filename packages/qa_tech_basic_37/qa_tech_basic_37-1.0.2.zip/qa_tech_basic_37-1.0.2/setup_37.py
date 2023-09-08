8bb65a9b429253fa9dc2d1655878457bc7e37f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:47 +0000

    Add package qa_tech_basic_37/1.0.2/qa_tech_basic_37-1.0.2.zip

---
+++ packages/qa_tech_basic_37/qa_tech_basic_37-1.0.2.zip/qa_tech_basic_37-1.0.2/setup_37.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# author:      hj
+# create_time: 2019/5/14 16:12
+# update_time: 2019/5/14 16:12
+from distutils.core import setup
+
+setup(
+    name='qa_tech_basic_37',  # 对外我们模块的名字
+    version='1.0.2',  # 版本号
+    description='齐安科技基础包',  # 描述
+    author='hj',  # 作者
+    author_email='56172032@qq.com',
+    # install_requires=['sqlite3', 'Crypto.Cipher.AES'],
+    py_modules=['qa_tech_basic_37.configuration', 'qa_tech_basic_37.regex_utils', 'qa_tech_basic_37.time_utils',
+                'qa_tech_basic_37.yaml_configuration_utils']
+)

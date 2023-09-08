26c706a99d06c04064970da47f9dd51305e3323e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:36:41 +0000

    Add package defi-state-querier/0.0.8/defi-state-querier-0.0.8.tar.gz

---
+++ packages/defi-state-querier/defi-state-querier-0.0.8.tar.gz/defi-state-querier-0.0.8/src/defi_services/constants/query_constant.py
@@ -0,0 +1,17 @@
+class Query:
+    address = "address"
+    abi = "abi"
+    function = "function"
+    params = "params"
+    block_number = "block_number"
+
+    # query types
+    token_balance = 'token_balance'
+    nft_balance = 'nft_balance'
+    deposit_borrow = 'deposit_borrow'
+    protocol_reward = 'protocol_reward'
+    protocol_apy = 'protocol_apy'
+    all = [token_balance, nft_balance, deposit_borrow, protocol_reward]
+
+    # entity_id
+    token = "token"

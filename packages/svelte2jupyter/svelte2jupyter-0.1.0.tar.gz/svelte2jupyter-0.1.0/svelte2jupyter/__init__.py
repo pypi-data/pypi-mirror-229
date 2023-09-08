80e592d5b5ade3421e8e865f747d1cefcdf52879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 05:15:33 +0000

    Add package svelte2jupyter/0.1.0/svelte2jupyter-0.1.0.tar.gz

---
+++ packages/svelte2jupyter/svelte2jupyter-0.1.0.tar.gz/svelte2jupyter-0.1.0/svelte2jupyter/__init__.py
@@ -0,0 +1,17 @@
+"""Module svelte2jupyter printing python version."""
+from svelte2jupyter.npm_utils import (
+    PROJECT_DIR,
+    add_npm_dependency,
+    build_components,
+    check_if_node_modules_exists,
+    init_npm_project,
+)
+from svelte2jupyter.package import package_component
+from svelte2jupyter.render_component import render_component
+
+# if node modules does notlocally, list a warning message to run npm init.
+if not check_if_node_modules_exists():
+    print(
+        f"WARNING! It looks like the node project hasn't been set up yet in {PROJECT_DIR}.\n Running `init_npm_project` to set it up now:"
+    )
+    init_npm_project()

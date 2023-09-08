e6f01c3837cf5e6f828b569f116c978d504d7831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:20 +0000

    Add package pyaml-object/2.0.0/pyaml_object-2.0.0-py3-none-any.whl

---
+++ packages/pyaml-object/pyaml_object-2.0.0-py3-none-any.whl/pyyaml_object/utils.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+class Node:
+    def __str__(self) -> str:
+        return f"Node(id={id(self)})"
+
+    def __repr__(self) -> str:
+        return f"Node(id={id(self)})"

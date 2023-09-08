9c3f4f576fdcaeeb25cffe74fbf6b1e208684a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:31 +0000

    Add package kot/0.38.20/kot-0.38.20.tar.gz

---
+++ packages/kot/kot-0.38.20.tar.gz/kot-0.38.20/kot/__init__.py
@@ -0,0 +1,10 @@
+from .kot import KOT
+from .kot import HASHES
+from .kot import KOT_Serial
+from .kot import KOT_Remote
+from .kot import KOT_Cloud
+from .kot import KOT_Cloud_Pro
+
+
+__version__ = '0.38.20'
+

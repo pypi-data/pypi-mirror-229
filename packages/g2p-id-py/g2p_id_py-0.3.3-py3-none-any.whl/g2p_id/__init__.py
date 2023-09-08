ea8bf6beebadd10e809b06b2b91225812196c2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:14 +0000

    Add package g2p-id-py/0.3.3/g2p_id_py-0.3.3-py3-none-any.whl

---
+++ packages/g2p-id-py/g2p_id_py-0.3.3-py3-none-any.whl/g2p_id/__init__.py
@@ -0,0 +1,7 @@
+from .g2p import G2p
+from .lstm import LSTM
+from .bert import BERT
+from .text_processor import TextProcessor
+
+__version__ = "0.3.3"
+__all__ = ["G2p", "LSTM", "BERT", "TextProcessor"]

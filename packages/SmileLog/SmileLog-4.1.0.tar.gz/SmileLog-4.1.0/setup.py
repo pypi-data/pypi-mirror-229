3fb1455c981b38a72da60b902b5269fb00e32352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:17 +0000

    Add package SmileLog/4.1.0/SmileLog-4.1.0.tar.gz

---
+++ packages/SmileLog/SmileLog-4.1.0.tar.gz/SmileLog-4.1.0/setup.py
@@ -0,0 +1,35 @@
+#import setuptools
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="SmileLog",
+    version="4.1.0",
+    author="Sitthykun LY",
+    author_email="ly.sitthykun@gmail.com",
+    description="Log library",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sitthykun/smilelog",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+        	"Programming Language :: Python :: 3.11",
+		"License :: OSI Approved :: MIT License",
+        	"Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'async-timeout>=4.0.2',
+        'Deprecated>=1.2.13',
+        'packaging>=21.3',
+        'pyparsing>=3.0.9',
+        'redis>=5.0.0',
+        'wrapt>=1.14.1'
+    ],
+)

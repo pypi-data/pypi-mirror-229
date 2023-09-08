e3aaafefc4f8e9283571dd869ff50b76f38776d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 05:14:29 +0000

    Add package robocorp-tasks/2.2.0/robocorp_tasks-2.2.0-py3-none-any.whl

---
+++ packages/robocorp-tasks/robocorp_tasks-2.2.0-py3-none-any.whl/robocorp/tasks/_protocols.py
@@ -0,0 +1,170 @@
+import typing
+from pathlib import Path
+from types import TracebackType
+from typing import Any, Callable, Optional, Sequence, Set, TypeVar, Union
+
+ExcInfo = tuple[type[BaseException], BaseException, TracebackType]
+OptExcInfo = Union[ExcInfo, tuple[None, None, None]]
+
+
+T = TypeVar("T")
+Y = TypeVar("Y", covariant=True)
+
+
+def check_implements(x: T) -> T:
+    """
+    Helper to check if a class implements some protocol.
+
+    :important: It must be the last method in a class due to
+                https://github.com/python/mypy/issues/9266
+
+        Example:
+
+    def __typecheckself__(self) -> None:
+        _: IExpectedProtocol = check_implements(self)
+
+    Mypy should complain if `self` is not implementing the IExpectedProtocol.
+    """
+    return x
+
+
+# Note: this is a bit messy as we're mixing task states with log levels.
+# Note2: This is for the log.html and not really for user APIs.
+class Status:
+    NOT_RUN = "NOT_RUN"  # Initial status for a task which is not run.
+    PASS = "PASS"  # Used for task pass
+    FAIL = "FAIL"  # Used for task failure
+
+    ERROR = "ERROR"  # log.critical
+    INFO = "INFO"  # log.info
+    WARN = "WARN"  # log.warn
+    DEBUG = "DEBUG"  # log.debug
+
+
+class ITask(typing.Protocol):
+    module_name: str
+    filename: str
+    method: typing.Callable
+
+    status: str
+    message: str
+    exc_info: Optional[OptExcInfo]
+
+    @property
+    def name(self) -> str:
+        pass
+
+    @property
+    def lineno(self) -> int:
+        pass
+
+    def run(self) -> None:
+        pass
+
+    @property
+    def failed(self) -> bool:
+        """
+        Returns true if the task failed.
+        (in which case usually exc_info is not None).
+        """
+
+
+class IContextErrorReport(typing.Protocol):
+    def show_error(self, message):
+        pass
+
+
+class ICallback(typing.Protocol):
+    """
+    Note: the actual __call__ must be defined in a subclass protocol.
+    """
+
+    def register(self, callback):
+        pass
+
+    def unregister(self, callback):
+        pass
+
+
+class IAutoUnregisterContextManager(typing.Protocol):
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+class IOnTaskFuncFoundCallback(ICallback, typing.Protocol):
+    def __call__(self, func: Callable):
+        pass
+
+    def register(self, callback: Callable) -> IAutoUnregisterContextManager:
+        pass
+
+    def unregister(self, callback: Callable) -> None:
+        pass
+
+
+class IBeforeCollectTasksCallback(ICallback, typing.Protocol):
+    def __call__(self, path: Path, task_names: Set[str]):
+        pass
+
+    def register(
+        self, callback: Callable[[Path, Set[str]], Any]
+    ) -> IAutoUnregisterContextManager:
+        pass
+
+    def unregister(self, callback: Callable[[Path, Set[str]], Any]) -> None:
+        pass
+
+
+class IBeforeTaskRunCallback(ICallback, typing.Protocol):
+    def __call__(self, task: ITask):
+        pass
+
+    def register(
+        self, callback: Callable[[ITask], Any]
+    ) -> IAutoUnregisterContextManager:
+        pass
+
+    def unregister(self, callback: Callable[[ITask], Any]) -> None:
+        pass
+
+
+class IBeforeAllTasksRunCallback(ICallback, typing.Protocol):
+    def __call__(self, tasks: Sequence[ITask]):
+        pass
+
+    def register(
+        self, callback: Callable[[Sequence[ITask]], Any]
+    ) -> IAutoUnregisterContextManager:
+        pass
+
+    def unregister(self, callback: Callable[[Sequence[ITask]], Any]) -> None:
+        pass
+
+
+class IAfterAllTasksRunCallback(ICallback, typing.Protocol):
+    def __call__(self, tasks: Sequence[ITask]):
+        pass
+
+    def register(
+        self, callback: Callable[[Sequence[ITask]], Any]
+    ) -> IAutoUnregisterContextManager:
+        pass
+
+    def unregister(self, callback: Callable[[Sequence[ITask]], Any]) -> None:
+        pass
+
+
+class IAfterTaskRunCallback(ICallback, typing.Protocol):
+    def __call__(self, task: ITask):
+        pass
+
+    def register(
+        self, callback: Callable[[ITask], Any]
+    ) -> IAutoUnregisterContextManager:
+        pass
+
+    def unregister(self, callback: Callable[[ITask], Any]) -> None:
+        pass
---
+++ packages/robocorp-tasks/robocorp_tasks-2.2.0-py3-none-any.whl/robocorp/tasks/cli.py
@@ -0,0 +1,30 @@
+"""Main entry point for running tasks from robocorp-tasks.
+
+Note that it's usually preferable to use `robocorp-tasks` as a command line
+tool, using it programmatically through the main(args) in this module is also
+possible.
+
+Note: when running tasks, clients using this approach MUST make sure that any
+code which must be automatically logged is not imported prior the the `cli.main`
+call.
+"""
+
+import sys
+
+# Just importing is enough to register the commands
+from . import _commands  # @UnusedImport
+from ._argdispatch import arg_dispatch as _arg_dispatch
+
+
+def main(args=None, exit: bool = True) -> int:
+    """Entry point for running tasks from robocorp-tasks."""
+    if args is None:
+        args = sys.argv[1:]
+    returncode = _arg_dispatch.process_args(args)
+    if exit:
+        sys.exit(returncode)
+    return returncode
+
+
+if __name__ == "__main__":
+    main()

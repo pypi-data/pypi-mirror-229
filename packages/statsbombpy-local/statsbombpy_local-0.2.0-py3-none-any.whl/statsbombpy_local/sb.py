59c6378a124214bd3a37b22b2e150a61b514b28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:15 +0000

    Add package statsbombpy-local/0.2.0/statsbombpy_local-0.2.0-py3-none-any.whl

---
+++ packages/statsbombpy-local/statsbombpy_local-0.2.0-py3-none-any.whl/statsbombpy_local/sb.py
@@ -0,0 +1,26 @@
+from ._api import (
+    competitions,
+    matches,
+    lineups,
+    events,
+    frames,
+    competition_events,
+    competition_frames,
+    player_match_stats,
+    player_season_stats,
+    team_season_stats,
+)
+
+
+__all__ = (
+    "competitions",
+    "matches",
+    "lineups",
+    "events",
+    "frames",
+    "competition_events",
+    "competition_frames",
+    "player_match_stats",
+    "player_season_stats",
+    "team_season_stats",
+)

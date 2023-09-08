b24b4befd11aa901e11bb191fa8c25c38fa54420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:28 +0000

    Add package Skylark-Grid/0.2/Skylark_Grid-0.2.tar.gz

---
+++ packages/Skylark-Grid/Skylark_Grid-0.2.tar.gz/Skylark_Grid-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('readme.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='Skylark_Grid',
+    version='0.2',
+    description='An image grid maker tool',
+    author='Prem Varma',
+    author_email='prem.varma@skylarklabs.ai',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'numpy'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)

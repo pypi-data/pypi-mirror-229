022a6f2b590a325de676e24eda56d8e35ef0d0a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:56 +0000

    Add package RepoDynamics/0.0.0.dev95/RepoDynamics-0.0.0.dev95-py3-none-any.whl

---
+++ packages/RepoDynamics/RepoDynamics-0.0.0.dev95-py3-none-any.whl/repodynamics/meta/files/__init__.py
@@ -0,0 +1,23 @@
+import json
+
+from repodynamics.logger import Logger
+from repodynamics.meta.reader import MetaReader
+from repodynamics.meta.files.config import ConfigFileGenerator
+from repodynamics.meta.files.health import HealthFileGenerator
+from repodynamics.meta.files.package import PackageFileGenerator
+from repodynamics.meta.files.readme import ReadmeFileGenerator
+
+def generate(metadata: dict, reader: MetaReader, logger: Logger = None) -> list[dict]:
+    if not isinstance(reader, MetaReader):
+        raise TypeError(f"reader must be of type MetaReader, not {type(reader)}")
+    logger = logger or reader.logger
+    logger.h2("Generate Files")
+    updates = [
+        dict(category="metadata", name="metadata", content=json.dumps(metadata)),
+        dict(category="license", name="license", content=metadata.get("license_txt", "")),
+    ]
+    updates += ReadmeFileGenerator(metadata=metadata, logger=logger, path_root=reader.path_root).generate()
+    updates += ConfigFileGenerator(metadata=metadata, logger=logger).generate()
+    updates += HealthFileGenerator(metadata=metadata, reader=reader, logger=logger).generate()
+    updates += PackageFileGenerator(metadata=metadata, reader=reader, logger=logger).generate()
+    return updates

19a40e692573ec7222c5a8270123d08546447962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:45 +0000

    Add package fixer-demo/0.0.1/fixer-demo-0.0.1.tar.gz

---
+++ packages/fixer-demo/fixer-demo-0.0.1.tar.gz/fixer-demo-0.0.1/fixer/config.py
@@ -0,0 +1,4 @@
+BASE_PATH = "http://data.fixer.io/api/latest?format=1&access_key="
+API_KEY = "c900e85aad6d4f2e29d594d8c355814a"
+
+url = BASE_PATH + API_KEY
---
+++ packages/fixer-demo/fixer-demo-0.0.1.tar.gz/fixer-demo-0.0.1/fixer/handler.py
@@ -0,0 +1,9 @@
+# import requests
+# from config import url
+
+
+def get_currency_rates():
+    # response = requests.get(url)
+    # data = response.json()
+    # return data
+    return "test currency"

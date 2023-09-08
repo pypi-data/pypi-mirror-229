a00fb47bc2be49ce573e2fc1c9cf2aa92d2a297b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:25:12 +0000

    Add package forestadmin-agent-toolkit/1.0.0b11/forestadmin_agent_toolkit-1.0.0b11-py3-none-any.whl

---
+++ packages/forestadmin-agent-toolkit/forestadmin_agent_toolkit-1.0.0b11-py3-none-any.whl/forestadmin/agent_toolkit/options.py
@@ -0,0 +1,39 @@
+import logging
+import sys
+from typing import Callable, Optional
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+class Options(TypedDict):
+    # application_url: str  # useless for now
+    auth_secret: str
+    prefix: str
+    env_secret: str
+    forest_server_url: str
+    is_production: bool
+    schema_path: str
+    logger: Callable[[str, str], None]
+    logger_level: int
+    permissions_cache_duration_in_seconds: int
+    customize_error_message: Callable[[Exception], str]
+    instant_cache_refresh: Optional[bool]
+    # typingsPath
+    # typingsMaxDepth
+    # skipSchemaUpdate
+    # forestAdminClient
+
+
+DEFAULT_OPTIONS: Options = {
+    "is_production": False,
+    "prefix": "forest",
+    "forest_server_url": "https://api.forestadmin.com",
+    "logger": None,
+    "logger_level": logging.INFO,
+    "customize_error_message": None,
+    "permissions_cache_duration_in_seconds": 15 * 60,
+    "instant_cache_refresh": None,
+}
---
+++ packages/forestadmin-agent-toolkit/forestadmin_agent_toolkit-1.0.0b11-py3-none-any.whl/forestadmin/agent_toolkit/utils/forest_schema/type.py
@@ -0,0 +1,137 @@
+import enum
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal, TypedDict
+else:
+    from typing_extensions import Literal, TypedDict
+
+from typing import Any, List, Optional, Union
+
+from forestadmin.datasource_toolkit.interfaces.fields import ColumnAlias
+from typing_extensions import NotRequired
+
+
+class ValidationType(enum.Enum):
+    PRESENT = "is present"
+    GREATER_THAN = "is greater than"
+    LESS_THAN = "is less than"
+    BEFORE = "is before"
+    AFTER = "is after"
+    LONGER_THAN = "is longer than"
+    SHORTER_THAN = "is shorter than"
+    CONTAINS = "contains"
+    LIKE = "is like"
+
+
+class ServerValidationType(TypedDict):
+    type: ValidationType
+    value: Optional[Any]
+    message: Optional[Any]
+
+
+LiteralHasOne = Literal["HasOne"]
+LiteralHasMany = Literal["HasMany"]
+LiteralBelongsTo = Literal["BelongsTo"]
+LiteralBelongsToMany = Literal["BelongsToMany"]
+
+RelationServer = Union[LiteralHasOne, LiteralHasMany, LiteralBelongsTo, LiteralBelongsToMany]
+
+
+class AgentStackMeta(TypedDict, total=False):
+    engine: Literal["python"]
+    engine_version: str
+    database_type: str
+    orm_version: str
+
+
+class AgentMeta(TypedDict):
+    liana: str
+    liana_version: str
+    stack: AgentStackMeta
+    schemaFileHash: str
+
+
+class ForestServerField(TypedDict, total=False):
+    field: str
+    type: ColumnAlias
+    defaultValue: Any
+    enums: Optional[List[str]]
+    integration: None
+    isFilterable: bool
+    isPrimaryKey: bool
+    isReadOnly: bool
+    isRequired: bool
+    isSortable: bool
+    isVirtual: bool
+    reference: Optional[str]
+    inverseOf: Optional[str]
+    relationship: RelationServer
+    validations: List[ServerValidationType]
+
+
+LiteralPage = Literal["page"]
+
+
+class ForestServerActionHooks(TypedDict):
+    load: bool
+    change: List[Any]
+
+
+class ForestServerActionField(TypedDict):
+    value: Any
+    defaultValue: Any
+    description: Optional[str]
+    enums: Optional[List[str]]
+    field: str
+    hook: Optional[str]
+    isReadOnly: bool
+    isRequired: bool
+    reference: Optional[str]
+    type: Union[ColumnAlias, Literal["File"]]
+    widget: Optional[Literal["belongsto select", "file picker"]]
+
+
+class ForestServerAction(TypedDict):
+    id: str
+    name: str
+    type: Literal["single", "bulk", "global"]
+    baseUrl: Optional[str]
+    endpoint: str
+    httpMethod: Literal["POST"]
+    redirect: Any
+    download: bool
+    fields: List[ForestServerActionField]
+    hooks: ForestServerActionHooks
+
+
+"""
+export type ForestServerSegment = {
+  id: string;
+  name: string;
+};
+"""
+
+
+class ForestServerSegment(TypedDict):
+    id: str
+    name: str
+
+
+class ForestServerCollection(TypedDict):
+    name: str
+    icon: None
+    integration: None
+    isReadOnly: bool
+    isSearchable: bool
+    isVirtual: bool
+    onlyForRelationships: bool
+    paginationType: LiteralPage
+    actions: NotRequired[List[ForestServerAction]]
+    fields: List[ForestServerField]
+    segments: NotRequired[List[ForestServerSegment]]
+
+
+class ForestSchema(TypedDict):
+    data: List[ForestServerCollection]
+    meta: AgentMeta

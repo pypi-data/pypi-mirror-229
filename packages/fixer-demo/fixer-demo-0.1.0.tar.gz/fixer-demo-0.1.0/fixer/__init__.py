b85766a0d6937d1632587ca1ef4f9f024d8d307c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:23 +0000

    Add package fixer-demo/0.1.0/fixer-demo-0.1.0.tar.gz

---
+++ packages/fixer-demo/fixer-demo-0.1.0.tar.gz/fixer-demo-0.1.0/fixer/__init__.py
@@ -0,0 +1,3 @@
+from fixer.handler import get_currency_rates
+
+__all__ = ['get_currency_rates']
---
+++ packages/fixer-demo/fixer-demo-0.1.0.tar.gz/fixer-demo-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fixer-demo',
+    version='0.1.0',
+    description='Fixer Demo',
+    author='Amir',
+    author_email='amirb938@gmail.com',
+    license='MIT',
+    packages=['fixer'])

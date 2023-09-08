4ea28ee544660459d5415466ac2c2858335eb347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:54 +0000

    Add package simple-commands/1.0.0/simple_commands-1.0.0.tar.gz

---
+++ packages/simple-commands/simple_commands-1.0.0.tar.gz/simple_commands-1.0.0/simple_commands/__init__.py
@@ -0,0 +1,7 @@
+from .file.file import (create_new_directory, list_files_in_current_directory, move_file, delete_directory,
+ main_folder, folder_file,)
+from .classpy.SQL_class.SQL import Database
+from .img.image import (load_images, adjust_img_color, show, new_data_image, gray_image, use_PIL)
+from .time_zone.time_zone import Time_zone
+from .help import help
+from .math_ import calculate_triangle_area, calculate_rectangle_area, calculate_square_area, calculate_circle_area, calculate_triangle_perimeter, calculate_rectangle_perimeter, calculate_square_perimeter, calculate_circle_circumference, calculate_cone_volume, calculate_cylinder_volume, calculate_oxygen_cylinder_volume, tempFtoC, tempCtoF

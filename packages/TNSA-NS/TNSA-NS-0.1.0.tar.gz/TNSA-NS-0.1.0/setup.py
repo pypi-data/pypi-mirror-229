293ee9d50088f112b4e5513173697247bc659886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:23:36 +0000

    Add package TNSA-NS/0.1.0/TNSA-NS-0.1.0.tar.gz

---
+++ packages/TNSA-NS/TNSA-NS-0.1.0.tar.gz/TNSA-NS-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='TNSA-NS',
+    version='0.1.0',
+    description='TNSA - NS IS A NEURAL NETWORK MODULE USED IN THE DEVELOPMENT OF NEURAL NETWORK PROCESSING',
+    author='TNSA',
+    author_email='thishyakethabimalla@gmail.com',
+    packages=['TNSA'],
+)

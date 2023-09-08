bc7e9adbffe01cf5a5f37866844ace2f234e8629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:24:36 +0000

    Add package PriorKnowledgeNeuralODE/1.0.4/PriorKnowledgeNeuralODE-1.0.4.tar.gz

---
+++ packages/PriorKnowledgeNeuralODE/PriorKnowledgeNeuralODE-1.0.4.tar.gz/PriorKnowledgeNeuralODE-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PriorKnowledgeNeuralODE',
+    version='1.0.4',
+    description='a compilation of methods to incorporate constraints into Neural ODE models',
+    license='MIT',
+    packages=['PriorKnowledgeNeuralODE'],
+    author='Cecília Coelho',
+    author_email='cmartins@cmat.uminho.pt',
+    keywords=['Neural ODE', 'Constrained Optimization', 'Time-series', "Neural Networks"],
+    url='https://github.com/CeciliaCoelho/PriorKnowledgeNeuralODE',
+    long_description='README',
+)

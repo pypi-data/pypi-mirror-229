3306ea0dfc577328f931b2096322a451181d89cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:36:19 +0000

    Add package netbox-secrets/1.9.0/netbox-secrets-1.9.0.tar.gz

---
+++ packages/netbox-secrets/netbox-secrets-1.9.0.tar.gz/netbox-secrets-1.9.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-secrets',
+    version='1.9.0',
+    description='Netbox Secrets',
+    long_description='A Secret store for NetBox',
+    url='https://github.com/Onemind-Services-LLC/netbox-secrets/',
+    author='Daniel Sheppard',
+    author_email='dans@dansheps.com',
+    license='Apache 2.0',
+    install_requires=[
+        'pycryptodome',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    zip_safe=False,
+)

2c0621beefc69e04fe49f4358f52524e5f263697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:48 +0000

    Add package nonebot-plugin-quickban/0.0.3/nonebot-plugin-quickban-0.0.3.tar.gz

---
+++ packages/nonebot-plugin-quickban/nonebot-plugin-quickban-0.0.3.tar.gz/nonebot-plugin-quickban-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="nonebot-plugin-quickban",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="0.0.3",  # 程序版本
+    author="ghb010417",  # 项目作者
+    author_email="113217194@qq.com",  # 作者邮件
+    description="快捷禁言",  # 项目的一句话描述
+    long_description=long_description,  # 加长版描述？
+    long_description_content_type="text/markdown",  # 描述使用Markdown
+    url="https://github.com/mas-alone/nonebot-plugin-sleep",  # 项目地址
+    packages=setuptools.find_packages(),  # 无需修改
+    classifiers=[
+        "Programming Language :: Python :: 3.8",  # 使用Python3.8
+        "License :: OSI Approved :: GNU Affero General Public License v3",  # 开源协议
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'nonebot2>=2.0.0rc3',
+        'nonebot-adapter-onebot>=2.0.0b1'
+    ]
+)

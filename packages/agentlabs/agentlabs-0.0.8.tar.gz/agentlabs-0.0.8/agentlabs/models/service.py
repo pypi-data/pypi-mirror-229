4d16f339ece3757919b91b0fb02e6ed326b85793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:50 +0000

    Add package agentlabs/0.0.8/agentlabs-0.0.8.tar.gz

---
+++ packages/agentlabs/agentlabs-0.0.8.tar.gz/agentlabs-0.0.8/agentlabs/models/service.py
@@ -0,0 +1,20 @@
+from typing import Callable, List, Dict, Any, Optional
+from pydantic import BaseModel
+
+
+class ServiceInputParam(BaseModel):
+    name: str
+    display_name: str
+    description: str
+    required: bool
+    type: str
+    placeholder: Optional[str]
+    choices: Optional[List[Any]]
+
+
+class Service(BaseModel):
+    id: int
+    name: str
+    description: str
+    func: Callable
+    input_params: List[ServiceInputParam]

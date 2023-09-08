907bdd7c2276a327d580ce0b33bf206b70c13a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:24:56 +0000

    Add package ssos/1.3.5/ssos-1.3.5.tar.gz

---
+++ packages/ssos/ssos-1.3.5.tar.gz/ssos-1.3.5/ssos/__init__.py
@@ -0,0 +1,20 @@
+name = "ssos"
+__version__ = "1.3.5"
+
+
+GREETING = (
+    """
+       *       +      ssos %s
+   +        x
+        '             https://ssos.readthedocs.io
+*   +     _.::'   |
+        :_.'    - o - ssos --default : Copy default settings to CWD
+|                 |   ssos --help    : Print detailed help
+O -     *        '    ssos [-PARAMETER] [-VALUE] path/to/images
+|                     ssos --inspect path/to/output : Inspect SSO candidates
+        _..:'
+  +   (_.:            max.mahlke(at)ias.u-psud.fr
+
+        """
+    % __version__
+)

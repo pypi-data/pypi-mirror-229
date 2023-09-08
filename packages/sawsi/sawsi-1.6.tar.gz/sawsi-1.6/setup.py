74e1bd378e83e1fa8b3104a6414a39d941af58f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:15 +0000

    Add package sawsi/1.6/sawsi-1.6.tar.gz

---
+++ packages/sawsi/sawsi-1.6.tar.gz/sawsi-1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sawsi",
+    version="1.6",
+    packages=find_packages(),
+    install_requires=[
+        'boto3==1.28.43',
+        'PyJWT==2.8.0',
+        'requests==2.31.0'
+    ],
+)
+

170ec8cd9a2a752fc07b65fb2659ea46727758dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:08:29 +0000

    Add package GeekFongPypiHellow/1.0.3/GeekFongPypiHellow-1.0.3.tar.gz

---
+++ packages/GeekFongPypiHellow/GeekFongPypiHellow-1.0.3.tar.gz/GeekFongPypiHellow-1.0.3/GeekFongPypiHellow/TestPypiHellow.py
@@ -0,0 +1,9 @@
+class TestPyTest:
+    def ppd(self):
+        print("pddddd")
+        
+    def ppc(self):
+        print("1111pddddd")
+        
+    def ppa(self):
+        print("1111pddddd")
---
+++ packages/GeekFongPypiHellow/GeekFongPypiHellow-1.0.3.tar.gz/GeekFongPypiHellow-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GeekFongPypiHellow',
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ]
+)

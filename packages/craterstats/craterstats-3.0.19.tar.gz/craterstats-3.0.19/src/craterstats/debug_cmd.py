037c881457b03a7561ff50251344908f82d962d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:08:29 +0000

    Add package craterstats/3.0.19/craterstats-3.0.19.tar.gz

---
+++ packages/craterstats/craterstats-3.0.19.tar.gz/craterstats-3.0.19/src/craterstats/debug_cmd.py
@@ -0,0 +1,19 @@
+
+import cli
+
+def main():
+    out = ' -o d:/mydocs/tmp/out.png'
+    cmd = '--help'
+    cmd = '-p source=sample/sample.scc'+out
+    cmd = r'-cs MarsNI2001 -p source=D:\mydocs\tmp\test_opencratertools\test1.scc -p type=bpois,range=[8,20]' + out
+    cmd = r'-pr cumul -cs neukumivanov -p source=sample\sample.binned'
+    cmd = r'-legend n#r -cs marsni2001 -p source=D:\mydocs\tmp\test_opencratertools\test1.scc -p type=bpoiss,range=[6,80],offset_age=[14,-14]'
+    cmd = r'-cs marsni2001 -o d:/mydocs/tmp/ -p src="D:\mydocs\data\congzhe\greg\scarp1.scc" -p type=bp,range=[3,20]'
+
+    
+    print(f'\nDebugging command: craterstats '+cmd)
+    a = cmd.split()
+    cli.main(a)
+
+if __name__ == '__main__':
+    main()

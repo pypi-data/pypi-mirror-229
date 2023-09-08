4afe0cec391c9b6187bbd98e96a2cecd9dabee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 06:14:03 +0000

    Add package LogicHW0-CianeeS/0.0.222/LogicHW0_CianeeS-0.0.222-py3-none-any.whl

---
+++ packages/LogicHW0-CianeeS/LogicHW0_CianeeS-0.0.222-py3-none-any.whl/LogicHW0/__init__.py
@@ -0,0 +1,3 @@
+from is_prime import IsPrime
+from nth_prime import NthPrime
+from prime_factorization import PrimeFactorization
---
+++ packages/LogicHW0-CianeeS/LogicHW0_CianeeS-0.0.222-py3-none-any.whl/LogicHW0/is_prime.py
@@ -0,0 +1,26 @@
+"""
+Cianee Sumowalt
+Logic and Computation
+Professor McTague
+1 September 2023
+
+"""
+    
+class IsPrime:
+
+    def __init__(self,n):
+        self.n = n
+    def is_prime(self):
+         for i in range(2,self.n):
+            if self.n%i == 0:
+                return False
+         if self.n == 1:
+            return False
+         return True
+
+if __name__ == "__main__":
+
+    while True:
+        user_input = int(input('> '))
+        num = IsPrime(user_input)
+        print (num.is_prime())

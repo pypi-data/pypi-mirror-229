f3e2377991fde2eaab5b2ac3305e49bf7c602852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:36:37 +0000

    Add package pharmbio/0.1.5/pharmbio-0.1.5.tar.gz

---
+++ packages/pharmbio/pharmbio-0.1.5.tar.gz/pharmbio-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pharmbio",
+    version="0.1.5",
+    url="https://github.com/pharmbio/pharmbio_package",
+    author="Nima Chamyani",
+    author_email="nima.ch@gmail.com",
+    description="This is a Python package for interacting with Pharmb.io routine analysis.",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "polars",
+        "pyarrow",
+        "sqlalchemy",
+        "connectorx>=0.3.1",
+        "psycopg2-binary",
+        "plotly",
+        "nbformat",
+        "tqdm"
+    ],
+)

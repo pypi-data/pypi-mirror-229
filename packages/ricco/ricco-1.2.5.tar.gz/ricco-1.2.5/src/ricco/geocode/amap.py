64eeb71aa1833c17123c4ef323f5b447a507ce30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:12 +0000

    Add package ricco/1.2.5/ricco-1.2.5.tar.gz

---
+++ packages/ricco/ricco-1.2.5.tar.gz/ricco-1.2.5/src/ricco/geocode/amap.py
@@ -0,0 +1,125 @@
+import logging
+
+import requests
+
+from ..util.util import is_empty
+from .util import MapKeys
+from .util import MapUrls
+from .util import error_amap
+from .util import fix_address
+from .util import gcj2xx
+from .util import rv_score
+
+KEY = MapKeys.amap
+
+
+def address_json(city: str, address: str, key=None):
+  """高德地理编码接口"""
+  if is_empty(address):
+    return
+  if not key:
+    key = KEY
+  url = f'{MapUrls.amap}?address={address}&city={city}&key={key}'
+  js = requests.get(url).json()
+  error_amap(js)
+  if js['status'] == '1' and int(js['count']) >= 1:
+    return js['geocodes'][0]
+  return
+
+
+def place_json(city: str, keywords: str, key=None):
+  """高德地点检索接口"""
+  if is_empty(keywords):
+    return
+  if not key:
+    key = KEY
+  url = f'{MapUrls.amap_poi}?keywords={keywords}&city={city}&key={key}'
+  js = requests.get(url).json()
+  error_amap(js)
+  if js['status'] == '1' and int(js['count']) >= 1:
+    return js['pois'][0]
+  return
+
+
+def get_amap(*,
+             address,
+             city,
+             source,
+             with_detail=True,
+             disable_cache=False,
+             key=None):
+  """脉策geocode服务"""
+  if is_empty(address):
+    return
+  url = f'{MapUrls.mdt}?address={address}&city={city}&disable_cache={disable_cache}&with_detail={with_detail}&source={source}'
+  req = requests.get(url)
+  if req.status_code == 200:
+    try:
+      return req.json()['result'][0]['extra']
+    except Exception as e:
+      logging.warning(f'{e}，{req}')
+      return
+  elif req.status_code in (400, 403):
+    if source == 'amap':
+      return address_json(city=city, address=address, key=key)
+    elif source == 'amap_poi':
+      return place_json(city=city, keywords=address, key=key)
+    else:
+      raise ValueError('source参数错误')
+  else:
+    return
+
+
+def get_address_amap(city: str, address: str, srs: str = 'wgs84', key=None):
+  if is_empty(address):
+    return {
+      'rv': None,
+      'score': 0,
+      'lng': None,
+      'lat': None,
+    }
+  city = city.rstrip('市')
+  address = fix_address(address).replace('|', '')
+  address_dict = get_amap(city=city, address=address, source='amap', key=key)
+  if address_dict:
+    rv = address_dict['formatted_address']
+    lnglat = address_dict['location'].split(',')
+    latlng = gcj2xx(lnglat, srs=srs)
+    lng = latlng[1]
+    lat = latlng[0]
+  else:
+    rv, lng, lat = None, None, None
+  return {
+    'rv': rv,
+    'score': 0,
+    'lng': lng,
+    'lat': lat,
+  }
+
+
+def get_place_amap(city: str, keywords: str, srs='wgs84', key=None):
+  if is_empty(keywords):
+    return {
+      'rv': None,
+      'score': 0,
+      'lng': None,
+      'lat': None,
+    }
+  city = city.rstrip('市')
+  keywords = fix_address(keywords)
+  res = get_amap(city=city, address=keywords, source='amap_poi', key=key)
+  if res:
+    rv = res['name']
+    lnglat = res['location'].split(',')
+    latlng = gcj2xx(lnglat, srs=srs)
+    lng = latlng[1]
+    lat = latlng[0]
+    score = rv_score(city, keywords, rv)
+  else:
+    rv, lng, lat, score = None, None, None, 0
+  return {
+    'rv': rv,
+    'score': score,
+    'lng': lng,
+    'lat': lat,
+  }
---
+++ packages/ricco/ricco-1.2.5.tar.gz/ricco-1.2.5/src/ricco/geocode/util.py
@@ -0,0 +1,64 @@
+import warnings
+
+from ..geometry.coord_trans import gcj2bd
+from ..geometry.coord_trans import gcj2wgs
+from ..geometry.util import is_empty
+
+
+class MapUrls:
+  # 百度地理编码
+  baidu = 'http://api.map.baidu.com/geocoding/v3/'
+  # 百度地点检索
+  baidu_poi = 'http://api.map.baidu.com/place/v2/search'
+  # 高德地理编码
+  amap = 'https://restapi.amap.com/v3/geocode/geo'
+  # 高德地点检索
+  amap_poi = 'https://restapi.amap.com/v3/place/text'
+  # 脉策geocode服务
+  mdt = 'https://geocode.idatatlas.com/geocode'
+
+
+class MapKeys:
+  amap = '7c14855824549a84c543e48990239f3d'
+  baidu = '9Fy1lMHbwpr07WVBFPLw9vpntGUSOUMN'
+
+
+def gcj2xx(lnglat, srs):
+  assert srs in ('wgs84', 'bd09', 'gcj02'), '可选参数为bd09,wgs84,gcj02'
+  if srs == 'wgs84':
+    return gcj2wgs(float(lnglat[1]), float(lnglat[0]))
+  elif srs == 'bd09':
+    return gcj2bd(float(lnglat[1]), float(lnglat[0]))
+  else:
+    return float(lnglat[1]), float(lnglat[0])
+
+
+def error_baidu(js):
+  if js['status'] != 0:
+    warnings.warn(
+        '接口错误，状态码【%s】，错误原因请查阅：'
+        'https://lbsyun.baidu.com/index.php?title=webapi/appendix' % js[
+          'status']
+    )
+
+
+def error_amap(js):
+  if js['status'] != '1':
+    warnings.warn(
+        '接口错误，状态码【%s】，错误原因请查阅：'
+        'https://lbs.amap.com/api/webservice/guide/tools/info' % js['infocode']
+    )
+
+
+def fix_address(string):
+  if is_empty(string):
+    return ''
+  return str(string)
+
+
+def rv_score(city, keywords, rv):
+  from fuzzywuzzy import fuzz
+  return max(
+      fuzz.ratio(rv, keywords.lstrip(city)),
+      fuzz.partial_ratio(rv, keywords.lstrip(city))
+  )

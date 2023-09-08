9c393c53cc7a920d60c2f3878a02a8499c455420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:46 +0000

    Add package pythondatasdk/0.0.1/pythondatasdk-0.0.1-py3-none-any.whl

---
+++ packages/pythondatasdk/pythondatasdk-0.0.1-py3-none-any.whl/pythondatasdk/dbclient.py
@@ -0,0 +1,45 @@
+# Importing NumPy as "np"
+import pymongo
+from urllib.parse import quote_plus
+import json
+
+
+def insert(simulation_data):
+    print("Data Received", simulation_data)
+    # Parse the JSON data
+    document = json.loads(simulation_data)
+
+    username = "prasanth-007"
+    password = "Aws@143"
+    cluster_url = "cluster0.vn5aqd9.mongodb.net"
+    database_name = "poc"
+
+    # Encode the username and password
+    encoded_username = quote_plus(username)
+    encoded_password = quote_plus(password)
+
+    # Create the MongoDB URI with encoded credentials
+    mongo_uri = f"mongodb+srv://{encoded_username}:{encoded_password}@{cluster_url}/{database_name}?retryWrites=true&w=majority&ssl=true"
+
+    # Create a MongoDB client
+    client = pymongo.MongoClient(mongo_uri)
+
+    # Specify the database and collection where you want to insert the document
+    db = client.get_database(database_name)
+    collection = db.get_collection("simulation")
+
+    try:
+        # Attempt to insert the document into the collection
+        result = collection.insert_one(document)
+
+        # Check if the insertion was successful
+        if result.acknowledged:
+            print("Document inserted successfully. Inserted ID:", result.inserted_id)
+        else:
+            print("Document insertion failed.")
+    except Exception as e:
+        print("An error occurred:", str(e))
+    finally:
+        # Close the MongoDB client
+        client.close()
+        print("Connection is closed")

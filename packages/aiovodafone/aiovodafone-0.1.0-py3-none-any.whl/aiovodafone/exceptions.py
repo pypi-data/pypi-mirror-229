8b9c6aef4b7bb227bdc1c4684fffc729bbfa660d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:23:14 +0000

    Add package aiovodafone/0.1.0/aiovodafone-0.1.0-py3-none-any.whl

---
+++ packages/aiovodafone/aiovodafone-0.1.0-py3-none-any.whl/aiovodafone/exceptions.py
@@ -0,0 +1,18 @@
+"""Vodafone Station library exceptions."""
+from __future__ import annotations
+
+
+class VodafoneError(Exception):
+    """Base class for aiovodafone errors."""
+
+
+class CannotConnect(VodafoneError):
+    """Exception raised when connection fails."""
+
+
+class CannotAuthenticate(VodafoneError):
+    """Exception raised when credentials are incorrect."""
+
+
+class AlreadyLogged(VodafoneError):
+    """Exception raised if a user is already logged."""

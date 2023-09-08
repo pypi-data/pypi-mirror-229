babbd5d69523024429b7186e597a2621a40bd2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 05:15:31 +0000

    Add package ft-suite/0.1.8/ft_suite-0.1.8.tar.gz

---
+++ packages/ft-suite/ft_suite-0.1.8.tar.gz/ft_suite-0.1.8/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fts', 'fts.inference', 'fts.processing', 'fts.trainer', 'fts.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['accelerate',
+ 'bitsandbytes',
+ 'datasets',
+ 'optimum',
+ 'peft',
+ 'torch',
+ 'transformers']
+
+setup_kwargs = {
+    'name': 'ft-suite',
+    'version': '0.1.8',
+    'description': 'A fine-tuning suite based on Transformers and LoRA.',
+    'long_description': 'None',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

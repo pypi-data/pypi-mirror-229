8b0be0f1152eb6629cde1568a7aa8013bad3f166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 05:14:52 +0000

    Add package dune-client/1.2.2/dune_client-1.2.2.tar.gz

---
+++ packages/dune-client/dune_client-1.2.2.tar.gz/dune_client-1.2.2/README.md
@@ -0,0 +1,42 @@
+# Dune Client
+
+A python framework for interacting with Dune Analytics' [officially supported API
+service](https://duneanalytics.notion.site/API-Documentation-1b93d16e0fa941398e15047f643e003a).
+
+## Installation
+
+Import as a project dependency
+
+```shell
+pip install dune-client
+```
+
+# Example Usage
+
+Export your `DUNE_API_KEY` (or place it in a `.env` file - as in
+here [.env.sample](./.env.sample).
+
+```python
+from dune_client.types import QueryParameter
+from dune_client.client import DuneClient
+from dune_client.query import QueryBase
+
+query = QueryBase(
+    name="Sample Query",
+    query_id=1215383,
+    params=[
+        QueryParameter.text_type(name="TextField", value="Word"),
+        QueryParameter.number_type(name="NumberField", value=3.1415926535),
+        QueryParameter.date_type(name="DateField", value="2022-05-04 00:00:00"),
+        QueryParameter.enum_type(name="EnumField", value="Option 1"),
+    ],
+)
+print("Results available at", query.url())
+
+dune = DuneClient.from_env()
+results = dune.refresh(query)
+```
+
+For a more elaborate example,
+visit [dune-alerts](https://github.com/cowprotocol/dune-alerts)
+
---
+++ packages/dune-client/dune_client-1.2.2.tar.gz/dune_client-1.2.2/tests/unit/test_query.py
@@ -0,0 +1,65 @@
+import unittest
+from datetime import datetime
+
+from dune_client.query import QueryBase
+from dune_client.types import QueryParameter
+
+
+class TestQueryMonitor(unittest.TestCase):
+    def setUp(self) -> None:
+        self.date = datetime(year=1985, month=3, day=10)
+        self.query_params = [
+            QueryParameter.enum_type("Enum", "option1"),
+            QueryParameter.text_type("Text", "plain text"),
+            QueryParameter.number_type("Number", 12),
+            QueryParameter.date_type("Date", "2021-01-01 12:34:56"),
+        ]
+        self.query = QueryBase(name="", query_id=0, params=self.query_params)
+
+    def test_base_url(self):
+        self.assertEqual(self.query.base_url(), "https://dune.com/queries/0")
+
+    def test_url(self):
+        self.assertEqual(
+            self.query.url(),
+            "https://dune.com/queries/0?Enum=option1&Text=plain+text&Number=12&Date=2021-01-01+12%3A34%3A56",
+        )
+        self.assertEqual(QueryBase(0, "", []).url(), "https://dune.com/queries/0")
+
+    def test_parameters(self):
+        self.assertEqual(self.query.parameters(), self.query_params)
+
+    def test_request_format(self):
+        expected_answer = {
+            "query_parameters": {
+                "Enum": "option1",
+                "Text": "plain text",
+                "Number": "12",
+                "Date": "2021-01-01 12:34:56",
+            }
+        }
+        self.assertEqual(self.query.request_format(), expected_answer)
+
+    def test_hash(self):
+        # Same ID, different params
+        query1 = QueryBase(
+            query_id=0, params=[QueryParameter.text_type("Text", "word1")]
+        )
+        query2 = QueryBase(
+            query_id=0, params=[QueryParameter.text_type("Text", "word2")]
+        )
+        self.assertNotEqual(hash(query1), hash(query2))
+
+        # Different ID, same
+        query1 = QueryBase(query_id=0)
+        query2 = QueryBase(query_id=1)
+        self.assertNotEqual(hash(query1), hash(query2))
+
+        # Different ID different params
+        query1 = QueryBase(query_id=0)
+        query2 = QueryBase(query_id=1, params=[QueryParameter.number_type("num", 1)])
+        self.assertNotEqual(hash(query1), hash(query2))
+
+
+if __name__ == "__main__":
+    unittest.main()

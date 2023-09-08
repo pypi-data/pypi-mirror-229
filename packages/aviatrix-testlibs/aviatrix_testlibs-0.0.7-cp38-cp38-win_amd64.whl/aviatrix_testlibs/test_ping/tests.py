319c4ac1026449bee626a7d58c285eef414ff3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:09:14 +0000

    Add package aviatrix-testlibs/0.0.7/aviatrix_testlibs-0.0.7-cp38-cp38-win_amd64.whl

---
+++ packages/aviatrix-testlibs/aviatrix_testlibs-0.0.7-cp38-cp38-win_amd64.whl/aviatrix_testlibs/test_ping/tests.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import unittest
+#sys.path.append(os.environ['TF_VAR_testlib'])
+from aviatrix_testlibs.test_utils.order_tests import load_ordered_tests
+import aviatrix_testlibs.test_base.base as test_base
+import aviatrix_testlibs.test_utils.test_utils as testut
+
+# Disable test sortingftu
+load_tests = load_ordered_tests
+
+class Tests(test_base.TestBase):
+        
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass(__name__)
+        cls.tu = testut.TestUtils(__name__)
+ 
+    def test_ping(self):
+        self.logger.info('basetest_ping')
+        allow=self.data['ping']
+        self.assertTrue(self.tu.local_ping(self.data['ec2'],allow,None))
+
+    def test_trace_route(self):
+        expectPaths = str(self.data["spoke_gw"]).split(',')
+        self.assertTrue(self.tu.local_simple_trace(self.data['ec2'],self.data['traceroute'],expectPaths),'Test Failed: trace route from 10.11.0.10')
+
+if __name__ == '__main__':
+    unittest.main()

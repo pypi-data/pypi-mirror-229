afa797b81af644a963be5530eb2d1b4859250819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:35 +0000

    Add package ayugespidertools/3.4.1/ayugespidertools-3.4.1-py3-none-any.whl

---
+++ packages/ayugespidertools/ayugespidertools-3.4.1-py3-none-any.whl/ayugespidertools/scraper/http/request/form.py
@@ -0,0 +1,39 @@
+from typing import Union
+
+from scrapy import FormRequest
+
+from ayugespidertools.common.typevars import AiohttpRequestArgs
+from ayugespidertools.scraper.http.request import AiohttpRequest
+
+__all__ = [
+    "AiohttpFormRequest",
+]
+
+
+class AiohttpFormRequest(AiohttpRequest, FormRequest):
+    """使用 aiohttp 发送 FormRequest 请求"""
+
+    def __init__(
+        self,
+        url=None,
+        callback=None,
+        method=None,
+        formdata=None,
+        body=None,
+        args: Union[AiohttpRequestArgs, dict] = None,
+        **kwargs
+    ):
+        # First init FormRequest to get url, body and method
+        if formdata:
+            FormRequest.__init__(self, url=url, method=method, formdata=formdata)
+            url, method, body = self.url, self.method, self.body
+        # Then pass all other kwargs to AiohttpRequest
+        AiohttpRequest.__init__(
+            self,
+            url=url,
+            callback=callback,
+            method=method,
+            body=body,
+            args=args,
+            **kwargs
+        )

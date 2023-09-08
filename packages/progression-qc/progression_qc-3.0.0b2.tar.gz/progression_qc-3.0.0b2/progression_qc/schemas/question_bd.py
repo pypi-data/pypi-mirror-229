47a3e396a91eac2acbf550f7fcb937480a0cf391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 05:15:21 +0000

    Add package progression-qc/3.0.0b2/progression_qc-3.0.0b2.tar.gz

---
+++ packages/progression-qc/progression_qc-3.0.0b2.tar.gz/progression_qc-3.0.0b2/progression_qc/schemas/question_bd.py
@@ -0,0 +1,9 @@
+{
+    **schema_registry.get("question_sys"),
+    "sgbd": {
+        "required": True,
+        "type": "string",
+        "empty": False,
+        "nullable": False,
+    },
+}
---
+++ packages/progression-qc/progression_qc-3.0.0b2.tar.gz/progression_qc-3.0.0b2/progression_qc/schemas/question_vcs.py
@@ -0,0 +1,4 @@
+{
+    **schema_registry.get("question_sys"),
+    "vcs": {"required": True, "type": "string", "empty": False, "nullable": False},
+}

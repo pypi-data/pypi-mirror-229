6b224f142e774b454cde6da7e79122d013defe59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 14:08:34 +0000

    Add package odoo14-addon-ssi-infrastructure/14.0.1.0.0/odoo14_addon_ssi_infrastructure-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-infrastructure/odoo14_addon_ssi_infrastructure-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_infrastructure/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Infrastructure",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_master_data_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "menu.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "demo": [],
+}

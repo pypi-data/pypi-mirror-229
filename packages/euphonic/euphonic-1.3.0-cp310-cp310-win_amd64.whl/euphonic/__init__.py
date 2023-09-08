0559080aa98ec0782725d2b1188f18bda65b2ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:23:57 +0000

    Add package euphonic/1.3.0/euphonic-1.3.0-cp310-cp310-win_amd64.whl

---
+++ packages/euphonic/euphonic-1.3.0-cp310-cp310-win_amd64.whl/euphonic/__init__.py
@@ -0,0 +1,20 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+import pint
+from pint import UnitRegistry
+from importlib_resources import files
+
+# Create ureg here so it is only created once
+ureg = UnitRegistry()
+ureg.enable_contexts('spectroscopy')
+Quantity = ureg.Quantity
+
+from .spectra import Spectrum1D, Spectrum1DCollection, Spectrum2D
+from .crystal import Crystal
+from .debye_waller import DebyeWaller
+from .qpoint_frequencies import QpointFrequencies
+from .structure_factor import StructureFactor
+from .qpoint_phonon_modes import QpointPhononModes
+from .force_constants import ForceConstants

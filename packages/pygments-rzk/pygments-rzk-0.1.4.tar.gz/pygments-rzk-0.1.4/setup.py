b5539cce6d7169a7ef16efce7f09f5c05ea181e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:25:11 +0000

    Add package pygments-rzk/0.1.4/pygments-rzk-0.1.4.tar.gz

---
+++ packages/pygments-rzk/pygments-rzk-0.1.4.tar.gz/pygments-rzk-0.1.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pygments-rzk',
+    version='0.1.4',
+    description='Pygments lexer for Rzk language (of proof assistant for synthetic ∞-categories).',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='pygments rzk lexer',
+    license='BSD 3',
+
+    author='Nikolai Kudasov',
+    author_email='nickolay.kudasov@gmail.com',
+
+    url='https://github.com/rzk-lang/pygments-rzk',
+
+    packages=find_packages(),
+    install_requires=['pygments >= 1.4'],
+
+    entry_points='''[pygments.lexers]
+                    rzklexer=pygments_rzk:RzkLexer''',
+
+    classifiers=[
+        'Environment :: Plugins',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)

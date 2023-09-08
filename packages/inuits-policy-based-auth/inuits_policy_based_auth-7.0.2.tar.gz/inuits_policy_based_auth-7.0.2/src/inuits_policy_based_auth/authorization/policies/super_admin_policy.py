218460f370a85ce4195b918be15c778da5e4436d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 04:25:24 +0000

    Add package inuits-policy-based-auth/7.0.2/inuits_policy_based_auth-7.0.2.tar.gz

---
+++ packages/inuits-policy-based-auth/inuits_policy_based_auth-7.0.2.tar.gz/inuits_policy_based_auth-7.0.2/src/inuits_policy_based_auth/authorization/policies/super_admin_policy.py
@@ -0,0 +1,24 @@
+import os
+
+from inuits_policy_based_auth.authorization.base_authorization_policy import (
+    BaseAuthorizationPolicy,
+)
+
+
+class SuperAdminPolicy(BaseAuthorizationPolicy):
+    """
+    An authorization policy that allows a user to do anything if he/she has a super
+    admin role.
+    """
+
+    super_admin_role: str = os.getenv("SUPER_ADMIN_ROLE", "super_admin")
+
+    def authorize(self, policy_context, user_context, _):
+        if not user_context.x_tenant:
+            policy_context.access_verdict = False
+            return policy_context, user_context
+
+        if self.super_admin_role in user_context.x_tenant.roles:
+            policy_context.access_verdict = True
+
+        return policy_context, user_context

8593e36bb7ad5773b66e879b0746293ac353455a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 05:13:22 +0000

    Add package poetry-snakemake-plugin/0.2.0/poetry_snakemake_plugin-0.2.0-py3-none-any.whl

---
+++ packages/poetry-snakemake-plugin/poetry_snakemake_plugin-0.2.0-py3-none-any.whl/poetry_snakemake_plugin/templates/init.py
@@ -0,0 +1,109 @@
+from dataclasses import dataclass, field
+from typing import List, Generator
+from snakemake_interface_executor_plugins.executors.base import SubmittedJobInfo
+from snakemake_interface_executor_plugins.executors.remote import RemoteExecutor
+from snakemake_interface_executor_plugins import ExecutorSettingsBase, CommonSettings
+from snakemake_interface_executor_plugins.workflow import WorkflowExecutorInterface
+from snakemake_interface_executor_plugins.logging import LoggerExecutorInterface
+from snakemake_interface_executor_plugins.jobs import (
+    ExecutorJobInterface,
+)
+
+
+# Optional:
+# define additional settings for your executor
+# They will occur in the Snakemake CLI as --<executor-name>-<param-name>
+# Omit this class if you don't need any.
+@dataclass
+class ExecutorSettings(ExecutorSettingsBase):
+    myparam: int = field(default=None, metadata={"help": "Some help text"})
+
+
+# Required:
+# Specify common settings shared by various executors.
+common_settings = CommonSettings(
+    # define whether your executor plugin executes locally
+    # or remotely. In virtually all cases, it will be remote execution
+    # (cluster, cloud, etc.). Only Snakemake's standard execution
+    # plugins (snakemake-executor-plugin-dryrun, snakemake-executor-plugin-local)
+    # are expected to specify False here.
+    non_local_exec=True,
+    # Define whether your executor plugin implies that there is no shared
+    # filesystem (True) or not (False).
+    # This is e.g. the case for cloud execution.
+    implies_no_shared_fs=...,
+)
+
+
+# Required:
+# Implementation of your executor
+class Executor(RemoteExecutor):
+    def __init__(
+        self,
+        workflow: WorkflowExecutorInterface,
+        logger: LoggerExecutorInterface,
+    ):
+        super().__init__(
+            workflow,
+            logger,
+            # configure behavior of RemoteExecutor below
+            # whether arguments for setting the remote provider shall  be passed to jobs
+            pass_default_remote_provider_args=True,
+            # whether arguments for setting default resources shall be passed to jobs
+            pass_default_resources_args=True,
+            # whether environment variables shall be passed to jobs
+            pass_envvar_declarations_to_cmd=True,
+        )
+        # access workflow
+        self.workflow
+        # access executor specific settings
+        self.workflow.executor_settings
+
+        # IMPORTANT: in your plugin, only access methods and properties of
+        # Snakemake objects (like Workflow, Persistence, etc.) that are
+        # defined in the interfaces found in the
+        # snakemake-interface-executor-plugins and the
+        # snakemake-interface-common package.
+        # Other parts of those objects are NOT guaranteed to remain
+        # stable across new releases.
+
+        # To ensure that the used interfaces are not changing, you should
+        # depend on these packages as >=a.b.c,<d with d=a+1 (i.e. pin the
+        # dependency on this package to be at least the version at time
+        # of development and less than the next major version which would
+        # introduce breaking changes).
+
+    def run_job(self, job: ExecutorJobInterface):
+        # Implement here how to run a job.
+        # You can access the job's resources, etc.
+        # via the job object.
+        # After submitting the job, you have to call
+        # self.report_job_submission(job_info).
+        # with job_info being of type
+        # snakemake_interface_executor_plugins.executors.base.SubmittedJobInfo.
+
+        ...
+
+    async def check_active_jobs(
+        self, active_jobs: List[SubmittedJobInfo]
+    ) -> Generator[SubmittedJobInfo, None, None]:
+        # Check the status of active jobs.
+
+        # You have to iterate over the given list active_jobs.
+        # For jobs that have finished successfully, you have to call
+        # self.report_job_success(job).
+        # For jobs that have errored, you have to call
+        # self.report_job_error(job).
+        # Jobs that are still running have to be yielded.
+        #
+        # For queries to the remote middleware, please use
+        # self.status_rate_limiter like this:
+        #
+        # async with self.status_rate_limiter:
+        #    # query remote middleware here
+        ...
+
+    def cancel_jobs(self, active_jobs: List[SubmittedJobInfo]):
+        # Cancel all active jobs.
+        # This method is called when Snakemake is interrupted.
+        ...

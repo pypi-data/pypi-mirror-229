2579740b13a581fea1c00363795bafec66a6e6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 06:13:13 +0000

    Add package rai-test-utils/0.4.1/rai_test_utils-0.4.1-py3-none-any.whl

---
+++ packages/rai-test-utils/rai_test_utils-0.4.1-py3-none-any.whl/rai_test_utils/version.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+name = 'rai_test_utils'
+_major = '0'
+_minor = '4'
+_patch = '1'
+version = '{}.{}.{}'.format(_major, _minor, _patch)

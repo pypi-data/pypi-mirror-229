d2ad733e27194ee05ac370f812ddf3df8b9e1dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:34:08 +0000

    Add package macrometa-target-s3/0.0.4/macrometa-target-s3-0.0.4.tar.gz

---
+++ packages/macrometa-target-s3/macrometa-target-s3-0.0.4.tar.gz/macrometa-target-s3-0.0.4/macrometa_target_s3/formats/format_json.py
@@ -0,0 +1,36 @@
+from json import dumps, JSONEncoder
+from datetime import datetime
+
+from bson import ObjectId
+
+from macrometa_target_s3.formats.format_base import FormatBase
+
+
+class JsonSerialize(JSONEncoder):
+    def default(self, obj: any) -> any:
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        else:
+            raise TypeError(f"Type {type(obj)} not serializable")
+
+
+class FormatJson(FormatBase):
+    def __init__(self, config, context) -> None:
+        super().__init__(config, context, "json")
+        pass
+
+    def _prepare_records(self):
+        # use default behavior, no additional prep needed
+        # TODO: validate json records?
+        return super()._prepare_records()
+
+    def _write(self) -> None:
+        for r in self.records:
+            key = self.create_key(r)
+            super()._write(dumps(r, cls=JsonSerialize), key)
+
+    def run(self) -> None:
+        # use default behavior, no additional run steps needed
+        return super().run(self.context["records"])
---
+++ packages/macrometa-target-s3/macrometa-target-s3-0.0.4.tar.gz/macrometa-target-s3-0.0.4/macrometa_target_s3/sinks.py
@@ -0,0 +1,59 @@
+"""s3 target sink class, which handles writing streams."""
+
+from __future__ import annotations
+import logging
+
+from singer_sdk.sinks import BatchSink
+
+from macrometa_target_s3.formats.format_base import FormatBase, format_type_factory
+from macrometa_target_s3.formats.format_parquet import FormatParquet
+from macrometa_target_s3.formats.format_csv import FormatCsv
+from macrometa_target_s3.formats.format_json import FormatJson
+
+
+LOGGER = logging.getLogger("macrometa-target-s3")
+FORMAT_TYPE = {"parquet": FormatParquet, "csv": FormatCsv, "json": FormatJson}
+
+
+class s3Sink(BatchSink):
+    """s3 target sink class."""
+
+    MAX_SIZE = 10000  # Max records to write in one batch
+
+    def __init__(
+        self,
+        target: any,
+        stream_name: str,
+        schema: dict,
+        key_properties: list[str] | None,
+    ) -> None:
+        super().__init__(target, stream_name, schema, key_properties)
+        # what type of file are we building?
+        self.format_type = self.config.get("format", None).get("format_type", None)
+        if self.format_type:
+            if self.format_type not in FORMAT_TYPE:
+                raise Exception(
+                    f"Unknown file type specified. {key_properties['type']}"
+                )
+        else:
+            raise Exception("No file type supplied.")
+
+    @property
+    def is_full(self) -> bool:
+        return False
+
+    def process_batch(self, context: dict) -> None:
+        """Write out any prepped records and return once fully written."""
+        # add stream name to context
+        context["stream_name"] = self.stream_name
+        context["logger"] = self.logger
+        # creates new object for each batch
+        format_type_client = format_type_factory(
+            FORMAT_TYPE[self.format_type], self.config, context
+        )
+        # force base object_type_client to object_type_base class
+        assert (
+            isinstance(format_type_client, FormatBase) is True
+        ), f"format_type_client must be of type Base; Type: {type(self.format_type_client)}."
+
+        format_type_client.run()

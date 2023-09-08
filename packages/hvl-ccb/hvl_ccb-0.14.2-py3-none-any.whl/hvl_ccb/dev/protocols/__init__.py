7b4f89eee11dd76bf12899b06deb3692e3cba40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-08 03:35:18 +0000

    Add package hvl-ccb/0.14.2/hvl_ccb-0.14.2-py3-none-any.whl

---
+++ packages/hvl-ccb/hvl_ccb-0.14.2-py3-none-any.whl/hvl_ccb/dev/protocols/__init__.py
@@ -0,0 +1,7 @@
+#  Copyright (c) ETH Zurich, SIS ID and HVL D-ITET
+#
+"""
+This module has some common protocols to interface similar device with the same
+methods. The benefit is to be able to switch between different devices with less
+effort of change the code and also to keep the same "look-and-feel".
+"""

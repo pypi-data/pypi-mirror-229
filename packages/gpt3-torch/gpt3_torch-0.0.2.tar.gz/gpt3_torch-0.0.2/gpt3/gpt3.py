20acb6a48bd4859631232705c178103211e5b410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:21:29 +0000

    Add package gpt3-torch/0.0.2/gpt3_torch-0.0.2.tar.gz

---
+++ packages/gpt3-torch/gpt3_torch-0.0.2.tar.gz/gpt3_torch-0.0.2/gpt3/gpt3.py
@@ -0,0 +1,31 @@
+from torch.nn import nn
+# from gpt3.model import Transformer, Decoder, AutoregressiveWrapper
+from zeta.nn.architecture.transformer import Transformer, Decoder, AutoregressiveWrapper
+
+
+class GPT3(nn.Module):
+    def __init__(
+            self,
+            num_tokens=50477,
+            max_seq_len=4096,
+            dim=12288,
+            depth=96,
+            heads=96,
+            attn_dim_head=128
+        ):
+        super().__init__()
+
+        self.model = Transformer(
+            num_tokens=self.num_tokens,
+            max_seq_len=self.max_seq_len,
+            attn_layers = Decoder(
+                dim=self.dim,
+                depth=self.depth,
+                heads=self.heads,
+            )
+        ).cuda()
+
+        self.model = AutoregressiveWrapper(self.model)
+
+    def forward(self, text, **kwargs):
+        return self.model(text)

16d99a35e0957b07cc8c56d64830da9ee925bcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:13 +0000

    Add package Interface2-py-ashersopro2/0.0.1/Interface2_py_ashersopro2-0.0.1.tar.gz

---
+++ packages/Interface2-py-ashersopro2/Interface2_py_ashersopro2-0.0.1.tar.gz/Interface2_py_ashersopro2-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Interface2py',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.10"',
+        "typing",
+        "functools",
+        "json",
+        "builtins",
+    ],
+)

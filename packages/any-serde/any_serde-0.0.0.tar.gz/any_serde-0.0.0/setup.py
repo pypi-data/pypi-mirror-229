78147fe481500016064be06c569048981048ae39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:06 +0000

    Add package any-serde/0.0.0/any_serde-0.0.0.tar.gz

---
+++ packages/any-serde/any_serde-0.0.0.tar.gz/any_serde-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="any_serde",
+    version="0.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)

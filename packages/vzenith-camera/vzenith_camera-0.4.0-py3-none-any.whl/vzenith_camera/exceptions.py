3b1db480ae674e30d3cba313b6b1ed1f2270bb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:22 +0000

    Add package vzenith-camera/0.4.0/vzenith_camera-0.4.0-py3-none-any.whl

---
+++ packages/vzenith-camera/vzenith_camera-0.4.0-py3-none-any.whl/vzenith_camera/exceptions.py
@@ -0,0 +1,34 @@
+class BadPacketHeader(ValueError):
+    ...
+
+
+class BadResponse(ValueError):
+    ...
+
+
+class ResponseError(RuntimeError):
+    ...
+
+
+class BadRequest(ResponseError):
+    ...
+
+
+class Unauthorized(ResponseError):
+    ...
+
+
+class NotFound(ResponseError):
+    ...
+
+
+class MethodNotAllowed(ResponseError):
+    ...
+
+
+class RequestTimeout(ResponseError):
+    ...
+
+
+class InternalServerError(ResponseError):
+    ...

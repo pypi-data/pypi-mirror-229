733d7be47a0b1ae154d308eb7da850b6c951d1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:43 +0000

    Add package faresadd/1.1/faresadd-1.1-py3-none-any.whl

---
+++ packages/faresadd/faresadd-1.1-py3-none-any.whl/faresadd/add.py
@@ -0,0 +1,17 @@
+def add(*args):
+    """
+    A function to calculate 'add' of some inputs
+    :param args:
+    :return: add of given numbers
+    """
+    return sum(args)
+
+
+def subtract(*args):
+    """
+    A function to calculate 'subtract' of some inputs
+    :param args:
+    :return: subtract of given numbers
+    """
+    return sum(args)*-1
+

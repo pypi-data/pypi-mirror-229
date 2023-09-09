e20805014d4ff50ecf6f55839c55b6dc1f9b6473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:30:43 +0000

    Add package shapeless/0.0.4/shapeless-0.0.4-py3-none-any.whl

---
+++ packages/shapeless/shapeless-0.0.4-py3-none-any.whl/shapeless/main.py
@@ -0,0 +1,162 @@
+import logging
+import threading
+from typing import Any, TypeVar, Generic
+import pickle
+
+T = TypeVar('T')
+
+class Poly(Generic[T]):
+    """
+    The Poly class is a utility class that provides dynamic type handling.
+    It allows you to determine, select, shift, validate, alias, annotate, extend, serialize, and deserialize types.
+    It also provides thread safety and optional logging.
+    """
+
+    def __init__(self, data: Any, verbose: bool = False):
+        """
+        Initialize a new Poly object.
+
+        :param data: The data whose type is to be handled.
+        :param verbose: If True, log all operations. Default is False.
+        """
+        self.data = data
+        self.verbose = verbose
+        self.type_mapping = {}
+        self.alias_mapping = {}
+        self.lock = threading.Lock()
+        if self.verbose:
+            logging.info(f"Created a new Poly object with data: {self.data}")
+
+    def determine(self):
+        """
+        Determine the type of the data.
+
+        :return: The type of the data.
+        """
+        with self.lock:
+            data_type = type(self.data)
+            if data_type not in self.type_mapping:
+                self.type_mapping[data_type] = data_type
+            if self.verbose:
+                logging.info(f"Determined type of data: {data_type}")
+            return self.type_mapping[data_type]
+
+    def select(self, target):
+        """
+        Select the type of the data.
+
+        :param target: The target type.
+        :return: The selected type.
+        """
+        selected_type = self.determine()
+        if self.verbose:
+            logging.info(f"Selected type: {selected_type}")
+        return selected_type
+
+    def shift(self, target):
+        """
+        Attempt to shift the data to the target type.
+
+        :param target: The target type.
+        :return: The data after shifting to the target type.
+        :raises TypeError: If the data cannot be shifted to the target type.
+        """
+        try:
+            return target(self.data)
+        except ValueError:
+            if self.verbose:
+                logging.error(f"Failed to shape shift {self.data} to {target}")
+            raise TypeError(f"Cannot shape shift {self.data} to {target}")
+
+    def validate(self, target):
+        """
+        Validate that the data is of the target type.
+
+        :param target: The target type.
+        :return: True if the data is of the target type, False otherwise.
+        :raises TypeError: If the data is not of the target type.
+        """
+        if not isinstance(self.data, target):
+            if self.verbose:
+                logging.error(f"{self.data} is not of type {target}")
+            raise TypeError(f"{self.data} is not of type {target}")
+        return True
+
+    def add_alias(self, alias, target):
+        """
+        Add an alias for a type.
+
+        :param alias: The alias.
+        :param target: The target type.
+        """
+        self.alias_mapping[alias] = target
+
+    def annotate(self, annotation):
+        """
+        Annotate the data with a type.
+
+        :param annotation: The type annotation.
+        """
+        self.data: annotation
+
+    def extend(self, extension):
+        """
+        Extend the type of the data with a new type.
+
+        :param extension: The new type.
+        """
+        class ExtendedType(type(self.data), extension):
+            pass
+        self.data = ExtendedType(self.data)
+
+    def serialize(self):
+        """
+        Serialize the data.
+
+        :return: The serialized data.
+        """
+        return pickle.dumps(self.data)
+
+    def deserialize(self, serialized_data):
+        """
+        Deserialize the data.
+
+        :param serialized_data: The serialized data.
+        :return: The deserialized data.
+        """
+        self.data = pickle.loads(serialized_data)
+        return self.data
+
+    def __instancecheck__(self, instance):
+        """
+        Check if an instance is of the selected type.
+
+        :param instance: The instance to check.
+        :return: True if the instance is of the selected type, False otherwise.
+        """
+        return isinstance(instance, self.select())
+    
+def shapeless(cls):
+    """
+    A decorator that makes all the variables in a class polymorphic.
+    """
+    for attr_name, attr_value in cls.__dict__.items():
+        if callable(attr_value):
+            def wrapper(*args, attr_value=attr_value, **kwargs):
+                poly_args = [Poly(arg).data for arg in args]
+                poly_kwargs = {k: Poly(v).data for k, v in kwargs.items()}
+                return attr_value(*poly_args, **poly_kwargs)
+            setattr(cls, attr_name, wrapper)
+    return cls
+
+@shapeless
+class SimpleClass:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return self.x + self.y
+    
+sc = SimpleClass(1, 2)
+print(sc.add())  # prints 3

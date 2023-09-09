dc2e7543a0d4085cf2224af265b4b3a693652a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:21:08 +0000

    Add package quantexa/0.1.4/quantexa-0.1.4.tar.gz

---
+++ packages/quantexa/quantexa-0.1.4.tar.gz/quantexa-0.1.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quantexa',
+    version='0.1.4',
+    packages=find_packages(include=['quantexa', 'quantexa.*']),
+    description='This is the almighty quantexa package'
+)

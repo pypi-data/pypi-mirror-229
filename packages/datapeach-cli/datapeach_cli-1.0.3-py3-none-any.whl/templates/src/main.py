8ccca36d4b456fe4c08763f6ffe960828f35979f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 14:07:26 +0000

    Add package datapeach-cli/1.0.3/datapeach_cli-1.0.3-py3-none-any.whl

---
+++ packages/datapeach-cli/datapeach_cli-1.0.3-py3-none-any.whl/templates/src/main.py
@@ -0,0 +1,9 @@
+'''
+   * perform the main logic of the function on the input data set
+   * @param *args - the input set of data records. The `*args` contains one or several records as a data window.
+   * @param **kwargs - configure the function with arguments. The `**kwargs` a JSON format configuration for the function.
+   * @return - the output set of data records. The result set contains records produced while processing data.
+'''
+def __process__(records: dict | list[dict], **kwargs) -> dict | list[dict]:
+   # implementation code here
+   return records

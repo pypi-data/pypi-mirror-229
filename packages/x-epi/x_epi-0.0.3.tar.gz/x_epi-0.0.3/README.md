c0b634a3aab258003d6d837eece7cfd50bc1ea4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 14:07:57 +0000

    Add package x-epi/0.0.3/x_epi-0.0.3.tar.gz

---
+++ packages/x-epi/x_epi-0.0.3.tar.gz/x_epi-0.0.3/README.md
@@ -0,0 +1,10 @@
+<p align="center">
+      <img src="docs/source/static/x_epi_logo.png" alt="drawing" width="250"/>
+</p>
+
+**x_epi** is a Python package for creating EPI sequences for X-nucleus imaging. It uses the [PyPulseq](github.com/imr-framework/pypulseq) package to create vendor neutral sequences in the [Pulseq](pulseq.github.io) format. 
+
+Users can create a custom EPI sequence using python functions, a command line program, or a simple graphical user interface. Please see the [documentation](https://x-epi.readthedocs.io) for more details.
+
+[![Documentation Status](https://readthedocs.org/projects/x-epi/badge/?version=latest)](https://x-epi.readthedocs.io/en/latest/?badge=latest)
+      
---
+++ packages/x-epi/x_epi-0.0.3.tar.gz/x_epi-0.0.3/docs/source/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../bin/'))
+
+# -- Project information
+
+project = 'x_epi'
+copyright = '2023, Tyler Blazey'
+author = 'Blazey'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinxarg.ext',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+autodoc_mock_imports = ['numpy', 'matplotlib', 'scipy', 'pypulseq']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'prev_next_buttons_location': None
+}
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+root_doc = 'index'

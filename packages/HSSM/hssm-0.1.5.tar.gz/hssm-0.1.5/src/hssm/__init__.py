a5412c4a6e6a9a6e4e13af2634d1989089b9abce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:17:16 +0000

    Add package HSSM/0.1.5/hssm-0.1.5.tar.gz

---
+++ packages/HSSM/hssm-0.1.5.tar.gz/hssm-0.1.5/src/hssm/__init__.py
@@ -0,0 +1,32 @@
+"""HSSM - Hierarchical Sequential Sampling Models."""
+
+import importlib.metadata
+import logging
+import sys
+
+from .config import ModelConfig
+from .datasets import load_data
+from .defaults import show_defaults
+from .hssm import HSSM
+from .param import Param
+from .prior import Prior
+from .simulator import simulate_data
+from .utils import set_floatX
+
+_logger = logging.getLogger("hssm")
+_logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(stream=sys.stdout)
+_logger.addHandler(handler)
+
+__version__ = importlib.metadata.version(__package__ or __name__)
+
+__all__ = [
+    "HSSM",
+    "load_data",
+    "ModelConfig",
+    "Param",
+    "Prior",
+    "simulate_data",
+    "set_floatX",
+    "show_defaults",
+]

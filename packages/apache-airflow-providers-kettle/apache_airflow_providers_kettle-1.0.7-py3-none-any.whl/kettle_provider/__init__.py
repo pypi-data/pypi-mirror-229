e9f407fde0e436d9f74edf604ed795038b7f9847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:28:05 +0000

    Add package apache-airflow-providers-kettle/1.0.7/apache_airflow_providers_kettle-1.0.7-py3-none-any.whl

---
+++ packages/apache-airflow-providers-kettle/apache_airflow_providers_kettle-1.0.7-py3-none-any.whl/kettle_provider/__init__.py
@@ -0,0 +1,12 @@
+__version__ = ['1.0.7', '1.0.6', '1.0.5', '1.0.4', '1.0.3', '1.0.2', '1.0.1', '1.0.0']
+
+from typing import Any, Dict
+
+
+def get_provider_info() -> Dict[str, Any]:
+    return {
+        "package-name": 'apache-airflow-providers-kettle',
+        "name": "Kettle Apache Airflow Provider",
+        "description": "A simple Apache Airflow Kettle Operator that can invoke jobs and transformations for Linux based systems.",
+        "versions": __version__,
+    }

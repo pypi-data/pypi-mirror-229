53fbb0726004970ab866b8762863b684400d29dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:16:01 +0000

    Add package chispa/0.9.3/chispa-0.9.3.tar.gz

---
+++ packages/chispa/chispa-0.9.3.tar.gz/chispa-0.9.3/chispa/schema_comparer.py
@@ -0,0 +1,80 @@
+from chispa.prettytable import PrettyTable
+from chispa.bcolors import *
+import chispa.six as six
+
+
+class SchemasNotEqualError(Exception):
+   """The schemas are not equal"""
+   pass
+
+
+def assert_schema_equality(s1, s2, ignore_nullable=False):
+    if ignore_nullable:
+        assert_schema_equality_ignore_nullable(s1, s2)
+    else:
+        assert_basic_schema_equality(s1, s2)
+
+
+def assert_basic_schema_equality(s1, s2):
+    if s1 != s2:
+        t = PrettyTable(["schema1", "schema2"])
+        zipped = list(six.moves.zip_longest(s1, s2))
+        for sf1, sf2 in zipped:
+            if sf1 == sf2:
+                t.add_row([blue(sf1), blue(sf2)])
+            else:
+                t.add_row([sf1, sf2])
+        raise SchemasNotEqualError("\n" + t.get_string())
+
+
+def assert_schema_equality_ignore_nullable(s1, s2):
+    if are_schemas_equal_ignore_nullable(s1, s2) == False:
+        t = PrettyTable(["schema1", "schema2"])
+        zipped = list(six.moves.zip_longest(s1, s2))
+        for sf1, sf2 in zipped:
+            if are_structfields_equal(sf1, sf2, True):
+                t.add_row([blue(sf1), blue(sf2)])
+            else:
+                t.add_row([sf1, sf2])
+        raise SchemasNotEqualError("\n" + t.get_string())
+
+
+def are_schemas_equal_ignore_nullable(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    zipped = list(six.moves.zip_longest(s1, s2))
+    for sf1, sf2 in zipped:
+        if not are_structfields_equal(sf1, sf2, True):
+            return False
+    return True
+
+
+def are_structfields_equal(sf1, sf2, ignore_nullability=False):
+    if ignore_nullability:
+        if sf1 is None or sf2 is None:
+            if sf1 is None and sf2 is None:
+                return True
+            else:
+                return False
+        if sf1.name != sf2.name:
+            return False
+        else:
+            return are_datatypes_equal_ignore_nullable(sf1.dataType, sf2.dataType)
+    else:
+        return sf1 == sf2
+
+
+def are_datatypes_equal_ignore_nullable(dt1, dt2):
+    """Checks if datatypes are equal, descending into structs and arrays to
+    ignore nullability.
+    """
+    if dt1.typeName() == dt2.typeName():
+        # Account for array types by inspecting elementType.
+        if dt1.typeName() == 'array':
+            return are_datatypes_equal_ignore_nullable(dt1.elementType, dt2.elementType)
+        elif dt1.typeName() == 'struct':
+            return are_schemas_equal_ignore_nullable(dt1, dt2)
+        else:
+            return True
+    else:
+        return False

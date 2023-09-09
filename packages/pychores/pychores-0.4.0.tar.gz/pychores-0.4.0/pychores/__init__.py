7ad4963ab988e50b959c6f7e95a03db73ba7dc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:51 +0000

    Add package pychores/0.4.0/pychores-0.4.0.tar.gz

---
+++ packages/pychores/pychores-0.4.0.tar.gz/pychores-0.4.0/pychores/__init__.py
@@ -0,0 +1,47 @@
+"""
+A flask app to help you manage your recurent tasks
+"""
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+from ._version import version
+from .configmodule import Config
+from .model import DeferedSession
+
+jwt = JWTManager()
+
+__version__ = version
+
+
+def respond_success(title, content):
+    response_object = {
+        "status": "success",
+        title: content,
+    }
+    return jsonify(response_object)
+
+
+def create_app(environment):
+    app = Flask(__name__)
+    app.config.from_object(Config.get_config(environment))
+    jwt.init_app(app)
+    app.db_session = DeferedSession.get_session_local(
+        app.config["SQLALCHEMY_DATABASE_URI"]
+    )
+
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    # sanity check route
+    @app.route("/infos", methods=["GET"])
+    def api_version():
+        return respond_success("version", __version__)
+
+    from .routes import auth, chores, tasks
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(tasks.bp)
+    app.register_blueprint(chores.bp)
+
+    return app

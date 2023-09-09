35198026cfc44636733d6730815978c672983d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:56 +0000

    Add package Facad1ng/0.0.4/Facad1ng-0.0.4.tar.gz

---
+++ packages/Facad1ng/Facad1ng-0.0.4.tar.gz/Facad1ng-0.0.4/mian/__init__.py
@@ -0,0 +1,7 @@
+from facad1ng import print_banners
+from facad1ng import parse_args
+from facad1ng import validate_web_url
+from facad1ng import validate_custom_domain
+from facad1ng import format_phish_keywords
+from facad1ng import mask_url
+from facad1ng import main

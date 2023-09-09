6dc598d34d693945927bbab2849ccbf89c279721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:28 +0000

    Add package easy-chromium-controller/1.0.5/easy_chromium_controller-1.0.5-py3-none-any.whl

---
+++ packages/easy-chromium-controller/easy_chromium_controller-1.0.5-py3-none-any.whl/easy_chromium_controller/__init__.py
@@ -0,0 +1,5 @@
+from .core import Browser, Locator
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chromium.webdriver import ChromiumDriver
+from enum import Enum as PageEnum

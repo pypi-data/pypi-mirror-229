5ca416adbe0058dc14fa2dbe5c608dfd280427d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:08 +0000

    Add package ambit-fe/1.0.6/ambit-fe-1.0.6.tar.gz

---
+++ packages/ambit-fe/ambit-fe-1.0.6.tar.gz/ambit-fe-1.0.6/src/ambit_fe/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2019-2023, Dr.-Ing. Marc Hirschvogel
+# All rights reserved.
+
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# from __future__ import absolute_import
+
+from . import ambit_main
+from . import resultcheck

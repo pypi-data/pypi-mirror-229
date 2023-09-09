1ff5478237dac5bbe552d5c335491707bb1317dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:16:26 +0000

    Add package kgdata/5.0.0a8/kgdata-5.0.0a8-cp311-none-win_amd64.whl

---
+++ packages/kgdata/kgdata-5.0.0a8-cp311-none-win_amd64.whl/kgdata/wikidata/datasets/class_count.py
@@ -0,0 +1,53 @@
+from operator import add
+from typing import Tuple
+
+import orjson
+
+from kgdata.dataset import Dataset
+from kgdata.spark import does_result_dir_exist
+from kgdata.wikidata.config import WikidataDirCfg
+from kgdata.wikidata.datasets.classes import classes
+from kgdata.wikidata.datasets.entity_types import entity_types
+
+
+def class_count() -> Dataset[Tuple[str, int]]:
+    cfg = WikidataDirCfg.get_instance()
+    ds = Dataset(
+        cfg.class_count / "*.gz",
+        deserialize=orjson.loads,
+        name="class-count",
+        dependencies=[entity_types(), classes()],
+    )
+
+    if not does_result_dir_exist(cfg.class_count):
+        class_count = (
+            entity_types()
+            .get_extended_rdd()
+            .flatMap(lambda x: [(c, 1) for c in x[1]])
+            .reduceByKey(add)
+        )
+        (
+            classes()
+            .get_extended_rdd()
+            .map(lambda x: (x.id, 0))
+            .leftOuterJoin(class_count)
+            .map(lambda x: (x[0], x[1][1] if x[1][1] is not None else 0))
+            .map(orjson.dumps)
+            .save_like_dataset(
+                ds, auto_coalesce=True, shuffle=True, max_num_partitions=512
+            )
+        )
+
+    if not (cfg.class_count / "../class_count_sorted.tsv").exists():
+        (
+            classes()
+            .get_extended_rdd()
+            .map(lambda x: (x.id, str(x.label)))
+            .join(ds.get_extended_rdd())
+            .map(lambda x: (x[0], x[1][0], x[1][1]))
+            .sortBy(lambda x: x[2], ascending=False)
+            .map(lambda x: "\t".join([str(y) for y in x]))
+            .save_as_single_text_file(cfg.class_count / "../class_count_sorted.tsv")
+        )
+
+    return ds

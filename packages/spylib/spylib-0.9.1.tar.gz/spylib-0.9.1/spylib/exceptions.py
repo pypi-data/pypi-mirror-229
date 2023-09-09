16f3af11dac000c16dc1d630412f41b5cc20c95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:31 +0000

    Add package spylib/0.9.1/spylib-0.9.1.tar.gz

---
+++ packages/spylib/spylib-0.9.1.tar.gz/spylib-0.9.1/spylib/exceptions.py
@@ -0,0 +1,51 @@
+class ShopifyError(Exception):
+    """Exception to identify any Shopify error."""
+
+    pass
+
+
+class ShopifyGQLError(Exception):
+    """Exception to identify any Shopify admin graphql error."""
+
+    pass
+
+
+class ShopifyGQLUserError(Exception):
+    """Exception to identify any Shopify admin graphql error caused by the user mistake."""
+
+    pass
+
+
+class ShopifyCallInvalidError(ShopifyError):
+    """Exception to identify errors that our due to bad data sent by us.
+
+    These should not be retried
+    """
+
+    pass
+
+
+class ShopifyThrottledError(ShopifyError):
+    """Exception to identify errors that are due to rate limit control."""
+
+    pass
+
+
+class ShopifyExceedingMaxCostError(ShopifyError):
+    """Exception to identify errors that are due to queries exceeding the max query size."""
+
+    pass
+
+
+class FastAPIImportError(ImportError):
+    """Exception to identify errors when spylip.oauth is accessed without fastapi installed."""
+
+    pass
+
+
+def not_our_fault(exception: BaseException) -> bool:
+    """Simple function to identify invalid Shopify calls, i.e. our mistake.
+
+    Probably the only way to make sure we retry for any other exception but those.
+    """
+    return not isinstance(exception, ShopifyCallInvalidError)

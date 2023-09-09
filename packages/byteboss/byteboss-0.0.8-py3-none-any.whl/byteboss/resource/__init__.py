c108fa043d7c78cbb6c7c7eba29fc1d275e6b025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:21:51 +0000

    Add package byteboss/0.0.8/byteboss-0.0.8-py3-none-any.whl

---
+++ packages/byteboss/byteboss-0.0.8-py3-none-any.whl/byteboss/resource/__init__.py
@@ -0,0 +1,9 @@
+from .sd_loras import SdLoras
+from .sdxl import Sdxl
+from .fcs import FCS
+from .kandinsky import Kandinsky
+from .mg import MusicGen
+from .whisper import Whisper
+from .control_lora import ControlLora
+
+from byteboss.resource.engines.engine import Engine

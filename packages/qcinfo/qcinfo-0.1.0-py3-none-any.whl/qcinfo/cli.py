10f9c5c5710630210307f6a33dc5dcec8d9a3547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:28:52 +0000

    Add package qcinfo/0.1.0/qcinfo-0.1.0-py3-none-any.whl

---
+++ packages/qcinfo/qcinfo-0.1.0-py3-none-any.whl/qcinfo/cli.py
@@ -0,0 +1,36 @@
+from resultsFile import getFile
+import argparse
+
+def main():
+    # read the command line arguments
+    parser = argparse.ArgumentParser(description='Provide the QC file name to be checked')
+    parser.add_argument('file', metavar='file', type=str, nargs='+',
+                        help='a QC calculation file to be processed')
+
+    args = parser.parse_args()
+
+    num_files = len(args.file)
+
+
+    print("__________________________________________________________")
+
+    print("             Quantum Chemistry File Insights              ")
+
+    print("__________________________________________________________")
+    print("----------------------------------------------------------")
+
+
+    for ind, f in enumerate(args.file):
+        file = getFile(f)
+        filetype = str(file).split('.')[-1].split()[0]
+
+
+        print("File ", ind+1, " of ", num_files, "  :: ", f)
+        print("File type        :: ", filetype)
+        print("MO types         :: ", file.mo_types)
+
+        print("----------------------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()

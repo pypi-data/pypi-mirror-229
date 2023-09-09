abc761cdacb18906e7d29ae1b0cfbb9bbad9ac32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:51 +0000

    Add package uscrn/0.1.0.dev2/uscrn-0.1.0.dev2-py3-none-any.whl

---
+++ packages/uscrn/uscrn-0.1.0.dev2-py3-none-any.whl/uscrn/__init__.py
@@ -0,0 +1,26 @@
+"""
+Load U.S. CRN data.
+"""
+
+__version__ = "0.1.0.dev2"
+
+from .attrs import load_attrs
+from .data import get_data, load_meta, read, read_daily, read_hourly, to_xarray
+
+ATTRS = load_attrs()
+"""Dataset and variable attributes, mainly taken from the respective readmes.
+The relevant top-level keys are ``hourly``, ``daily``, etc.
+"""
+
+del load_attrs
+
+__all__ = [
+    "ATTRS",
+    "get_data",
+    "load_meta",
+    "read",
+    "read_daily",
+    "read_hourly",
+    "to_xarray",
+    "__version__",
+]

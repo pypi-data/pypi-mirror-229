142c0b03e2e2011c1465efd01212598abb57ab57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:23:58 +0000

    Add package pyroplus/0.0.5/pyroplus-0.0.5.tar.gz

---
+++ packages/pyroplus/pyroplus-0.0.5.tar.gz/pyroplus-0.0.5/pyroplus/handlers/read_history_handler.py
@@ -0,0 +1,16 @@
+from typing import Callable
+from pyroplus.handlers.handler import Handler
+
+__all__ = ['ReadHistoryInboxHandler', 'ReadHistoryOutboxHandler']
+
+
+class ReadHistoryInboxHandler(Handler):
+
+    def __init__(self, callback: Callable, filters=None):
+        super().__init__(callback, filters)
+
+
+class ReadHistoryOutboxHandler(Handler):
+
+    def __init__(self, callback: Callable, filters=None):
+        super().__init__(callback, filters)

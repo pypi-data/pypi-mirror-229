fbcffd9429fd4e14d60054a954b5f0f5dd04f05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:26:22 +0000

    Add package dupfilter/0.0.1/dupfilter-0.0.1.tar.gz

---
+++ packages/dupfilter/dupfilter-0.0.1.tar.gz/dupfilter-0.0.1/dupfilter/filters/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+# author: kusen
+# email: 1194542196@qq.com
+# date: 2023/9/6
+from dupfilter import utils
+
+
+class Filter(object):
+    def __init__(self, value_hash_func=utils.md5,
+                 value_compress_func=None):
+        self.value_hash_func = value_hash_func
+        self.value_compress_func = value_compress_func or (lambda value: value)
+
+    def exists(self, value):
+        raise NotImplemented
+
+    def insert(self, value):
+        raise NotImplemented
+
+    def exists_many(self, values):
+        raise NotImplemented
+
+    def insert_many(self, values):
+        raise NotImplemented
+
+    def _value_hash(self, value):
+        return self.value_hash_func(value)
+
+    def _value_compress(self, value):
+        return self.value_compress_func(value)
+
+    def _value_hash_and_compress(self, value):
+        return self.value_compress_func(self.value_hash_func(value))

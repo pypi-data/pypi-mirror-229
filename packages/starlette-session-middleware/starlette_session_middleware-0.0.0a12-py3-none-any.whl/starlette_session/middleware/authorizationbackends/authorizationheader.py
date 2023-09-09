99b5014c659873167fbc1f6bcf82c04a98cef9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:41 +0000

    Add package starlette-session-middleware/0.0.0a12/starlette_session_middleware-0.0.0a12-py3-none-any.whl

---
+++ packages/starlette-session-middleware/starlette_session_middleware-0.0.0a12-py3-none-any.whl/starlette_session/middleware/authorizationbackends/authorizationheader.py
@@ -0,0 +1,21 @@
+import re
+import typing
+
+from starlette.requests import HTTPConnection
+
+from starlette_session.middleware.authorizationbackends import (
+    AuthorizationBackendInterface,
+)
+
+
+class AuthorizationHeaderAuthorizationBackend(AuthorizationBackendInterface):
+    def __init__(self, type: str = "Bearer"):
+        self.pattern = re.compile(f"^{type.lower()} (.*)$", re.I)
+
+    def get_token(self, connection: HTTPConnection) -> typing.Union[str, None]:
+        header = "Authorization"
+        if header not in connection.headers:
+            return None
+        value = connection.headers[header]
+        match = self.pattern.match(value)
+        return str(match[1]) if match is not None else None

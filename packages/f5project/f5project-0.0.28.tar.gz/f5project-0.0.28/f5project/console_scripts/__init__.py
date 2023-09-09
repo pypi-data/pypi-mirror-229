243c461c35e8ce79fb9e0b9acf8da44a760c5a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:17:16 +0000

    Add package f5project/0.0.28/f5project-0.0.28.tar.gz

---
+++ packages/f5project/f5project-0.0.28.tar.gz/f5project-0.0.28/f5project/console_scripts/__init__.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import sys
+
+__all__ = ["main"]
+
+
+def main() -> None:
+    """Entry point for the console scripts."""
+
+    if len(sys.argv) != 3:
+        raise ValueError("Invalid number of arguments")
+
+    _, task, *args = sys.argv
+
+    if task == "create-project":
+        create_project(*args)
+    else:
+        print(f"Unknown task: {task}. `create-project` is the only task available.")
+
+
+def create_project(dst: Path | str = ".") -> None:
+    """Create a project using templates"""
+
+    if isinstance(dst, str):
+        dst = Path(dst).resolve()
+
+    dst.resolve().parent.mkdir(parents=True, exist_ok=True)
+
+    src = Path(__file__).parent / "templates"
+
+    all_src_files = src.glob("**/*")
+    src_files = [f for f in all_src_files if "__pycache__" not in f.parts]
+    for src_file in src_files:
+        if src_file.is_file():
+            dst_file = dst / src_file.relative_to(src)
+            dst_file.parent.mkdir(parents=True, exist_ok=True)
+            dst_file.write_text(src_file.read_text())
---
+++ packages/f5project/f5project-0.0.28.tar.gz/f5project-0.0.28/setup.py
@@ -0,0 +1,46 @@
+"""Publishing package to PyPI
+
+Use this command to "Build & Upload & Cleanup":
+```python
+python setup.py sdist bdist_wheel && python -m twine upload dist/* && rm -rf dist build *egg-info
+```
+"""
+
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+base_dir = Path(__file__).parent.resolve()
+long_description = (base_dir / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="f5project",
+    version="0.0.28",
+    install_requires=[
+        "finlab==0.4.5",
+        "fugle-trade==0.4.0",
+        "python-dotenv==1.0.0",
+        "functions-framework==3.4.0",
+        "loguru==0.7.0",
+        "github-secret-syncer==0.0.10",
+    ],
+    author="thejimmylin",
+    author_email="b00502013@gmail.com",
+    description="F5 project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thejimmylin/f5project",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "f5project = f5project.console_scripts:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)

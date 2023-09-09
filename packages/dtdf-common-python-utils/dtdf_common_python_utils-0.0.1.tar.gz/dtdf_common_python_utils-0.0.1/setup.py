59b6fb17104e5a31e931e5822bc03f6feab17b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:27:18 +0000

    Add package dtdf-common-python-utils/0.0.1/dtdf_common_python_utils-0.0.1.tar.gz

---
+++ packages/dtdf-common-python-utils/dtdf_common_python_utils-0.0.1.tar.gz/dtdf_common_python_utils-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='dtdf_common_python_utils',
+    version='0.0.1',
+    description='PyPi package created by Schibsted\'s Product & Application Security team.',
+    long_description='',
+    url='https://schibsted.com',
+    author='Antoine Vlassenbroek',
+    author_email='antoine.vlassenbroek@schibsted.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)

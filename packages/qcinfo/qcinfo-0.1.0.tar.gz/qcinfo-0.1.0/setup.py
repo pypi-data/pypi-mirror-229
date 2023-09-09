aee2856a7746639358f958eb66da66bf5ce8537d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:28:52 +0000

    Add package qcinfo/0.1.0/qcinfo-0.1.0.tar.gz

---
+++ packages/qcinfo/qcinfo-0.1.0.tar.gz/qcinfo-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='qcinfo',
+    version='0.1.0',
+    description='A Python package for getting insights from quantum chemical calculation data.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Dr. Ravindra Shinde',
+    author_email='r.l.shinde@utwente.nl',
+    url='https://github.com/neelravi/qcinfo',
+    packages=['qcinfo'],
+    install_requires=["resultsFile"],
+    entry_points={
+        'console_scripts': [
+            'qcinfo = qcinfo.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)

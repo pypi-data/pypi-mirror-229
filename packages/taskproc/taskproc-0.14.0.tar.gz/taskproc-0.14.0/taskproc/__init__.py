904f1df313903426f6e4fe6e6422608348d5f8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:20:43 +0000

    Add package taskproc/0.14.0/taskproc-0.14.0.tar.gz

---
+++ packages/taskproc/taskproc-0.14.0.tar.gz/taskproc-0.14.0/taskproc/__init__.py
@@ -0,0 +1,20 @@
+""" A lightweight workflow management tool written in pure Python.
+
+Key features:
+    - Intuitive and flexible task graph creation with small boilerblates.
+    - Automatic cache/data management (source code change detection, cache/data dependency tracking).
+    - Task queue with rate limits.
+
+Limitations:
+    - No priority-based scheduling.
+"""
+from .task import Task, TaskBase, Req, Requires, RequiresList, RequiresDict, Const, CONTEXT
+from .graph import FailedTaskError
+
+
+__EXPORT__ = [
+        Task, Const, TaskBase,
+        Req, Requires, RequiresList, RequiresDict,
+        CONTEXT,
+        FailedTaskError
+        ]

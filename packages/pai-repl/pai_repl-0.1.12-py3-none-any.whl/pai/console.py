a50eaae9ad9469bec904ad8d8a47c0ff4a7d2e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:28:44 +0000

    Add package pai-repl/0.1.12/pai_repl-0.1.12-py3-none-any.whl

---
+++ packages/pai-repl/pai_repl-0.1.12-py3-none-any.whl/pai/console.py
@@ -0,0 +1,355 @@
+import ast
+import code
+import io
+import sys
+from typing import Any, Generator, Literal, Optional
+from uuid import UUID
+from pydantic.dataclasses import dataclass
+
+from pai.history import HistoryNode, HistoryTree
+from pai.llms.llm_protocol import (
+    LLM,
+    LLMError,
+    LLMResponse,
+    LLMResponseCode,
+    LLMResponseMessage,
+    LLMStreamChunk,
+)
+
+
+@dataclass
+class UserInput:
+    code: str
+
+
+@dataclass
+class LLMCodeInput:
+    prompt: str
+    message: Optional[str]
+    code: str
+    raw_resp: Any
+    agent_mode: bool = False
+
+
+@dataclass
+class LLMMessageInput:
+    prompt: str
+    message: str
+    raw_resp: Any
+
+
+@dataclass
+class LLMErrorInput:
+    prompt: str
+    error: str
+    raw_resp: Any
+
+
+ConsoleInput = UserInput | LLMCodeInput | LLMMessageInput | LLMErrorInput
+
+
+class WrappedInteractiveConsole(code.InteractiveConsole):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.last_exception = None
+
+    def showtraceback(self, *args, **kwargs):
+        """Override the default traceback behavior to store the last exception."""
+        self.last_exception = sys.exc_info()[1]
+        super().showtraceback(*args, **kwargs)
+
+    def _is_expression(self, code: str) -> bool:
+        """Check if the given code is an expression."""
+        try:
+            ast.parse(code, mode="eval")
+            return True
+        except:
+            return False
+
+    def custom_run_source(self, source: str) -> str:
+        """
+        Push a block of code and get the string output.
+
+        This behaves like a REPL. If the last line of the code
+        is an expression, it is evaluated and the result is added to the output.
+
+        For example:
+        source:
+            1+1
+        returns:
+            "2"
+
+        If a block of code is given, the output is printed and the last expression
+        is returned.
+
+        source:
+            def add(a, b):
+                print("Adding a and b")
+                return a + b
+            add(1, 2)
+        returns:
+            "Adding a and b\n3"
+        """
+        collector = io.StringIO()
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        sys.stdout = collector
+        sys.stderr = collector
+
+        all_lines = source.strip().split("\n")
+        last_line = all_lines[-1]
+
+        # If the last line of the full_code is an expression, evaluate it after executing the rest
+        if self._is_expression(last_line):
+            # Execute all lines except the last one
+            exec_code = "\n".join(all_lines[:-1])
+            try:
+                compiled_code = compile(exec_code, "<string>", "exec")
+                self.runcode(compiled_code)
+
+                # if there was not exception, then we can evaluate the last line
+                if not self.last_exception:
+                    self.push(last_line)
+            except Exception as e:
+                # handle an error compiling the exec_code
+                sys.stdout = original_stdout
+                sys.stderr = original_stderr
+                return f"{e}"
+        else:
+            # If the last line is not an expression, try to compile and execute the full_code
+            try:
+                compiled_code = compile(source, "<string>", "exec")
+                self.runcode(compiled_code)
+            except SyntaxError as e:
+                sys.stdout = original_stdout
+                sys.stderr = original_stderr
+                return f"{e}\n"
+
+        # clear the last exception
+        self.last_exception = None
+
+        # restore stdout and stderr
+        sys.stdout = original_stdout
+        sys.stderr = original_stderr
+
+        # get the output from the collector
+        output = collector.getvalue()
+        return output
+
+
+@dataclass
+class WaitingForInput:
+    """Waiting for input from the user."""
+
+    pass
+
+
+@dataclass
+class WaitingForInputApproval:
+    """Approve the given code."""
+
+    code: LLMCodeInput
+
+
+@dataclass
+class WaitingForLLM:
+    """Waiting for the LLM to return a response."""
+
+    pass
+
+
+@dataclass
+class NewLLMMessage:
+    value: str
+
+
+@dataclass
+class NewOuput:
+    value: str
+
+
+ConsoleEvent = (
+    WaitingForInput
+    | WaitingForInputApproval
+    | WaitingForLLM
+    | NewOuput
+    | NewLLMMessage
+    | LLMStreamChunk
+)
+
+InputState = WaitingForInput | WaitingForInputApproval
+
+
+class Console:
+    "Manages the state of the console."
+    console: WrappedInteractiveConsole
+    history_tree: HistoryTree
+    llm: LLM
+    max_history_nodes_for_llm_context: Optional[int]
+
+    input_state: WaitingForInput | WaitingForInputApproval
+
+    def __init__(self, llm: LLM, llm_context_nodes: Optional[int] = None):
+        self.history_tree = HistoryTree()
+        self.llm = llm
+        self.max_history_nodes_for_llm_context = llm_context_nodes
+
+        # holds input that is not yet processed. This is used for generated code.
+        self.input_state = WaitingForInput()
+        self.agent_mode = False
+
+        locals = {"history": self.history_tree}
+        self.console = WrappedInteractiveConsole(locals=locals)
+
+    def _code_gen(self, prompt: str) -> Generator[ConsoleEvent, None, InputState]:
+        "Handles code gen commands. calls the llm, sets correct input state, updates the history"
+        # set the input state to waiting for the LLM and yield it
+        yield WaitingForLLM()
+
+        history = self.history_tree.lineage(
+            max_nodes=self.max_history_nodes_for_llm_context
+        )
+        resp = yield from self.llm.call(history, prompt)
+
+        if isinstance(resp, LLMResponseCode):
+            llm_inp = LLMCodeInput(
+                prompt=resp.prompt,
+                message=resp.message,
+                code=resp.code,
+                raw_resp=resp.raw,
+            )
+            if resp.message:
+                yield NewLLMMessage(resp.message)
+
+            return WaitingForInputApproval(llm_inp)
+        elif isinstance(resp, LLMResponseMessage):
+            new_history_node = HistoryNode.LLMMessage(
+                prompt=resp.prompt,
+                message=resp.message,
+                raw_resp=resp.raw,
+            )
+            yield NewLLMMessage(value=resp.message)
+            self.history_tree.add_node(new_history_node)
+        elif isinstance(resp, LLMError):
+            new_history_node = HistoryNode.LLMError(
+                prompt=resp.prompt,
+                error=resp.error,
+                raw_resp=resp.raw,
+            )
+            yield NewLLMMessage(resp.error)
+            self.history_tree.add_node(new_history_node)
+        else:
+            raise ValueError(f"Unknown LLM response type: {type(resp)}")
+
+        return WaitingForInput()
+
+    def handle_input(
+        self, console_input: ConsoleInput
+    ) -> Generator[ConsoleEvent, None, InputState]:
+        """
+        Yields a console event.
+        Returns the next input state.
+
+        There maybe multiple console events that are yielded before the next input state is returned.
+        The caller can use the events to update the UI in realtime.
+        The next input state describes what sort of input to collect next.
+        """
+        # reset the input state
+        self.input_state = WaitingForInput()
+
+        if isinstance(console_input, UserInput):
+            if console_input.code.strip() == "":
+                return WaitingForInput()
+
+            if console_input.code.startswith("gen:"):
+                prompt = console_input.code[4:].strip()
+                next_input_state = yield from self._code_gen(prompt)
+                return next_input_state
+            elif console_input.code.startswith("pai:"):
+                prompt = console_input.code[4:].strip()
+                next_input_state = yield from self._code_gen(prompt)
+                if isinstance(next_input_state, WaitingForInputApproval):
+                    # this is the agent command
+                    # so enable agent mode in the llm code input
+                    # that way when it is passed back to this method
+                    # we can immediately call the llm again after running the code
+                    next_input_state.code.agent_mode = True
+                return next_input_state
+            else:
+                # if the input is not a special command, then run it
+                result = self.console.custom_run_source(console_input.code)
+                yield NewOuput(result)
+                self.history_tree.add_node(
+                    HistoryNode.UserCode(code=console_input.code, result=result)
+                )
+                return WaitingForInput()
+        elif isinstance(console_input, LLMCodeInput):
+            result = self.console.custom_run_source(console_input.code)
+            yield NewOuput(result)
+
+            new_history_node = HistoryNode.LLMCode(
+                prompt=console_input.prompt,
+                code=console_input.code,
+                result=result,
+                raw_resp=console_input.raw_resp,
+            )
+            self.history_tree.add_node(new_history_node)
+
+            if console_input.agent_mode:
+                # if agent mode is enabled, that means that we want to immediately
+                # call the LLM again with the result of the previous code
+                next_input_state = yield from self._code_gen("")
+                if isinstance(next_input_state, WaitingForInputApproval):
+                    # TODO: this happens in two places, so it could be refactored
+                    # this is the agent command
+                    # so enable agent mode in the llm code input
+                    # that way when it is passed back to this method
+                    # we can immediately call the llm again after running the code
+                    next_input_state.code.agent_mode = True
+                return next_input_state
+            else:
+                # otherwise, just return to waiting for input
+                return WaitingForInput()
+        elif isinstance(console_input, LLMMessageInput):
+            new_history_node = HistoryNode.LLMMessage(
+                prompt=console_input.prompt,
+                message=console_input.message,
+                raw_resp=console_input.raw_resp,
+            )
+            self.history_tree.add_node(new_history_node)
+            return WaitingForInput()
+        elif isinstance(console_input, LLMErrorInput):
+            new_history_node = HistoryNode.LLMError(
+                prompt=console_input.prompt,
+                error=console_input.error,
+                raw_resp=console_input.raw_resp,
+            )
+            self.history_tree.add_node(new_history_node)
+            return WaitingForInput()
+        else:
+            raise ValueError(f"Unknown input type: {type(console_input)}")
+
+    def get_history(self) -> list[HistoryNode]:
+        """Get the history of the console."""
+        return self.history_tree.lineage(self.max_history_nodes_for_llm_context)
+
+    def get_history_since(self, idx: int) -> list[HistoryNode]:
+        return self.history_tree.lineage_since(idx)
+
+    def get_prompt(self, prompt: str) -> Any:
+        """Get the prompt for the LLM"""
+        return self.llm.prompt(self.get_history(), prompt)
+
+    def start_generator(self) -> Generator[ConsoleEvent, ConsoleInput, None]:
+        # yield the initial waiting for input state
+        input_state = WaitingForInput()
+        while True:
+            user_input: ConsoleInput = yield input_state
+            input_state = yield from self.handle_input(user_input)
+
+
+def append_new_line(text: str) -> str:
+    """Append a new line to the end of the string if it doesn't already have one."""
+    if text[-1] != "\n":
+        text += "\n"
+    return text
---
+++ packages/pai-repl/pai_repl-0.1.12-py3-none-any.whl/pai/repl.py
@@ -0,0 +1,208 @@
+import sys
+import threading
+import time
+from typing import Generator, Optional, Tuple
+
+from prompt_toolkit import HTML, PromptSession, print_formatted_text
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.styles import Style
+from pai.version import VERSION
+
+
+from pai.console import (
+    Console,
+    ConsoleInput,
+    LLMCodeInput,
+    NewLLMMessage,
+    NewOuput,
+    UserInput,
+    WaitingForInputApproval,
+    WaitingForInput,
+    WaitingForLLM,
+)
+from pai.llms.llm_protocol import LLMStreamChunk
+
+
+# Create a session object
+key_bindings = KeyBindings()
+
+
+@key_bindings.add(Keys.Tab)
+def _(event):
+    "Insert four spaces for tab key."
+    event.cli.current_buffer.insert_text("    ")
+
+
+@key_bindings.add("escape", "enter")
+@key_bindings.add("c-o")
+def _(event):
+    "Bind meta+enter or esc+enter to insert a newline."
+    event.current_buffer.insert_text("\n")
+
+
+@key_bindings.add("enter")
+def _(event):
+    event.current_buffer.validate_and_handle()
+
+
+prompt_style = Style.from_dict(
+    {
+        "inp": "bold",
+        "gen": "bold",
+        "multi": "bold",
+        "out": "bold",
+    }
+)
+
+
+class REPL:
+    console: Console
+    session: PromptSession
+    current_history_index: int
+
+    def __init__(self, console: Console):
+        self.console = console
+        self.session = PromptSession(key_bindings=key_bindings)
+        self.current_history_index = -1
+
+    def _current_index(self) -> int:
+        """Get the current index of the history tree."""
+        return self.console.history_tree.current_position().depth
+
+    def _inp_prompt(self) -> HTML:
+        """Generate the input prompt."""
+        return HTML(f"<inp>INP [{self._current_index()}]> </inp>")
+
+    def _gen_prompt(self) -> HTML:
+        """Generate the code gen prompt."""
+        return HTML(f"<gen>LLM [{self._current_index()}]> </gen>")
+
+    def _ok_prompt(self) -> HTML:
+        """Generate the code gen prompt."""
+        return HTML(f"<gen>OK? [{self._current_index()}]> </gen>")
+
+    def _out_prompt(self) -> HTML:
+        """Generate the output prompt."""
+        return HTML(f"<out>OUT [{self._current_index()}]> </out>")
+
+    def _multi_prompt(self) -> HTML:
+        """Generate the multi-line prompt."""
+        ellipsis = "." * (2 + len(str(self._current_index())))
+        return HTML(f"<multi>    {ellipsis}> </multi>")
+
+    def _handle_console_input(self, console_inp: ConsoleInput):
+        """Push a line input to the console and print the result."""
+        resp = self.console.handle_input(console_inp)
+        if resp:
+            print_formatted_text(self._out_prompt(), style=prompt_style, end="")
+            print(resp, end="")
+
+    def go(self, initial_prompt: Optional[str] = None):
+        print(f"pai v{VERSION} using {self.console.llm.description()}")
+        print("'Ctrl+D' to exit. 'Ctrl+o' to insert a newline.")
+
+        generator = self.console.start_generator()
+        last_event = None
+        event = next(generator)  # Start the generator
+
+        if initial_prompt:
+            # if there is an initial prompt, print it and send it to the console
+            # we assume the initial prompt is a prompt, not code. So, we
+            # prepend 'pai: ' to it (if it doesn't already start with 'pai: ').
+
+            if not initial_prompt.startswith("pai: "):
+                initial_prompt = f"pai: {initial_prompt}"
+
+            print_formatted_text(self._inp_prompt(), style=prompt_style, end="")
+            print(initial_prompt)
+            inp = UserInput(initial_prompt)
+            event = generator.send(inp)
+
+        while True:
+            sys.stdout.flush()
+            try:
+                if isinstance(event, WaitingForInput):
+                    # get the next str input from the user
+                    line: str = self.session.prompt(
+                        self._inp_prompt(),
+                        prompt_continuation=self._multi_prompt(),
+                        style=prompt_style,
+                    )
+                    console_inp = UserInput(line)
+                    last_event = event
+                    event = generator.send(console_inp)
+                elif isinstance(event, WaitingForInputApproval):
+                    # The LLM generated code but it hasn't been approved yet
+                    # Now we prompt the user with the generated code
+                    # So they can edit it, approve it, or cancel it
+                    llm_code = event.code
+                    edited: str = self.session.prompt(
+                        self._ok_prompt(),
+                        default=llm_code.code,
+                        prompt_continuation=self._multi_prompt(),
+                        style=prompt_style,
+                    )
+
+                    # push the edited code to the console
+                    console_inp = LLMCodeInput(
+                        prompt=llm_code.prompt,
+                        message=llm_code.message,
+                        code=edited,
+                        raw_resp=llm_code.raw_resp,
+                        agent_mode=llm_code.agent_mode,
+                    )
+                    last_event = event
+                    event = generator.send(console_inp)
+                elif isinstance(event, NewOuput):
+                    # print the general output
+                    if event.value:
+                        print_formatted_text(
+                            self._out_prompt(), style=prompt_style, end=""
+                        )
+                        print(event.value, end="")
+                        # print a newline if the output doesn't end with one
+                        if not event.value.endswith("\n"):
+                            print()
+                    last_event = event
+                    event = next(generator)
+                elif isinstance(event, NewLLMMessage):
+                    # only print the message if it wasn't just streamed
+                    # we can tell by looking at the last event
+                    if not isinstance(last_event, LLMStreamChunk):
+                        # print the LLM output
+                        print_formatted_text(
+                            self._gen_prompt(), style=prompt_style, end=""
+                        )
+                        print(event.value, end="")
+                        # print a newline if the output doesn't end with one
+                        if not event.value.endswith("\n"):
+                            print()
+                    last_event = event
+                    event = next(generator)
+                elif isinstance(event, LLMStreamChunk):
+                    if not isinstance(last_event, LLMStreamChunk):
+                        print_formatted_text(self._gen_prompt(), style=prompt_style)
+                    print(event.text, end="")
+                    last_event = event
+                    event = next(generator)
+                elif isinstance(event, WaitingForLLM):
+                    last_event = event
+                    event = next(generator)
+                else:
+                    raise ValueError(f"Unknown input state: {type(event)}")
+
+            except KeyboardInterrupt as e:
+                # Handle Ctrl+C (cancel)
+                string_error = str(e)
+                if string_error:
+                    print(string_error)
+
+                # Reset the generator and continue
+                generator = self.console.start_generator()
+                event = next(generator)  # Start the generator
+                continue
+            except EOFError:
+                # Handle Ctrl+D (exit)
+                print("\nGoodbye!")
+                break

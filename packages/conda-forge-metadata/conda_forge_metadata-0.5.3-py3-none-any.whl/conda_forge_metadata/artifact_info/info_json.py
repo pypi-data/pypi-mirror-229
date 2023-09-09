c7da18b047e24d783ebbd4a3c1d13bf48036be01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:29:09 +0000

    Add package conda-forge-metadata/0.5.3/conda_forge_metadata-0.5.3-py3-none-any.whl

---
+++ packages/conda-forge-metadata/conda_forge_metadata-0.5.3-py3-none-any.whl/conda_forge_metadata/artifact_info/info_json.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from conda_forge_metadata.libcfgraph import get_libcfgraph_artifact_data
+from conda_forge_metadata.types import ArtifactData
+
+
+def get_artifact_info_as_json(
+    channel: str, subdir: str, artifact: str, backend: str = "libcfgraph"
+) -> ArtifactData | None:
+    """Get a blob of artifact data from the conda info directory.
+
+    Parameters
+    ----------
+    channel : str
+        The channel (e.g., "conda-forge").
+    subdir : str
+        The subdir for the artifact (e.g., "noarch", "linux-64", etc.).
+    artifact : str
+        The full artifact name with extension (e.g.,
+        "21cmfast-3.0.2-py36h13dd421_0.tar.bz2").
+
+    Returns
+    -------
+    info_blob : dict
+        A dictionary of data. Possible keys are
+
+            "metadata_version": the metadata version format
+            "name": the package name
+            "version": the package version
+            "index": the info/index.json file contents
+            "about": the info/about.json file contents
+            "rendered_recipe": the fully rendered recipe at
+                either info/recipe/meta.yaml or info/meta.yaml
+                as a dict
+            "raw_recipe": the template recipe as a string from
+                info/recipe/meta.yaml.template - could be
+                the rendered recipe as a string if no template was found
+            "conda_build_config": the conda_build_config.yaml used for building
+                the recipe at info/recipe/conda_build_config.yaml
+            "files": a list of files in the recipe from info/files with
+                elements ending in .pyc or .txt filtered out.
+    """
+    if backend == "libcfgraph":
+        return get_libcfgraph_artifact_data(channel, subdir, artifact)
+    elif backend == "oci":
+        from conda_forge_metadata.oci import get_oci_artifact_data
+
+        return get_oci_artifact_data(channel, subdir, artifact)
+    else:
+        raise ValueError(f"Unknown backend {backend!r}")

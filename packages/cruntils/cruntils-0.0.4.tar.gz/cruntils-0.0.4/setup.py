011418369e65f626602c7db7e562a190df3a9c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:30:28 +0000

    Add package cruntils/0.0.4/cruntils-0.0.4.tar.gz

---
+++ packages/cruntils/cruntils-0.0.4.tar.gz/cruntils-0.0.4/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="cruntils",
+    version="0.0.4",
+    description="A collection of utilities, mainly for me.",
+    author="Nambarc",
+    packages=["cruntils"]
+)

116b481fb9f6cb86dc6c3948ad67f969c089c7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:24:04 +0000

    Add package mpzinke/0.3.0/mpzinke-0.3.0-py3-none-any.whl

---
+++ packages/mpzinke/mpzinke-0.3.0-py3-none-any.whl/mpzinke/__init__.py
@@ -0,0 +1,19 @@
+#!/opt/homebrew/bin/python3
+# -*- coding: utf-8 -*-
+__author__ = "MPZinke"
+
+########################################################################################################################
+#                                                                                                                      #
+#   created by: MPZinke                                                                                                #
+#   on 2023.07.31                                                                                                      #
+#                                                                                                                      #
+#   DESCRIPTION:                                                                                                       #
+#   BUGS:                                                                                                              #
+#   FUTURE:                                                                                                            #
+#                                                                                                                      #
+########################################################################################################################
+
+
+from .Generic import Generic
+from .IsJSON import is_json
+from .Server import Server

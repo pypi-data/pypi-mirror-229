0363bb0afb2548e80d418b3dade95aa86a608f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:21:48 +0000

    Add package django-bandits/0.1.6a0/django_bandits-0.1.6a0.tar.gz

---
+++ packages/django-bandits/django_bandits-0.1.6a0.tar.gz/django_bandits-0.1.6a0/README.md
@@ -0,0 +1,121 @@
+# django-bandits
+
+This package enables optimization of your Django site via multi-armed bandits.
+
+This package extends [Django-Waffle](https://waffle.readthedocs.io/en/stable/) by wrapping the feature flipper functions with bandit algorithms. This allows automatic testing and optimization as users interact with your site.
+
+## Installation
+
+`pip install django-bandits`
+
+or 
+
+`poetry add django-bandits`
+
+Additionally, you can install it directly from GitHub:
+
+### Pip
+`pip install git+https://github.com/hubbs5/django-bandits.git`
+### Poetry
+`poetry add git+https://github.com/hubbs5/django-bandits.git`
+
+### Django Settings
+
+Go to your `settings.py` file and add `waffle` to your installed apps list:
+```
+INSTALLED_APPS = [
+    ...
+    "waffle",
+    "django_bandits",
+    ...
+]
+```
+
+Additionally, add `django_bandits.middleware.UserActivityMiddleware` to `MIDDLEWARE` after CSRF and authentication middleware, e.g.:
+
+```
+MIDDLEWARE = [
+    ...
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django_bandits.middleware.UserActivityMiddleware", # It goes here!
+    ...
+]
+```
+
+Update `settings.py` to exclude any particular URL from conversion tracking, e.g.:
+```
+EXCLUDE_FROM_TRACKING = [
+    ADMIN_URL,
+]
+```
+
+Additionally, you can add a REGEX option to filter URLs. Helps to ensure you aren't picking up bot traffic that's trying to break into a Wordpress site or something of the sort.
+```
+EXCLUDE_FROM_TRACKING_REGEX = r"\.php|wordpress|\/wp-"
+```
+
+Finally add the following line to your `settings.py` file to ensure the bandit model is overriding Waffle's Flags.
+```
+WAFFLE_FLAG_MODEL = "django_bandits.BanditFlag"
+```
+
+### Migrations
+
+Create migrations by running `python manage.py makemigrations` followed by `python manage.py migrate`.
+
+### Enabling Bandits
+
+With migrations complete, go to the Django site admin page. You should see something like this:
+
+![View of Django admin page](docs/images/django_admin_bandit_1.png)
+
+Select `Flags` under `DJANGO_BANDITS` and click "Add."
+
+Make a name for the flag - typically the feature you want to test. Set it to test for whatever group you'd like to test it on. Refer to the [Waffle documentation](https://waffle.readthedocs.io/en/stable/) for details.
+
+The bandit needs a `FLAG URL` to be set - this is the URL where the flag will be shown to users and is needed to track conversions and call the bandit to flip the feature when users reach that URL. Enter this as the `Source URL`.
+
+To track a conversion, then you need to add a `Target URL`, which is where you want the user to end up.
+
+In the example below, we're tracking the homepage (`/`) and want to see how many users click on a link to go to the contact page (`/contact/`).
+
+![FLAG URL settings determines a successful conversion](docs/images/django_admin_bandit_flag_url.png)
+
+This will consider any user who views your source URL and target URL to be a conversion during the session and will update the count shown in the image above.
+
+To add a bandit, you'll need to select one of the bandit options from the list below:
+![Select from Epsilon Greedy, Epsilon Decay, or UCB1 Bandits](docs/images/bandit-selection.png)
+
+Some bandits have customizable parameters (e.g. how frequently a random action is taken such as $\epsilon$). All allow you to set a minimum number of views and confidence interval before a winning version is selected (see more details below).
+
+Now that a bandit is enabled, you need to update your templates or views in as done with Waffle to enable the feature flipping flag.
+
+In a template, it may be something like this:
+
+```
+{% extends "base.html" %}
+{% load waffle_tags %}
+
+{% block content %}
+{% flag "headline-flag" %}
+    <h1>This is my test headline: Waffle Flag is <strong>active</strong></h1>
+{% else %}
+    <h1>This is my base headline: Waffle Flag is <strong>inactive</strong></h1>
+{% endflag %}
+    <a href="{% url 'ab_test:target' %}">Click here to see the target page</a>
+{% endblock %}
+```
+
+Where "`headline-flag`" is the name of the flag you defined in the admin page.
+
+### Performance Tracking
+
+The bandits will automatically select a winning option when the given criteria are met. When setting up a bandit, you can select the `SIGNIFICANCE LEVEL`(default is 0.05) and the `MIN VIEWS` (default is 100). 
+
+After the `MIN VIEWS` threshold is reached, the model will perform a two-sided t-test to determine if there's significant difference in the two options (i.e. p-value < `SIGNIFICANCE LEVEL`). If this criteria is met, the bandit will then default to the winning option for all future visits to avoid potential conversion losses from testing.
+
+As a user, you can also view the conversion rate and the upper and lower bounds of the confidence intervals at any time within the admin page.
+
+![A view of the bandit stats](docs/images/bandit-stats.png)
---
+++ packages/django-bandits/django_bandits-0.1.6a0.tar.gz/django_bandits-0.1.6a0/django_bandits/models.py
@@ -0,0 +1,332 @@
+import numpy as np
+from scipy.stats import ttest_ind, norm
+from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth import models as auth_models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.core.exceptions import ValidationError
+from abc import ABC, abstractmethod
+
+from waffle.models import Flag  # as WaffleFlag
+from waffle.models import AbstractUserFlag
+
+DEBUG = settings.DEBUG if hasattr(settings, "DEBUG") else False
+
+BANDIT_ALGORITHMS = [
+    ("EG", "Epsilon Greedy"),
+    ("ED", "Epsilon Decay"),
+    ("UCB1", "Upper Confidence Bound"),
+]
+
+
+class BanditFlag(AbstractUserFlag):
+    # TODO: Are content_type and object_id necessary?
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, null=True, blank=True
+    )
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    content_object = GenericForeignKey("content_type", "object_id")
+    ignore_for_authenticated_users = models.BooleanField(
+        default=False,
+        help_text="If checked, the flag will not be active for authenticated users and no stats will be recorded.",
+    )
+
+    def is_active_for_user(self, user):
+        User = get_user_model()
+        if not user:
+            try:
+                user = User.objects.get(
+                    username="username"
+                )  # replace 'username' with actual username
+            except User.DoesNotExist:
+                user = auth_models.AnonymousUser()
+
+        # Try to retrieve the active bandit model
+        bandit_model_instance = None
+        for related_set in [
+            self.epsilongreedymodel_set,
+            self.epsilondecaymodel_set,
+            self.ucb1model_set,
+        ]:  # Add more sets as needed
+            bandit_model_instance = related_set.filter(is_active=True).first()
+            if bandit_model_instance:
+                break
+
+        if bandit_model_instance is None:
+            return super().is_active_for_user(user)
+
+        # Call pull method on the bandit model instance
+        active = bandit_model_instance.pull()
+        return active
+
+
+class URLSanitizationMixin:
+    url_field_names = ["url", "source_url", "target_url"]
+
+    def save(self, *args, **kwargs):
+        for field_name in self.url_field_names:
+            url = getattr(self, field_name, None)
+            if url is None:
+                continue
+            setattr(self, field_name, self.ensure_trailing_slash(url))
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def ensure_trailing_slash(url):
+        # Ensure it has a trailing and leading slash
+        if not url.endswith("/"):
+            url += "/"
+
+        if not url.startswith("/"):
+            url = "/" + url
+
+        return url
+
+
+class FlagUrl(URLSanitizationMixin, models.Model):
+    flag = models.OneToOneField(BanditFlag, on_delete=models.CASCADE)
+    source_url = models.CharField(null=True, max_length=200, blank=True)
+    target_url = models.CharField(null=True, max_length=200, blank=True)
+    active_flag_views = models.IntegerField(default=0)
+    inactive_flag_views = models.IntegerField(default=0)
+    active_flag_conversions = models.IntegerField(default=0)
+    inactive_flag_conversions = models.IntegerField(default=0)
+
+
+class UserActivity(URLSanitizationMixin, models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL
+    )
+    session_key = models.CharField(max_length=40, null=True)
+    url = models.URLField()
+    target_url_visit = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    flags = models.ManyToManyField(BanditFlag, blank=True)
+    is_staff = models.BooleanField(default=False)
+
+
+class UserActivityFlag(models.Model):
+    user_activity = models.ForeignKey(UserActivity, on_delete=models.CASCADE)
+    flag = models.ForeignKey(BanditFlag, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False, null=True)
+
+
+class Bandit(models.Model):
+    name = models.CharField(max_length=200, choices=BANDIT_ALGORITHMS, default="EG")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    is_active = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        """Ensure only one active bandit is available at a time."""
+        if self.is_active and isinstance(self.content_object, BanditFlag):
+            bandit_flag_content_type = ContentType.objects.get_for_model(BanditFlag)
+            active_bandits = Bandit.objects.filter(
+                content_type=bandit_flag_content_type,
+                object_id=self.content_object.id,
+                is_active=True,
+            )
+            if self.pk:
+                active_bandits = active_bandits.exclude(pk=self.pk)
+            if active_bandits.exists():
+                raise ValidationError("Only one bandit can be active at a time.")
+        super().save(*args, **kwargs)
+
+
+# TODO: Deprecated, delete
+class BanditInstance(models.Model):
+    # Used for Admin purposes
+    flag = models.ForeignKey(BanditFlag, on_delete=models.CASCADE)
+    bandit = models.OneToOneField(Bandit, on_delete=models.CASCADE)
+
+
+class AbstractBanditModel(models.Model):
+    flag = models.ForeignKey(
+        BanditFlag,
+        related_name="%(class)s_set",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    is_active = models.BooleanField(default=False)
+    significance_level = models.FloatField(default=0.05)
+    min_views = models.IntegerField(default=100)
+    winning_arm = models.IntegerField(null=True, blank=True)
+
+    k = 2  # Number of options
+
+    class Meta:
+        abstract = True
+        constraints = [
+            models.UniqueConstraint(
+                fields=["flag", "is_active"],
+                condition=models.Q(is_active=True),
+                name="unique_active_bandit_%(class)s",
+            )
+        ]
+
+    # def __str__(self):
+    #   return self.display_bounds()
+
+    @abstractmethod
+    def pull(self):
+        """Determines whether or not the flag is active 0 = False, 1 = True"""
+        raise NotImplementedError("Pull method not implemented.")
+
+    def get_rewards(self):
+        """Returns the rewards for each option"""
+        rewards = self.get_number_of_conversions() / np.maximum(
+            self.get_number_of_views(), 1
+        )
+        return rewards
+
+    def get_number_of_views(self):
+        """Returns the number of views for each option"""
+        flag_url = self.flag.flagurl
+        return np.array([flag_url.inactive_flag_views, flag_url.active_flag_views])
+
+    def get_number_of_conversions(self):
+        """Returns the number of conversions for each option"""
+        flag_url = self.flag.flagurl
+        return np.array(
+            [flag_url.inactive_flag_conversions, flag_url.active_flag_conversions]
+        )
+
+    def save(self, *args, **kwargs):
+        """Ensure only one bandit is active at a time."""
+        if self.is_active:
+            active_bandits = type(self).objects.filter(flag=self.flag, is_active=True)
+            if self.pk:
+                active_bandits = active_bandits.exclude(pk=self.pk)
+            if active_bandits.exists():
+                raise ValidationError("Only one bandit can be active at a time.")
+        super().save(*args, **kwargs)
+
+    def test_arms(self):
+        """
+        Performs a two-sample t-test on the rewards for each arm
+        """
+        n_views = self.get_number_of_views()
+        if n_views.sum() < self.min_views:
+            return None
+
+        # Converts outcomes to list of binary outcomes for use with ttest_ind
+        n_convs = self.get_number_of_conversions()
+        inactive_results = [1] * n_convs[0] + [0] * (n_views[0] - n_convs[0])
+        active_results = [1] * n_convs[1] + [0] * (n_views[1] - n_convs[1])
+
+        t, p_value = ttest_ind(inactive_results, active_results)
+        if p_value < self.significance_level:
+            self.winning_arm = (
+                0 if np.mean(inactive_results) > np.mean(active_results) else 1
+            )
+            self.save()
+
+    def get_confidence_intervals(self, arm: int) -> tuple:
+        """Gets upper and lower bounds for the given arm"""
+        n_views = self.get_number_of_views()[arm]
+        n_convs = self.get_number_of_conversions()[arm]
+        alpha = 1 - self.significance_level
+        prop = n_convs / n_views
+        z = norm.ppf(1 - alpha / 2)
+        error = z * np.sqrt(prop * (1 - prop) / n_views)
+        return prop - error, prop + error
+
+    def get_inactive_flag_confidence_intervals(self) -> str:
+        low, up = self.get_confidence_intervals(0)
+        return f"{low:.2f} - {up:.2f}"
+
+    def get_active_flag_confidence_intervals(self) -> str:
+        low, up = self.get_confidence_intervals(1)
+        return f"{low:.2f} - {up:.2f}"
+
+    def display_confidence_intervals(self):
+        inactive = self.get_inactive_flag_confidence_intervals()
+        active = self.get_active_flag_confidence_intervals()
+        return f"Active Flag:\t{active}\nInactive Flag:\t{inactive}"
+
+    def display_conversion_rate(self):
+        rewards = self.get_rewards()
+        return f"Active Flag:\t{rewards[1]:.2%}\nInactive Flag:\t{rewards[0]:.2%}"
+
+
+class EpsilonGreedyModel(AbstractBanditModel):
+    epsilon = models.FloatField(default=0.1)
+    prob_flag = models.FloatField(default=0.5)
+
+    def pull(self) -> bool:
+        if self.winning_arm is not None:
+            return bool(self.winning_arm)
+        p = np.random.random()
+        if p < self.epsilon:
+            if DEBUG:
+                print("Making random choice")
+            flag = np.random.randint(0, self.k)
+        else:
+            rewards = self.get_rewards()
+            if DEBUG:
+                print("Making greedy choice")
+                print(f"Rewards: {rewards}")
+            if rewards[0] == rewards[1]:
+                flag = np.random.randint(0, self.k)
+            else:
+                flag = np.argmax(rewards)
+
+        return bool(flag)
+
+    def update(self):
+        """
+        Updates probability of flag vs. no flag and any other stats
+
+        TODO: Move this to be calculated on admin page
+        """
+        rewards = self.get_rewards()
+        if rewards[0] == rewards[1]:
+            self.prob_flag = 0.5
+        else:
+            flag = bool(np.argmax(rewards))
+            if flag:
+                self.prob_flag = 1 - self.epsilon / 2
+            else:
+                self.prob_flag = self.epsilon / 2
+
+
+class EpsilonDecayModel(AbstractBanditModel):
+    def pull(self) -> bool:
+        if self.winning_arm is not None:
+            return bool(self.winning_arm)
+        p = np.random.random()
+        if p < (1 / (1 + self.get_number_of_views().sum() / self.k)):
+            flag = np.random.randint(0, self.k)
+        else:
+            rewards = self.get_rewards()
+            if rewards[0] == rewards[1]:
+                flag = np.random.randint(0, self.k)
+            else:
+                flag = np.argmax(rewards)
+
+        return bool(flag)
+
+
+class UCB1Model(AbstractBanditModel):
+    c = models.FloatField(default=2.0)
+
+    def pull(self) -> bool:
+        """
+        Pulls the arm with the highest upper confidence bound
+        """
+        if self.winning_arm is not None:
+            return bool(self.winning_arm)
+        rewards = self.get_rewards()
+        n_views = self.get_number_of_views()
+        flag = np.argmax(
+            rewards
+            + self.c
+            * np.sqrt(np.log(np.max([n_views.sum(), 1])) / np.maximum(n_views, 1))
+        )
+        return bool(flag)

be7329498a189ee775990ce2ddc4382dbbb7f554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:16:13 +0000

    Add package marshmallow-recipe/0.0.24/marshmallow-recipe-0.0.24.tar.gz

---
+++ packages/marshmallow-recipe/marshmallow-recipe-0.0.24.tar.gz/marshmallow-recipe-0.0.24/marshmallow_recipe/metadata.py
@@ -0,0 +1,67 @@
+from typing import Any, Callable, Mapping
+
+from .missing import MISSING
+
+
+def metadata(
+    *,
+    name: str = MISSING,
+    validate: Callable[[Any], Any] | None = None,
+) -> Mapping[str, Any]:
+    result: dict[str, Any] = {}
+    if name is not MISSING:
+        result.update(name=name)
+    if validate is not None:
+        result.update(validate=validate)
+    return result
+
+
+def decimal_metadata(
+    *,
+    name: str = MISSING,
+    places: int = MISSING,
+    as_string: bool = MISSING,
+    validate: Callable[[Any], Any] | None = None,
+) -> Mapping[str, Any]:
+    result: dict[str, Any] = {}
+    if name is not MISSING:
+        result.update(name=name)
+    if places is not MISSING:
+        result.update(places=places)
+    if as_string is not MISSING:
+        result.update(as_string=as_string)
+    if validate is not None:
+        result.update(validate=validate)
+    return result
+
+
+def datetime_metadata(
+    *,
+    name: str = MISSING,
+    validate: Callable[[Any], Any] | None = None,
+    format: str | None = None,
+) -> Mapping[str, Any]:
+    result: dict[str, Any] = {}
+    if name is not MISSING:
+        result.update(name=name)
+    if validate is not None:
+        result.update(validate=validate)
+    if format is not None:
+        result.update(format=format)
+    return result
+
+
+def list_metadata(
+    *,
+    name: str = MISSING,
+    validate: Callable[[Any], Any] | None = None,
+    validate_item: Callable[[Any], Any] | None = None,
+) -> Mapping[str, Any]:
+    result: dict[str, Any] = {}
+    if name is not MISSING:
+        result.update(name=name)
+    if validate is not None:
+        result.update(validate=validate)
+    if validate_item is not None:
+        result.update(validate_item=validate_item)
+    return result

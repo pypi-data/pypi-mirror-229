ce179c9298b3628162947370f7353ba938c60348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:28:16 +0000

    Add package kalite-cli/0.1.2/kalite-cli-0.1.2.tar.gz

---
+++ packages/kalite-cli/kalite-cli-0.1.2.tar.gz/kalite-cli-0.1.2/src/kalite/cli/app.py
@@ -0,0 +1,6 @@
+from textwrap import dedent
+
+
+raise RuntimeError(dedent('''\
+    You have downloaded the wrong kalite-cli, please check installation guide, this stub library is from public PyPI repo.
+    '''))

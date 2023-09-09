1d3b6d29dde17cf9a23b0a84736e37abd6bb7a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:16:58 +0000

    Add package kami-filemanager/0.1.0/kami_filemanager-0.1.0.tar.gz

---
+++ packages/kami-filemanager/kami_filemanager-0.1.0.tar.gz/kami_filemanager-0.1.0/README.md
@@ -0,0 +1,2 @@
+# KAMICO Filemanager Python Library
+## KAMICO Python Library to deal with local files and folders
---
+++ packages/kami-filemanager/kami_filemanager-0.1.0.tar.gz/kami_filemanager-0.1.0/kami_filemanager/__init__.py
@@ -0,0 +1,15 @@
+from .filemanager import (
+    csv_to_json,
+    delete_files_from,
+    filemanager_logger,
+    get_file_list_from,
+    get_folder_list_from,
+)
+
+__all__ = [
+    'filemanager_logger',
+    'csv_to_json',
+    'get_file_list_from',
+    'get_folder_list_from',
+    'delete_files_from',
+]

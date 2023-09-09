479ff06a77b03dd652f98ab53a5eeb6ec75ddedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:07 +0000

    Add package bauplan/0.0.3a5/bauplan-0.0.3a5.tar.gz

---
+++ packages/bauplan/bauplan-0.0.3a5.tar.gz/bauplan-0.0.3a5/bauplan/__init__.py
@@ -0,0 +1,17 @@
+import sys
+
+if sys.version_info[:2] >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+from .decorators import model
+
+__version__ = metadata.version(__package__ or 'bauplan')
+
+del metadata
+
+__all__ = [
+    'model',
+    '__version__',
+]

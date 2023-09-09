e65f40f702a7ad657751e8f4893540d156ea9315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:22:13 +0000

    Add package dlm-matrix/0.7.3/dlm_matrix-0.7.3-py3-none-any.whl

---
+++ packages/dlm-matrix/dlm_matrix-0.7.3-py3-none-any.whl/dlm_matrix/chaintrees/base.py
@@ -0,0 +1,80 @@
+from dlm_matrix.models import Content, Chain, AuthorList, Author, RoleType
+from dlm_matrix.transformation.coordinate import Coordinate
+from typing import Optional, Dict, Any
+import uuid
+
+
+def _convert_dict_to_message(message_dict: dict) -> Chain:
+    role = message_dict["role"]
+    content = Content(raw=message_dict["content"])
+    coordinate = Coordinate(x=0, y=0, z=0, t=0)  # Use default coordinates for now
+
+    if role == "user":
+        return UserChain(id=str(uuid.uuid4()), content=content, coordinate=coordinate)
+    elif role == "assistant":
+        return AssistantChain(
+            id=str(uuid.uuid4()), content=content, coordinate=coordinate
+        )
+    elif role == "system":
+        return SystemChain(id=str(uuid.uuid4()), content=content, coordinate=coordinate)
+    else:
+        raise ValueError(f"Got unknown role {role}")
+
+
+class RoleChain(Chain):
+    def __init__(
+        self,
+        id: str,
+        content: Content,
+        coordinate: Optional[Dict[str, Any]],
+        metadata: Optional[Dict[str, Any]] = None,
+        author_roles: Optional[
+            AuthorList
+        ] = None,  # You can pass it or let the subclasses define it
+    ):
+        super().__init__(
+            id=id,
+            content=content,
+            coordinate=coordinate,
+            metadata=metadata,
+            author_roles=author_roles,
+        )
+
+
+class AssistantChain(RoleChain):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+            author_roles=AuthorList(authors=[Author(role=RoleType.ASSISTANT)]),
+        )
+
+
+class UserChain(RoleChain):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+            author_roles=AuthorList(authors=[Author(role=RoleType.USER)]),
+        )
+
+
+class SystemChain(RoleChain):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+            author_roles=AuthorList(authors=[Author(role=RoleType.SYSTEM)]),
+        )
+
+
+# For a chain with multiple authors:
+class MultiAuthorChain(RoleChain):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+            author_roles=AuthorList(
+                authors=[Author(role=RoleType.USER), Author(role=RoleType.ASSISTANT)]
+            ),
+        )

47b12cf5e0446c83267e6e6d7786a01d048fb161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:16:16 +0000

    Add package meross-iot/0.4.6.0rc2/meross_iot-0.4.6.0rc2.tar.gz

---
+++ packages/meross-iot/meross_iot-0.4.6.0rc2.tar.gz/meross_iot-0.4.6.0rc2/tests/test_http.py
@@ -0,0 +1,86 @@
+import os
+
+from aiohttp import web
+from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+
+from meross_iot.http_api import MerossHttpClient
+from meross_iot.model.http.exception import BadLoginException, BadDomainException
+from tests import async_get_client, _TEST_EMAIL, _TEST_PASSWORD, _TEST_API_BASE_URL
+
+if os.name == 'nt':
+    import asyncio
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+else:
+    import asyncio
+
+
+class TestHttpMethods(AioHTTPTestCase):
+    async def get_application(self):
+        return web.Application()
+
+    async def setUpAsync(self):
+        # Wait some time before next test-burst
+        await asyncio.sleep(10)
+        self.meross_client, self.requires_logout = await async_get_client()
+
+    @unittest_run_loop
+    async def test_subdevice_listing(self):
+        devices = await self.meross_client.async_list_devices()
+        # look for a msxh0 hub
+        hub = None
+        for d in devices:
+            if d.device_type == "msh300":
+                hub = d
+                break
+
+        if hub is None:
+            self.skipTest("No hub was found on this subscription. Cannot test hub listing.")
+        print(f"Testing device {hub.dev_name}")
+        result = await self.meross_client.async_list_hub_subdevices(hub.uuid)
+        if len(result) < 1:
+            self.skipTest("No subdevice found for selected hub")
+            return
+        self.assertGreater(len(result), 0)
+        return result
+
+    @unittest_run_loop
+    async def test_bad_login(self):
+        with self.assertRaises(BadLoginException):
+            return await MerossHttpClient.async_from_user_password(api_base_url=_TEST_API_BASE_URL,
+                                                                   email="albertogeniola@gmail.com",
+                                                                   password="thisIzWRONG!")
+
+    @unittest_run_loop
+    async def test_device_listing(self):
+        devices = await self.meross_client.async_list_devices()
+        assert devices is not None
+        assert len(devices) > 0
+
+
+    @unittest_run_loop
+    async def test_bad_domain(self):
+        with self.assertRaises(BadDomainException):
+            return await MerossHttpClient.async_from_user_password(api_base_url=_TEST_API_BASE_URL, email=_TEST_EMAIL, password=_TEST_PASSWORD, auto_retry_on_bad_domain=False)
+
+
+    @unittest_run_loop
+    async def test_subdevice_listing(self):
+        devices = await self.meross_client.async_list_devices()
+        # look for a msxh0 hub
+        hub = None
+        for d in devices:
+            if d.device_type == "msh300":
+                hub = d
+                break
+
+        if hub is None:
+            self.skipTest("No hub was found on this subscription. Cannot test hub listing.")
+        print(f"Testing device {hub.dev_name}")
+        result = await self.meross_client.async_list_hub_subdevices(hub.uuid)
+        self.assertGreater(len(result), 0)
+        return result
+
+    async def tearDownAsync(self):
+        if self.requires_logout:
+            await self.meross_client.async_logout()

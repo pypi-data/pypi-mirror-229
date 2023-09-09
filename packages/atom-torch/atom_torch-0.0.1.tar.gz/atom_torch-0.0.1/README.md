f9c20c73c2f1f82dc2cb9553d8694c395c37591b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:06 +0000

    Add package atom-torch/0.0.1/atom_torch-0.0.1.tar.gz

---
+++ packages/atom-torch/atom_torch-0.0.1.tar.gz/atom_torch-0.0.1/README.md
@@ -0,0 +1,49 @@
+[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)
+
+# Atom
+Atom is a finetuned LLAMA to create better LLMS through Pytorch Data!
+
+
+
+
+## Installation
+
+You can install the package using pip
+
+```python
+git clone https://github.com/jquesnelle/yarn
+cd Atom
+pip install -e .
+```
+
+### Training
+
+To train the models, run `accelerate config` and enable DeepSpeed acceleration. `deepspeed/zero3.json` was the configuration file used for training.
+
+```sh
+# ./train.sh
+```
+
+The tokenized training data is available on [Hugging Face](https://huggingface.co/datasets/emozilla/pg_books-tokenized-bos-eos-chunked-65536) and was derived from the [pg19](https://huggingface.co/datasets/emozilla/pg19) dataset.
+
+### Evaluation
+
+To reproduce the evaluations, install [lm-evaluation-harness](https://github.com/EleutherAI/lm-evaluation-harness) with `pip install git+https://github.com/EleutherAI/lm-evaluation-harness` and then run the two provided scripts.
+
+```sh
+# ./eval.sh
+# ./eval-harness.sh
+```
+
+### Citation
+
+```
+@misc{peng2023yarn,
+      title={YaRN: Efficient Context Window Extension of Large Language Models}, 
+      author={Bowen Peng and Jeffrey Quesnelle and Honglu Fan and Enrico Shippole},
+      year={2023},
+      eprint={2309.00071},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
+```
---
+++ packages/atom-torch/atom_torch-0.0.1.tar.gz/atom_torch-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['atom']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch']
+
+setup_kwargs = {
+    'name': 'atom-torch',
+    'version': '0.0.1',
+    'description': 'atom - Pytorch',
+    'long_description': '[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)\n\n# Atom\nAtom is a finetuned LLAMA to create better LLMS through Pytorch Data!\n\n\n\n\n## Installation\n\nYou can install the package using pip\n\n```python\ngit clone https://github.com/jquesnelle/yarn\ncd Atom\npip install -e .\n```\n\n### Training\n\nTo train the models, run `accelerate config` and enable DeepSpeed acceleration. `deepspeed/zero3.json` was the configuration file used for training.\n\n```sh\n# ./train.sh\n```\n\nThe tokenized training data is available on [Hugging Face](https://huggingface.co/datasets/emozilla/pg_books-tokenized-bos-eos-chunked-65536) and was derived from the [pg19](https://huggingface.co/datasets/emozilla/pg19) dataset.\n\n### Evaluation\n\nTo reproduce the evaluations, install [lm-evaluation-harness](https://github.com/EleutherAI/lm-evaluation-harness) with `pip install git+https://github.com/EleutherAI/lm-evaluation-harness` and then run the two provided scripts.\n\n```sh\n# ./eval.sh\n# ./eval-harness.sh\n```\n\n### Citation\n\n```\n@misc{peng2023yarn,\n      title={YaRN: Efficient Context Window Extension of Large Language Models}, \n      author={Bowen Peng and Jeffrey Quesnelle and Honglu Fan and Enrico Shippole},\n      year={2023},\n      eprint={2309.00071},\n      archivePrefix={arXiv},\n      primaryClass={cs.CL}\n}\n```',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/atom',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

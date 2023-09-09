dd8b1cda7224dab8781a35cdef311519782198f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:31:13 +0000

    Add package zamzar/0.0.2/zamzar-0.0.2.tar.gz

---
+++ packages/zamzar/zamzar-0.0.2.tar.gz/zamzar-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zamzar', 
+    version='0.0.2',  # 当前版本
+    author='tong_jin_xin',  # 作者
+    author_email='tongjinxin2011@qq.com', # 作者邮箱
+    licence='MIT License',  # 许可协议
+
+    install_requires=['requests'],  # 依赖包
+)

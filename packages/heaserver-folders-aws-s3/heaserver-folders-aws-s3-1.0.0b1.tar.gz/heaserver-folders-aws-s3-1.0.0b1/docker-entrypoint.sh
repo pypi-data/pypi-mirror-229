0e2ee88539f8f9855e685d55e7695609afb00cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 14:07:19 +0000

    Add package heaserver-folders-aws-s3/1.0.0b1/heaserver-folders-aws-s3-1.0.0b1.tar.gz

---
+++ packages/heaserver-folders-aws-s3/heaserver-folders-aws-s3-1.0.0b1.tar.gz/heaserver-folders-aws-s3-1.0.0b1/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+cat > .hea-config.cfg <<EOF
+[DEFAULT]
+Registry=${HEASERVER_REGISTRY_URL:-http://heaserver-registry:8080}
+
+[MongoDB]
+ConnectionString=mongodb://${MONGO_HEA_USERNAME}:${MONGO_HEA_PASSWORD}@${MONGO_HOSTNAME}:27017/${MONGO_HEA_DATABASE}?authSource=${MONGO_HEA_AUTH_SOURCE:-admin}
+
+[MessageBroker]
+Hostname = ${RABBITMQ_HOSTNAME:-rabbitmq}
+Port = ${RABBITMQ_AMQP_PORT:-5672}
+Username = ${RABBITMQ_USERNAME}
+Password = ${RABBITMQ_PASSWORD}
+EOF
+
+exec heaserver-folders-aws-s3 -f .hea-config.cfg -b ${HEASERVER_FOLDERS_AWS_S3_URL:-http://localhost:8080}
+
+

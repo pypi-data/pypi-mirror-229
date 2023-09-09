86f90cf60e23d3ff913a934aae347c32af5a8dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:21:39 +0000

    Add package egen111222-cart-project/1.0/egen111222-cart_project-1.0.tar.gz

---
+++ packages/egen111222-cart-project/egen111222-cart_project-1.0.tar.gz/egen111222-cart_project-1.0/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='egen111222-cart_project',
+      version='1.0',
+      description='Корзина для магазину',
+      author='Kirill',
+      author_email='egen13@ukr.net',
+      packages=['cart_project'],
+     )

5ef5fa330ca22d7525889d3e43c5228ac22faf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:15:25 +0000

    Add package Interface2py/0.0.5/Interface2py-0.0.5.tar.gz

---
+++ packages/Interface2py/Interface2py-0.0.5.tar.gz/Interface2py-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Interface2py',
+    version='0.0.5',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.x"',
+    ],
+)

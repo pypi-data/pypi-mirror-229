8b4aed3b0f7ca5cb8a878f1698c6afbd8c2ce93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 14:06:35 +0000

    Add package klongpy/0.5.0/klongpy-0.5.0.tar.gz

---
+++ packages/klongpy/klongpy-0.5.0.tar.gz/klongpy-0.5.0/klongpy/utils.py
@@ -0,0 +1,16 @@
+import collections
+
+
+class ReadonlyDict(collections.abc.Mapping):
+
+    def __init__(self, data):
+        self._data = data
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        return iter(self._data)

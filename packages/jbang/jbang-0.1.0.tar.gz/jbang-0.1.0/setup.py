75852ebba6d7c021d6472f8e1e1b3ab9ee9dd23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 05:16:11 +0000

    Add package jbang/0.1.0/jbang-0.1.0.tar.gz

---
+++ packages/jbang/jbang-0.1.0.tar.gz/jbang-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jbang',
+      version='0.1.0',
+      description='Python for JBang - Java Script in your Python',
+      url='http://jbang.dev',
+      author='JBang Developers',
+      author_email='team@jbang.dev',
+      license='MIT',
+      packages=['jbang'],
+      zip_safe=False)

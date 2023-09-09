d940cb49f23a3aea17a5c0b8f8b140e370e75432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 04:23:56 +0000

    Add package program-markers/0.5.3/program_markers-0.5.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/program-markers/program_markers-0.5.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/test/tracked_markers_test.py
@@ -0,0 +1,115 @@
+from diopter.compiler import Language, SourceProgram
+from program_markers.instrumenter import InstrumenterMode, instrument_program
+from program_markers.markers import DCEMarker, VRMarker
+
+from .utils import get_system_gcc_O0
+
+
+def test_dce_marker_tracking() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int foo(int a){
+        if (a > 1)
+            return 1;
+        return 0;
+    }
+    int main(int argc, char* argv[]){
+         foo(argc);
+         return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+    )
+    assert set(
+        (DCEMarker.from_str("DCEMarker0_"), DCEMarker.from_str("DCEMarker1_"))
+    ) == set(iprogram.enabled_markers)
+    gcc = get_system_gcc_O0()
+
+    executed_markers_without_args = iprogram.track_reachable_markers((), gcc)
+    assert set((DCEMarker.from_str("DCEMarker0_"),)) == set(
+        executed_markers_without_args
+    )
+
+    executed_markers_with_args = iprogram.track_reachable_markers(("foo",), gcc)
+    assert set((DCEMarker.from_str("DCEMarker1_"),)) == set(executed_markers_with_args)
+
+
+def test_dce_marker_tracking_loop() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int main(){
+         for(int i = 0 ; i < 10; i++){
+         }
+         return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+    )
+    assert set((DCEMarker.from_str("DCEMarker0_"),)) == set(iprogram.enabled_markers)
+    gcc = get_system_gcc_O0()
+
+    executed_markers = iprogram.track_reachable_markers((), gcc)
+    assert set((DCEMarker.from_str("DCEMarker0_"),)) == set(executed_markers)
+
+
+def test_vr_marker_tracking() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int foo(int a){
+        if (a > 1)
+            return 1;
+        return 0;
+    }
+    int main(int argc, char* argv[]){
+         foo(argc);
+         return 0;
+    }
+    """,
+            language=Language.C,
+        ),
+        mode=InstrumenterMode.VR,
+    )
+    assert set(
+        (
+            VRMarker.from_str("VRMarker0_", "int"),
+            VRMarker.from_str("VRMarker1_", "int"),
+        )
+    ) == set(iprogram.enabled_markers)
+    iprogram = iprogram.disable_markers((VRMarker.from_str("VRMarker1_", "int"),))
+
+    gcc = get_system_gcc_O0()
+
+    executed_markers = iprogram.track_reachable_markers((), gcc)
+    assert set((VRMarker.from_str("VRMarker0_", "int"),)) == set(executed_markers)
+
+
+def test_vr_marker_int_max() -> None:
+    iprogram = instrument_program(
+        SourceProgram(
+            code="""
+    int main(int argc, char* argv[]){
+         unsigned long a = 18446744073709551615UL;
+         return a-a;
+    }
+    """,
+            language=Language.C,
+        ),
+        mode=InstrumenterMode.VR,
+    )
+    assert set((VRMarker.from_str("VRMarker0_", "unsigned long"),)) == set(
+        iprogram.enabled_markers
+    )
+    gcc = get_system_gcc_O0()
+
+    rprogram, reachable_markers = iprogram.refine_markers_with_runtime_information(
+        (), gcc
+    )
+    for marker in reachable_markers:
+        assert isinstance(marker, VRMarker)
+        assert marker.upper_bound == 18446744073709551615
+        assert marker.lower_bound == 18446744073709551615

b018907ba1cc36a9e71c68c4a19fdc08fe183256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:06 +0000

    Add package deepsearchkit/1.0/deepsearchkit-1.0-py3-none-any.whl

---
+++ packages/deepsearchkit/deepsearchkit-1.0-py3-none-any.whl/deepsearchkit/error_handler.py
@@ -0,0 +1,12 @@
+class DSKUndefinedVariableError(Exception):
+    """Custom DeepSearchKit error"""
+    pass
+class DSKEmptyDataError(Exception):
+    """Custom DeepSearchKit error"""
+    pass
+class DSKUnindexedError(Exception):
+    """Custom DeepSearchKit error"""
+    pass
+class DSKFeatureNotSupportedError(Exception):
+    """Custom DeepSearchKit error"""
+    pass

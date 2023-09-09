9b9be2e2a78f02d8bf85a73e53be6a686a7f7bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 06:09:34 +0000

    Add package Facad1ng/0.0.2/Facad1ng-0.0.2-py3-none-any.whl

---
+++ packages/Facad1ng/Facad1ng-0.0.2-py3-none-any.whl/mian/__init__.py
@@ -0,0 +1,5 @@
+from facad1ng import print_banners
+from facad1ng import validate_web_url
+from facad1ng import validate_custom_domain
+from facad1ng import format_phish_keywords
+from facad1ng import mask_url

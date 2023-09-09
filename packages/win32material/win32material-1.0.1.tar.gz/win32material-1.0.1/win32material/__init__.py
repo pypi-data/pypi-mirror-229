16012ff4da800ec827b5e5b6c2ed734c9a8d9641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 14:07:38 +0000

    Add package win32material/1.0.1/win32material-1.0.1.tar.gz

---
+++ packages/win32material/win32material-1.0.1.tar.gz/win32material-1.0.1/win32material/__init__.py
@@ -0,0 +1,7 @@
+"""Load functions from win32material.py"""
+from platform import platform
+
+if not platform().startswith("Windows-10"):
+    raise OSError("To use this package, you need to use Windows version >= 10")
+
+from .win32material import ApplyAcrylic, ApplyBlur, ApplyMica

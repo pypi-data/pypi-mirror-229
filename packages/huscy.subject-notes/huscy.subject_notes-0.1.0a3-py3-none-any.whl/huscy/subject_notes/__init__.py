cc2b7a757d3b08e7cdcdffe84620b55fe381bc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:26:22 +0000

    Add package huscy.subject-notes/0.1.0a3/huscy.subject_notes-0.1.0a3-py3-none-any.whl

---
+++ packages/huscy.subject-notes/huscy.subject_notes-0.1.0a3-py3-none-any.whl/huscy/subject_notes/__init__.py
@@ -0,0 +1,4 @@
+# major.minor.patch
+VERSION = (0, 1, 0, 'alpha', 3)
+
+__version__ = '.'.join(str(x) for x in VERSION)
---
+++ packages/huscy.subject-notes/huscy.subject_notes-0.1.0a3-py3-none-any.whl/huscy/subject_notes/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('subjects', '0001_squashed_0009_delete_contactold'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SubjectNoteTag',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('text', models.CharField(max_length=255, verbose_name='Text')),
+            ],
+            options={
+                'verbose_name': 'Subject note tag',
+                'verbose_name_plural': 'Subject note tags',
+                'ordering': ('text',),
+            },
+        ),
+        migrations.CreateModel(
+            name='SubjectNote',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('text', models.TextField(blank=True, verbose_name='Text')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created at')),
+                ('creator', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL, verbose_name='Creator')),
+                ('note_tag', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='+', to='subject_notes.subjectnotetag', verbose_name='Subject note tag')),
+                ('subject', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='notes', to='subjects.subject', verbose_name='Subject')),
+            ],
+            options={
+                'verbose_name': 'Subject note',
+                'verbose_name_plural': 'Subject notes',
+                'ordering': ('subject__contact__display_name', '-created_at'),
+            },
+        ),
+    ]

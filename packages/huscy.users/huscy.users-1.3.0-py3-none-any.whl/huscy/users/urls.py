aba5a46ed619e4b0fa59c5cf9da6d88a50820241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-09 03:28:12 +0000

    Add package huscy.users/1.3.0/huscy.users-1.3.0-py3-none-any.whl

---
+++ packages/huscy.users/huscy.users-1.3.0-py3-none-any.whl/huscy/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from huscy.users import views
+
+
+router = DefaultRouter()
+router.register('users', views.UserViewSet)
+
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]

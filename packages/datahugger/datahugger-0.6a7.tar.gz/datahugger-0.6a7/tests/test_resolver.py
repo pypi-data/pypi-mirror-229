10b067f07c2f85957de705af5ac045d3863d31f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:07:44 +0000

    Add package datahugger/0.6a7/datahugger-0.6a7.tar.gz

---
+++ packages/datahugger/datahugger-0.6a7.tar.gz/datahugger-0.6a7/tests/test_resolver.py
@@ -0,0 +1,19 @@
+import pytest
+
+import datahugger
+from datahugger.api import _resolve_service
+from datahugger.services import DataverseDataset
+
+
+def test_resolve_service():
+    url = "https://dataverse.nl/dataset.xhtml?persistentId=doi:10.34894/FXUGHW"
+    doi = "10.34894/FXUGHW"
+
+    assert _resolve_service(url, doi) == DataverseDataset
+
+
+@pytest.mark.xfail(raises=ValueError)
+def test_doi_not_found(tmpdir):
+    doi = "10.1038/s42256-020-00287"
+
+    datahugger.get(doi, tmpdir)

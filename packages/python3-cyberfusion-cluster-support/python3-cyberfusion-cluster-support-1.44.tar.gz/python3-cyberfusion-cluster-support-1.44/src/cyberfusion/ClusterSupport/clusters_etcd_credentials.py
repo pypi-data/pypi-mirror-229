516376698e647dbf7770a3882c4042d3d44ea3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:35 +0000

    Add package python3-cyberfusion-cluster-support/1.44/python3-cyberfusion-cluster-support-1.44.tar.gz

---
+++ packages/python3-cyberfusion-cluster-support/python3-cyberfusion-cluster-support-1.44.tar.gz/python3-cyberfusion-cluster-support-1.44/src/cyberfusion/ClusterSupport/clusters_etcd_credentials.py
@@ -0,0 +1,22 @@
+"""Helper classes for scripts for cluster support packages."""
+
+from cyberfusion.ClusterSupport._interfaces import APIObjectInterface
+
+
+class ClusterEtcdCredentials(APIObjectInterface):
+    """Represents object."""
+
+    def _set_attributes_from_model(
+        self,
+        obj: dict,
+    ) -> None:
+        """Set class attributes from API output."""
+        self.id = obj["id"]
+        self.etcd_username = obj["etcd_username"]
+        self.etcd_role_name = obj["etcd_role_name"]
+        self.etcd_password = obj["etcd_password"]
+        self.cluster_id = obj["cluster_id"]
+        self.created_at = obj["created_at"]
+        self.updated_at = obj["updated_at"]
+
+        self.cluster = self.support.get_clusters(id_=self.cluster_id)[0]

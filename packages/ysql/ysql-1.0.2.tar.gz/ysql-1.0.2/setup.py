405e2b2419b27ec7aef52a6e0974300f30d8f962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:08:23 +0000

    Add package ysql/1.0.2/ysql-1.0.2.tar.gz

---
+++ packages/ysql/ysql-1.0.2.tar.gz/ysql-1.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="ysql",
+    version="1.0.2",
+    author="dfqxhhh",
+    author_email="dfqxhhh@163.com",
+    description="based on android room, developed a better sqlite frame",
+    long_description="entity, dao, and database; decorators, metaprogramming, and modularity",
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/darlingxyz/ysql",
+    packages=['ysql'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+

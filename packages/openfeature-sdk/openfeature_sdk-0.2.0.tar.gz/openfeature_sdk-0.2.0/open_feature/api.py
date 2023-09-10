e6780687c36603670fee7a2bfd286391b3c97f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:09:30 +0000

    Add package openfeature-sdk/0.2.0/openfeature_sdk-0.2.0.tar.gz

---
+++ packages/openfeature-sdk/openfeature_sdk-0.2.0.tar.gz/openfeature_sdk-0.2.0/open_feature/api.py
@@ -0,0 +1,65 @@
+import typing
+
+from open_feature.client import OpenFeatureClient
+from open_feature.evaluation_context import EvaluationContext
+from open_feature.exception import GeneralError
+from open_feature.hook import Hook
+from open_feature.provider.metadata import Metadata
+from open_feature.provider.no_op_provider import NoOpProvider
+from open_feature.provider.provider import AbstractProvider
+
+_provider: AbstractProvider = NoOpProvider()
+
+_evaluation_context = EvaluationContext()
+
+_hooks: typing.List[Hook] = []
+
+
+def get_client(
+    name: typing.Optional[str] = None, version: typing.Optional[str] = None
+) -> OpenFeatureClient:
+    return OpenFeatureClient(name=name, version=version, provider=_provider)
+
+
+def set_provider(provider: AbstractProvider):
+    global _provider
+    if provider is None:
+        raise GeneralError(error_message="No provider")
+    _provider = provider
+
+
+def get_provider() -> typing.Optional[AbstractProvider]:
+    global _provider
+    return _provider
+
+
+def get_provider_metadata() -> typing.Optional[Metadata]:
+    global _provider
+    return _provider.get_metadata()
+
+
+def get_evaluation_context() -> EvaluationContext:
+    global _evaluation_context
+    return _evaluation_context
+
+
+def set_evaluation_context(evaluation_context: EvaluationContext):
+    global _evaluation_context
+    if evaluation_context is None:
+        raise GeneralError(error_message="No api level evaluation context")
+    _evaluation_context = evaluation_context
+
+
+def add_hooks(hooks: typing.List[Hook]):
+    global _hooks
+    _hooks = _hooks + hooks
+
+
+def clear_hooks():
+    global _hooks
+    _hooks = []
+
+
+def get_hooks() -> typing.List[Hook]:
+    global _hooks
+    return _hooks
---
+++ packages/openfeature-sdk/openfeature_sdk-0.2.0.tar.gz/openfeature_sdk-0.2.0/open_feature/provider/no_op_provider.py
@@ -0,0 +1,78 @@
+import typing
+
+from open_feature.evaluation_context import EvaluationContext
+from open_feature.flag_evaluation import FlagResolutionDetails, Reason
+from open_feature.hook import Hook
+from open_feature.provider.metadata import Metadata
+from open_feature.provider.no_op_metadata import NoOpMetadata
+from open_feature.provider.provider import AbstractProvider
+
+PASSED_IN_DEFAULT = "Passed in default"
+
+
+class NoOpProvider(AbstractProvider):
+    def get_metadata(self) -> Metadata:
+        return NoOpMetadata()
+
+    def get_provider_hooks(self) -> typing.List[Hook]:
+        return []
+
+    def resolve_boolean_details(
+        self,
+        flag_key: str,
+        default_value: bool,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[bool]:
+        return FlagResolutionDetails(
+            value=default_value,
+            reason=Reason.DEFAULT,
+            variant=PASSED_IN_DEFAULT,
+        )
+
+    def resolve_string_details(
+        self,
+        flag_key: str,
+        default_value: str,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[str]:
+        return FlagResolutionDetails(
+            value=default_value,
+            reason=Reason.DEFAULT,
+            variant=PASSED_IN_DEFAULT,
+        )
+
+    def resolve_integer_details(
+        self,
+        flag_key: str,
+        default_value: int,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[int]:
+        return FlagResolutionDetails(
+            value=default_value,
+            reason=Reason.DEFAULT,
+            variant=PASSED_IN_DEFAULT,
+        )
+
+    def resolve_float_details(
+        self,
+        flag_key: str,
+        default_value: float,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[float]:
+        return FlagResolutionDetails(
+            value=default_value,
+            reason=Reason.DEFAULT,
+            variant=PASSED_IN_DEFAULT,
+        )
+
+    def resolve_object_details(
+        self,
+        flag_key: str,
+        default_value: typing.Union[dict, list],
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[typing.Union[dict, list]]:
+        return FlagResolutionDetails(
+            value=default_value,
+            reason=Reason.DEFAULT,
+            variant=PASSED_IN_DEFAULT,
+        )

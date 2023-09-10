20797be36acd3cee5a9e2675d02fd2884d5a99ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:07:41 +0000

    Add package cipherspy/0.0.1/cipherspy-0.0.1.tar.gz

---
+++ packages/cipherspy/cipherspy-0.0.1.tar.gz/cipherspy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cipherspy',
+    version='0.0.1',
+    packages=['cipherspy'],
+    url='https://github.com/fathiabdelmalek/cipher-py',
+    license='',
+    author='Fathi Abdelmalek',
+    author_email='abdelmalek.fathi.2001@gmail.com',
+    description='Common cipher algorithms implementation'
+)

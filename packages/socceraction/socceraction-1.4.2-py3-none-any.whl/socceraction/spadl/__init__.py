d359e2dc78316608b6669b83a678a43815fd02e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:09:19 +0000

    Add package socceraction/1.4.2/socceraction-1.4.2-py3-none-any.whl

---
+++ packages/socceraction/socceraction-1.4.2-py3-none-any.whl/socceraction/spadl/__init__.py
@@ -0,0 +1,19 @@
+"""Implementation of the SPADL language."""
+
+__all__ = [
+    'opta',
+    'statsbomb',
+    'wyscout',
+    'config',
+    'SPADLSchema',
+    'bodyparts_df',
+    'actiontypes_df',
+    'results_df',
+    'add_names',
+    'play_left_to_right',
+]
+
+from . import config, opta, statsbomb, wyscout
+from .config import actiontypes_df, bodyparts_df, results_df
+from .schema import SPADLSchema
+from .utils import add_names, play_left_to_right

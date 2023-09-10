588b4bd66d4e2eb989cfd9f3b74962f9260af775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:07:47 +0000

    Add package parea-ai/0.2.7/parea_ai-0.2.7.tar.gz

---
+++ packages/parea-ai/parea_ai-0.2.7.tar.gz/parea_ai-0.2.7/parea/client.py
@@ -0,0 +1,82 @@
+import asyncio
+import time
+
+from attrs import asdict, define, field
+
+from parea.api_client import HTTPClient
+from parea.helpers import gen_trace_id
+from parea.parea_logger import parea_logger
+from parea.schemas.models import Completion, CompletionResponse, FeedbackRequest, UseDeployedPrompt, UseDeployedPromptResponse
+from parea.utils.trace_utils import default_logger, get_current_trace_id, trace_data
+
+COMPLETION_ENDPOINT = "/completion"
+DEPLOYED_PROMPT_ENDPOINT = "/deployed-prompt"
+RECORD_FEEDBACK_ENDPOINT = "/feedback"
+
+
+@define
+class Parea:
+    api_key: str = field(init=True, default="")
+    _client: HTTPClient = field(init=False, default=HTTPClient())
+
+    def __attrs_post_init__(self):
+        self._client.set_api_key(self.api_key)
+        parea_logger.set_client(self._client)
+
+    def completion(self, data: Completion) -> CompletionResponse:
+        inference_id = gen_trace_id()
+        data.inference_id = inference_id
+        r = self._client.request(
+            "POST",
+            COMPLETION_ENDPOINT,
+            data=asdict(data),
+        )
+        if parent_trace_id := get_current_trace_id():
+            trace_data.get()[parent_trace_id].children.append(inference_id)
+            default_logger(parent_trace_id)
+        return CompletionResponse(**r.json())
+
+    async def acompletion(self, data: Completion) -> CompletionResponse:
+        inference_id = gen_trace_id()
+        data.inference_id = inference_id
+        r = await self._client.request_async(
+            "POST",
+            COMPLETION_ENDPOINT,
+            data=asdict(data),
+        )
+        if parent_trace_id := get_current_trace_id():
+            trace_data.get()[parent_trace_id].children.append(inference_id)
+            default_logger(parent_trace_id)
+        return CompletionResponse(**r.json())
+
+    def get_prompt(self, data: UseDeployedPrompt) -> UseDeployedPromptResponse:
+        r = self._client.request(
+            "POST",
+            DEPLOYED_PROMPT_ENDPOINT,
+            data=asdict(data),
+        )
+        return UseDeployedPromptResponse(**r.json())
+
+    async def aget_prompt(self, data: UseDeployedPrompt) -> UseDeployedPromptResponse:
+        r = await self._client.request_async(
+            "POST",
+            DEPLOYED_PROMPT_ENDPOINT,
+            data=asdict(data),
+        )
+        return UseDeployedPromptResponse(**r.json())
+
+    def record_feedback(self, data: FeedbackRequest) -> None:
+        time.sleep(2)  # give logs time to update
+        self._client.request(
+            "POST",
+            RECORD_FEEDBACK_ENDPOINT,
+            data=asdict(data),
+        )
+
+    async def arecord_feedback(self, data: FeedbackRequest) -> None:
+        await asyncio.sleep(2)  # give logs time to update
+        await self._client.request_async(
+            "POST",
+            RECORD_FEEDBACK_ENDPOINT,
+            data=asdict(data),
+        )

1108c1e21a3495574bafff37525e94140c62ef2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:16 +0000

    Add package protein-distribution/0.1.1/protein_distribution-0.1.1-py2.py3-none-any.whl

---
+++ packages/protein-distribution/protein_distribution-0.1.1-py2.py3-none-any.whl/protein_distribution/__init__.py
@@ -0,0 +1,27 @@
+"""pymetadata - Python utilities for metadata."""
+from pathlib import Path
+
+__author__ = "Matthias Koenig"
+__version__ = "0.1.1"
+
+
+program_name: str = "protein-distribution"
+
+BASE_DIR = Path(__file__).parent.parent.parent
+
+# data for analysis (not distributed)
+DATA_DIR = BASE_DIR / "data"
+PROTEIN_PATH = DATA_DIR / "protein_overview.xlsx"
+
+# results of analysis
+RESULTS_DIR = Path(__file__).parent.parent.parent / "results"
+RESULTS_DIR.mkdir(exist_ok=True)
+
+# resources (distributed)
+RESOURCES_DIR = Path(__file__).parent / "resources"
+
+UNIPROT_PATH = RESOURCES_DIR / "uniprot.json"
+DATA_RAW_XLSX = RESOURCES_DIR / "data_raw.xlsx"
+DATA_XLSX = RESOURCES_DIR / "data.xlsx"
+DATA_MERGED_XLSX = RESOURCES_DIR / "data_merged.xlsx"
+DATA_NORMALIZATION_XLSX = RESOURCES_DIR / "data_normalization.xlsx"

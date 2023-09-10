2d761c4dbcefa2b0af84c9c63868c0e208306e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:09:05 +0000

    Add package clyjin/0.2.10/clyjin-0.2.10.tar.gz

---
+++ packages/clyjin/clyjin-0.2.10.tar.gz/clyjin-0.2.10/clyjin/__main__.py
@@ -0,0 +1,14 @@
+import asyncio
+from pathlib import Path
+
+from clyjin.core.boot import Boot
+from clyjin.log import Log
+
+
+@Log.catch
+def main() -> None:
+    asyncio.run(Boot().start())
+
+
+if __name__ == "__main__":
+    main()

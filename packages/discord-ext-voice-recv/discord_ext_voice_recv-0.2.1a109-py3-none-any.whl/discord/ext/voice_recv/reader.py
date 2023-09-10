ae14d12c89d37cc4665f0177138d3c2664377851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:08:07 +0000

    Add package discord-ext-voice-recv/0.2.1a109/discord_ext_voice_recv-0.2.1a109-py3-none-any.whl

---
+++ packages/discord-ext-voice-recv/discord_ext_voice_recv-0.2.1a109-py3-none-any.whl/discord/ext/voice_recv/reader.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+import time
+import select
+import socket
+import logging
+import threading
+import traceback
+
+from typing import TYPE_CHECKING
+
+from . import rtp
+from .sinks import AudioSink
+from .opus import PacketRouter
+
+try:
+    import nacl.secret
+    from nacl.exceptions import CryptoError
+except ImportError:
+    pass
+
+if TYPE_CHECKING:
+    from typing import Optional, Callable, Any
+    from .voice_client import VoiceRecvClient
+    from .rtp import RTPPacket, RTCPPacket
+
+    DecryptRTP = Callable[[RTPPacket], bytes]
+    DecryptRTCP = Callable[[bytes], bytes]
+    AfterCB = Callable[[Optional[Exception]], Any]
+
+log = logging.getLogger(__name__)
+
+__all__ = [
+    'AudioReader'
+]
+
+
+class _ReaderBase(threading.Thread):
+    def __init__(self, sink: AudioSink, client: VoiceRecvClient, **kwargs):
+        daemon = kwargs.pop('daemon', True)
+        super().__init__(daemon=daemon, **kwargs)
+
+        self.sink = sink
+        self.client = client
+
+        self.sink._voice_client = client
+
+        self.box = nacl.secret.SecretBox(bytes(client.secret_key))
+        self.decrypt_rtp: DecryptRTP = getattr(self, '_decrypt_rtp_' + client.mode)
+        self.decrypt_rtcp: DecryptRTCP = getattr(self, '_decrypt_rtcp_' + client.mode)
+
+    def run(self):
+        raise NotImplementedError
+
+    def set_sink(self, sink: AudioSink) -> AudioSink:
+        """Sets the new sink for the reader and returns the old one.
+        Does not call cleanup()
+        """
+
+        old_sink = self.sink
+        old_sink._voice_client = None
+
+        sink._voice_client = self.client
+        self.sink = sink
+
+        return old_sink
+
+    def update_secret_box(self):
+        # Sure hope this isn't hilariously threadunsafe
+        # if so this might not be the way i need to do this
+        self.box = nacl.secret.SecretBox(bytes(self.client.secret_key))
+
+    def _decrypt_rtp_xsalsa20_poly1305(self, packet):
+        nonce = bytearray(24)
+        nonce[:12] = packet.header
+        result = self.box.decrypt(bytes(packet.data), bytes(nonce))
+
+        if packet.extended:
+            offset = packet.update_ext_headers(result)
+            result = result[offset:]
+
+        return result
+
+    def _decrypt_rtcp_xsalsa20_poly1305(self, data: bytes):
+        nonce = bytearray(24)
+        nonce[:8] = data[:8]
+        result = self.box.decrypt(data[8:], bytes(nonce))
+
+        return data[:8] + result
+
+    def _decrypt_rtp_xsalsa20_poly1305_suffix(self, packet):
+        nonce = packet.data[-24:]
+        voice_data = packet.data[:-24]
+        result = self.box.decrypt(bytes(voice_data), bytes(nonce))
+
+        if packet.extended:
+            offset = packet.update_ext_headers(result)
+            result = result[offset:]
+
+        return result
+
+    def _decrypt_rtcp_xsalsa20_poly1305_suffix(self, data: bytes):
+        nonce = data[-24:]
+        header = data[:8]
+        result = self.box.decrypt(data[8:-24], nonce)
+
+        return header + result
+
+    def _decrypt_rtp_xsalsa20_poly1305_lite(self, packet):
+        nonce = bytearray(24)
+        nonce[:4] = packet.data[-4:]
+        voice_data = packet.data[:-4]
+        result = self.box.decrypt(bytes(voice_data), bytes(nonce))
+
+        if packet.extended:
+            offset = packet.update_ext_headers(result)
+            result = result[offset:]
+
+        return result
+
+    def _decrypt_rtcp_xsalsa20_poly1305_lite(self, data: bytes):
+        nonce = bytearray(24)
+        nonce[:4] = data[-4:]
+        header = data[:8]
+        result = self.box.decrypt(data[8:-4], bytes(nonce))
+
+        return header + result
+
+
+class AudioReader(_ReaderBase):
+    def __init__(self,
+        sink: AudioSink,
+        client: VoiceRecvClient,
+        *,
+        after: Optional[AfterCB]=None
+    ):
+        if after is not None and not callable(after):
+            raise TypeError('Expected a callable for the "after" parameter.')
+
+        super().__init__(sink, client)
+
+        self.after = after
+        self.router = PacketRouter(sink)
+
+        self._current_error: Optional[Exception] = None
+        self._end = threading.Event()
+
+    @property
+    def connected(self):
+        return self.client._connected
+
+    def set_sink(self, sink: AudioSink) -> AudioSink:
+        # Definitely not threadsafe but idk if it matters yet
+        sink = super().set_sink(sink)
+        self.router.set_sink(sink)
+        return sink
+
+    def _do_run(self):
+        while not self._end.is_set():
+            if not self.connected.is_set():
+                self.connected.wait()
+
+            ready, _, err = select.select([self.client.socket], [],
+                                          [self.client.socket], 0.01)
+            if not ready:
+                if err:
+                    log.warning("Socket error in %s", self)
+                continue
+
+            try:
+                raw_data = self.client.socket.recv(4096)
+            except socket.error as e:
+                t0 = time.time()
+
+                if e.errno == 10038: # ENOTSOCK
+                    continue
+
+                if e.errno == 9: # Bad file descriptor
+                    self.stop()
+                    return
+
+                log.exception("Socket error in reader thread %s", self)
+
+                with self.client._connecting:
+                    log.info("Waiting for client connection")
+                    timed_out = self.client._connecting.wait(20)
+
+                if not timed_out:
+                    raise
+                elif self.client.is_connected():
+                    log.info("Reconnected in %.4fs", time.time()-t0)
+                    continue
+                else:
+                    raise
+
+            packet = None
+            rtcp = False
+            try:
+                if not rtp.is_rtcp(raw_data):
+                    packet = rtp.decode(raw_data)
+                    assert isinstance(packet, rtp.RTPPacket)
+
+                    packet.decrypted_data = self.decrypt_rtp(packet)
+                else:
+                    rtcp = True
+                    packet = rtp.decode(self.decrypt_rtcp(raw_data))
+                    assert isinstance(packet, rtp.RTCPPacket)
+
+                    if not isinstance(packet, rtp.ReceiverReportPacket):
+                        log.warning(
+                            "Received unusual rtcp packet%s",
+                            f"\n{'*'*78}\n{packet}\n{'*'*78}"
+                        )
+            except CryptoError:
+                msg = "CryptoError decoding data:\n  packet=%s\n  raw_data=%s"
+                log.exception(msg, packet, raw_data)
+                continue
+
+            except:
+                log.exception("Error unpacking packet")
+                traceback.print_exc()
+
+            else:
+                if not rtcp and packet.ssrc not in self.client._ssrc_to_id:
+                    if packet.is_silence():
+                        log.debug("Skipping silence packet for unknown ssrc %s", packet.ssrc)
+                        continue
+                    else:
+                        log.info("Received packet for unknown ssrc %s:\n%s", packet.ssrc, packet)
+
+            finally:
+                if not packet:
+                    continue
+
+            # I could combine these in a function in the router but this is faster
+            if rtcp:
+                self.router.feed_rtcp(packet) # type: ignore
+            else:
+                self.client._speaking_cache[packet.ssrc] = time.time()
+                self.router.feed_rtp(packet) # type: ignore
+
+    def is_listening(self):
+        return not self._end.is_set()
+
+    def stop(self):
+        self._end.set()
+
+    def run(self):
+        try:
+            self._do_run()
+        except socket.error as err:
+            self._current_error = err
+
+        except Exception as err:
+            log.exception("Error in %s", self)
+            self._current_error = err
+
+        finally:
+            self.stop()
+
+            try:
+                self.router.stop()
+            except Exception:
+                log.exception("Error stopping router in %s", self)
+
+            self._call_after()
+
+            try:
+                self.sink.cleanup()
+            except Exception:
+                log.exception("Error calling sink cleanup() in %s", self)
+
+    def _call_after(self):
+         if self.after is not None:
+            try:
+                self.after(self._current_error)
+            except Exception:
+                log.exception('Calling the after function failed.')
---
+++ packages/discord-ext-voice-recv/discord_ext_voice_recv-0.2.1a109-py3-none-any.whl/discord/ext/voice_recv/voice_client.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+import time
+import asyncio
+import logging
+import threading
+
+import discord
+from discord.gateway import DiscordVoiceWebSocket
+from discord.utils import MISSING
+
+from typing import TYPE_CHECKING
+
+from .gateway import hook
+from .reader import AudioReader
+from .sinks import AudioSink
+
+if TYPE_CHECKING:
+    from typing import Optional, Dict, Any
+    from discord.ext.commands._types import CoroFunc
+    from .reader import AfterCB
+
+from pprint import pformat
+
+__all__ = [
+    "VoiceRecvClient"
+]
+
+log = logging.getLogger(__name__)
+
+class VoiceRecvClient(discord.VoiceClient):
+    def __init__(self, client, channel):
+        super().__init__(client, channel)
+
+        self._connecting = threading.Condition()
+        self._reader: Optional[AudioReader] = None
+        self._ssrc_to_id: Dict[int, int] = {}
+        self._id_to_ssrc: Dict[int, int] = {}
+        self._event_listeners: Dict[str, list] = {}
+        self._speaking_cache: Dict[int, float] = {}
+
+    async def connect_websocket(self):
+        ws = await DiscordVoiceWebSocket.from_client(self, hook=hook)
+        self._connected.clear()
+        while ws.secret_key is None:
+            await ws.poll_event()
+        self._connected.set()
+        return ws
+
+    async def on_voice_state_update(self, data):
+        old_channel_id = self.channel.id if self.channel else None
+
+        await super().on_voice_state_update(data)
+
+        log.debug("Got voice_client VSU: \n%s", pformat(data, compact=True))
+
+        channel_id = int(data['channel_id'])
+
+        # if we joined, left, or switched channels, reset the decoders
+        if self._reader and channel_id != old_channel_id:
+            log.debug("Destroying all decoders in guild %s", self.guild.id)
+            self._reader.router.destroy_all_decoders()
+
+    def add_listener(self, func: CoroFunc, *, name: str=MISSING):
+        name = func.__name__ if name is MISSING else name
+
+        if not asyncio.iscoroutinefunction(func):
+            raise TypeError('Listeners must be coroutines')
+
+        if name in self._event_listeners:
+            self._event_listeners[name].append(func)
+        else:
+            self._event_listeners[name] = [func]
+
+    def remove_listener(self, func: CoroFunc, *, name: str=MISSING):
+        name = func.__name__ if name is MISSING else name
+
+        if name in self._event_listeners:
+            try:
+                self._event_listeners[name].remove(func)
+            except ValueError:
+                pass
+
+    async def _run_event(self, coro, event_name, *args, **kwargs):
+        try:
+            await coro(*args, **kwargs)
+        except asyncio.CancelledError:
+            pass
+        except Exception:
+            log.exception("Error calling %s", event_name)
+
+    def _schedule_event(self, coro, event_name, *args, **kwargs):
+        wrapped = self._run_event(coro, event_name, *args, **kwargs)
+        return self.client.loop.create_task(wrapped, name=f"ext.voice_recv: {event_name}")
+
+    def dispatch(self, event: str, /, *args: Any, **kwargs: Any):
+        log.debug("Dispatching voice_client event %s", event)
+
+        event_name = f"on_{event}"
+        for coro in self._event_listeners.get(event_name, []):
+            self._schedule_event(coro, event_name, *args, **kwargs)
+
+        if self._reader:
+            self._reader.router.dispatch(event, *args, **kwargs)
+
+        self.client.dispatch(event, *args, **kwargs)
+
+    def cleanup(self):
+        super().cleanup()
+        self._event_listeners.clear()
+        self.stop()
+
+    def _add_ssrc(self, user_id: int, ssrc: int):
+        self._ssrc_to_id[ssrc] = user_id
+        self._id_to_ssrc[user_id] = ssrc
+
+        if self._reader:
+            self._reader.router.notify(ssrc, user_id)
+
+    def _remove_ssrc(self, *, user_id: int):
+        ssrc = self._id_to_ssrc.pop(user_id, None)
+        if ssrc:
+            self._ssrc_to_id.pop(ssrc, None)
+
+    def _get_ssrc_from_id(self, user_id: int) -> Optional[int]:
+        return self._id_to_ssrc.get(user_id)
+
+    def _get_id_from_ssrc(self, ssrc: int) -> Optional[int]:
+        return self._ssrc_to_id.get(ssrc)
+
+    def listen(self, sink: AudioSink, *, after: Optional[AfterCB]=None):
+        """Receives audio into a :class:`AudioSink`. TODO: more info"""
+
+        if not self.is_connected():
+            raise discord.ClientException('Not connected to voice.')
+
+        if not isinstance(sink, AudioSink):
+            raise TypeError('sink must be an AudioSink not {0.__class__.__name__}'.format(sink))
+
+        if self.is_listening():
+            raise discord.ClientException('Already receiving audio.')
+
+        self._reader = AudioReader(sink, self, after=after)
+        self._reader.start()
+
+    def is_listening(self) -> bool:
+        """Indicates if we're currently receiving audio."""
+        return self._reader is not None and self._reader.is_listening()
+
+    def stop_listening(self):
+        """Stops receiving audio."""
+        if self._reader:
+            self._reader.stop()
+            self._reader.join(5)
+            self._reader = None
+
+    def stop_playing(self):
+        """Stops playing audio."""
+        if self._player:
+            self._player.stop()
+            self._player = None
+
+    def stop(self):
+        """Stops playing and receiving audio."""
+        self.stop_playing()
+        self.stop_listening()
+
+    @property
+    def sink(self) -> Optional[AudioSink]:
+        return self._reader.sink if self._reader else None
+
+    @sink.setter
+    def sink(self, sink: AudioSink):
+        if not isinstance(sink, AudioSink):
+            raise TypeError('expected AudioSink not {0.__class__.__name__}.'.format(sink))
+
+        if self._reader is None:
+            raise ValueError('Not receiving anything.')
+
+        self._reader.set_sink(sink)
+
+    def get_speaking(self, member: discord.Member | discord.User) -> Optional[bool]:
+        """Returns if a member is speaking (approximately), or None if not found."""
+
+        ssrc = self._get_ssrc_from_id(member.id)
+        if ssrc is None:
+            return
+
+        last_packet_time = self._speaking_cache.get(ssrc, None)
+        if last_packet_time is None:
+            return
+
+        return time.time() - last_packet_time < 0.02

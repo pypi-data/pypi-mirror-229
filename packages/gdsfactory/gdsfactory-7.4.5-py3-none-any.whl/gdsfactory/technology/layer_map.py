a439da24d9c0a4100fcbb320884c6d6221ccf7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:20:05 +0000

    Add package gdsfactory/7.4.5/gdsfactory-7.4.5-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-7.4.5-py3-none-any.whl/gdsfactory/technology/layer_map.py
@@ -0,0 +1,38 @@
+import pathlib
+
+from gdsfactory.technology.layer_views import LayerViews
+
+
+def lyp_to_dataclass(lyp_filepath: str | pathlib.Path, overwrite: bool = True) -> str:
+    """Returns python LayerMap script from a klayout layer properties file lyp."""
+    filepathin = pathlib.Path(lyp_filepath)
+    filepathout = filepathin.with_suffix(".py")
+
+    if filepathout.exists() and not overwrite:
+        raise FileExistsError(f"You can delete {filepathout}")
+
+    script = """
+from pydantic import BaseModel
+from gdsfactory.typings import Layer
+
+
+class LayerMap(BaseModel):
+"""
+    lys = LayerViews.from_lyp(filepathin)
+    for layer_name, layer in sorted(lys.get_layer_views().items()):
+        script += f"    {layer_name}: Layer = ({layer.layer[0]}, {layer.layer[1]})\n"
+
+    script += """
+    model_config = {'extra': 'forbid', 'frozen': True}
+
+LAYER = LayerMap()
+"""
+
+    filepathout.write_text(script)
+    return script
+
+
+if __name__ == "__main__":
+    from gdsfactory.config import PATH
+
+    print(lyp_to_dataclass(PATH.klayout_lyp))

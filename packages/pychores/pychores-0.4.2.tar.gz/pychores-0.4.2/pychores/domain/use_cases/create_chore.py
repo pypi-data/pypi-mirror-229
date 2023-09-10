7402a6ae3af207fb7d8c79d09b7083f463c93815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:08:12 +0000

    Add package pychores/0.4.2/pychores-0.4.2.tar.gz

---
+++ packages/pychores/pychores-0.4.2.tar.gz/pychores-0.4.2/pychores/domain/use_cases/create_chore.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from datetime import date
+from typing import Callable
+
+from pychores.domain.entity.chore import Chore
+
+
+class ICreateChore(ABC):
+    @abstractmethod
+    def save_new_chore(self, username: str, chore: Chore) -> Chore:
+        """Save a new Chore for username"""
+
+
+class CreateChore:
+    def __init__(self, repo: ICreateChore, date_provider: Callable[[], date]):
+        self.repo = repo
+        self.date_provider = date_provider
+
+    def execute(self, username: str, payload: dict) -> Chore:
+        chore = Chore(
+            name=payload["name"],
+            description=payload["description"],
+            period=payload["period"],
+            current_date=self.date_provider(),
+            last_execution=None,
+        )
+        created_chore = self.repo.save_new_chore(username=username, chore=chore)
+        return created_chore

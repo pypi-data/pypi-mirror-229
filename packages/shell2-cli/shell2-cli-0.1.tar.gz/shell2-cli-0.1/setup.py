fe73f4fd7b88c3ef3fa2b052d8fde9f8ac2f0e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:09:27 +0000

    Add package shell2-cli/0.1/shell2-cli-0.1.tar.gz

---
+++ packages/shell2-cli/shell2-cli-0.1.tar.gz/shell2-cli-0.1/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='shell2-cli',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'shell2',
+        'prettyprinter',
+        'inquirer',
+        'rich',
+        'prompt_toolkit',
+        'colorama',
+        'google-cloud-firestore',
+        'google-auth',
+        'requests',
+        'retry',
+        'python-slugify',
+        'py7zr'
+    ],
+    entry_points={
+        'console_scripts': [
+            'shell2_cli_menu = scripts.shell2_cli_menu:main', # i switch the order of these two and it ceases working ... !
+            'shell2_cli_live = scripts.shell2_cli_live:main',
+            'shell2 = scripts.shell2_cli:main'
+        ]
+    }
+
+)

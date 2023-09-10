f2f386210535643ea64fcc854842e6f3be0d3fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:17 +0000

    Add package speaksynk-flow-processor/1.0.3/speaksynk_flow_processor-1.0.3.tar.gz

---
+++ packages/speaksynk-flow-processor/speaksynk_flow_processor-1.0.3.tar.gz/speaksynk_flow_processor-1.0.3/speaksynk_flow_processor/interfaces/IFlow.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod
+
+
+class IFlow():
+
+    @abstractmethod
+    def download(self, filekey):
+        pass
+
+    @abstractmethod
+    def upload(self, filekey):
+        pass
---
+++ packages/speaksynk-flow-processor/speaksynk_flow_processor-1.0.3.tar.gz/speaksynk_flow_processor-1.0.3/speaksynk_flow_processor/interfaces/IProcessor.py
@@ -0,0 +1,8 @@
+from abc import abstractmethod
+
+
+class IProcessor():
+
+    @abstractmethod
+    def run(self):
+        pass

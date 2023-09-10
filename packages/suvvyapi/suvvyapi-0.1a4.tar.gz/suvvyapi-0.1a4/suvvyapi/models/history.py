7b98c8492afbfca24083dc2a0e4e6e465c116e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:18:03 +0000

    Add package suvvyapi/0.1a4/suvvyapi-0.1a4.tar.gz

---
+++ packages/suvvyapi/suvvyapi-0.1a4.tar.gz/suvvyapi-0.1a4/suvvyapi/models/history.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Literal, Optional
+
+from pydantic import BaseModel, Field
+
+from suvvyapi.enums import Role
+
+
+class FunctionDetails(BaseModel):
+    name: str
+    args: Optional[dict] = None
+
+
+class Message(BaseModel):
+    text: str = Field(default="", description="Message text, function result")
+    role: Role = Role.HUMAN
+    function: Optional[FunctionDetails] = Field(default=None,
+                                                description="Needed for functions. Unused if role != \"function_*\".")
+
+
+class HistoryMessage(Message):
+    tokens: int = 0
+    time: datetime
+    message_id: int
+    context: str = ""
+
+
+class History(BaseModel):
+    history: list[HistoryMessage]
+    unique_id: str
+    stopped: bool = False
+    stop_reason: str = "unknown"

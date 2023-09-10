cab81f457c6905fe11661628b6cc6788b10c62a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:17:56 +0000

    Add package python-logdog/0.1.2/python-logdog-0.1.2.tar.gz

---
+++ packages/python-logdog/python-logdog-0.1.2.tar.gz/python-logdog-0.1.2/CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+
+# [v0.1.2] - 2023.09.09
+
+[v0.1.2]: https://github.com/a-was/logdog.py/compare/v0.1.0...v0.1.2
+
+*Version v0.1.1 was omitted due to a mistake.*
+
+- LogMessageWrapper:
+    - Add missing `stacklevel` to log functions
+- Update project metadata
+
+
+# [v0.1.0] - 2023.09.06
+
+[v0.1.0]: https://github.com/a-was/logdog.py/releases/tag/v0.1.0
+
+First release!
+
+Features:
+
+- `LogMessageWrapper`
+    - `LogfmtRenderer`
+    - `JsonRenderer`
---
+++ packages/python-logdog/python-logdog-0.1.2.tar.gz/python-logdog-0.1.2/src/logdog/wrapper/wrapper.py
@@ -0,0 +1,80 @@
+import logging
+
+from .base import BaseRenderer
+from .logfmt_renderer import LogfmtRenderer
+
+
+class LogMessageWrapper:
+    """
+    LogMessageWrapper wraps a logging.Logger and provides log message formatting.
+    """
+
+    def __init__(
+        self,
+        logger: logging.Logger,
+        *,
+        prefix: str = " ",
+        suffix: str | None = None,
+        renderer: BaseRenderer | None = None,
+    ):
+        if not isinstance(logger, logging.Logger):
+            raise TypeError("logger must be a logging.Logger")
+        self.logger = logger
+
+        if not isinstance(prefix, str):
+            raise TypeError("prefix must be a string")
+        self._prefix = prefix
+
+        if suffix is None:
+            suffix = ""
+        elif not isinstance(suffix, str):
+            raise TypeError("suffix must be a string")
+        self._suffix = suffix
+
+        if renderer is None:
+            renderer = LogfmtRenderer()
+        elif not isinstance(renderer, BaseRenderer):
+            raise TypeError(f"renderer must be an instance of {BaseRenderer.__name__}")
+        self._renderer = renderer
+
+    def _wrap(self, msg: str, kwargs: dict) -> str:
+        if not kwargs:
+            return msg
+        return msg + self._prefix + self._renderer.render(kwargs) + self._suffix
+
+    def debug(self, msg: str, **kwargs):
+        self.logger.debug(
+            self._wrap(msg, kwargs),
+            stacklevel=2,
+        )
+
+    def info(self, msg: str, **kwargs):
+        self.logger.info(
+            self._wrap(msg, kwargs),
+            stacklevel=2,
+        )
+
+    def warning(self, msg: str, **kwargs):
+        self.logger.warning(
+            self._wrap(msg, kwargs),
+            stacklevel=2,
+        )
+
+    def error(self, msg: str, **kwargs):
+        self.logger.error(
+            self._wrap(msg, kwargs),
+            stacklevel=2,
+        )
+
+    def exception(self, msg: str, **kwargs):
+        self.logger.exception(
+            self._wrap(msg, kwargs),
+            stacklevel=3,
+        )
+
+    def critical(self, msg: str, *, exc_info: bool = False, **kwargs):
+        self.logger.critical(
+            self._wrap(msg, kwargs),
+            exc_info=exc_info,
+            stacklevel=2,
+        )

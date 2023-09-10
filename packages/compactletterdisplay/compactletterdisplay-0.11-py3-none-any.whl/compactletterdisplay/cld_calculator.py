30603dfeceb61ea18a6c30f141343e8844a6ae4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:18:25 +0000

    Add package compactletterdisplay/0.11/compactletterdisplay-0.11-py3-none-any.whl

---
+++ packages/compactletterdisplay/compactletterdisplay-0.11-py3-none-any.whl/compactletterdisplay/cld_calculator.py
@@ -0,0 +1,88 @@
+def get_next_unused_letter(columns):
+    """
+    Identify the next unused lowercase letter to use for compact lettering.
+  
+    Parameters:
+    columns (list of strs): List of current column groups.
+
+    Returns:
+    str or None: Returns the next available lowercase letter, or None if all 26 letters are already used.
+    """
+    used_letters = set(letter for col in columns for letter in col if letter != '')
+    
+    # Iterate through the alphabet to find an unused letter.
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        if letter not in used_letters:
+            return letter
+    
+    # Return None if all letters are used (which should only happen with >26 columns).
+    return None  
+
+def absorb_columns(columns):
+    """
+    Absorbs redundant columns by comparing indices.
+
+    Parameters:
+    columns (list of strs): List of current column groups.
+
+    Returns:
+    list of strs: The processed list of column groups.
+    """
+    absorbed = True
+    while absorbed:
+        absorbed = False
+        for i, col1 in enumerate(columns):
+            for j, col2 in enumerate(columns):
+                if i != j:
+                    indices1 = {index for index, letter in enumerate(col1) if letter != ''}
+                    indices2 = {index for index, letter in enumerate(col2) if letter != ''}
+                    if indices1.issubset(indices2):
+                        absorbed = True
+                        columns.pop(i)
+                        break
+            if absorbed:
+                break
+    return columns
+
+def compact_letter_display(significant_pairs, columns):
+    """
+    Generate compact letter display (CLD) for columns based on significant pairs.
+    
+    Parameters:
+    significant_pairs (list of tuples): Significant pairs identified in a Tukey HSD test.
+    columns (list of str): Columns in the DataFrame.
+
+    Returns:
+    list of str: The compact letter display representation.
+    """
+    num_groups = len(columns)
+
+    # Map column names to indices.
+    col_to_index = {col: idx for idx, col in enumerate(columns)}
+
+    # Map significant pair names to indices.
+    significant_pairs = [(col_to_index[col1], col_to_index[col2]) for col1, col2 in significant_pairs]
+
+
+    columns = [['a'] * num_groups]
+    for pair_idx, (i, j) in enumerate(significant_pairs):
+        connected = False
+        for idx, column in enumerate(columns):
+            # When current pair have the same letter...
+            if column[i] == column[j] and column[i] != '':
+                connected = True
+                new_letter = get_next_unused_letter(columns)
+                new_column = column.copy() 
+                new_column = [new_letter if column[i] != '' else '' for i in range(num_groups)]
+                new_column[i] = ''
+                column[j] = ''
+                columns[idx] = column
+                columns.append(new_column)
+                columns = absorb_columns(columns)
+            if connected:
+                break 
+
+    # Generate compact letter displays from the columns list.
+    result = [''.join(columns[k][n] for k in range(len(columns)) if columns[k][n] != '') for n in range(num_groups)]
+ 
+    return result

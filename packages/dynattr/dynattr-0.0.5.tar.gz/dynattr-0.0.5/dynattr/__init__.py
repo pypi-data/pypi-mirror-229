44e02b81955c9aaa0aad8806a0c9abf48e3ed9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:31 +0000

    Add package dynattr/0.0.5/dynattr-0.0.5.tar.gz

---
+++ packages/dynattr/dynattr-0.0.5.tar.gz/dynattr-0.0.5/dynattr/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.5"
+
+from .core import *
+
+__all__ = ['ClassAttributeError', 'DynamicAttributeError', 'noop', 'optarg_fallback', 'optarg_fallback_to_clsattr',
+           'DynamicAttributesMixin']

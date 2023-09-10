a0f3fd86c6bdc3769da8f1f750f86dcf3847c275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:08:55 +0000

    Add package data-to-snowflake/2.0.4/data_to_snowflake-2.0.4.tar.gz

---
+++ packages/data-to-snowflake/data_to_snowflake-2.0.4.tar.gz/data_to_snowflake-2.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_to_snowflake',
+    version='2.0.4',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package requires to run
+        'snowflake-connector-python[pandas]',
+        'python-dotenv',
+        'pandas',
+        'pygsheets',
+    ],
+)
+

66175d547d23cad7498674738a8658e24724e5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:18:55 +0000

    Add package shell-whiz/0.3.0/shell_whiz-0.3.0.tar.gz

---
+++ packages/shell-whiz/shell_whiz-0.3.0.tar.gz/shell_whiz-0.3.0/shell_whiz/constants.py
@@ -0,0 +1,10 @@
+SW_ERROR = "[bold yellow]Error[/]"
+
+INV_CLI_ARGS_EXIT_CODE = 1
+OPENAI_ERROR_EXIT_CODE = 2
+SW_ERROR_EXIT_CODE = 3
+
+SW_WAIT_MSG = "Wait, Shell Whiz is thinking"
+SW_WAIT_MSG_COLOR = "green"
+
+DELIMITER = "####"

498e4c0abea2aee6b4757a8963469dd42ac5a248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:09:04 +0000

    Add package PyHQ/1.3.3/PyHQ-1.3.3.tar.gz

---
+++ packages/PyHQ/PyHQ-1.3.3.tar.gz/PyHQ-1.3.3/PyHQ/Fun/__init__.py
@@ -0,0 +1,124 @@
+# PyHQ (Fun) - Init
+
+''' This is the __init__.py file. '''
+
+'''
+Copyright 2023 Aniketh Chavare
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+# Imports
+import json
+import random
+import pyjokes
+import requests
+import randfacts
+
+# Function 1 - Joke
+def joke(topic="random"):
+    # Variables
+    joke_topics = ["random", "general", "programming", "knock-knock"]
+    programming_joke_random = random.choice([1, 2])
+    api_endpoint = "https://official-joke-api.appspot.com/jokes/{0}"
+
+    # Checking the Data Type of "topic"
+    if (isinstance(topic, str)):
+        # Checking the Value of "topic"
+        if (topic in joke_topics):
+            # Fetching and Returning the Joke
+            if (topic == "random"):
+                # Fetching the Joke
+                try:
+                    response = json.loads(requests.get(api_endpoint.format("random")).text)
+                except requests.ConnectionError:
+                    raise ConnectionError("A connection error occurred. Please try again.")
+                except:
+                    raise Exception("Something went wrong. Please try again.")
+
+                # Deleting Unwanted Keys
+                del response["type"]
+                del response["id"]
+
+                # Returning the Joke
+                return response["setup"] + " " + response["punchline"]
+            elif (topic in ["general", "knock-knock"]):
+                # Fetching the Joke
+                try:
+                    response = json.loads(requests.get(api_endpoint.format(topic + "/random")).text)[0]
+                except requests.ConnectionError:
+                    raise ConnectionError("A connection error occurred. Please try again.")
+                except:
+                    raise Exception("Something went wrong. Please try again.")
+
+                # Deleting Unwanted Keys
+                del response["type"]
+                del response["id"]
+
+                # Returning the Joke
+                return response["setup"] + " " + response["punchline"]
+            elif (topic == "programming"):
+                # Checking the Value of "programming_joke_random"
+                if (programming_joke_random == 1):
+                    # Fetching the Joke
+                    try:
+                        response = json.loads(requests.get(api_endpoint.format(topic + "/random")).text)[0]
+                    except requests.ConnectionError:
+                        raise ConnectionError("A connection error occurred. Please try again.")
+                    except:
+                        raise Exception("Something went wrong. Please try again.")
+
+                    # Deleting Unwanted Keys
+                    del response["type"]
+                    del response["id"]
+
+                    # Returning the Joke
+                    return response["setup"] + " " + response["punchline"]
+                elif (programming_joke_random == 2):
+                    # Returning the Joke
+                    return pyjokes.get_joke()
+        else:
+            raise Exception("The 'topic' argument must be either 'random', 'general', 'programming', or 'knock-knock'.")
+    else:
+        raise TypeError("The 'topic' argument must be a string.")
+
+# Function 2 - Fact
+def fact(filter=True, unsafe=False):
+    # Variables
+    parameters = ["filter", "unsafe"]
+
+    # Parameters & Data Types
+    paramaters_data = {
+        "filter": [bool, "a boolean"],
+        "unsafe": [bool, "a boolean"]
+    }
+
+    # Checking the Data Types
+    for parameter in parameters:
+        if (isinstance(eval(parameter), paramaters_data[parameter][0])):
+            pass
+        else:
+            raise Exception("The '{0}' argument must be {1}.".format(parameter, paramaters_data[parameter][1]))
+
+    # Returning the Fact
+    return randfacts.get_fact(filter_enabled=filter, only_unsafe=unsafe)
+
+# Function 3 - Bored
+def bored():
+    # Fetching and Returning the Data
+    try:
+        return json.loads(requests.get("https://boredapi.com/api/activity").text)
+    except requests.ConnectionError:
+        raise ConnectionError("A connection error occurred. Please try again.")
+    except:
+        raise Exception("Something went wrong. Please try again.")

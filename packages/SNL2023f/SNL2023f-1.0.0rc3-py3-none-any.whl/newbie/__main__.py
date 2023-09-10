f41e25ff22fd0396301f4ed6fea57fdc2ac997b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 14:08:42 +0000

    Add package SNL2023f/1.0.0rc3/SNL2023f-1.0.0rc3-py3-none-any.whl

---
+++ packages/SNL2023f/SNL2023f-1.0.0rc3-py3-none-any.whl/newbie/__main__.py
@@ -0,0 +1,24 @@
+import click
+import newbie
+
+__version__ = "1.0.0-pre3"
+
+@click.version_option(prog_name="SNL2023f", version=__version__)
+@click.group()
+def main():
+    pass
+
+@main.command("server")
+def server():
+    newbie.server()
+
+@main.command("container")
+def container():
+    newbie.container()
+
+@main.command("client")
+def client():
+    newbie.client()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/SNL2023f/SNL2023f-1.0.0rc3-py3-none-any.whl/newbie/commands/server.py
@@ -0,0 +1,8 @@
+import newbie
+
+from flack import Flack
+
+app = Flack(__name__)
+
+def server():
+    pass

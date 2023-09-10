dfbcf2dbd92f88f34ac62a1e29dbb698314a6206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:12 +0000

    Add package asocialgraph/1.0.0/asocialgraph-1.0.0.tar.gz

---
+++ packages/asocialgraph/asocialgraph-1.0.0.tar.gz/asocialgraph-1.0.0/README.md
@@ -0,0 +1,7 @@
+# Social Graph API v1
+
+<a href="https://t.me/friendly_graph_bot">Social Graph</a> - a product for analyzing social connections based on digital representations of individuals.
+
+pip install social-graph-api
+
+<a href="https://pypi.org/project/social-graph-api/">Social Graph API</a>
---
+++ packages/asocialgraph/asocialgraph-1.0.0.tar.gz/asocialgraph-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='asocialgraph',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Social Graph API',
+    install_requires=required,
+    author='Graphy',
+    # description='API for working with the product Social Graph'
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)

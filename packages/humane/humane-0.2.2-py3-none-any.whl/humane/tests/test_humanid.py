f97914f17e0ae5ede775d312bd447b6fb14ea546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:27 +0000

    Add package humane/0.2.2/humane-0.2.2-py3-none-any.whl

---
+++ packages/humane/humane-0.2.2-py3-none-any.whl/humane/tests/test_humanid.py
@@ -0,0 +1,9 @@
+
+from humane.humanid import human_id, short_human_id
+
+
+def test_humanid_and_short_common() -> None:
+    assert human_id("test") == "12-tasty-walruses-know-irritably"
+    
+    assert short_human_id("test") in human_id("test")
+    

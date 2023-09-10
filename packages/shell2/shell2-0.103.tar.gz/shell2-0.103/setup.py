0e88fb626abb813d61a6a4ace3282f68a0335f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:30 +0000

    Add package shell2/0.103/shell2-0.103.tar.gz

---
+++ packages/shell2/shell2-0.103.tar.gz/shell2-0.103/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='shell2',
+    version='0.103',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)

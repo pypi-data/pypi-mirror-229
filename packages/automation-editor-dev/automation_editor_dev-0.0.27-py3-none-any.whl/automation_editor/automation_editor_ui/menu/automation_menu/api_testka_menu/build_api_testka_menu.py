0c9311d0ce1b1b4dd8e65e7f221e492a0f809a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:18:21 +0000

    Add package automation-editor-dev/0.0.27/automation_editor_dev-0.0.27-py3-none-any.whl

---
+++ packages/automation-editor-dev/automation_editor_dev-0.0.27-py3-none-any.whl/automation_editor/automation_editor_ui/menu/automation_menu/api_testka_menu/build_api_testka_menu.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from automation_editor.automation_editor_ui.menu.menu_utils import open_web_browser
+
+if TYPE_CHECKING:
+    from automation_editor.automation_editor_ui.editor_main.main_ui import AutomationEditor
+import sys
+
+from PySide6.QtGui import QAction
+
+from automation_editor.extend.process_executor.api_testka.api_testka_process import call_api_testka, \
+    call_api_testka_with_send, call_api_testka_multi_file, call_api_testka_multi_file_and_send
+
+
+def set_apitestka_menu(ui_we_want_to_set: AutomationEditor):
+    """
+    Build menu include APITestka feature.
+    :param ui_we_want_to_set: main window to add menu.
+    :return: None
+    """
+    ui_we_want_to_set.apitestka_menu = ui_we_want_to_set.automation_menu.addMenu("APITestka")
+    ui_we_want_to_set.apitestka_run_menu = ui_we_want_to_set.apitestka_menu.addMenu("Run")
+    # Run APITestka Script
+    ui_we_want_to_set.run_apitestka_action = QAction("Run APITestka Script")
+    ui_we_want_to_set.run_apitestka_action.triggered.connect(
+        lambda: call_api_testka(
+            ui_we_want_to_set,
+        )
+    )
+    ui_we_want_to_set.apitestka_run_menu.addAction(ui_we_want_to_set.run_apitestka_action)
+    # Run APITestka Script With Send
+    ui_we_want_to_set.run_apitestka_action_with_send = QAction("Run APITestka With Send")
+    ui_we_want_to_set.run_apitestka_action_with_send.triggered.connect(
+        lambda: call_api_testka_with_send(
+            ui_we_want_to_set,
+        )
+    )
+    ui_we_want_to_set.apitestka_run_menu.addAction(
+        ui_we_want_to_set.run_apitestka_action_with_send
+    )
+    # Run Multi APITestka Script
+    ui_we_want_to_set.run_multi_apitestka_action = QAction("Run Multi APITestka Script")
+    ui_we_want_to_set.run_multi_apitestka_action.triggered.connect(
+        lambda: call_api_testka_multi_file(
+            ui_we_want_to_set,
+        )
+    )
+    ui_we_want_to_set.apitestka_run_menu.addAction(
+        ui_we_want_to_set.run_multi_apitestka_action
+    )
+    # Run Multi APITestka Script With Send
+    ui_we_want_to_set.run_multi_apitestka_action_with_send = QAction("Run Multi APITestka Script With Send")
+    ui_we_want_to_set.run_multi_apitestka_action_with_send.triggered.connect(
+        lambda: call_api_testka_multi_file_and_send(
+            ui_we_want_to_set,
+        )
+    )
+    ui_we_want_to_set.apitestka_run_menu.addAction(
+        ui_we_want_to_set.run_multi_apitestka_action_with_send
+    )
+    ui_we_want_to_set.apitestka_help_menu = ui_we_want_to_set.apitestka_menu.addMenu("HELP")
+    # Open Doc
+    ui_we_want_to_set.open_apitestka_doc_action = QAction("Open APITestka Doc")
+    ui_we_want_to_set.open_apitestka_doc_action.triggered.connect(
+        lambda: open_web_browser(
+            ui_we_want_to_set,
+            "https://apitestka.readthedocs.io/en/latest/",
+            "APITestka Doc"
+        )
+    )
+    ui_we_want_to_set.apitestka_help_menu.addAction(
+        ui_we_want_to_set.open_apitestka_doc_action
+    )
+    # Open Github
+    ui_we_want_to_set.open_apitestka_github_action = QAction("Open APITestka GitHub")
+    ui_we_want_to_set.open_apitestka_github_action.triggered.connect(
+        lambda: open_web_browser(
+            ui_we_want_to_set,
+            "https://github.com/Intergration-Automation-Testing/APITestka",
+            "APITestka Github"
+        )
+    )
+    ui_we_want_to_set.apitestka_help_menu.addAction(
+        ui_we_want_to_set.open_apitestka_github_action
+    )
+    ui_we_want_to_set.apitestka_project_menu = ui_we_want_to_set.apitestka_menu.addMenu("Project")
+    # Create Project
+    ui_we_want_to_set.create_apitestka_project_action = QAction("Create APITestka Project")
+    ui_we_want_to_set.create_apitestka_project_action.triggered.connect(
+        create_project
+    )
+    ui_we_want_to_set.apitestka_project_menu.addAction(
+        ui_we_want_to_set.create_apitestka_project_action
+    )
+
+
+def create_project() -> None:
+    try:
+        import je_api_testka
+        package = je_api_testka
+        if package is not None:
+            package.create_project_dir()
+    except ImportError as error:
+        print(repr(error), file=sys.stderr)
---
+++ packages/automation-editor-dev/automation_editor_dev-0.0.27-py3-none-any.whl/automation_editor/automation_editor_ui/menu/automation_menu/mail_thunder_menu/build_mail_thunder_menu.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from automation_editor.automation_editor_ui.menu.menu_utils import open_web_browser
+
+if TYPE_CHECKING:
+    from automation_editor.automation_editor_ui.editor_main.main_ui import AutomationEditor
+import sys
+
+from PySide6.QtGui import QAction
+
+from automation_editor.extend.process_executor.mail_thunder.mail_thunder_process import call_mail_thunder
+
+
+def set_mail_thunder_menu(ui_we_want_to_set: AutomationEditor):
+    """
+    Build menu include LoadDensity feature.
+    :param ui_we_want_to_set: main window to add menu.
+    :return: None
+    """
+    ui_we_want_to_set.mail_thunder_menu = ui_we_want_to_set.automation_menu.addMenu("MailThunder")
+    ui_we_want_to_set.mail_thunder_run_menu = ui_we_want_to_set.mail_thunder_menu.addMenu("Run")
+    # Run MailThunder
+    ui_we_want_to_set.run_mail_thunder_action = QAction("Run MailThunder")
+    ui_we_want_to_set.run_mail_thunder_action.triggered.connect(
+        lambda: call_mail_thunder(
+            ui_we_want_to_set,
+        )
+    )
+    ui_we_want_to_set.mail_thunder_run_menu.addAction(
+        ui_we_want_to_set.run_mail_thunder_action
+    )
+    # Help menu
+    ui_we_want_to_set.mail_thunder_help_menu = ui_we_want_to_set.mail_thunder_menu.addMenu("HELP")
+    # Open Doc
+    ui_we_want_to_set.open_mail_thunder_doc_action = QAction("Open MailThunder Doc")
+    ui_we_want_to_set.open_mail_thunder_doc_action.triggered.connect(
+        lambda: open_web_browser(
+            ui_we_want_to_set,
+            "https://mailthunder.readthedocs.io/en/latest/",
+            "MailThunder Doc"
+        )
+    )
+    ui_we_want_to_set.mail_thunder_help_menu.addAction(
+        ui_we_want_to_set.open_mail_thunder_doc_action
+    )
+    # Open Github
+    ui_we_want_to_set.open_mail_thunder_github_action = QAction("Open MailThunder GitHub")
+    ui_we_want_to_set.open_mail_thunder_github_action.triggered.connect(
+        lambda: open_web_browser(
+            ui_we_want_to_set,
+            "https://github.com/Integration-Automation/MailThunder",
+            "MailThunder GitHub"
+        )
+    )
+    ui_we_want_to_set.mail_thunder_help_menu.addAction(
+        ui_we_want_to_set.open_mail_thunder_github_action
+    )
+    ui_we_want_to_set.mail_thunder_project_menu = ui_we_want_to_set.mail_thunder_menu.addMenu("Project")
+    # Create Project
+    ui_we_want_to_set.create_mail_thunder_project_action = QAction("Create MailThunder Project")
+    ui_we_want_to_set.create_mail_thunder_project_action.triggered.connect(
+        create_project
+    )
+    ui_we_want_to_set.mail_thunder_project_menu.addAction(
+        ui_we_want_to_set.create_mail_thunder_project_action
+    )
+
+
+def create_project() -> None:
+    try:
+        import je_mail_thunder
+        package = je_mail_thunder
+        if package is not None:
+            package.create_project_dir()
+    except ImportError as error:
+        print(repr(error), file=sys.stderr)

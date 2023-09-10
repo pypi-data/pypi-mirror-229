00d5f4f7950548f836c66598e84464eaceb8b443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:18:33 +0000

    Add package scrules/1.0.7/scrules-1.0.7.tar.gz

---
+++ packages/scrules/scrules-1.0.7.tar.gz/scrules-1.0.7/README.md
@@ -0,0 +1,3 @@
+# scrules
+
+A python library that mines association rules between genes and mines regulatory networks through the association rule algorithm fp-growth
---
+++ packages/scrules/scrules-1.0.7.tar.gz/scrules-1.0.7/setup.py
@@ -0,0 +1,51 @@
+# -*-coding:utf-8-*-
+"""
+@Author: Phantom
+@Time:2023/9/4 7:46
+@Email: 2909981736@qq.com
+"""
+
+from setuptools import setup, find_packages
+__version__ = '1.0.7'
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="scrules",
+    version=__version__,
+    author="phantom",
+    author_email="2909981736@qq.com",
+    description="A library that mines association rules between genes and mines regulatory networks through the association rule algorithm fp-growth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/PH-Open/scrules",
+    packages=find_packages(),
+    install_requires=[
+        "pyfim",
+        "igraph",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "pymysql",
+        "sqlalchemy",
+        "requests"
+    ],
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)

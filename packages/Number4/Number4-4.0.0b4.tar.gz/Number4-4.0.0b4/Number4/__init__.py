610c871f5ff5f43cba7904556c23c2c628aeb0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-10 03:19:53 +0000

    Add package Number4/4.0.0b4/Number4-4.0.0b4.tar.gz

---
+++ packages/Number4/Number4-4.0.0b4.tar.gz/Number4-4.0.0b4/Number4/__init__.py
@@ -0,0 +1,15 @@
+"""
+Powerful Numbers for Python
+"""
+
+from .Number4 import *
+from . import NumMath4 as math
+
+
+__all__ = ["Num", 
+           "ComplexNum", 
+           "RealNum", "ImagNum", 
+           "RationalNum", "IntegralNum", "Digit", "Sign", 
+           "num", "built", "math"]
+
+__version__ = (4, 0, 0)

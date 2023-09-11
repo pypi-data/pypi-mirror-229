6464622a349fee2fc49261406758bda541bcfe58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:08:12 +0000

    Add package vaxila-opentelemetry-util/0.0.1/vaxila_opentelemetry_util-0.0.1-py3-none-any.whl

---
+++ packages/vaxila-opentelemetry-util/vaxila_opentelemetry_util-0.0.1-py3-none-any.whl/vaxila/opentelemetry/util/__init__.py
@@ -0,0 +1,9 @@
+from ._internal.exception_recorder import (
+    enable_exc_local_variables_recording,
+    disable_exc_local_variables_recording,
+)
+
+__all__ = [
+    "enable_exc_local_variables_recording",
+    "disable_exc_local_variables_recording",
+]

6023a1360a4ccba8a187feaed51e98ece768db19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:08:07 +0000

    Add package manifestoo-core/1.0/manifestoo_core-1.0.tar.gz

---
+++ packages/manifestoo-core/manifestoo_core-1.0.tar.gz/manifestoo_core-1.0/src/manifestoo_core/exceptions.py
@@ -0,0 +1,46 @@
+class ManifestooException(Exception):  # noqa: N818
+    """Base class of all manifestoo_core exceptions."""
+
+
+class UnsupportedOdooVersion(ManifestooException):
+    pass
+
+
+class UnsupportedManifestVersion(ManifestooException):
+    pass
+
+
+class InvalidManifest(ManifestooException):
+    pass
+
+
+class AddonNotFound(ManifestooException):
+    pass
+
+
+class AddonNotFoundNotInstallable(AddonNotFound):
+    pass
+
+
+class AddonNotFoundNoInit(AddonNotFound):
+    pass
+
+
+class AddonNotFoundNoManifest(AddonNotFound):
+    pass
+
+
+class AddonNotFoundNotADirectory(AddonNotFound):
+    pass
+
+
+class AddonNotFoundInvalidManifest(AddonNotFound):
+    pass
+
+
+class InvalidDistributionName(ManifestooException):
+    pass
+
+
+class UnknownPostVersionStrategy(ManifestooException):
+    pass

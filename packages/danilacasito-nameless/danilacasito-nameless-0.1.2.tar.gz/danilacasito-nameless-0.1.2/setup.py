88b9f5697830b45a7a18edc71358640a0d3a74f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:08:02 +0000

    Add package danilacasito-nameless/0.1.2/danilacasito-nameless-0.1.2.tar.gz

---
+++ packages/danilacasito-nameless/danilacasito-nameless-0.1.2.tar.gz/danilacasito-nameless-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="danilacasito-nameless",
+      version='0.1.2',
+      description="A python module for easily interacting with NamelessMC Forums",
+      author="danilacasito",
+      author_email="danilacasito8@gmail.com",
+      license="MIT",
+      packages=['nameless'],
+      zip_safe=False)

376a09d98d8f35888ba5aeb50bc76733a6c2e417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:07:11 +0000

    Add package quotexpy/1.0.1/quotexpy-1.0.1.tar.gz

---
+++ packages/quotexpy/quotexpy-1.0.1.tar.gz/quotexpy-1.0.1/setup.py
@@ -0,0 +1,25 @@
+"""The python wrapper for Quotex API package setup."""
+import shutup
+from pathlib import Path
+from setuptools import find_packages, setup
+
+shutup.please()
+
+this_dir = Path(__file__).parent
+
+requirements = []
+requirements_path = this_dir / "requirements.txt"
+if requirements_path.is_file():
+    with open(requirements_path, "r", encoding="utf-8") as requirements_file:
+        requirements = requirements_file.read().splitlines()
+
+# -----------------------------------------------------------------------------
+
+setup(
+    name="quotexpy",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.10",
+)

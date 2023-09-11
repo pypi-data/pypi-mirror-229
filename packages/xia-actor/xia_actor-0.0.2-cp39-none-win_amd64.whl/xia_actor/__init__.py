d297487e5c97454581cfb0789827f6bd39a13527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:07:55 +0000

    Add package xia-actor/0.0.2/xia_actor-0.0.2-cp39-none-win_amd64.whl

---
+++ packages/xia-actor/xia_actor-0.0.2-cp39-none-win_amd64.whl/xia_actor/__init__.py
@@ -0,0 +1,12 @@
+from xia_actor.actor import Actor, MockActor
+from xia_actor.work import Skill, Job, MissionJob, CampaignJob
+from xia_actor.work import MissionWorker, MissionReviewer, MissionOwner, CampaignOwner
+
+
+__all__ = [
+    "Actor", "MockActor",
+    "Skill", "Job", "MissionJob", "CampaignJob",
+    "MissionWorker", "MissionReviewer", "MissionOwner", "CampaignOwner"
+]
+
+__version__ = "0.0.2"

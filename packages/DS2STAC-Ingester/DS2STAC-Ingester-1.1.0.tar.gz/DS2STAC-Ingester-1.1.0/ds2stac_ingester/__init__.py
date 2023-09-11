699868f5062e32fb00f24b2ef52cfa52188e2634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:07:45 +0000

    Add package DS2STAC-Ingester/1.1.0/DS2STAC-Ingester-1.1.0.tar.gz

---
+++ packages/DS2STAC-Ingester/DS2STAC-Ingester-1.1.0.tar.gz/DS2STAC-Ingester-1.1.0/ds2stac_ingester/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+__author__ = "Mostafa Hadizadeh"
+__copyright__ = "Copyright (C) 2023 Karlsruher Institut für Technologie"
+__credits__ = [
+    "Mostafa Hadizadeh",
+]
+__license__ = "EUPL-1.2"
+
+__maintainer__ = "Karlsruher Institut für Technologie"
+__email__ = "mostafa.hadizadeh@kit.edu"
+
+__status__ = "Pre-Alpha"

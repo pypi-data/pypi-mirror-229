1a4a9415c183217856d6629923546065689ce34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:06:56 +0000

    Add package CrazyDG/2.0.1/CrazyDG-2.0.1.tar.gz

---
+++ packages/CrazyDG/CrazyDG-2.0.1.tar.gz/CrazyDG-2.0.1/CrazyDG/__init__.py
@@ -0,0 +1,13 @@
+version = "__2.0.1__"
+
+from .control import Controller
+
+from .crazy import CrazyDragon
+
+from .guidance import Guidance
+
+from .guidance import utils
+
+from .navigation import Navigation
+
+from .recorder import Recorder
